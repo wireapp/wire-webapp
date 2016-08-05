a148a49ebeb7be4537193b5bccba7c723df19247
Benny Neugebauer <bn@bennyn.de>
2016-08-05 18:04:13 +0200

    Testing encrypted announcements

--- bin/deployment_notification.sh
+++ bin/deployment_notification.sh
@@ -25,10 +25,5 @@ CONVERSATION_IDS[0]="9fe8b359-b9e0-4624-b63c-71747664e4fa" # ω Webapp Releases
 # Post message to conversations
 for CONVERSATION_ID in "${CONVERSATION_IDS[@]}"
 do
-  MESSAGE_NONCE=$(python -c 'import base64, uuid; print uuid.uuid4()')
-  EVENT='{"content":"'$MESSAGE_CONTENT'","entities":[],"nonce":"'$MESSAGE_NONCE'"}'
-  curl -i "https://prod-nginz-https.wire.com/conversations/${CONVERSATION_ID}/messages" -H "Authorization: Bearer ${ACCESS_TOKEN}" -H "User-Agent: Wire's Webapp Bot" -H "Content-Type: application/json; charset=UTF-8" --data-binary "${EVENT}"
+  java -jar ./wire-notify-0.1.0-jar-with-dependencies.jar -e ${BOT_EMAIL} -p ${BOT_PASSWORD} -c ${CONVERSATION_ID} -m ${MESSAGE_CONTENT}
 done
-
-# Logout
-curl -i "https://prod-nginz-https.wire.com/access/logout" -X OPTIONS -H "Access-Control-Request-Method: POST" -H "Authorization: Bearer ${ACCESS_TOKEN}" -H "Access-Control-Request-Headers: accept, authorization" --compressed
