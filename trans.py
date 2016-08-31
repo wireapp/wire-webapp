97a4461f1f153f7a3c87a66b42dfdb6ef8d77939
Benny Neugebauer <bn@bennyn.de>
2016-08-31 16:43:19 +0200

    Changed pattern matcher

--- trans.py
+++ trans.py
@@ -36,7 +36,7 @@ root = 'app/script/localization/'
 for filename in os.listdir(root):
   locale = get_locale(filename)
   if locale:
-    if locale is not 'de':
+    if locale != 'de':
       file_to_delete = os.path.join(root, filename)
       sys.stdout.write('Removing unsupported locale "{}" ({})\n'.format(locale, file_to_delete))
       os.remove(file_to_delete)
