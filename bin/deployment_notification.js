dc551f2517e3c7894b201aa0bbbfd55edb2c2ed6
Benny Neugebauer <bn@bennyn.de>
2016-12-21 17:49:33 +0100

    Pull Request Review

--- bin/deployment_notification.js
+++ bin/deployment_notification.js
@@ -1,37 +1,49 @@
+'use strict';
+
 const wire = require('wire-webapp-core');
 
-var branch = process.env.TRAVIS_BRANCH;
-var commitAuthor = process.argv[2];
-var commitMessage = process.argv[3];
-var conversationId = '9fe8b359-b9e0-4624-b63c-71747664e4fa';
-var email = process.env.WIRE_WEBAPP_BOT_EMAIL;
-var password = process.env.WIRE_WEBAPP_BOT_PASSWORD;
-var webAppUrl = 'https://app.wire.com/';
+let login = {
+  email: process.env.WIRE_WEBAPP_BOT_EMAIL,
+  password: process.env.WIRE_WEBAPP_BOT_PASSWORD
+};
 
-switch (branch) {
+let commit = {
+  author: process.argv[2],
+  branch: process.env.TRAVIS_BRANCH,
+  message: process.argv[3]
+};
+
+let build = {
+  number: process.env.TRAVIS_BUILD_NUMBER,
+  url: 'https://app.wire.com/'
+}
+
+let content = {
+  conversationId: '9fe8b359-b9e0-4624-b63c-71747664e4fa',
+  message: 'Hello World'
+};
+
+switch (commit.branch) {
   case 'dev':
-    webAppUrl = 'https://wire-webapp-dev.zinfra.io/auth/?env=prod#login';
-    break;
-  case 'edge':
-    webAppUrl = 'https://wire-webapp-edge.zinfra.io/auth/?env=prod#login';
+    build.url = 'https://wire-webapp-dev.zinfra.io/auth/?env=prod#login';
     break;
   case 'prod':
-    webAppUrl = 'https://wire-webapp-prod-next.wire.com/auth/#login';
+    build.url = 'https://wire-webapp-prod-next.wire.com/auth/#login';
     break;
   case 'staging':
-    webAppUrl = 'https://wire-webapp-staging.zinfra.io/auth/?env=prod#login';
+    build.url = 'https://wire-webapp-staging.zinfra.io/auth/?env=prod#login';
     break;
 }
 
-var message =
-  `**New Wire for Web version #$TRAVIS_BUILD_NUMBER online for $TRAVIS_BRANCH.** ᕦ(￣ ³￣)ᕤ`
-  + `\r\n- ${webAppUrl}`
-  + `\r\n- Last commit from: ${commitAuthor}`
-  + `\r\n- Last commit summary: ${commitMessage}`;
+content.message =
+  `**Travis build ${build.number} deployed on '${commit.branch}' environment.** ᕦ(￣ ³￣)ᕤ`
+  + `\r\n- Link: ${build.url}`
+  + `\r\n- Last commit from: ${commit.author}`
+  + `\r\n- Last commit message: ${commit.message}`;
 
-var user = new wire.User(email, password).login(false)
+let user = new wire.User(login.email, login.password).login(false)
 .then(function(service) {
-  return service.conversation.sendTextMessage(conversationId, message);
+  return service.conversation.sendTextMessage(content.conversationId, content.message);
 })
 .then(function(service) {
   return service.user.logout();
@@ -40,6 +52,6 @@ var user = new wire.User(email, password).login(false)
   return process.exit(0);
 })
 .catch(function(error) {
-  console.log('Error: ' + error.message);
+  console.log(error.message);
   return process.exit(1);
 });
