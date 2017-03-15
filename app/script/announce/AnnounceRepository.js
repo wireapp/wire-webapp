01915fd7e311dcee73585f29a1ddb213a9a4c031
Benny Neugebauer <bn@bennyn.de>
2017-03-15 15:24:33 +0100

    process_announce_list

--- app/script/announce/AnnounceRepository.js
+++ app/script/announce/AnnounceRepository.js
@@ -29,5 +29,28 @@
           this.logger.error(`Failed to fetch announcements: ${error}`);
         });
     }
+
+    check_version() {
+      return this.announce_service.get_version()
+        .then((server_version) => {
+          this.logger.info(`Found new version ${server_version}`);
+
+          if (server_version > z.util.Environment.version(false, true)) {
+            amplify.publish(z.event.WebApp.LIFECYCLE.UPDATE, z.announce.UPDATE_SOURCE.WEBAPP);
+          }
+        })
+        .catch((error) => {
+          this.logger.error(`Failed to fetch version: ${error}`);
+        });
+    }
+
+    schedule_checks() {
+      window.setInterval(this.check_announcements, ANNOUNCE_CONFIG.CHECK_INTERVAL);
+      window.setInterval(this.check_version, ANNOUNCE_CONFIG.CHECK_INTERVAL);
+    }
+
+    process_announce_list(announcements_list) {
+      // TODO
+    }
   };
 })();
