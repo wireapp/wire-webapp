c1fcb44780f4f750d3da3ae6ecec57731c59c556
rehez <rene@wearezeta.com>
2017-04-03 15:03:17 +0200

    code review

--- app/sw.js
+++ app/sw.js
@@ -1,10 +1,29 @@
-(global => {
+/*
+ * Wire
+ * Copyright (C) 2016 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+((global) => {
   'use strict';
 
   importScripts('/ext/js/sw-toolbox.js');
   importScripts('/worker/lru-cache-strategy.js');
 
-  global.toolbox.options.debug = true;
+  global.toolbox.options.debug = false;
   global.toolbox.router.default = global.toolbox.networkOnly;
   global.toolbox.router.get(/forceCaching=true/, global.cacheLRU, {
     cache: {
--- app/worker/lru-cache-strategy.js
+++ app/worker/lru-cache-strategy.js
@@ -1,11 +1,24 @@
-(global => {
-  'use strict';
+/*
+ * Wire
+ * Copyright (C) 2016 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
 
-  function stripSearchParameters(url) {
-    const strippedUrl = new URL(url);
-    strippedUrl.search = '';
-    return strippedUrl.toString();
-  }
+((global) => {
+  'use strict';
 
   function cacheRequest(cache, request, response) {
     return cache.put(stripSearchParameters(request.url), response.clone()).then(() => response);
@@ -18,6 +31,12 @@
       }
       return cache.delete(keys[0]).then(() => cacheRequest(cache, request, response));
     });
+  }
+
+  function stripSearchParameters(url) {
+    const strippedUrl = new URL(url);
+    strippedUrl.search = '';
+    return strippedUrl.toString();
   }
 
   global.cacheLRU = function (request, values, options) {
