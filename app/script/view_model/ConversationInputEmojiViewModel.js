20a81fe5e9175c88a9257b8f2333a47b829d6851
Maxim Baz <git@maximbaz.com>
2017-04-11 15:22:21 +0200

    Remove bubble arrow

--- app/script/view_model/ConversationInputEmojiViewModel.js
+++ app/script/view_model/ConversationInputEmojiViewModel.js
@@ -23,7 +23,7 @@ window.z = window.z || {};
 z.ViewModel = z.ViewModel || {};
 
 const EMOJI_LIST_LENGTH = 5;
-const EMOJI_LIST_OFFSET_LEFT = 20;
+const EMOJI_LIST_OFFSET_LEFT = 10;
 const EMOJI_LIST_OFFSET_TOP = 10;
 const QUERY_MIN_LENGTH = 2;
 
