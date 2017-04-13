7eaaad1604a462e9b046289e5d5ca2ea7f75a271
Benny Neugebauer <bn@bennyn.de>
2017-04-13 14:46:48 +0200

    z.entity.MediumImage

---
+++ app/script/entity/message/MediumImage.js
@@ -0,0 +1,49 @@
+/*
+ * Wire
+ * Copyright (C) 2017 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+'use strict';
+
+window.z = window.z || {};
+window.z.entity = z.entity || {};
+
+z.entity.MediumImage = class MediumImage extends z.entity.Asset {
+  constructor(id) {
+    super(id);
+    this.type = z.assets.AssetType.IMAGE;
+
+    this.correlation_id = '';
+
+    this.width = '0px';
+    this.height = '0px';
+
+    this.file_name = '';
+    this.file_size = '';
+    this.file_type = '';
+
+    // z.assets.AssetRemoteData
+    this.resource = ko.observable();
+  }
+
+  download(file_name) {
+    return this.resource().load()
+      .then(function(blob) {
+        z.util.download_blob(blob, file_name);
+      });
+  }
+};
