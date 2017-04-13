c7ce65d4dfca1f7615175527c8c7e6e37125a95a
Benny Neugebauer <bn@bennyn.de>
2017-04-13 15:26:16 +0200

    z.entity.CallMessage

---
+++ app/script/entity/message/CallMessage.js
@@ -0,0 +1,65 @@
+/*
+ * Wire
+ * Copyright (C) 2017 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+'use strict';
+
+window.z = window.z || {};
+window.z.entity = z.entity || {};
+
+z.entity.CallMessage = class CallMessage extends z.entity.Message {
+  // Construct a new content message.
+  constructor() {
+    super();
+    this.super_type = z.message.SuperType.CALL;
+    this.call_message_type = '';
+    this.finished_reason = '';
+
+    this.caption = ko.pureComputed(() => {
+        if (this.user().is_me) {
+          return z.localization.Localizer.get_text(z.string.conversation_voice_channel_deactivate_you);
+        }
+        return z.localization.Localizer.get_text(z.string.conversation_voice_channel_deactivate);
+      }
+      , this, {deferEvaluation: true});
+  }
+
+  /*
+   Check if call message is call activation.
+   @return [Boolean] Is message of type activate
+   */
+  is_activation() {
+    return this.call_message_type === z.message.CALL_MESSAGE_TYPE.ACTIVATED;
+  }
+
+  /*
+   Check if call message is call activation.
+   @return [Boolean] Is message of type deactivate
+   */
+  is_deactivation() {
+    return this.call_message_type === z.message.CALL_MESSAGE_TYPE.DEACTIVATED;
+  }
+
+  was_completed() {
+    return this.finished_reason === z.calling.enum.TERMINATION_REASON.COMPLETED;
+  }
+
+  was_missed() {
+    return this.finished_reason === z.calling.enum.TERMINATION_REASON.MISSED;
+  }
+};
