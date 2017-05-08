0b8a4b5389e25aef4299e8468bfac9900b02c9dc
Benny Neugebauer <bn@bennyn.de>
2017-05-08 17:26:42 +0200

    User

---
+++ app/script/entity/User.js
@@ -0,0 +1,162 @@
+/*
+ * Wire
+ * Copyright (C) 2017 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+'use strict';
+
+window.z = window.z || {};
+window.z.entity = z.entity || {};
+
+// Please note: The own user has a "locale"
+z.entity.User = class User {
+  constructor(id = '') {
+    this.id = id;
+    this.is_me = false;
+    this.is_bot = false;
+
+    this.joaat_hash = -1;
+
+    this.accent_id = ko.observable(z.config.ACCENT_ID.BLUE);
+    this.accent_theme = ko.pureComputed(() => {
+      switch (this.accent_id()) {
+        case z.config.ACCENT_ID.BLUE:
+          return this.THEME.BLUE;
+        case z.config.ACCENT_ID.GREEN:
+          return this.THEME.GREEN;
+        case z.config.ACCENT_ID.ORANGE:
+          return this.THEME.ORANGE;
+        case z.config.ACCENT_ID.PINK:
+          return this.THEME.PINK;
+        case z.config.ACCENT_ID.PURPLE:
+          return this.THEME.PURPLE;
+        case z.config.ACCENT_ID.RED:
+          return this.THEME.RED;
+        case z.config.ACCENT_ID.YELLOW:
+          return this.THEME.YELLOW;
+        default:
+          return this.THEME.BLUE;
+      }
+    }, this, {deferEvaluation: true});
+
+    this.accent_color = ko.pureComputed(() => {
+      switch (this.accent_id()) {
+        case z.config.ACCENT_ID.BLUE:
+          return this.ACCENT_COLOR.BLUE;
+        case z.config.ACCENT_ID.GREEN:
+          return this.ACCENT_COLOR.GREEN;
+        case z.config.ACCENT_ID.ORANGE:
+          return this.ACCENT_COLOR.ORANGE;
+        case z.config.ACCENT_ID.PINK:
+          return this.ACCENT_COLOR.PINK;
+        case z.config.ACCENT_ID.PURPLE:
+          return this.ACCENT_COLOR.PURPLE;
+        case z.config.ACCENT_ID.RED:
+          return this.ACCENT_COLOR.RED;
+        case z.config.ACCENT_ID.YELLOW:
+          return this.ACCENT_COLOR.YELLOW;
+        default:
+          return this.ACCENT_COLOR.BLUE;
+      }
+    }, this, {deferEvaluation: true});
+
+    this.email = ko.observable();
+    this.phone = ko.observable();
+
+    this.name = ko.observable('');
+    this.first_name = ko.pureComputed(() => {
+      return this.name().split(' ')[0];
+    });
+
+    this.last_name = ko.pureComputed(() => {
+      const parts = this.name().split(' ');
+      if (parts.length > 1) {
+        return parts.pop();
+      }
+    });
+
+    this.initials = ko.pureComputed(() => {
+      let initials = '';
+      if ((this.first_name() != null) && (this.last_name() != null)) {
+        const first = z.util.StringUtil.get_first_character(this.first_name());
+        const last = z.util.StringUtil.get_first_character(this.last_name());
+        initials = `${first}${last}`;
+      } else {
+        initials = this.first_name().slice(0, 2);
+      }
+      return initials.toUpperCase();
+    });
+
+    this.username = ko.observable();
+
+    this.mutual_friends_total = ko.observable(0);
+
+    this.preview_picture_resource = ko.observable();
+    this.medium_picture_resource = ko.observable();
+
+    this.connection = ko.observable(new z.entity.Connection());
+
+    // connection state shorthands TODO add others too since this is used very often?
+    this.blocked = ko.pureComputed(() => this.connection().status() === z.user.ConnectionStatus.BLOCKED);
+    this.connected = ko.pureComputed(() => this.connection().status() === z.user.ConnectionStatus.ACCEPTED);
+    this.sent = ko.pureComputed(() => this.connection().status() === z.user.ConnectionStatus.SENT);
+
+    // e2ee
+    this.devices = ko.observableArray();
+    return this.is_verified = ko.pureComputed(() => {
+      if ((this.devices().length === 0) && !this.is_me) {
+        return false;
+      }
+      return this.devices().every((client_et) => client_et.meta.is_verified());
+    });
+  }
+
+  add_client(new_client_et) {
+    for (const client_et of this.devices()) {
+      if (client_et.id === new_client_et.id) {
+        return false;
+      }
+    }
+
+    this.devices.push(new_client_et);
+
+    if (this.is_me) {
+      this.devices.sort((client_a, client_b) => {
+        return new Date(client_b.time) - new Date(client_a.time);
+      });
+    }
+
+    return true;
+  }
+
+  remove_client(client_id) {
+    return this.devices.remove((client_et) => client_et.id === client_id);
+  }
+
+  /**
+   * Check whether username or name matches the given query
+   * @param {string} query - Query
+   * @param {boolean} is_username - Query string is username
+   * @returns {undefined} No return value
+   */
+  matches(query, is_username) {
+    if (is_username) {
+      return z.util.StringUtil.starts_with(this.username(), query);
+    }
+    return z.util.StringUtil.compare_transliteration(this.name(), query) || (this.username() === query);
+  }
+};
