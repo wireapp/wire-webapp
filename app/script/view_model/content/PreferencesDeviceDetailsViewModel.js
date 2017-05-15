91a6acda1d4d73304e3f00ed04ce6f20d28733c4
Gregor Herdmann <gregor@wire.com>
2017-05-15 17:07:40 +0200

    ES6: Migrate 'view_model/content'

---
+++ app/script/view_model/content/PreferencesDeviceDetailsViewModel.js
@@ -0,0 +1,141 @@
+/*
+ * Wire
+ * Copyright (C) 2017 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+'use strict';
+
+window.z = window.z || {};
+window.z.ViewModel = z.ViewModel || {};
+window.z.ViewModel.content = z.ViewModel.content || {};
+
+z.ViewModel.content.PreferencesDeviceDetailsViewModel = class PreferencesDeviceDetailsViewModel {
+  static get SESSION_RESET_STATE() {
+    return {
+      CONFIRMATION: 'confirmation',
+      ONGOING: 'ongoing',
+      RESET: 'reset',
+    };
+  }
+
+  constructor(element_id, client_repository, conversation_repository, cryptography_repository) {
+    this.client_repository = client_repository;
+    this.conversation_repository = conversation_repository;
+    this.cryptography_repository = cryptography_repository;
+    this.logger = new z.util.Logger('z.ViewModel.content.PreferencesDeviceDetailsViewModel', z.config.LOGGER.OPTIONS);
+
+    this.self_user = this.client_repository.self_user;
+
+    this.device = ko.observable();
+    this.device.subscribe((device_et) => {
+      if (device_et) {
+        this.session_reset_state(z.ViewModel.content.PreferencesDeviceDetailsViewModel.SESSION_RESET_STATE.RESET);
+        this.fingerprint('');
+        this._update_fingerprint();
+        this._update_activation_location('?');
+        this._update_activation_time(device_et.time);
+        if (device_et.location) {
+          this._update_device_location(device_et.location);
+        }
+      }
+    });
+
+    this.session_reset_state = ko.observable(z.ViewModel.content.PreferencesDeviceDetailsViewModel.SESSION_RESET_STATE.RESET);
+    this.fingerprint = ko.observable('');
+
+    this.activated_in = ko.observable(z.l10n.text(z.string.preferences_devices_activated_in));
+    this.activated_on = ko.observable(z.l10n.text(z.string.preferences_devices_activated_on));
+  }
+
+  _update_activation_location(location) {
+    this.activated_in(z.localization.Localizer.get_text({
+      id: z.string.preferences_devices_activated_in,
+      replace: {
+        content: `<span class='preferences-devices-activated-bold'>${location}</span>`,
+        placeholder: '%location',
+      },
+    }));
+  }
+
+  _update_activation_time(time) {
+    this.activated_on(z.localization.Localizer.get_text({
+      id: z.string.preferences_devices_activated_on,
+      replace: {
+        content: `<span class='preferences-devices-activated-bold'>${z.util.format_timestamp(time)}</span>`,
+        placeholder: '%time',
+      },
+    }));
+  }
+
+  _update_device_location(location) {
+    z.location.get_location(location.lat, location.lon)
+      .then((retrieved_location) => {
+        if (retrieved_location) {
+          this._update_activation_location(`${retrieved_location.place}, ${retrieved_location.country_code}`);
+        }
+      });
+  }
+
+  _update_fingerprint() {
+    this.cryptography_repository.get_remote_fingerprint(this.self_user().id, this.device().id)
+      .then((fingerprint) => {
+        this.fingerprint(fingerprint);
+      });
+  }
+
+  click_on_details_close() {
+    amplify.publish(z.event.WebApp.CONTENT.SWITCH, z.ViewModel.content.CONTENT_STATE.PREFERENCES_DEVICES);
+    this.device(null);
+  }
+
+  click_on_reset_session() {
+    this.session_reset_state(z.ViewModel.content.PreferencesDeviceDetailsViewModel.SESSION_RESET_STATE.ONGOING);
+
+    this.conversation_repository.reset_session(this.self_user().id, this.device().id, this.conversation_repository.self_conversation().id)
+      .then(() => {
+        window.setTimeout(() => {
+          this.session_reset_state(z.ViewModel.content.PreferencesDeviceDetailsViewModel.SESSION_RESET_STATE.CONFIRMATION);
+        }, 550);
+
+        window.setTimeout(() => {
+          this.session_reset_state(z.ViewModel.content.PreferencesDeviceDetailsViewModel.SESSION_RESET_STATE.RESET);
+        }, 5000);
+      })
+      .catch((error) => {
+        this.session_reset_state(z.ViewModel.content.PreferencesDeviceDetailsViewModel.SESSION_RESET_STATE.RESET);
+        throw error;
+      });
+  }
+
+  click_on_remove_device() {
+    amplify.publish(z.event.WebApp.WARNING.MODAL, z.ViewModel.ModalType.REMOVE_DEVICE, {
+      action: (password) => {
+        // @todo Add failure case ux WEBAPP-3570
+        this.client_repository.delete_client(this.device().id, password)
+          .then(() => {
+            this.click_on_details_close();
+          });
+      },
+      data: this.device().model,
+    });
+  }
+
+  toggle_device_verification() {
+    const toggle_verified = !this.device().meta.is_verified();
+    this.client_repository.verify_client(this.self_user().id, this.device(), toggle_verified);
+  }
+};
---
+++ app/script/view_model/content/PreferencesOptionsViewModel.js
@@ -0,0 +1,76 @@
+/*
+ * Wire
+ * Copyright (C) 2017 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+'use strict';
+
+window.z = window.z || {};
+window.z.ViewModel = z.ViewModel || {};
+window.z.ViewModel.content = z.ViewModel.content || {};
+
+z.ViewModel.content.PreferencesOptionsViewModel = class PreferencesOptionsViewModel {
+  constructor(element_id, properties_repository) {
+    this.properties_repository = properties_repository;
+    this.logger = new z.util.Logger('z.ViewModel.content.PreferencesOptionsViewModel', z.config.LOGGER.OPTIONS);
+
+    this.option_privacy = ko.observable();
+    this.option_privacy.subscribe((privacy_preference) => {
+      this.properties_repository.save_preference(z.properties.PROPERTIES_TYPE.PRIVACY, privacy_preference);
+    });
+
+    this.option_audio = ko.observable();
+    this.option_audio.subscribe((audio_preference) => {
+      const tracking_value = (() => {
+        switch (audio_preference) {
+          case z.audio.AudioPreference.ALL:
+            return 'alwaysPlay';
+          case z.audio.AudioPreference.SOME:
+            return 'firstMessageOnly';
+          case z.audio.AudioPreference.NONE:
+            return 'neverPlay';
+          default:
+            return;
+        }
+      })();
+
+      this.properties_repository.save_preference(z.properties.PROPERTIES_TYPE.SOUND_ALERTS, audio_preference);
+      amplify.publish(z.event.WebApp.ANALYTICS.EVENT, z.tracking.EventName.SOUND_SETTINGS_CHANGED, {value: tracking_value});
+    });
+
+    this.option_notifications = ko.observable();
+    this.option_notifications.subscribe((notifications_preference) => {
+      this.properties_repository.save_preference(z.properties.PROPERTIES_TYPE.NOTIFICATIONS, notifications_preference);
+    });
+
+    amplify.subscribe(z.event.WebApp.PROPERTIES.UPDATED, this.update_properties.bind(this));
+  }
+
+  connect_google_contacts() {
+    amplify.publish(z.event.WebApp.CONNECT.IMPORT_CONTACTS, z.connect.ConnectSource.GMAIL);
+  }
+
+  connect_macos_contacts() {
+    amplify.publish(z.event.WebApp.CONNECT.IMPORT_CONTACTS, z.connect.ConnectSource.ICLOUD);
+  }
+
+  update_properties(properties) {
+    this.option_audio(properties.settings.sound.alerts);
+    this.option_privacy(properties.settings.privacy.report_errors);
+    this.option_notifications(properties.settings.notifications);
+  }
+};
