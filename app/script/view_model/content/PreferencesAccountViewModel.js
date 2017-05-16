1920c64d68291eb1fa06c185cb98370b5f983c02
rehez <rene@wearezeta.com>
2017-05-16 16:27:11 +0200

    bind removed_from_view

--- app/script/view_model/content/PreferencesAccountViewModel.js
+++ app/script/view_model/content/PreferencesAccountViewModel.js
@@ -30,6 +30,7 @@ z.ViewModel.content.PreferencesAccountViewModel = class PreferencesAccountViewMo
 
   constructor(element_id, client_repository, user_repository) {
     this.check_new_clients = this.check_new_clients.bind(this);
+    this.removed_from_view = this.removed_from_view.bind(this);
 
     this.client_repository = client_repository;
     this.user_repository = user_repository;
