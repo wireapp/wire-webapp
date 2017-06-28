e568c172912ad463b6f83b97e0788ee3c2999e49
Gregor Herdmann <gregor@wire.com>
2017-06-28 18:27:26 +0200

    Add missed events check (#1450)

--- app/script/event/NotificationService.js
+++ app/script/event/NotificationService.js
@@ -27,6 +27,7 @@ z.event.NotificationService = class NotificationService {
     return {
       PRIMARY_KEY_LAST_EVENT: 'z.storage.StorageKey.EVENT.LAST_DATE',
       PRIMARY_KEY_LAST_NOTIFICATION: 'z.storage.StorageKey.NOTIFICATION.LAST_ID',
+      PRIMARY_KEY_MISSED: 'z.storage.StorageKey.NOTIFICATION.MISSED',
       URL_NOTIFICATIONS: '/notifications',
       URL_NOTIFICATIONS_LAST: '/notifications/last',
     };
@@ -39,8 +40,6 @@ z.event.NotificationService = class NotificationService {
    * @param {z.storage.StorageService} storage_service - Service for all storage related tasks
    */
   constructor(client, storage_service) {
-    this.get_last_notification_id_from_db = this.get_last_notification_id_from_db.bind(this);
-    this.save_last_notification_id_to_db = this.save_last_notification_id_to_db.bind(this);
     this.client = client;
     this.storage_service = storage_service;
     this.logger = new z.util.Logger('z.event.NotificationService', z.config.LOGGER.OPTIONS);
@@ -118,6 +117,19 @@ z.event.NotificationService = class NotificationService {
   }
 
   /**
+   * Load missed ID from storage.
+   * @returns {Promise} Resolves with the stored missed ID.
+   */
+  get_missed_id_from_db() {
+    return this.storage_service.load(z.storage.StorageService.OBJECT_STORE.AMPLIFY, NotificationService.CONFIG.PRIMARY_KEY_MISSED)
+      .then((record) => {
+        if (record && record.value) {
+          return record.value;
+        }
+      });
+  }
+
+  /**
    * Save last event date to storage.
    * @param {string} event_date - Event date to be stored
    * @returns {Promise} Resolves with the stored record
@@ -133,5 +145,14 @@ z.event.NotificationService = class NotificationService {
    */
   save_last_notification_id_to_db(notification_id) {
     return this.storage_service.save(z.storage.StorageService.OBJECT_STORE.AMPLIFY, NotificationService.CONFIG.PRIMARY_KEY_LAST_NOTIFICATION, {value: notification_id});
+  }
+
+  /**
+   * Save missed notifications ID to storage.
+   * @param {string} notification_id - Notification ID to be stored
+   * @returns {Promise} Resolves with the stored record
+   */
+  save_missed_id_to_db(notification_id) {
+    return this.storage_service.save(z.storage.StorageService.OBJECT_STORE.AMPLIFY, NotificationService.CONFIG.PRIMARY_KEY_MISSED, {value: notification_id});
   }
 };
