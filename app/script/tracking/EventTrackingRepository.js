6e095e8237588f387f488c240cc518d1b1fcc4dc
Gregor Herdmann <gregor@wire.com>
2017-09-21 14:55:02 +0200

    fix: Use members as contacts in teams (#1727)

--- app/script/tracking/EventTrackingRepository.js
+++ app/script/tracking/EventTrackingRepository.js
@@ -182,8 +182,10 @@ z.tracking.EventTrackingRepository = class EventTrackingRepository {
     this.is_user_tracking_activated = true;
     this.mixpanel.unregister('$ignore');
     this._subscribe_to_tracking_events();
+
+    const contacts = this.user_repository.is_team() ? this.user_repository.team_users() : this.user_repository.connected_users();
+    this._set_super_property(z.tracking.SuperProperty.CONTACTS, contacts.length);
     this._track_event(z.tracking.EventName.TRACKING.OPT_IN);
-    this._set_super_property(z.tracking.SuperProperty.CONTACTS, this.user_repository.connected_users().length);
   }
 
   _init_tracking() {
