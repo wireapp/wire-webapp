1611f0b4fe21ffc82c3091750672b895a48c3d7d
Gregor Herdmann <gregor@wire.com>
2017-10-04 17:29:10 +0200

    fix: Update server timestamp on link preview update (#1790)

--- app/script/entity/Conversation.js
+++ app/script/entity/Conversation.js
@@ -360,8 +360,8 @@ z.entity.Conversation = class Conversation {
   }
 
   get_latest_timestamp(time_offset) {
-    const current_timestamp = z.util.TimeUtil.adjust_current_timestamp(time_offset);
-    return Math.max(this.last_server_timestamp(), this.last_event_timestamp()) || current_timestamp;
+    const last_known_timestamp = Math.max(this.last_server_timestamp(), this.last_event_timestamp());
+    return last_known_timestamp || z.util.TimeUtil.adjust_current_timestamp(time_offset);
   }
 
   get_next_iso_date(time_offset) {
