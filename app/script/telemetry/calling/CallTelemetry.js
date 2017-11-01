d98f98da7f273ec395dec2dfc9d109c90794c73d
Gregor Herdmann <gregor@wire.com>
2017-11-01 11:07:43 +0100

    fix: Use get_number_of_participants for conversation

--- app/script/telemetry/calling/CallTelemetry.js
+++ app/script/telemetry/calling/CallTelemetry.js
@@ -180,7 +180,7 @@ z.telemetry.calling.CallTelemetry = class CallTelemetry {
       }
 
       const attributes = {
-        conversation_participants: conversation_et.number_of_participants(),
+        conversation_participants: conversation_et.get_number_of_participants(),
         conversation_participants_in_call: max_number_of_participants,
         conversation_type: is_group
           ? z.tracking.attribute.ConversationType.GROUP
