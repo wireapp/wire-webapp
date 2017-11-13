18a24d9e7c0d8587633369ec5e858fb684e9f631
Michael Koppen <Yserz@users.noreply.github.com>
2017-11-13 15:35:11 +0100

    feat: Link component (#4)

---
+++ react-ui-kit/src/Link.jsx
@@ -0,0 +1,55 @@
+/*
+ * Wire
+ * Copyright (C) 2017 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import {COLOR} from './variables';
+import PropTypes from 'prop-types';
+import {Text} from './Text';
+import {transition} from './mixins';
+import React from 'react';
+
+const Link = ({component = 'a', ...props}) => {
+  const StyledLink = Text.withComponent(component).extend`
+    /* appearance */
+    font-weight: 400;
+    text-decoration: none;
+    ${transition}
+
+    /* positioning */
+
+    &:visited,
+    &:link,
+    &:active {
+      color: ${props => props.color};
+    }
+    &:hover {
+      cursor: pointer;
+      color: ${COLOR.GRAY_DARKEN_88};
+    }
+  `;
+  return <StyledLink {...props} />;
+};
+Link.propTypes = {
+  ...Text.propTypes,
+};
+
+Link.defaultProps = {
+  ...Text.defaultProps,
+};
+
+export {Link};
