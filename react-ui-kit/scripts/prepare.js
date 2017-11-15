b9f48a733f7248bce652237d22e74eabb5c7f5d9
AndyTheGiant <andy.bialystock@gmail.com>
2017-11-15 17:41:55 +0100

    fix(react-ui-kit): prepare script

--- react-ui-kit/scripts/prepare.js
+++ react-ui-kit/scripts/prepare.js
@@ -2,5 +2,6 @@ const fs = require('fs');
 
 const originalPackage = require('../package.json');
 originalPackage.main = './index.js';
+delete originalPackage.scripts;
 
 fs.writeFileSync('./dist/package.json', JSON.stringify(originalPackage, null, '  '));
