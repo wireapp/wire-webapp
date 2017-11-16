046efdf709349fc4769609142668126fdefcad1c
AndyTheGiant <andy.bialystock@gmail.com>
2017-11-16 16:02:50 +0100

    react-ui-kit -> 0.0.4

--- app/script/auth/page/Root.js
+++ app/script/auth/page/Root.js
@@ -21,42 +21,44 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {Logo} from '@wireapp/react-ui-kit/Identity';
 import {Small, H3, Link} from '@wireapp/react-ui-kit/Text';
-import {Columns, Column, ContainerXS, ContainerMD} from '@wireapp/react-ui-kit/Grid';
+import {StyledApp, Content, Columns, Column, ContainerXS} from '@wireapp/react-ui-kit/Layout';
 
 const Root = ({name}) => (
-  <ContainerMD>
-    <ContainerXS center>
-      <br />
-      <Logo id="wire-logo" />
-      <br />
-      Secure messaging for everyone
-      <br />
-      <br />
-      <Columns>
-        <Column>
-          <Link href="#" data-uie-name="go-register-personal">
-            <img src="#" width="100" height="100" />
-            <br />
-            <H3 center>Create an account</H3>
-            for personal use
-          </Link>
-        </Column>
-        <Column>
-          <Link href="#" data-uie-name="go-register-team">
-            <img src="#" width="100" height="100" />
-            <br />
-            <H3 center>Create a team</H3>
-            for work
-          </Link>
-        </Column>
-      </Columns>
-      <br />
-      <br />
-      <Small>Already have an account?</Small>
-      <br />
-      <Link href="#">Login</Link>
-    </ContainerXS>
-  </ContainerMD>
+  <StyledApp>
+    <Content>
+      <ContainerXS centerText verticalCenter>
+        <br />
+        <Logo id="wire-logo" />
+        <br />
+        Secure messaging for everyone
+        <br />
+        <br />
+        <Columns>
+          <Column>
+            <Link href="#" data-uie-name="go-register-personal">
+              <img src="#" width="100" height="100" />
+              <br />
+              <H3 center>Create an account</H3>
+              for personal use
+            </Link>
+          </Column>
+          <Column>
+            <Link href="#" data-uie-name="go-register-team">
+              <img src="#" width="100" height="100" />
+              <br />
+              <H3 center>Create a team</H3>
+              for work
+            </Link>
+          </Column>
+        </Columns>
+        <br />
+        <br />
+        <Small>Already have an account?</Small>
+        <br />
+        <Link href="#">Login</Link>
+      </ContainerXS>
+    </Content>
+  </StyledApp>
 );
 
 export default connect(state => ({
