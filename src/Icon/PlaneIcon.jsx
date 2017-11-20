27517162ba97da7d7481b1e9c8f46e0e5e72f4dc
AndyLnd <andy.bialystock@gmail.com>
2017-11-20 15:51:53 +0100

    feat(react-ui-kit): new icons (#31)

--- src/Icon/PlaneIcon.jsx
+++ src/Icon/PlaneIcon.jsx
@@ -17,8 +17,20 @@
  *
  */
 
-export * from './ArrowIcon';
-export * from './PlaneIcon';
-export * from './ProfileIcon';
-export * from './TeamIcon';
-export * from './RoundContainer';
+import IconBase from './IconBase';
+import React from 'react';
+
+class PlaneIcon extends IconBase {
+  /* eslint-disable no-magic-numbers */
+  width = 12;
+  height = 12;
+  renderSVG(width, height, color) {
+    return (
+      <svg width={width} height={height} viewBox="0 0 12 12">
+        <path fill={color} d="M0 10.7c0 1 .8 1.6 1.8 1L11.3 7c1-.6 1-1.4 0-2L1.8.3C.8-.3 0 .3 0 1.3V6h9L0 7.5v3.2z" />
+      </svg>
+    );
+  }
+}
+
+export {PlaneIcon};
