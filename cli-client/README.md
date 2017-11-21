d0ed4ab9af51a1036f68b532d6db156423ed7c49
Florian Keller <ffflorian@users.noreply.github.com>
2017-11-21 23:39:32 +0100

    feat(cli-client): Command-line interface (#38)

---
+++ cli-client/README.md
@@ -0,0 +1,25 @@
+# Wire
+
+This repository is part of the source code of Wire. You can find more information at [wire.com](https://wire.com) or by contacting opensource@wire.com.
+
+You can find the published source code at [github.com/wireapp](https://github.com/wireapp).
+
+For licensing information, see the attached LICENSE file and the list of third-party licenses at [wire.com/legal/licenses/](https://wire.com/legal/licenses/).
+
+## CLI Client
+
+Command-line interface for Wire's secure messaging platform.
+
+### Usage
+
+Type `wire-cli.js --help` for usage information.
+
+```bash
+#!/bin/bash
+
+WIRE_LOGIN_EMAIL="yourname@email.com"
+WIRE_LOGIN_PASSWORD="secret"
+WIRE_CONVERSATION_ID="594f0908-b9b7-40f9-a06a-45612145e64e"
+
+wire-cli.js -e "$WIRE_LOGIN_EMAIL" -p "$WIRE_LOGIN_PASSWORD" -c "$WIRE_CONVERSATION_ID"
+```
---
+++ cli-client/src/main/index.ts
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+
+const { description, version } = require('../../package.json');
+const {Account} = require('@wireapp/core');
+const {StoreEngine} = require('@wireapp/store-engine');
+const program = require('commander');
+const stdin = process.openStdin();
+import {PayloadBundle} from '@wireapp/core/dist/commonjs/crypto/';
+import * as os from 'os';
+import * as path from 'path';
+import APIClient = require('@wireapp/api-client');
+
+require('dotenv').config();
+
+program
+  .version(version)
+  .description(description)
+  .option('-e, --email <address>', 'Your email address')
+  .option('-p, --password <password>', 'Your password')
+  .option('-c, --conversation <conversationId>', 'The conversation to write in')
+  .parse(process.argv);
+
+const loginData = {
+  email: program.email || process.env.WIRE_LOGIN_EMAIL,
+  password: program.password || process.env.WIRE_LOGIN_PASSWORD,
+  persist: true,
+};
+
+const conversationID = program.conversation || process.env.WIRE_CONVERSATION_ID;
+
+const directory = path.join(os.homedir(), '.wire-cli', loginData.email);
+const storeEngine = new StoreEngine.FileEngine(directory, {fileExtension: '.json'});
+
+const apiClient: APIClient = new APIClient({
+  urls: APIClient.BACKEND.PRODUCTION,
+  store: storeEngine,
+});
+
+const account = new Account(apiClient);
+
+account.on(Account.INCOMING.TEXT_MESSAGE, (data: PayloadBundle) => {
+  console.log(`Received message from user ID "${data.from}" in conversation ID "${data.conversation}": ${data.content}`);
+});
+
+account
+  .listen(loginData)
+  .then(() => console.log(`Connected to Wire — Client ID "${account.context.clientID}"`))
+  .then(() => {
+    stdin.addListener('data', data => {
+      const message = data.toString().trim();
+      account.sendTextMessage(conversationID, message);
+    });
+  })
+  .catch((error: Error) => {
+    console.error(error.message, error);
+    process.exit(1);
+  });
