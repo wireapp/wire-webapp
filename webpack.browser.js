aace667ddfd88e18d1e6be36843d5c337b84cc19
Benny Neugebauer <bn@bennyn.de>
2017-11-21 11:46:25 +0100

    fix: Deployment output paths

--- webpack.browser.js
+++ webpack.browser.js
@@ -20,10 +20,8 @@
 const pkg = require('./package.json');
 const webpack = require('webpack');
 
-let repositoryName = pkg.repository.url.substr(pkg.repository.url.lastIndexOf('/') + 1);
-repositoryName = repositoryName.substr(0, repositoryName.indexOf('.git'));
-
-const camelCasedRepositoryName = repositoryName.replace(/-([a-z])/g, glob => glob[1].toUpperCase());
+const repositoryName = 'wire-web-api-client';
+const camelCasedRepositoryName = 'wireWebApiClient';
 
 module.exports = {
   entry: {
