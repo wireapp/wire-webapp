6270d7ef696e618e15270cddd98fc019df00de36
Michael Koppen <Yserz@users.noreply.github.com>
2017-11-28 15:23:58 +0100

    refactor: Extract billing info endpoint from payment data endpoint (#67)

--- src/main/team/payment/PaymentData.ts
+++ src/main/team/payment/PaymentData.ts
@@ -1,21 +1,3 @@
-//
-// Wire
-// Copyright (C) 2017 Wire Swiss GmbH
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program. If not, see http://www.gnu.org/licenses/.
-//
-
 /*
  * Wire
  * Copyright (C) 2017 Wire Swiss GmbH
@@ -35,7 +17,7 @@
  *
  */
 
-import {InvoiceData, PaymentBillingData, PaymentPlan} from '.';
+import {InvoiceData, PaymentPlan} from '.';
 
 interface PaymentData {
   card: {
@@ -47,7 +29,6 @@ interface PaymentData {
     holder: string;
     zip: string;
   };
-  billingInfo: PaymentBillingData;
   invoice: InvoiceData;
   plan: PaymentPlan;
   planId: 'wire_annual_plan' | 'wire_monthly_plan';
