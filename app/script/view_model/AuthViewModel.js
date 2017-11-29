f369b8587afc7afbaa9fb57e5a564d0c3a29c424
Gregor Herdmann <gregor@wire.com>
2017-11-29 19:00:11 +0100

    fix: Show general invite message on registration form

--- app/script/view_model/AuthViewModel.js
+++ app/script/view_model/AuthViewModel.js
@@ -317,6 +317,7 @@ z.ViewModel.AuthViewModel = class AuthViewModel {
     const is_connect = z.util.get_url_parameter(z.auth.URLParameter.CONNECT);
     if (is_connect) {
       this.get_wire(true);
+      this.account_mode(z.auth.AuthView.MODE.ACCOUNT_REGISTER);
       return (this.registration_context = z.auth.AuthView.REGISTRATION_CONTEXT.GENERIC_INVITE);
     }
 
