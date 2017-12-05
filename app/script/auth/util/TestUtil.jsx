b613957c563e156809c964c503864dfe0a75a82c
Florian Keller <florian.keller@wire.com>
2017-12-05 17:12:11 +0100

    Add withIntl

--- app/script/auth/util/TestUtil.jsx
+++ app/script/auth/util/TestUtil.jsx
@@ -1,8 +1,11 @@
 import configureStore from 'redux-mock-store';
 import React from 'react';
-import renderer from 'react-test-renderer';
+import {HashRouter} from 'react-router-dom';
+import {IntlProvider} from 'react-intl';
 import {Provider} from 'react-redux';
+import renderer from 'react-test-renderer';
 import thunk from 'redux-thunk';
+import {mount} from 'enzyme';
 
 export const mockStore = (state = {}, extraArgument = {}) => {
   const middlewares = [thunk.withExtraArgument(extraArgument)];
@@ -10,5 +13,9 @@ export const mockStore = (state = {}, extraArgument = {}) => {
 };
 
 export const withStore = (children, store) => <Provider store={store}>{children}</Provider>;
+
+export const withIntl = (component) => <IntlProvider><HashRouter>{component}</HashRouter></IntlProvider>;
+
+export const mountWithStore = (component, store = () => {}) => mount(withStore(withIntl(component), store));
 
 export const renderWithStore = (component, store = () => {}) => renderer.create(withStore(component, store));
