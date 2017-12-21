a4e285ca355ad2ac5f8aa50a0437edd97e2deaf6
Benny Neugebauer <bn@bennyn.de>
2017-12-21 18:10:32 +0100

    Improved bindings

--- app/script/components/deviceCard.js
+++ app/script/components/deviceCard.js
@@ -86,7 +86,7 @@ ko.components.register('device-card', {
           <span data-bind="l10n_text: z.string.preferences_devices_id"></span>
           <span data-uie-name="device-id" data-bind="foreach: z.util.zero_padding(id, 16).match(/.{1,2}/g)"><span class="device-id-part" data-bind="text: $data"></span></span>
         </div>
-        <div class="label-xs" data-bind="foreach: activated_in"><span class="preferences-devices-activated-bold" data-bind="text: $data.text">?</span></div>
+        <div class="label-xs" data-bind="foreach: activated_in"><span data-bind="css: {'preferences-devices-activated-bold': $data.isStyled}, text: $data.text">?</span></div>
         <div class="label-xs" data-bind="text: z.util.format_timestamp(device.time)"></div>
       <!-- /ko -->
       <!-- ko ifnot: detailed -->
