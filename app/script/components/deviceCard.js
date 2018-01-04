2b6a1c01a93fcbdf0125a7d920968420fd9e47be
Benny Neugebauer <bn@bennyn.de>
2018-01-04 19:30:46 +0100

    fix: Use plain client ID for "data-uie-uid" (#2165)

--- app/script/components/deviceCard.js
+++ app/script/components/deviceCard.js
@@ -27,7 +27,8 @@ z.components.DeviceCard = class DeviceCard {
     this.device = ko.unwrap(params.device) || {};
 
     const {class: device_class, id, label, model} = this.device;
-    this.id = id ? this.device.formatId() : [];
+    this.id = id || '';
+    this.formattedId = id ? this.device.formatId() : [];
     this.label = label || '?';
     this.model = model || device_class || '?'; // devices for other users will only provide the device class
     this.class = device_class || '?';
@@ -84,7 +85,7 @@ ko.components.register('device-card', {
         <div class="label-xs device-label" data-bind="text: label"></div>
         <div class="label-xs">
           <span data-bind="l10n_text: z.string.preferences_devices_id"></span>
-          <span data-uie-name="device-id" data-bind="foreach: id"><span class="device-id-part" data-bind="text: $data"></span></span>
+          <span data-uie-name="device-id" data-bind="foreach: formattedId"><span class="device-id-part" data-bind="text: $data"></span></span>
         </div>
         <div class="label-xs" data-bind="foreach: activated_in"><span data-bind="css: {'preferences-devices-activated-bold': $data.isStyled}, text: $data.text">?</span></div>
         <div class="label-xs" data-bind="text: z.util.format_timestamp(device.time)"></div>
@@ -96,7 +97,7 @@ ko.components.register('device-card', {
         </div>
         <div class="text-graphite-dark label-xs">
           <span data-bind="l10n_text: z.string.preferences_devices_id"></span>
-          <span data-uie-name="device-id" data-bind="foreach: id"><span class="device-id-part" data-bind="text: $data"></span></span>
+          <span data-uie-name="device-id" data-bind="foreach: formattedId"><span class="device-id-part" data-bind="text: $data"></span></span>
         </div>
       <!-- /ko -->
     </div>
