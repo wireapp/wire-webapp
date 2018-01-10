ad4589104c8b672c023f9f7882db7f68c987c2de
Michael Koppen <Yserz@users.noreply.github.com>
2018-01-10 12:48:28 +0100

    chore: Query param constants (#2189)

--- app/script/auth/localeConfig.js
+++ app/script/auth/localeConfig.js
@@ -5,15 +5,20 @@ export const SUPPORTED_LANGUAGE = require('./supportedLocales');
 export const DEFAULT_CURRENCY = 'eur';
 export const DEFAULT_LANGUAGE = 'en';
 
+const QUERY = {
+  CURRENCY: 'currency',
+  LANGUAGE: 'hl',
+};
+
 function getLocale() {
   return navigator.languages && navigator.languages.length ? navigator.languages[0] : navigator.language;
 }
 
 export function currentLanguage() {
   const LANGUAGE_SHORTHAND_LENGTH = 2;
-  return getURLParameter('hl') || getLocale().substr(0, LANGUAGE_SHORTHAND_LENGTH) || DEFAULT_LANGUAGE;
+  return getURLParameter(QUERY.LANGUAGE) || getLocale().substr(0, LANGUAGE_SHORTHAND_LENGTH) || DEFAULT_LANGUAGE;
 }
 
 export function currentCurrency() {
-  return getURLParameter('currency') || DEFAULT_CURRENCY;
+  return getURLParameter(QUERY.CURRENCY) || DEFAULT_CURRENCY;
 }
