fb2388237a80b0c28392db05db6e3356425c5370
Gregor Herdmann <gregor@wire.com>
2018-02-08 13:20:39 +0100

    fix: Position of group creation starting point in personal account

--- app/script/view_model/list/StartUIViewModel.js
+++ app/script/view_model/list/StartUIViewModel.js
@@ -144,9 +144,7 @@ z.ViewModel.list.StartUIViewModel = class StartUIViewModel {
     this.showMatches = ko.observable(false);
 
     this.showNoContacts = ko.pureComputed(() => !this.isTeam() && !this.showContent());
-    this.showInviteMember = ko.pureComputed(
-      () => this.selfUser().isTeamOwner() && this.teamSize() === 1 && !this.showSearchResults()
-    );
+    this.showInviteMember = ko.pureComputed(() => this.selfUser().isTeamOwner() && this.teamSize() === 1);
     this.showNoMatches = ko.pureComputed(() => {
       const isTeamOrMatch = this.isTeam() || this.showMatches();
       return isTeamOrMatch && !this.showInviteMember() && !this.showContacts() && !this.showSearchResults();
