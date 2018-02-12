c16a7f253f89d9f0ca7d56891e69208e8b402a2e
Gregor Herdmann <gregor@wire.com>
2018-02-12 12:15:06 +0100

    fix: Prevent long wait on participant step (#2433)

--- app/script/view_model/content/GroupCreationViewModel.js
+++ app/script/view_model/content/GroupCreationViewModel.js
@@ -44,24 +44,26 @@ z.ViewModel.content.GroupCreationViewModel = class GroupCreationViewModel {
     this.modal = undefined;
     this.state = ko.observable(GroupCreationViewModel.STATE.DEFAULT);
 
-    this.contacts = ko.pureComputed(() => {
-      if (this.teamRepository.isTeam()) {
-        return this.teamRepository.teamUsers();
-      }
-
-      return this.userRepository.connected_users();
-    });
-
     this.isCreatingConversation = false;
     this.method = undefined;
     this.nameError = ko.observable('');
     this.nameInput = ko.observable('');
     this.selectedContacts = ko.observableArray([]);
+    this.showContacts = ko.observable(false);
     this.participantsInput = ko.observable('');
 
-    this.nameInput.subscribe(() => this.nameError(''));
-
     this.activateNext = ko.pureComputed(() => this.nameInput().length);
+    this.contacts = ko.pureComputed(() => {
+      if (this.showContacts()) {
+        if (this.teamRepository.isTeam()) {
+          return this.teamRepository.teamUsers();
+        }
+
+        return this.userRepository.connected_users();
+      }
+
+      return [];
+    });
     this.participantsActionText = ko.pureComputed(() => {
       const stringSelector = this.selectedContacts().length
         ? z.string.groupCreationParticipantsActionCreate
@@ -74,10 +76,10 @@ z.ViewModel.content.GroupCreationViewModel = class GroupCreationViewModel {
         : z.string.groupCreationParticipantsHeader;
       return z.l10n.text(stringSelector, {number: this.selectedContacts().length});
     });
-
     this.stateIsPreferences = ko.pureComputed(() => this.state() === GroupCreationViewModel.STATE.PREFERENCES);
     this.stateIsParticipants = ko.pureComputed(() => this.state() === GroupCreationViewModel.STATE.PARTICIPANTS);
 
+    this.nameInput.subscribe(() => this.nameError(''));
     this.stateIsPreferences.subscribe(stateIsPreference => {
       if (stateIsPreference) {
         return $(document).on('keydown.groupCreation', keyboard_event => {
@@ -87,6 +89,12 @@ z.ViewModel.content.GroupCreationViewModel = class GroupCreationViewModel {
         });
       }
       return $(document).off('keydown.groupCreation');
+    });
+    this.stateIsParticipants.subscribe(stateIsParticipants => {
+      if (stateIsParticipants) {
+        return window.setTimeout(() => this.showContacts(true));
+      }
+      this.showContacts(false);
     });
 
     this.shouldUpdateScrollbar = ko
@@ -168,7 +176,7 @@ z.ViewModel.content.GroupCreationViewModel = class GroupCreationViewModel {
         method: this.method,
       });
 
-      return this.state(GroupCreationViewModel.STATE.PARTICIPANTS);
+      this.state(GroupCreationViewModel.STATE.PARTICIPANTS);
     }
   }
 
