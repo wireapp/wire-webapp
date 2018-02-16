1097ce3915e0848155c0d7da47383d4cb2e3dc2d
AndyTheGiant <andy.bialystock@gmail.com>
2018-02-16 15:20:48 +0100

    initial resizing integrated

--- app/script/view_model/MainViewModel.js
+++ app/script/view_model/MainViewModel.js
@@ -37,5 +37,88 @@ z.ViewModel.MainViewModel = class MainViewModel {
     });
 
     ko.applyBindings(this, document.getElementById(element_id));
+
+    this.isLeftColumnOpen = ko.observable(true);
+    this.isRightColumnOpen = ko.observable(false);
+  }
+
+  openRightColumn() {
+    this.resize(document.querySelector('#left-column').clientWidth, 200);
+    this.isRightColumnOpen(true);
+  }
+
+  closeRightColumn() {
+    this.resize(document.querySelector('#left-column').clientWidth, 0);
+    this.isRightColumnOpen(false);
+  }
+
+  resize(newWidthLeft, newWidthRight, duration = 100) {
+    const smoothStep = x => (x > 1 ? 1 : x < 0 ? 0 : x * x * (3 - 2 * x));
+    const lerp = (v1, v2, t) => v1 + (v2 - v1) * t;
+    const setStyle = (el, size) => (el.style.minWidth = el.style.flexBasis = `${size}px`);
+
+    const main = document.querySelector('#wire-main');
+    const leftColumn = document.querySelector('#left-column');
+    const centerColumn = document.querySelector('#center-column');
+    const rightColumn = document.querySelector('#right-column');
+
+    const oldWidthLeft = leftColumn.clientWidth;
+    const oldWidthCenter = centerColumn.clientWidth;
+    const oldWidthRight = rightColumn.clientWidth;
+
+    const newWidthCenter = main.clientWidth - newWidthLeft - newWidthRight;
+
+    const moveInLeft = oldWidthLeft === 0;
+    const moveOutLeft = newWidthLeft === 0;
+
+    const moveInRight = oldWidthRight === 0;
+    const moveOutRight = newWidthRight === 0;
+
+    const scaleLeft0 = moveInLeft || moveOutLeft ? 1 : oldWidthLeft / newWidthLeft;
+    const scaleCenter0 = oldWidthCenter / newWidthCenter;
+    const scaleRight0 = moveInRight || moveOutRight ? 1 : oldWidthRight / newWidthRight;
+
+    const transLeft0 = moveInLeft ? -newWidthLeft : 0;
+    const transCenter0 = moveOutLeft ? 0 : oldWidthLeft - newWidthLeft;
+    const transRight0 = oldWidthCenter - newWidthCenter - (moveInLeft ? newWidthLeft : 0);
+
+    const trans1 = moveOutLeft ? -oldWidthLeft : 0;
+
+    leftColumn.style.transformOrigin = centerColumn.style.transformOrigin = rightColumn.style.transformOrigin = 'left';
+
+    setStyle(leftColumn, newWidthLeft || oldWidthLeft);
+    setStyle(centerColumn, newWidthCenter);
+    setStyle(rightColumn, newWidthRight || oldWidthRight);
+
+    const startTime = Date.now();
+    function animate() {
+      const currTime = Date.now() - startTime;
+      if (currTime < duration) {
+        requestAnimationFrame(animate);
+      } else {
+        leftColumn.style.transform = centerColumn.style.transform = rightColumn.style.transform = '';
+        centerColumn.style.minWidth = centerColumn.style.flexBasis = '';
+        setStyle(leftColumn, newWidthLeft);
+        setStyle(rightColumn, newWidthRight);
+        return;
+      }
+      const step = smoothStep(currTime / duration);
+      leftColumn.style.transform = `scale(${lerp(scaleLeft0, 1, step)}, 1) translateX(${lerp(
+        transLeft0,
+        trans1,
+        step
+      )}px)`;
+      centerColumn.style.transform = `scale(${lerp(scaleCenter0, 1, step)}, 1) translateX(${lerp(
+        transCenter0,
+        trans1,
+        step
+      )}px)`;
+      rightColumn.style.transform = `scale(${lerp(scaleRight0, 1, step)}, 1) translateX(${lerp(
+        transRight0,
+        trans1,
+        step
+      )}px)`;
+    }
+    animate();
   }
 };
