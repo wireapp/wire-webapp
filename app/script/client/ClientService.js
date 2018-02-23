8ce967db95e69734829494d1f5bef8584293a2e7
Michael Koppen <michael.koppen@googlemail.com>
2018-02-23 12:21:23 +0100

    lookup clients by meta.primaryKey

--- app/script/client/ClientService.js
+++ app/script/client/ClientService.js
@@ -158,7 +158,7 @@ z.client.ClientService = class ClientService {
    */
   loadClientFromDb(primaryKey) {
     return this.storageService.db[z.storage.StorageService.OBJECT_STORE.CLIENTS]
-      .where(':id')
+      .where('meta.primaryKey')
       .equals(primaryKey)
       .first()
       .then(clientRecord => {
