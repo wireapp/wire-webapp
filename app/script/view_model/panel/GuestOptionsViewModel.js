d5072da3ef40ab59f9e7bec5be314dd0d92df1f6
Gregor Herdmann <gregor@wire.com>
2018-03-02 11:01:52 +0100

    feat: Add flag for link creation

--- app/script/view_model/panel/GuestOptionsViewModel.js
+++ app/script/view_model/panel/GuestOptionsViewModel.js
@@ -30,15 +30,18 @@ z.viewModel.panel.GuestOptionsViewModel = class GuestOptionsViewModel {
     this.stateHandler = this.conversationRepository.stateHandler;
 
     this.conversationEntity = this.conversationRepository.active_conversation;
+    this.enableConversationLinks = this.conversationRepository.enableConversationLinks;
     this.panelState = this.panelViewModel.state;
     this.isGuestRoom = this.panelViewModel.isGuestRoom;
     this.isTeamOnly = this.panelViewModel.isTeamOnly;
     this.isVisible = this.panelViewModel.guestOptionsVisible;
-    this.isGuestEnabled = ko.pureComputed(() => !this.isTeamOnly());
+
     this.isLinkCopied = ko.observable(false);
+    this.requestOngoing = ko.observable(false);
 
     this.hasAccessCode = ko.pureComputed(() => (this.isGuestRoom() ? !!this.conversationEntity().accessCode() : false));
-    this.requestOngoing = ko.observable(false);
+    this.isGuestEnabled = ko.pureComputed(() => !this.isTeamOnly());
+    this.showLinkOptions = ko.pureComputed(() => this.enableConversationLinks() && this.isGuestEnabled());
 
     this.conversationEntity.subscribe(conversationEntity => this._updateCode(this.isVisible(), conversationEntity));
     this.isVisible.subscribe(isVisible => this._updateCode(isVisible, this.conversationEntity()));
@@ -137,7 +140,7 @@ z.viewModel.panel.GuestOptionsViewModel = class GuestOptionsViewModel {
 
   _updateCode(isVisible, conversationEntity) {
     const updateCode = conversationEntity && conversationEntity.isGuestRoom() && !conversationEntity.accessCode();
-    if (isVisible && updateCode) {
+    if (isVisible && updateCode && this.enableConversationLinks()) {
       this.requestOngoing(true);
       this.stateHandler.getAccessCode(conversationEntity).then(() => this.requestOngoing(false));
     }
