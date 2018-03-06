f192cb311db52c658c63eaa6ea856bfb9af85ab8
Florian Keller <florian.keller@wire.com>
2018-03-06 14:05:08 +0100

    Use RecordNotFoundError

--- app/script/cryptography/CryptographyRepository.js
+++ app/script/cryptography/CryptographyRepository.js
@@ -420,7 +420,7 @@ z.cryptography.CryptographyRepository = class CryptographyRepository {
       .encrypt(sessionId, genericMessage.toArrayBuffer())
       .then(cipherText => ({cipherText: z.util.array_to_base64(cipherText), sessionId}))
       .catch(error => {
-        if (error.code === 2) {
+        if (error instanceof StoreEngine.error.RecordNotFoundError) {
           this.logger.log(`Session '${sessionId}' needs to get initialized...`);
           return {sessionId};
         }
