97c9dedf63643e00e0166c3b1e4b0af13e714f81
Benny Neugebauer <bn@bennyn.de>
2018-03-08 16:58:41 +0100

    fix(core): Update cookie if it already exists (#350)

--- src/demo/sender.js
+++ src/demo/sender.js
@@ -9,30 +9,34 @@ const path = require('path');
 require('dotenv').config({path: path.join(__dirname, 'echo2.env')});
 
 const {Account} = require('@wireapp/core');
+const APIClient = require('@wireapp/api-client');
+const {FileEngine} = require('@wireapp/store-engine');
 
-const account = new Account();
+(async () => {
+  const CONVERSATION_ID = process.env.WIRE_CONVERSATION_ID;
 
-const CONVERSATION_ID = process.env.WIRE_CONVERSATION_ID;
+  const login = {
+    email: process.env.WIRE_EMAIL,
+    password: process.env.WIRE_PASSWORD,
+    persist: true,
+  };
 
-const login = {
-  email: process.env.WIRE_EMAIL,
-  password: process.env.WIRE_PASSWORD,
-  persist: false,
-};
-
-account
-  .listen(login)
-  .then(() => {
-    async function sendMessage() {
-      const timeoutInMillis = 2000;
-      setTimeout(async () => {
-        account.service.conversation.sendTextMessage(CONVERSATION_ID, 'Hello World!').then(() => sendMessage());
-      }, timeoutInMillis);
-    }
-
-    sendMessage();
-  })
-  .catch(error => {
-    console.error(error);
-    process.exit(1);
+  const engine = new FileEngine(path.join(__dirname, '.tmp', 'sender'));
+  await engine.init(undefined, {fileExtension: '.json'});
+  const apiClient = new APIClient({
+    store: engine,
+    urls: APIClient.BACKEND.PRODUCTION,
   });
+  const account = new Account(apiClient);
+  await account.listen(login);
+
+  function sendMessage() {
+    const timeoutInMillis = 2000;
+    setTimeout(async () => {
+      await account.service.conversation.sendTextMessage(CONVERSATION_ID, 'Hello World!');
+      sendMessage();
+    }, timeoutInMillis);
+  }
+
+  sendMessage();
+})();
