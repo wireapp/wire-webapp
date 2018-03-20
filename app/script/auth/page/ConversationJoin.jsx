4a1f6681be8a8aba11e2c712eeeaf447e06ea99f
Michael Koppen <michael.koppen@googlemail.com>
2018-03-20 12:19:39 +0100

    fix: Show join page if current user is temporary guest

--- app/script/auth/page/ConversationJoin.jsx
+++ app/script/auth/page/ConversationJoin.jsx
@@ -254,7 +254,7 @@ class ConversationJoin extends Component {
   };
 
   renderJoin = () => {
-    const {error, isAuthenticated} = this.props;
+    const {error, isAuthenticated, isTemporaryGuest} = this.props;
     const {isValidLink, forceNewTemporaryGuestAccount} = this.state;
 
     if (!isValidLink) {
@@ -263,7 +263,7 @@ class ConversationJoin extends Component {
     if (this.isConversationFullError(error)) {
       return this.renderFullConversation();
     }
-    const renderTemporaryGuestAccountCreation = !isAuthenticated || forceNewTemporaryGuestAccount;
+    const renderTemporaryGuestAccountCreation = !isAuthenticated || isTemporaryGuest || forceNewTemporaryGuestAccount;
     return renderTemporaryGuestAccountCreation ? this.renderTemporaryGuestAccount() : this.renderActivatedAccount();
   };
 
@@ -288,6 +288,7 @@ export default withRouter(
         error: ConversationSelector.getError(state),
         isAuthenticated: AuthSelector.isAuthenticated(state),
         isFetching: ConversationSelector.isFetching(state),
+        isTemporaryGuest: SelfSelector.isTemporaryGuest(state),
         selfName: SelfSelector.getSelfName(state),
       }),
       {...ConversationAction, ...AuthAction}
