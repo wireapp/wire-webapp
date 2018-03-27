7786a7c4397134d7e01dfef732bfa78aa99a882e
Michael Koppen <michael.koppen@googlemail.com>
2018-03-27 14:16:59 +0200

    fix: Get persist from login state

--- app/script/auth/page/Login.js
+++ app/script/auth/page/Login.js
@@ -149,7 +149,7 @@ class Login extends React.PureComponent {
       .catch(error => {
         switch (error.label) {
           case BackendError.LABEL.NEW_CLIENT: {
-            const isFirstPersistentClient = login.persist && this.props.clients.length === 1;
+            const isFirstPersistentClient = this.state.persist && this.props.clients.length === 1;
             return isFirstPersistentClient
               ? window.location.replace(URLUtil.pathWithParams(EXTERNAL_ROUTE.WEBAPP))
               : this.props.history.push(ROUTE.HISTORY_INFO);
