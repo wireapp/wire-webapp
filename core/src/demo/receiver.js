b6e2a51292a4e96373ff253d9bfc22cb13405951
Florian Keller <ffflorian@users.noreply.github.com>
2018-04-25 15:05:34 +0200

    feat(core): Receive images (#509)

--- core/src/demo/receiver.js
+++ core/src/demo/receiver.js
@@ -1,3 +1,5 @@
+//@ts-check
+
 process.on('uncaughtException', error =>
   console.error(`Uncaught exception "${error.constructor.name}" (${error.code}): ${error.message}`, error)
 );
@@ -9,20 +11,47 @@ const path = require('path');
 require('dotenv').config({path: path.join(__dirname, 'echo1.env')});
 
 const {Account} = require('@wireapp/core');
+const APIClient = require('@wireapp/api-client');
+const fs = require('fs');
+const {promisify} = require('util');
+const {Config} = require('@wireapp/api-client/dist/commonjs/Config');
+const {MemoryEngine} = require('@wireapp/store-engine/dist/commonjs/engine');
+const engine = new MemoryEngine();
 
-const account = new Account();
+(async () => {
+  const login = {
+    email: process.env.WIRE_EMAIL,
+    password: process.env.WIRE_PASSWORD,
+    persist: false,
+  };
 
-account.on(Account.INCOMING.TEXT_MESSAGE, data => {
-  console.log(`Message in "${data.conversation}" from "${data.from}": "${data.content}"`);
-});
+  await engine.init('receiver');
 
-const login = {
-  email: process.env.WIRE_EMAIL,
-  password: process.env.WIRE_PASSWORD,
-  persist: false,
-};
+  const apiClient = new APIClient(new Config(engine, APIClient.BACKEND.STAGING));
+  const account = new Account(apiClient);
 
-account.listen(login).catch(error => {
-  console.error(error);
-  process.exit(1);
-});
+  account.on(Account.INCOMING.TEXT_MESSAGE, data => {
+    console.log(`Message in "${data.conversation}" from "${data.from}":`, data.content);
+  });
+
+  account.on(Account.INCOMING.ASSET, async data => {
+    const {
+      conversation,
+      from,
+      content: {uploaded, original},
+    } = data;
+    console.log(`Asset in "${conversation}" from "${from}":`, original);
+    const fileType = original.mimeType.replace(/[^\/]+\//g, '');
+    const image = await account.service.conversation.getImage(uploaded);
+    await promisify(fs.writeFile)(path.join('.', `received_image.${fileType}`), image);
+  });
+
+  try {
+    await account.login(login);
+    await account.listen();
+    console.log('Listening for messages ...');
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+})();
