9deccba771e2673aa3338ebc8eea80f4898cd780
Michael Koppen <Yserz@users.noreply.github.com>
2018-05-03 11:49:10 +0200

    fix(core): Pass empty content in decodeEvent when message text is empty (#551)

--- src/main/Account.ts
+++ src/main/Account.ts
@@ -249,7 +249,7 @@ class Account extends EventEmitter {
           this.service.cryptography.decrypt(sessionId, ciphertext).then((decryptedMessage: Uint8Array) => {
             const genericMessage = this.protocolBuffers.GenericMessage.decode(decryptedMessage);
             resolve({
-              content: genericMessage.text.content,
+              content: genericMessage.text && genericMessage.text.content,
               id: genericMessage.messageId,
               type: genericMessage.content,
             });
