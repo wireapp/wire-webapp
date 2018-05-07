b36339f84d8884631eaf09adb570bc471558e076
Lipis <lipiridis@gmail.com>
2018-05-07 18:28:52 +0200

    lint: Add TSLint support (#563)

--- src/main/http/HttpClient.ts
+++ src/main/http/HttpClient.ts
@@ -17,12 +17,11 @@
  *
  */
 
-import axios, {AxiosError, AxiosPromise, AxiosRequestConfig, AxiosResponse} from 'axios';
-import {AccessTokenData} from '../auth';
-import {AccessTokenStore, AuthAPI} from '../auth';
-import {ContentType} from '../http';
-import {CRUDEngine} from '@wireapp/store-engine/dist/commonjs/engine';
 import {PriorityQueue} from '@wireapp/priority-queue';
+import {CRUDEngine} from '@wireapp/store-engine/dist/commonjs/engine';
+import axios, {AxiosError, AxiosPromise, AxiosRequestConfig, AxiosResponse} from 'axios';
+import {AccessTokenData, AccessTokenStore, AuthAPI} from '../auth';
+import {ContentType} from '../http';
 import {sendRequestWithCookie} from '../shims/node/cookie';
 
 const logdown = require('logdown');
