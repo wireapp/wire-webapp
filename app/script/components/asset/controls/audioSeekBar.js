9c9a0bcab510077332e46ef55cf1bda3d39a5639
Gregor Herdmann <gregor@wire.com>
2018-06-19 12:57:13 +0200

    chore: Use z.util.NumberUtil() where applicable (#3719)

--- app/script/components/asset/controls/audioSeekBar.js
+++ app/script/components/asset/controls/audioSeekBar.js
@@ -85,8 +85,10 @@ z.components.AudioSeekBarComponent = class AudioSeekBarComponent {
 
   _on_level_click(event) {
     const mouse_x = event.pageX - $(event.currentTarget).offset().left;
-    const currentTime = (this.audio_element.duration * mouse_x) / event.currentTarget.clientWidth;
-    this.audio_element.currentTime = Math.max(0, Math.min(currentTime, this.audio_element.duration));
+    const calculatedTime = (this.audio_element.duration * mouse_x) / event.currentTarget.clientWidth;
+    const currentTime = window.isNaN(calculatedTime) ? 0 : calculatedTime;
+
+    this.audio_element.currentTime = z.util.NumberUtil.clamp(currentTime, 0, this.audio_element.duration);
     this._on_time_update();
   }
 
