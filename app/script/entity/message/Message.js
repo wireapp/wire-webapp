6cbccfd7d04376469b51e4df8ef74f56e5b561c5
Thomas Belin <thomasbelin4@gmail.com>
2018-06-25 18:59:34 +0200

    fix: send ephemeral messages to own devices (WEBAPP-5186) (#3802)

--- app/script/entity/message/Message.js
+++ app/script/entity/message/Message.js
@@ -269,9 +269,10 @@ z.entity.Message = class Message {
     }
 
     if (this.ephemeral_status() === z.message.EphemeralStatusType.INACTIVE) {
-      const expirationDate = `${Date.now() + this.ephemeral_expires()}`;
+      const startingDate = this.user().is_me ? this.timestamp() : Date.now();
+      const expirationDate = `${startingDate + this.ephemeral_expires()}`;
       this.ephemeral_expires(expirationDate);
-      this.ephemeral_started(`${Date.now()}`);
+      this.ephemeral_started(`${startingDate}`);
     }
 
     const remainingTime = this.ephemeral_expires() - Date.now();
