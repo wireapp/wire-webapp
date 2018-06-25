f4da3365d35b750a33e19712497f2323bc1ee621
Gregor Herdmann <gregor@wire.com>
2018-06-25 18:35:56 +0200

    fix: Hide timed message options in 1:1 conversations (#3801)

--- app/script/view_model/panel/ConversationDetailsViewModel.js
+++ app/script/view_model/panel/ConversationDetailsViewModel.js
@@ -130,7 +130,9 @@ z.viewModel.panel.ConversationDetailsViewModel = class ConversationDetailsViewMo
       return !this.conversationEntity().is_request() && !this.conversationEntity().is_cleared();
     });
     this.showActionGuestOptions = ko.pureComputed(() => this.conversationEntity().inTeam());
-    this.showActionTimedMessages = ko.pureComputed(() => !this.conversationEntity().isGuest());
+    this.showActionTimedMessages = ko.pureComputed(() => {
+      return this.conversationEntity().is_group() && !this.conversationEntity().isGuest();
+    });
     this.showActionLeave = ko.pureComputed(() => {
       return this.conversationEntity().is_group() && !this.conversationEntity().removed_from_conversation();
     });
