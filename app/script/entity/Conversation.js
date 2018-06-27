e8ea70468751ff0d9b04a244e1bf9f3206582b8a
Thomas Belin <me@thomasbelin.fr>
2018-06-27 16:48:07 +0200

    review

--- app/script/entity/Conversation.js
+++ app/script/entity/Conversation.js
@@ -570,13 +570,13 @@ z.entity.Conversation = class Conversation {
    * @returns {z.entity.Message} Last delivered message
    */
   getLastDeliveredMessage() {
-    const messageEntities = this.messages()
+    return this.messages()
       .slice()
-      .reverse();
-    return messageEntities.find(messageEntity => {
-      const isDelivered = messageEntity.status() === z.message.StatusType.DELIVERED;
-      return isDelivered && messageEntity.user().is_me;
-    });
+      .reverse()
+      .find(messageEntity => {
+        const isDelivered = messageEntity.status() === z.message.StatusType.DELIVERED;
+        return isDelivered && messageEntity.user().is_me;
+      });
   }
 
   /**
