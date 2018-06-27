ec5d1ac2b81526c0f808b83c05e4ab20b4086b62
Gregor Herdmann <gregor@wire.com>
2018-06-27 17:43:17 +0200

    refactor: Use single in viewport observer

---
+++ app/script/ui/ViewportObserver.js
@@ -0,0 +1,69 @@
+/*
+ * Wire
+ * Copyright (C) 2018 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+'use strict';
+
+window.z = window.z || {};
+window.z.ui = z.ui || {};
+
+z.ui.ViewportObserver = (() => {
+  let intersectionObserver = undefined;
+  const observedElements = [];
+
+  const _addElement = (element, callback) => {
+    if (!intersectionObserver) {
+      _createObserver();
+    }
+    observedElements.push({callback, element});
+    intersectionObserver.observe(element);
+  };
+
+  const _createObserver = () => {
+    const onIntersect = entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const trackedElement = _getElement(entry.target);
+          trackedElement.callback();
+          intersectionObserver.unobserve(trackedElement.element);
+        }
+      });
+    };
+
+    const options = {root: null, rootMargin: '0px', threshold: 0.0};
+    intersectionObserver = new IntersectionObserver(onIntersect, options);
+    return intersectionObserver;
+  };
+
+  const _getElement = element => {
+    return observedElements.find(trackedElement => trackedElement.element === element);
+  };
+
+  const _removeElement = element => {
+    const removedElement = _getElement(element);
+    if (removedElement) {
+      observedElements.splice(observedElements.indexOf(removedElement), 1);
+    }
+    intersectionObserver.unobserve(element);
+  };
+
+  return {
+    addElement: _addElement,
+    removeElement: _removeElement,
+  };
+})();
