721a8cc42b8e23cc436a1fa0a88e2f7a72346cd3
Michael Koppen <Yserz@users.noreply.github.com>
2018-07-02 18:28:43 +0200

    refactor(react-ui-kit): Gather animation defaults (#790)

--- react-ui-kit/src/Identity/motions.js
+++ react-ui-kit/src/Identity/motions.js
@@ -17,4 +17,55 @@
  *
  */
 
+import {keyframes} from 'styled-components';
+
+// https://motion.wire.com/_guidelines_/rules.html
+
+/**
+ * Duration exception - When a motion longer than 0.7 seconds is needed use a multiplier of 700.
+ */
+export const DURATION = {
+  DEFAULT: '0.55s',
+  EXTRA_LONG: '2.4s',
+  PROACTIVE_FAST: '0.15s',
+  PROACTIVE_SLOW: '0.35s',
+  SYSTEM: '0.7s',
+};
+
+/**
+ * The rule of thumb is using Quart Out when animating only the object's opacity,
+ * otherwise use Expo Out ease.
+ */
+export const EASE = {
+  EXPONENTIAL: 'cubic-bezier(0.19, 1, 0.22, 1)',
+  QUART: 'cubic-bezier(0.165, 0.84, 0.44, 1)',
+};
+
+export const ANIMATION = {
+  bottomUpMovement: keyframes`
+  0% {
+    transform: translateY(100%);
+  }
+  100% {
+    transform: translateY(0);
+  }
+`,
+  fadeIn: keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`,
+  rotate: keyframes`
+  0% {
+    transform: rotate(0);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`,
+};
+
 export const defaultTransition = 'transition: all 0.24s;';
