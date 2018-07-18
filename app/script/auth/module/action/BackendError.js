d023abb73811133e01952429e220023bc5d9c8c5
Michael Koppen <Yserz@users.noreply.github.com>
2018-07-18 17:39:36 +0200

    fix: BackendError constructor is still needed for synthetic errors (#4080)

--- app/script/auth/module/action/BackendError.js
+++ app/script/auth/module/action/BackendError.js
@@ -18,6 +18,14 @@
  */
 
 export default class BackendError extends Error {
+  constructor(params) {
+    super();
+    this.name = this.constructor.name;
+    this.code = params.code;
+    this.label = params.label;
+    this.message = params.message;
+  }
+
   static AUTH_ERRORS = {
     BLACKLISTED_EMAIL: 'blacklisted-email',
     BLACKLISTED_PHONE: 'blacklisted-phone',
