16e5736525cd0a044b16005f498aca59937c5237
Benny Neugebauer <bn@bennyn.de>
2018-07-19 11:42:49 +0200

    test(core): Fix master branch

--- core/src/main/Account.smoke-test.node.js
+++ core/src/main/Account.smoke-test.node.js
@@ -189,7 +189,7 @@ describe('Account', () => {
       try {
         await bob.service.conversation.addUser(conversationId, getId(eve));
       } catch (error) {
-        expect(error.name).toBe(`${UnconnectedUserError.name}a`);
+        expect(error.name).toBe(UnconnectedUserError.name);
       }
 
       // Alice adds Eve to the conversation
