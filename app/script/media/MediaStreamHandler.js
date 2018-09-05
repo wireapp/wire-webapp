71efd1f713c2f42815d300850ac0725f048996d0
AndyLnd <andy.bialystock@gmail.com>
2018-09-05 15:23:33 +0200

    feat: Show "no camera access" in preview space (#4510)

--- app/script/media/MediaStreamHandler.js
+++ app/script/media/MediaStreamHandler.js
@@ -663,11 +663,11 @@ z.media.MediaStreamHandler = class MediaStreamHandler {
    * @returns {undefined} No return value
    */
   _showPermissionDeniedHint(mediaType) {
-    if (mediaType === z.media.MediaType.AUDIO_VIDEO || mediaType === z.media.MediaType.VIDEO) {
-      return this.mediaRepository.showNoCameraModal();
+    const videoTypes = [z.media.MediaType.AUDIO_VIDEO, z.media.MediaType.VIDEO];
+    if (!videoTypes.includes(mediaType)) {
+      const warningType = this._selectPermissionDeniedWarningType(mediaType);
+      amplify.publish(z.event.WebApp.WARNING.SHOW, warningType);
     }
-    const warningType = this._selectPermissionDeniedWarningType(mediaType);
-    amplify.publish(z.event.WebApp.WARNING.SHOW, warningType);
   }
 
   /**
