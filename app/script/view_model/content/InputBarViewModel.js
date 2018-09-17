2cd2ffa0190712d9881db07caf2f498ade2c6867
Thomas Belin <me@thomasbelin.fr>
2018-09-17 17:20:42 +0200

    fix: clean test code

--- app/script/view_model/content/InputBarViewModel.js
+++ app/script/view_model/content/InputBarViewModel.js
@@ -96,19 +96,8 @@ z.viewModel.content.InputBarViewModel = class InputBarViewModel {
       },
     });
 
-    this.mentionSuggestions = ko.pureComputed(() => {
-      const inputValue = this.input();
-      const incompleteMention = inputValue.match(/@(\w+)$/);
-      const candidates = this.conversationEntity().participating_user_ets();
-      if (incompleteMention) {
-        return this.searchRepository.searchUserInSet(incompleteMention[1], candidates);
-      }
-      return [];
-    });
-
     this.richTextInput = ko.pureComputed(() => {
       const input = this.input();
-      const suggestions = this.mentionSuggestions();
       const participatingUserEntities = this.conversationEntity().participating_user_ets();
 
       const pieces = this.parseForMentions(input, participatingUserEntities)
