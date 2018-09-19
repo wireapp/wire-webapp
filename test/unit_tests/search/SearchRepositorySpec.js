81de50369ebbe01e97c3161cb9e2dc489371f0c0
Thomas Belin <me@thomasbelin.fr>
2018-09-19 12:15:48 +0200

    feat: Upgrade search algorithm to tokenize properties

--- test/unit_tests/search/SearchRepositorySpec.js
+++ test/unit_tests/search/SearchRepositorySpec.js
@@ -29,15 +29,15 @@ describe('z.search.SearchRepository', () => {
   });
 
   describe('searchUserInSet', () => {
-    const sabine = generateUser('jesuissabine', 'Sabine', 'Duchemin');
-    const janina = generateUser('yosoyjanina', 'Janina', 'Felix');
-    const felix = generateUser('iamfelix', 'Felix', 'Felicitation');
-    const felicien = generateUser('ichbinfelicien', 'Felicien', 'Delatour');
-    const lastguy = generateUser('lastfelicien', 'lastsabine', 'lastjanina');
-    const noMatch1 = generateUser(undefined, 'yyy', 'yyy');
-    const noMatch2 = generateUser('xxx', undefined, 'xxx');
-    const noMatch3 = generateUser('zzz', 'zzz', undefined);
-    const users = [lastguy, noMatch1, felix, felicien, sabine, janina, noMatch2, noMatch3];
+    const sabine = generateUser('jesuissabine', 'Sabine Duchemin');
+    const janina = generateUser('yosoyjanina', 'Janina Felix');
+    const felixa = generateUser('iamfelix', 'Felix Abo');
+    const felix = generateUser('iamfelix', 'Felix Oulala');
+    const felicien = generateUser('ichbinfelicien', 'Felicien Delatour');
+    const lastguy = generateUser('lastfelicien', 'lastsabine lastjanina');
+    const noMatch1 = generateUser(undefined, 'yyy yyy');
+    const noMatch2 = generateUser('xxx', undefined);
+    const users = [lastguy, noMatch1, felix, felicien, sabine, janina, noMatch2, felixa];
 
     const tests = [
       {expected: users, term: '', testCase: 'returns the whole user list if no term is given'},
@@ -48,17 +48,17 @@ describe('z.search.SearchRepository', () => {
         testCase: 'puts matches that start with the pattern on top of the list',
       },
       {
-        expected: [felix, felicien, janina, lastguy],
+        expected: [felicien, felixa, felix, janina, lastguy],
         term: 'fel',
-        testCase: 'sorts by firstname, lastname, handle and inside match',
+        testCase: 'sorts by firstname, lastname, handle, inside match and alphabetically',
       },
       {
-        expected: [felix, janina],
+        expected: [felixa, felix, janina],
         term: 'felix',
         testCase: 'sorts by firstname and lastname',
       },
       {
-        expected: [felicien, felix, lastguy],
+        expected: [felicien, lastguy],
         term: 'felici',
         testCase: 'sorts by firstname, lastname and inside match',
       },
@@ -82,7 +82,7 @@ describe('z.search.SearchRepository', () => {
         term: 'sabine',
         testCase: 'puts matches that start with the pattern on top of the list',
       },
-      {expected: [felicien, felix, lastguy], term: 'ic', testCase: 'matches inside the properties'},
+      {expected: [felicien, lastguy], term: 'ic', testCase: 'matches inside the properties'},
     ];
 
     tests.forEach(({expected, term, testCase}) => {
@@ -94,10 +94,10 @@ describe('z.search.SearchRepository', () => {
   });
 });
 
-function generateUser(handle, firstname = '', lastname = '') {
+function generateUser(handle, name) {
   const user = new z.entity.User();
   user.username(handle);
-  user.name(`${firstname} ${lastname}`);
+  user.name(name);
   return user;
 }
 function serializeUser(userEntity) {
