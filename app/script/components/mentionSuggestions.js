667c9d944227578f0542afad8f7118cd02561ace
AndyTheGiant <andy.bialystock@gmail.com>
2018-09-20 16:12:13 +0200

    feat: Style suggestion popup

--- app/script/components/mentionSuggestions.js
+++ app/script/components/mentionSuggestions.js
@@ -111,12 +111,18 @@ z.components.MentionSuggestions = class MentionSuggestions {
 ko.components.register('mention-suggestions', {
   template: `
   <!-- ko if: isVisible() -->
-    <div class="conversation-input-bar-mention-suggestion" data-uie-name="list-mention-suggestions" data-bind="style: positions()">
-      <ul class="mention-suggestion-list" data-bind="foreach: {data: suggestions, as: 'suggestion'}">
-        <li data-bind="style: {color: suggestion === $parent.selectedSuggestion() ? 'green' : ''}">
-          <span data-bind="text: suggestion.username()"></span>
-        </li>
-      </ul>
+    <div class="
+    " data-uie-name="list-mention-suggestions" data-bind="style: positions()">
+      <div class="mention-suggestion-list" data-bind="foreach: {data: suggestions, as: 'suggestion'}">
+        <div class="mention-suggestion-list__item" data-bind="css: {'mention-suggestion-list__item--highlighted': suggestion === $parent.selectedSuggestion()}">
+          <participant-avatar params="participant: suggestion, size: z.components.ParticipantAvatar.SIZE.XXX_SMALL"></participant-avatar>
+          <div class="mention-suggestion-list__item__name" data-bind="text: suggestion.name()"></div>
+          <div class="mention-suggestion-list__item__username"data-bind="text: suggestion.username()"></div>
+          <!-- ko if: suggestion.isGuest() -->
+            <guest-icon class="mention-suggestion-list__item__guest-badge"></guest-icon>
+          <!-- /ko -->
+        </div>
+      </div>
     </div>
   <!-- /ko -->`,
 
