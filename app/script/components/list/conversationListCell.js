b176ab297c9951c4ca7cd9aeecfc81ae6df54388
Gregor Herdmann <gregor@wire.com>
2018-09-26 17:26:38 +0200

    feat: Remove bold conversation list name (#4718)

--- app/script/components/list/conversationListCell.js
+++ app/script/components/list/conversationListCell.js
@@ -68,7 +68,7 @@ ko.components.register('conversation-list-cell', {
           </availability-state>
         <!-- /ko -->
         <!-- ko ifnot: conversation.is_one2one() && conversation.self.inTeam() -->
-          <span class="conversation-list-cell-name" data-bind="text: conversation.display_name(), css: {'text-theme': is_selected(conversation), 'conversation-list-cell-name--bold': !!cell_state().description}"></span>
+          <span class="conversation-list-cell-name" data-bind="text: conversation.display_name(), css: {'text-theme': is_selected(conversation)}"></span>
         <!-- /ko -->
         <span class="conversation-list-cell-description" data-bind="text: cell_state().description" data-uie-name="secondary-line"></span>
       </div>
