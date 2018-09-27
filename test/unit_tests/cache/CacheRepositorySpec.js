f720cbb182038193dd8e61e07d256ea4fe019d3c
Thomas Belin <me@thomasbelin.fr>
2018-09-27 14:13:13 +0200

    test: restore test on conversation input preservation

--- test/unit_tests/cache/CacheRepositorySpec.js
+++ test/unit_tests/cache/CacheRepositorySpec.js
@@ -29,13 +29,14 @@ describe('z.cache.CacheRepository', () => {
     beforeEach(() => {
       cache_repository.clearCache();
 
+      amplify.store(z.storage.StorageKey.CONVERSATION.INPUT, 'text');
       amplify.store(z.storage.StorageKey.AUTH.SHOW_LOGIN, true);
       amplify.store(TEMP_KEY, true);
     });
 
     it('deletes cached keys', () => {
       const deleted_keys = cache_repository.clearCache(false);
-      expect(deleted_keys.length).toBe(1);
+      expect(deleted_keys.length).toBe(2);
     });
 
     it('preserves cached conversation inputs while deleting other keys', () => {
