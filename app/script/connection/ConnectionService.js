86ef0feabcb65ab9397602e42ac3e51366413cc3
Gregor Herdmann <gregor@wire.com>
2018-10-18 17:52:28 +0200

    Update Swagger links

--- app/script/connection/ConnectionService.js
+++ app/script/connection/ConnectionService.js
@@ -45,7 +45,7 @@ z.connection.ConnectionService = class ConnectionService {
    * @note The list is already pre-ordered by the backend, so in order to fetch more connections
    * than the limit, you only have to pass the User ID (which is not from the self user)
    * of the last connection item from the received list.
-   * @see https://staging-nginz-https.zinfra.io/swagger-ui/#!/users/connections
+   * @see https://staging-nginz-https.zinfra.io/swagger-ui/tab.html#!//connections
    *
    * @param {number} limit - Number of results to return (default 100, max 500)
    * @param {string} userId - User ID to start from
@@ -65,7 +65,7 @@ z.connection.ConnectionService = class ConnectionService {
   /**
    * Create a connection request to another user.
    *
-   * @see https://staging-nginz-https.zinfra.io/swagger-ui/#!/users/createConnection
+   * @see https://staging-nginz-https.zinfra.io/swagger-ui/tab.html#!//createConnection
    *
    * @param {string} userId - User ID of the user to request a connection with
    * @param {string} name - Name of the conversation being initiated (1 - 256 characters)
@@ -87,7 +87,7 @@ z.connection.ConnectionService = class ConnectionService {
    * Updates a connection to another user.
    *
    * @example status: ['accepted', 'blocked', 'pending', 'ignored', 'sent' or 'cancelled']
-   * @see https://staging-nginz-https.zinfra.io/swagger-ui/#!/users/updateConnection
+   * @see https://staging-nginz-https.zinfra.io/swagger-ui/tab.html#!//updateConnection
    *
    * @param {string} userId - User ID of the other user
    * @param {z.connection.ConnectionStatus} connectionStatus - New relation status
