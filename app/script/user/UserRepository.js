53ad121299a62577783e904a121c060e2ee07b99
Gregor Herdmann <gregor@wire.com>
2018-10-18 18:42:33 +0200

    feat: Move self related calls to SelfService (#4929)

--- app/script/user/UserRepository.js
+++ app/script/user/UserRepository.js
@@ -40,13 +40,15 @@ z.user.UserRepository = class UserRepository {
    * @param {z.user.UserService} user_service - Backend REST API user service implementation
    * @param {z.assets.AssetService} asset_service - Backend REST API asset service implementation
    * @param {z.connection.ConnectionService} connectionService - Backend REST API connection service implementation
+   * @param {z.self.SelfService} selfService - Backend REST API self service implementation
    * @param {z.client.ClientRepository} client_repository - Repository for all client interactions
    * @param {z.time.ServerTimeRepository} serverTimeRepository - Handles time shift between server and client
    */
-  constructor(user_service, asset_service, connectionService, client_repository, serverTimeRepository) {
+  constructor(user_service, asset_service, connectionService, selfService, client_repository, serverTimeRepository) {
     this.user_service = user_service;
     this.asset_service = asset_service;
     this.connectionService = connectionService;
+    this.selfService = selfService;
     this.client_repository = client_repository;
     this.logger = new z.util.Logger('z.user.UserRepository', z.config.LOGGER.OPTIONS);
 
@@ -615,14 +617,10 @@ z.user.UserRepository = class UserRepository {
    * @returns {Promise} Promise that resolves when account deletion process has been initiated
    */
   delete_me() {
-    return this.user_service
-      .delete_self()
-      .then(() => {
-        this.logger.info('Account deletion initiated');
-      })
-      .catch(error => {
-        this.logger.error(`Unable to delete self: ${error}`);
-      });
+    return this.selfService
+      .deleteSelf()
+      .then(() => this.logger.info('Account deletion initiated'))
+      .catch(error => this.logger.error(`Unable to delete self: ${error}`));
   }
 
   /**
@@ -702,8 +700,8 @@ z.user.UserRepository = class UserRepository {
    * @returns {Promise} Promise that will resolve with the self user entity
    */
   getSelf() {
-    return this.user_service
-      .get_own_user()
+    return this.selfService
+      .getSelf()
       .then(response => this.user_mapper.map_self_user_from_object(response))
       .then(userEntity => {
         const promises = [this.save_user(userEntity, true), this.getMarketingConsent()];
@@ -892,8 +890,8 @@ z.user.UserRepository = class UserRepository {
    * @returns {Promise} Resolves when accent color was changed
    */
   change_accent_color(accent_id) {
-    return this.user_service
-      .update_own_user_profile({accent_id})
+    return this.selfService
+      .putSelf({accent_id})
       .then(() => this.user_update({user: {accent_id: accent_id, id: this.self().id}}));
   }
 
@@ -904,9 +902,7 @@ z.user.UserRepository = class UserRepository {
    */
   change_name(name) {
     if (name.length >= UserRepository.CONFIG.MINIMUM_NAME_LENGTH) {
-      return this.user_service
-        .update_own_user_profile({name})
-        .then(() => this.user_update({user: {id: this.self().id, name: name}}));
+      return this.selfService.putSelf({name}).then(() => this.user_update({user: {id: this.self().id, name: name}}));
     }
 
     return Promise.reject(new z.error.UserError(z.userUserError.TYPE.INVALID_UPDATE));
@@ -952,8 +948,8 @@ z.user.UserRepository = class UserRepository {
    */
   change_username(username) {
     if (username.length >= UserRepository.CONFIG.MINIMUM_USERNAME_LENGTH) {
-      return this.user_service
-        .change_own_username(username)
+      return this.selfService
+        .putSelfHandle(username)
         .then(() => {
           this.should_set_username = false;
           return this.user_update({user: {handle: username, id: this.self().id}});
@@ -1021,8 +1017,8 @@ z.user.UserRepository = class UserRepository {
           {key: previewImageKey, size: 'preview', type: 'image'},
           {key: mediumImageKey, size: 'complete', type: 'image'},
         ];
-        return this.user_service
-          .update_own_user_profile({assets})
+        return this.selfService
+          .putSelf({assets})
           .then(() => this.user_update({user: {assets: assets, id: this.self().id}}));
       })
       .catch(error => {
@@ -1050,8 +1046,8 @@ z.user.UserRepository = class UserRepository {
   }
 
   getMarketingConsent() {
-    return this.user_service
-      .getConsent()
+    return this.selfService
+      .getSelfConsent()
       .then(consents => {
         for (const {type: consentType, value: consentValue} of consents) {
           const isMarketingConsent = consentType === z.user.ConsentType.MARKETING;
@@ -1073,7 +1069,7 @@ z.user.UserRepository = class UserRepository {
   }
 
   setConsent(consentType, consentValue) {
-    return this.user_service.putConsent(consentType, consentValue, `Webapp ${z.util.Environment.version(false)}`);
+    return this.selfService.putSelfConsent(consentType, consentValue, `Webapp ${z.util.Environment.version(false)}`);
   }
 
   changeMarketingConsent(consentGiven) {
