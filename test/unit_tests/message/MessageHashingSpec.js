f28909deb7f521007c917c82fb00bb95742c70d1
Florian Keller <florian.keller@wire.com>
2018-10-26 10:13:58 +0200

    Remove async from tests

--- test/unit_tests/message/MessageHashingSpec.js
+++ test/unit_tests/message/MessageHashingSpec.js
@@ -31,7 +31,6 @@ describe('z.message.MessageHashing', () => {
       messageEntity.timestamp(timestamp);
 
       const bytesArray = z.message.MessageHashing._getTimestampArray(timestamp);
-
       const hexValue = z.util.StringUtil.bytesToHex(bytesArray);
 
       expect(hexValue).toBe(expectedHexValue);
@@ -39,7 +38,7 @@ describe('z.message.MessageHashing', () => {
   });
 
   describe('getTextMessageHash', () => {
-    it('correctly creates a markdown text bytes buffer.', async () => {
+    it('correctly creates a markdown text bytes buffer.', () => {
       const expectedHexValue =
         'feff005400680069007300200068006100730020002a002a006d00610072006b0064006f0077006e002a002a000000005bcdcccd';
       const expectedHashValue = 'f25a925d55116800e66872d2a82d8292adf1d4177195703f976bc884d32b5c94';
@@ -60,13 +59,14 @@ describe('z.message.MessageHashing', () => {
       messageEntity.timestamp(timestamp);
       messageEntity.add_asset(textEntity);
 
-      const hashArray = await z.message.MessageHashing.getTextMessageHash(messageEntity);
-      const hashValue = z.util.StringUtil.bytesToHex(hashArray);
+      return z.message.MessageHashing.getTextMessageHash(messageEntity).then(hashArray => {
+        const hashValue = z.util.StringUtil.bytesToHex(hashArray);
 
-      expect(hashValue).toBe(expectedHashValue);
+        expect(hashValue).toBe(expectedHashValue);
+      });
     });
 
-    it('correctly creates an arabic text bytes buffer.', async () => {
+    it('correctly creates an arabic text bytes buffer.', () => {
       const expectedHexValue = 'feff0628063a062f0627062f000000005bcdcccd';
       const expectedHashValue = '5830012f6f14c031bf21aded5b07af6e2d02d01074f137d106d4645e4dc539ca';
 
@@ -86,13 +86,14 @@ describe('z.message.MessageHashing', () => {
       messageEntity.timestamp(timestamp);
       messageEntity.add_asset(textEntity);
 
-      const hashArray = await z.message.MessageHashing.getTextMessageHash(messageEntity);
-      const hashValue = z.util.StringUtil.bytesToHex(hashArray);
+      return z.message.MessageHashing.getTextMessageHash(messageEntity).then(hashArray => {
+        const hashValue = z.util.StringUtil.bytesToHex(hashArray);
 
-      expect(hashValue).toBe(expectedHashValue);
+        expect(hashValue).toBe(expectedHashValue);
+      });
     });
 
-    it('correctly creates an emoji text bytes buffer.', async () => {
+    it('correctly creates an emoji text bytes buffer.', () => {
       const expectedHexValue =
         'feff00480065006c006c006f0020d83ddc69200dd83ddcbbd83ddc68200dd83ddc69200dd83ddc670021000000005bcdcc09';
       const expectedHashValue = '4f8ee55a8b71a7eb7447301d1bd0c8429971583b15a91594b45dee16f208afd5';
@@ -113,13 +114,14 @@ describe('z.message.MessageHashing', () => {
       messageEntity.timestamp(timestamp);
       messageEntity.add_asset(textEntity);
 
-      const hashArray = await z.message.MessageHashing.getTextMessageHash(messageEntity);
-      const hashValue = z.util.StringUtil.bytesToHex(hashArray);
+      return z.message.MessageHashing.getTextMessageHash(messageEntity).then(hashArray => {
+        const hashValue = z.util.StringUtil.bytesToHex(hashArray);
 
-      expect(hashValue).toBe(expectedHashValue);
+        expect(hashValue).toBe(expectedHashValue);
+      });
     });
 
-    it('correctly creates a link text bytes buffer.', async () => {
+    it('correctly creates a link text bytes buffer.', () => {
       const expectedHexValue =
         'feff00680074007400700073003a002f002f007700770077002e0079006f00750074007500620065002e0063006f006d002f00770061007400630068003f0076003d0044004c007a00780072007a004600430079004f0073000000005bcdcc09';
       const expectedHashValue = 'ef39934807203191c404ebb3acba0d33ec9dce669f9acec49710d520c365b657';
@@ -140,15 +142,16 @@ describe('z.message.MessageHashing', () => {
       messageEntity.timestamp(timestamp);
       messageEntity.add_asset(textEntity);
 
-      const hashArray = await z.message.MessageHashing.getTextMessageHash(messageEntity);
-      const hashValue = z.util.StringUtil.bytesToHex(hashArray);
+      return z.message.MessageHashing.getTextMessageHash(messageEntity).then(hashArray => {
+        const hashValue = z.util.StringUtil.bytesToHex(hashArray);
 
-      expect(hashValue).toBe(expectedHashValue);
+        expect(hashValue).toBe(expectedHashValue);
+      });
     });
   });
 
   describe('getLocationMessageHash', () => {
-    it('correctly creates a location bytes buffer.', async () => {
+    it('correctly creates a location bytes buffer.', () => {
       const expectedHexValue = '000000000000cd250000000000003458000000005bcdcc09';
       const expectedHashValue = '56a5fa30081bc16688574fdfbbe96c2eee004d1fb37dc714eec6efb340192816';
 
@@ -171,13 +174,14 @@ describe('z.message.MessageHashing', () => {
       messageEntity.timestamp(timestamp);
       messageEntity.add_asset(locationEntity);
 
-      const hashArray = await z.message.MessageHashing.getLocationMessageHash(messageEntity);
-      const hashValue = z.util.StringUtil.bytesToHex(hashArray);
+      return z.message.MessageHashing.getLocationMessageHash(messageEntity).then(hashArray => {
+        const hashValue = z.util.StringUtil.bytesToHex(hashArray);
 
-      expect(hashValue).toBe(expectedHashValue);
+        expect(hashValue).toBe(expectedHashValue);
+      });
     });
 
-    it('correctly creates another location bytes buffer.', async () => {
+    it('correctly creates another location bytes buffer.', () => {
       const expectedHexValue = '000000000000c935ffffffffffffff8b000000005bcdcc09';
       const expectedHashValue = '803b2698104f58772dbd715ec6ee5853d835df98a4736742b2a676b2217c9499';
 
@@ -200,15 +204,16 @@ describe('z.message.MessageHashing', () => {
       messageEntity.timestamp(timestamp);
       messageEntity.add_asset(locationEntity);
 
-      const hashArray = await z.message.MessageHashing.getLocationMessageHash(messageEntity);
-      const hashValue = z.util.StringUtil.bytesToHex(hashArray);
+      return z.message.MessageHashing.getLocationMessageHash(messageEntity).then(hashArray => {
+        const hashValue = z.util.StringUtil.bytesToHex(hashArray);
 
-      expect(hashValue).toBe(expectedHashValue);
+        expect(hashValue).toBe(expectedHashValue);
+      });
     });
   });
 
   describe('getAssetMessageHash', () => {
-    it('correctly creates an asset bytes buffer.', async () => {
+    it('correctly creates an asset bytes buffer.', () => {
       const expectedHexValue = '38d4f5b961a34228870637e75043dbaa000000005bcdcc09';
       const expectedHashValue = '585764d2b2741454628e908c57bad9d482a1ea048dc3f11ba82a5e613653e65b';
 
@@ -229,13 +234,14 @@ describe('z.message.MessageHashing', () => {
       messageEntity.timestamp(timestamp);
       messageEntity.add_asset(fileAsset);
 
-      const hashArray = await z.message.MessageHashing.getAssetMessageHash(messageEntity);
-      const hashValue = z.util.StringUtil.bytesToHex(hashArray);
+      return z.message.MessageHashing.getAssetMessageHash(messageEntity).then(hashArray => {
+        const hashValue = z.util.StringUtil.bytesToHex(hashArray);
 
-      expect(hashValue).toBe(expectedHashValue);
+        expect(hashValue).toBe(expectedHashValue);
+      });
     });
 
-    it('correctly creates another asset bytes buffer.', async () => {
+    it('correctly creates another asset bytes buffer.', () => {
       const expectedHexValue = '82a6273535f24d4a8190284327979ca7000000005bcdcccd';
       const expectedHashValue = '46f392cb88a8e4f69d1c755f2c6c4e8b8ca0cf2db11b3de6499404548ef29905';
 
@@ -256,10 +262,11 @@ describe('z.message.MessageHashing', () => {
       messageEntity.timestamp(timestamp);
       messageEntity.add_asset(fileAsset);
 
-      const hashArray = await z.message.MessageHashing.getAssetMessageHash(messageEntity);
-      const hashValue = z.util.StringUtil.bytesToHex(hashArray);
+      return z.message.MessageHashing.getAssetMessageHash(messageEntity).then(hashArray => {
+        const hashValue = z.util.StringUtil.bytesToHex(hashArray);
 
-      expect(hashValue).toBe(expectedHashValue);
+        expect(hashValue).toBe(expectedHashValue);
+      });
     });
   });
 });
