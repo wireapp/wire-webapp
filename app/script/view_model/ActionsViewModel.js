808016ff8de4a5f98fdb307d09ce64aad68fd71d
Benny Neugebauer <bn@bennyn.de>
2018-10-30 17:27:41 +0100

    return Promise

--- app/script/view_model/ActionsViewModel.js
+++ app/script/view_model/ActionsViewModel.js
@@ -93,34 +93,30 @@ z.viewModel.ActionsViewModel = class ActionsViewModel {
 
   deleteClient(clientEntity) {
     // @todo Add failure case ux WEBAPP-3570
+    if (this.userRepository.self().isSingleSignOn) {
+      // SSO users can remove their clients without the need of entering a password
+      return this.clientRepository.deleteClient(clientEntity.id);
+    }
     return new Promise((resolve, reject) => {
-      if (this.userRepository.self().isSingleSignOn) {
-        // SSO users can remove their clients without the need of entering a password
-        this.clientRepository
-          .deleteClient(clientEntity.id)
-          .then(resolve)
-          .catch(reject);
-      } else {
-        amplify.publish(z.event.WebApp.WARNING.MODAL, z.viewModel.ModalsViewModel.TYPE.INPUT, {
-          action: password => {
-            this.clientRepository
-              .deleteClient(clientEntity.id, password)
-              .then(resolve)
-              .catch(error => {
-                amplify.publish(z.event.WebApp.AUDIO.PLAY, z.audio.AudioType.ALERT);
-                reject(error);
-              });
-          },
-          preventClose: true,
-          text: {
-            action: z.l10n.text(z.string.modalAccountRemoveDeviceAction),
-            input: z.l10n.text(z.string.modalAccountRemoveDevicePlaceholder),
-            message: z.l10n.text(z.string.modalAccountRemoveDeviceMessage),
-            title: z.l10n.text(z.string.modalAccountRemoveDeviceHeadline, clientEntity.model),
-          },
-          warning: false,
-        });
-      }
+      amplify.publish(z.event.WebApp.WARNING.MODAL, z.viewModel.ModalsViewModel.TYPE.INPUT, {
+        action: password => {
+          this.clientRepository
+            .deleteClient(clientEntity.id, password)
+            .then(resolve)
+            .catch(error => {
+              amplify.publish(z.event.WebApp.AUDIO.PLAY, z.audio.AudioType.ALERT);
+              reject(error);
+            });
+        },
+        preventClose: true,
+        text: {
+          action: z.l10n.text(z.string.modalAccountRemoveDeviceAction),
+          input: z.l10n.text(z.string.modalAccountRemoveDevicePlaceholder),
+          message: z.l10n.text(z.string.modalAccountRemoveDeviceMessage),
+          title: z.l10n.text(z.string.modalAccountRemoveDeviceHeadline, clientEntity.model),
+        },
+        warning: false,
+      });
     });
   }
 
