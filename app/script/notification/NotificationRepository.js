707ccc5c6d06e93ef8b00975ed8bab16eceee0b6
Florian Keller <ffflorian@users.noreply.github.com>
2018-11-05 10:37:24 +0100

    fix: Replies and mentions in notifications (#5049)

--- app/script/notification/NotificationRepository.js
+++ app/script/notification/NotificationRepository.js
@@ -218,10 +218,7 @@ z.notification.NotificationRepository = class NotificationRepository {
           if (assetEntity.isUserMentioned(this.selfUser().id)) {
             notificationText = z.l10n.text(z.string.notificationMention, assetEntity.text);
           } else if (messageEntity.isUserQuoted(this.selfUser().id)) {
-            notificationText = z.l10n.text(z.string.notificationReply, {
-              text: assetEntity.text,
-              user: messageEntity.user().first_name(),
-            });
+            notificationText = z.l10n.text(z.string.notificationReply, assetEntity.text);
           } else {
             notificationText = assetEntity.text;
           }
