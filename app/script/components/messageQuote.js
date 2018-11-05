8dda213056d01ef184569d8ad23dc191898b97dd
AndyLnd <andy@wire.com>
2018-11-05 11:39:57 +0100

    feat: Basic support for all message types in quotes (WEBAPP-5627)

---
+++ app/script/components/messageQuote.js
@@ -0,0 +1,100 @@
+/*
+ * Wire
+ * Copyright (C) 2018 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+'use strict';
+
+window.z = window.z || {};
+window.z.components = z.components || {};
+
+z.components.MessageQuote = class MessageQuote {
+  constructor({quote, conversationRepository, locationRepository}) {
+    this.updateCanShowMore = this.updateCanShowMore.bind(this);
+    this.toggleShowMore = this.toggleShowMore.bind(this);
+
+    const activeConversation = conversationRepository.active_conversation;
+    this.locationRepository = locationRepository;
+    this.message = ko.observable();
+    conversationRepository.get_message_in_conversation_by_id(activeConversation(), quote().messageId).then(message => {
+      this.message(message);
+    });
+
+    this.canShowMore = ko.observable(false);
+    this.showFullText = ko.observable(false);
+  }
+
+  updateCanShowMore(elements) {
+    const textQuote = elements.find(element => element.classList && element.classList.contains('message-quote__text'));
+    if (textQuote) {
+      this.canShowMore(textQuote.scrollWidth > textQuote.clientWidth);
+    }
+  }
+
+  toggleShowMore() {
+    this.showFullText(!this.showFullText());
+  }
+};
+
+ko.components.register('message-quote', {
+  template: `
+  <!-- ko if: message() -->
+    <div class="message-quote" data-bind="template: {afterRender: updateCanShowMore}">
+      <!-- ko if: message().error -->
+        <div class="message-quote__error" data-bind="l10n_text: z.string.replyQuoteError"></div>
+      <!-- /ko -->
+      <!-- ko ifnot: message().error -->
+        <div class="message-quote__sender" data-bind="text: message().headerSenderName()"></div>
+        <!-- ko foreach: {data: message().assets, as: 'asset'} -->
+          <!-- ko if: asset.is_image() -->
+              <div class="message-quote__image" data-bind="background_image: asset.resource">
+                <img data-bind="attr: {src: asset.dummy_url}"/>
+              </div>
+          <!-- /ko -->
+
+          <!-- ko if: asset.is_text() -->
+            <div class="message-quote__text" data-bind="html: asset.render(), css: {'message-quote__text--full': $parent.showFullText()}" dir="auto"></div>
+            <!-- ko if: $parent.canShowMore -->
+              <div class="message-quote__text__show-more" data-bind="click: $parent.toggleShowMore">
+                <span data-bind="l10n_text: $parent.showFullText() ? z.string.replyQuoteShowLess : z.string.replyQuoteShowMore"></span>
+                <disclose-icon data-bind="css: {'upside-down': $parent.showFullText()}"></disclose-icon>
+              </div>
+            <!-- /ko -->
+          <!-- /ko -->
+
+          <!-- ko if: asset.is_video() -->
+            <video-asset class="message-quote__video" params="message: $parent.message, isQuote: true"></video-asset>
+          <!-- /ko -->
+
+          <!-- ko if: asset.is_audio() -->
+            <audio-asset class="message-quote__audio" params="message: $parent.message"></audio-asset>
+          <!-- /ko -->
+
+          <!-- ko if: asset.is_file() -->
+            <file-asset class="message-quote__file" params="message: $parent.message"></file-asset>
+          <!-- /ko -->
+
+          <!-- ko if: asset.is_location() -->
+            <location-asset params="asset: asset, locationRepository: $parent.locationRepository"></location-asset>
+          <!-- /ko -->
+        <!-- /ko -->
+      <!-- /ko -->
+    </div>
+  <!-- /ko -->
+  `,
+  viewModel: z.components.MessageQuote,
+});
