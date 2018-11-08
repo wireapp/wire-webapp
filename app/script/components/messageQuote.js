5c0d4807ee5a329b697bd2cf8153eb0a5d94c261
AndyLnd <andy@wire.com>
2018-11-08 15:13:22 +0100

    runfix: Truncate multiline quotes properly

--- app/script/components/messageQuote.js
+++ app/script/components/messageQuote.js
@@ -104,7 +104,9 @@ z.components.MessageQuote = class MessageQuote {
   updateCanShowMore(elements) {
     const textQuote = elements.find(element => element.classList && element.classList.contains('message-quote__text'));
     if (textQuote) {
-      this.canShowMore(textQuote.scrollWidth > textQuote.clientWidth);
+      this.canShowMore(
+        textQuote.scrollWidth > textQuote.clientWidth || textQuote.scrollHeight > textQuote.clientHeight
+      );
     }
   }
 
