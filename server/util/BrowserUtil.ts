5018f10992e057e59e5fe89c4bbdd90e527c6550
Michael Koppen <Yserz@users.noreply.github.com>
2018-11-19 18:15:34 +0100

    runfix: Platform detection for iOS and MacOS (#5182)

--- server/util/BrowserUtil.ts
+++ server/util/BrowserUtil.ts
@@ -85,8 +85,8 @@ function parseUserAgent(userAgent?: string): ParsedUserAgent | null {
   const isElectron = !!electronVersion;
   const isFranz = !!franzVersion;
   const isGoogleBot = userAgent.includes('googlebot');
-  const isIOS = agent.platform.includes('ios');
-  const isOSX = agent.platform.includes('mac');
+  const isIOS = agent.platform.toLowerCase().includes('ios');
+  const isOSX = agent.platform.toLowerCase().includes('mac');
   const isWire = !!wireVersion;
   const isYahooBot = userAgent.includes('yahoo');
 
