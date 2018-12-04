407dbc6222d40202cc2a40a6ebe55728e75a1c83
Florian Keller <florian.keller@wire.com>
2018-12-04 14:26:40 +0100

    Add safeguard

--- app/script/assets/AssetService.js
+++ app/script/assets/AssetService.js
@@ -93,7 +93,11 @@ z.assets.AssetService = class AssetService {
         const assetRemoteData = new z.proto.Asset.RemoteData(keyBytes, sha256, key, token);
 
         protoAsset.set(z.cryptography.PROTO_MESSAGE_TYPE.ASSET_UPLOADED, assetRemoteData);
-        protoAsset.set(z.cryptography.PROTO_MESSAGE_TYPE.EXPECTS_READ_CONFIRMATION, options.expectsReadConfirmation);
+
+        if (options.expectsReadConfirmation) {
+          protoAsset.set(z.cryptography.PROTO_MESSAGE_TYPE.EXPECTS_READ_CONFIRMATION, options.expectsReadConfirmation);
+        }
+
         return protoAsset;
       });
   }
@@ -120,7 +124,11 @@ z.assets.AssetService = class AssetService {
 
         protoAsset.set(z.cryptography.PROTO_MESSAGE_TYPE.ASSET_ORIGINAL, assetOriginal);
         protoAsset.set(z.cryptography.PROTO_MESSAGE_TYPE.ASSET_UPLOADED, assetRemoteData);
-        protoAsset.set(z.cryptography.PROTO_MESSAGE_TYPE.EXPECTS_READ_CONFIRMATION, options.expectsReadConfirmation);
+
+        if (options.expectsReadConfirmation) {
+          protoAsset.set(z.cryptography.PROTO_MESSAGE_TYPE.EXPECTS_READ_CONFIRMATION, options.expectsReadConfirmation);
+        }
+
         return protoAsset;
       });
     });
