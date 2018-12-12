14c34822b8b1dff05ef553d89b5b3d1645ce80dc
Benny Neugebauer <bn@bennyn.de>
2018-12-12 12:43:11 +0100

    refactored "shouldSendReadReceipt"

--- app/script/view_model/content/MessageListViewModel.js
+++ app/script/view_model/content/MessageListViewModel.js
@@ -635,9 +635,18 @@ z.viewModel.content.MessageListViewModel = class MessageListViewModel {
 
     const isUnreadMessage = messageTimestamp > conversationTimestamp;
     const isNotOwnMessage = !messageEntity.user().is_me;
-    const expectsConfirmation = messageEntity.expectsReadConfirmation;
-    const shouldSendReadReceipt = this.conversation_repository.expectReadReceipt(conversationEntity);
-    if (isUnreadMessage && isNotOwnMessage && expectsConfirmation && shouldSendReadReceipt) {
+
+    let shouldSendReadReceipt = false;
+
+    if (messageEntity.expectsReadConfirmation) {
+      if (conversationEntity.is1to1()) {
+        shouldSendReadReceipt = this.conversation_repository.expectReadReceipt(conversationEntity);
+      } else if (conversationEntity.isGroup()) {
+        shouldSendReadReceipt = true;
+      }
+    }
+
+    if (isUnreadMessage && isNotOwnMessage && shouldSendReadReceipt) {
       callbacks.push(sendReadReceipt);
     }
 
