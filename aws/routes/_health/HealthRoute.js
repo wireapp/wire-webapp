57403d419f7dc7cf4ba9504795c0d13d077b9aa0
Sabri <pwnsdx@protonmail.ch>
2018-12-20 14:56:13 +0100

    Remove webapp dist

--- aws/routes/_health/HealthRoute.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const HealthRoute = () => express_1.Router().get('/_health/?', (req, res) => res.sendStatus(200));
-exports.default = HealthRoute;
-//# sourceMappingURL=HealthRoute.js.map
