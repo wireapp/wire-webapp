7066dad127f6dc8c839eabadc1a74a23afa96562
Florian Keller <florian.keller@wire.com>
2019-01-10 14:42:46 +0100

    fix tests

--- test/unit_tests/links/LinkPreviewProtoBuilderSpec.js
+++ test/unit_tests/links/LinkPreviewProtoBuilderSpec.js
@@ -23,9 +23,9 @@ describe('LinkPreviewProtoBuilder', () => {
     expect(preview.preview).toBe('article');
     expect(preview.url).toBe(url);
     expect(preview.url_offset).toBe(offset);
-    expect(preview.article.title).toBe(mock.title);
+    expect(preview.article.title).toBe(mock.title || '');
     expect(preview.article.permanent_url).toBe(mock.url);
-    expect(preview.article.summary).toEqual(mock.description || null);
+    expect(preview.article.summary).toEqual(mock.description || '');
     expect(() => preview.toArrayBuffer()).not.toThrow();
   };
 
