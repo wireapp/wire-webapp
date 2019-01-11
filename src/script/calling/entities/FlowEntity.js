732a6cdec6aedcbe50fe8059bfea8e74967bfa8a
Thomas Belin <thomasbelin4@gmail.com>
2019-01-11 18:17:21 +0100

    fix: Reset ice candidate generation attemps when sending SDP  (#5611)

--- src/script/calling/entities/FlowEntity.js
+++ src/script/calling/entities/FlowEntity.js
@@ -883,6 +883,8 @@ z.calling.entities.FlowEntity = class FlowEntity {
           }
         }
 
+        this.iceCandidatesGatheringAttempts = 1;
+
         const iceCandidateTypes = z.util.PeerConnectionUtil.getIceCandidatesTypes(iceCandidates);
 
         const iceCandidateTypesLog = Object.keys(iceCandidateTypes)
@@ -901,6 +903,7 @@ z.calling.entities.FlowEntity = class FlowEntity {
             obfuscated: [
               transformedSdp.type,
               iceCandidates.length,
+              iceCandidateTypesLog,
               this.callLogger.obfuscate(this.remoteUser.id),
               this.callLogger.obfuscateSdp(transformedSdp.sdp),
             ],
