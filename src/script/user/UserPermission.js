54d3d3ae81a869c41500d65581294d33704dbf74
Thomas Belin <me@thomasbelin.fr>
2019-01-22 16:01:56 +0100

    review

--- src/script/user/UserPermission.js
+++ src/script/user/UserPermission.js
@@ -160,7 +160,7 @@ export function generatePermissionHelpers(boundRole = ROLE.NONE) {
       .split('_')
       .map(stringUtil.capitalizeFirstChar)
       .join('');
-    return Object.assign({}, helpers, {
+    return Object.assign(helpers, {
       [`can${camelCasedFeature}`]: role => hasAccessToFeature(featureValue, role || boundRole),
     });
   }, {});
