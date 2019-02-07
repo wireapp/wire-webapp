4b210973f96e976b6abea6fedbbe46794aec7d27
Thomas Belin <thomasbelin4@gmail.com>
2019-02-07 18:32:47 +0100

    test: Add tests for the participant avatar component (#5751)

---
+++ test/api/knockoutHelpers.js
@@ -0,0 +1,32 @@
+/*
+ * Wire
+ * Copyright (C) 2019 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import ko from 'knockout';
+
+export function instantiateComponent(componentName, viewModel, params) {
+  return new Promise(resolve => {
+    const destination = document.body;
+    ko.cleanNode(destination);
+    destination.innerHTML = `<div data-bind="component: {name: '${componentName}', params: {${params}}}"></div>`; // eslint-disable-line no-unsanitized/property
+    ko.applyBindings(viewModel, destination);
+    setImmediate(() => {
+      resolve(destination);
+    });
+  });
+}
