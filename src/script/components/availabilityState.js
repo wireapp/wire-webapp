6b510fa8317a08ab2c24934dbb6291052c6c030b
Michael Koppen <michael.koppen@googlemail.com>
2019-02-13 12:13:14 +0100

    refactor: Extract availability icons

--- src/script/components/availabilityState.js
+++ src/script/components/availabilityState.js
@@ -32,19 +32,13 @@ z.components.AvailabilityState = class AvailabilityState {
 ko.components.register('availability-state', {
   template: `
       <!-- ko if: $data.availability() === z.user.AvailabilityType.AVAILABLE -->
-        <svg class="availability-state-icon" viewBox="0 0 10 10" data-uie-name="available">
-          <circle cx="5" cy="5" r="5" stroke="none"></circle>
-        </svg>
+        <availability-available-icon class="availability-state-icon" data-uie-name="available"></availability-available-icon>
       <!-- /ko -->
       <!-- ko if: $data.availability() === z.user.AvailabilityType.AWAY -->
-        <svg class="availability-state-icon" viewBox="0 0 10 10" data-uie-name="away">
-          <circle cx="5" cy="5" r="4" stroke-width="2" fill="none"></circle>
-        </svg>
+        <availability-away-icon class="availability-state-icon" data-uie-name="away"></availability-away-icon>
       <!-- /ko -->
       <!-- ko if: $data.availability() === z.user.AvailabilityType.BUSY -->
-        <svg class="availability-state-icon" viewBox="0 0 10 10" data-uie-name="busy">
-          <path stroke="none" d="M5 10A5 5 0 1 1 5 0a5 5 0 0 1 0 10zM3 4a1 1 0 0 0 0 2h4a1 1 0 0 0 0-2H3z"></path>
-        </svg>
+        <availability-busy-icon class="availability-state-icon" data-uie-name="busy"></availability-busy-icon>
       <!-- /ko -->
       <!-- ko if: $data.label -->
         <div class="availability-state-label" data-bind="css: {'text-theme': theme}, text: $data.label" data-uie-name="status-label"></div>
