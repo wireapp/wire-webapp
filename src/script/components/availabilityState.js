49dd53bd4cbd1dec1b70feb873ed29624cc99b6f
Michael Koppen <michael.koppen@googlemail.com>
2019-02-14 16:22:25 +0100

    chore: Apply data-uie-name & value to availability icons

--- src/script/components/availabilityState.js
+++ src/script/components/availabilityState.js
@@ -32,13 +32,13 @@ z.components.AvailabilityState = class AvailabilityState {
 ko.components.register('availability-state', {
   template: `
       <!-- ko if: $data.availability() === z.user.AvailabilityType.AVAILABLE -->
-        <availability-available-icon class="availability-state-icon" data-uie-name="available"></availability-available-icon>
+        <availability-available-icon class="availability-state-icon" data-uie-name="status-availability-icon" data-uie-value="available"></availability-available-icon>
       <!-- /ko -->
       <!-- ko if: $data.availability() === z.user.AvailabilityType.AWAY -->
-        <availability-away-icon class="availability-state-icon" data-uie-name="away"></availability-away-icon>
+        <availability-away-icon class="availability-state-icon" data-uie-name="status-availability-icon" data-uie-value="away"></availability-away-icon>
       <!-- /ko -->
       <!-- ko if: $data.availability() === z.user.AvailabilityType.BUSY -->
-        <availability-busy-icon class="availability-state-icon" data-uie-name="busy"></availability-busy-icon>
+        <availability-busy-icon class="availability-state-icon" data-uie-name="status-availability-icon" data-uie-value="busy"></availability-busy-icon>
       <!-- /ko -->
       <!-- ko if: $data.label -->
         <div class="availability-state-label" data-bind="css: {'text-theme': theme}, text: $data.label" data-uie-name="status-label"></div>
