dbbfb67bda3eabf5f98c7b8c13d48f3f2f5c9381
Thomas Belin <me@thomasbelin.fr>
2019-02-15 10:59:01 +0100

    sort dependencies graph

--- src/script/config/dependenciesGraph.js
+++ src/script/config/dependenciesGraph.js
@@ -20,17 +20,17 @@
 import AssetService from '../assets/AssetService';
 import AssetUploader from '../assets/AssetUploader';
 import AudioRepository from '../audio/AudioRepository';
-import CacheRepository from '../cache/CacheRepository';
 import BackendClient from '../service/BackendClient';
 import BackupService from '../backup/BackupService';
-import GiphyService from '../extension/GiphyService';
+import CacheRepository from '../cache/CacheRepository';
 import GiphyRepository from '../extension/GiphyRepository';
+import GiphyService from '../extension/GiphyService';
 import MediaRepository from '../media/MediaRepository';
 import PermissionRepository from '../permission/PermissionRepository';
-import PropertiesService from '../properties/PropertiesService';
 import PropertiesRepository from '../properties/PropertiesRepository';
-import StorageService from '../storage/StorageService';
+import PropertiesService from '../properties/PropertiesService';
 import SelfService from '../self/SelfService';
+import StorageService from '../storage/StorageService';
 import UserService from '../user/UserService';
 
 /** Dependencies is a Map that will contain all the dependencies of the app
@@ -38,37 +38,37 @@ import UserService from '../user/UserService';
  */
 const dependencies = new WeakMap();
 
-dependencies.set(CacheRepository, {dependencies: [], name: 'CacheRepository'});
-dependencies.set(AudioRepository, {dependencies: [], name: 'AudioRepository'});
-dependencies.set(BackendClient, {dependencies: [], name: 'BackendClient'});
-dependencies.set(GiphyService, {dependencies: [BackendClient], name: 'GiphyService'});
-dependencies.set(GiphyRepository, {dependencies: [GiphyService], name: 'GiphyRepository'});
 dependencies.set(AssetService, {dependencies: [BackendClient], name: 'AssetService'});
 dependencies.set(AssetUploader, {dependencies: [AssetService], name: 'AssetUploader'});
-dependencies.set(MediaRepository, {dependencies: [PermissionRepository], name: 'MediaRepository'});
-dependencies.set(PropertiesService, {dependencies: [BackendClient], name: 'PropertiesService'});
-dependencies.set(PropertiesRepository, {dependencies: [PropertiesService, SelfService], name: 'PropertiesRepository'});
-dependencies.set(PermissionRepository, {dependencies: [], name: 'PermissionRepository'});
-dependencies.set(StorageService, {dependencies: [], name: 'StorageService'});
-dependencies.set(SelfService, {dependencies: [BackendClient], name: 'SelfService'});
+dependencies.set(AudioRepository, {dependencies: [], name: 'AudioRepository'});
+dependencies.set(BackendClient, {dependencies: [], name: 'BackendClient'});
 dependencies.set(BackupService, {dependencies: [StorageService], name: 'BackupService'});
+dependencies.set(CacheRepository, {dependencies: [], name: 'CacheRepository'});
+dependencies.set(GiphyRepository, {dependencies: [GiphyService], name: 'GiphyRepository'});
+dependencies.set(GiphyService, {dependencies: [BackendClient], name: 'GiphyService'});
+dependencies.set(MediaRepository, {dependencies: [PermissionRepository], name: 'MediaRepository'});
+dependencies.set(PermissionRepository, {dependencies: [], name: 'PermissionRepository'});
+dependencies.set(PropertiesRepository, {dependencies: [PropertiesService, SelfService], name: 'PropertiesRepository'});
+dependencies.set(PropertiesService, {dependencies: [BackendClient], name: 'PropertiesService'});
+dependencies.set(SelfService, {dependencies: [BackendClient], name: 'SelfService'});
+dependencies.set(StorageService, {dependencies: [], name: 'StorageService'});
 dependencies.set(UserService, {dependencies: [BackendClient, StorageService], name: 'UserService'});
 
 export {
   dependencies,
-  CacheRepository,
-  AudioRepository,
-  BackendClient,
-  GiphyService,
-  GiphyRepository,
   AssetService,
   AssetUploader,
-  MediaRepository,
-  PropertiesService,
-  PropertiesRepository,
-  PermissionRepository,
-  StorageService,
-  SelfService,
+  AudioRepository,
+  BackendClient,
   BackupService,
+  CacheRepository,
+  GiphyRepository,
+  GiphyService,
+  MediaRepository,
+  PermissionRepository,
+  PropertiesRepository,
+  PropertiesService,
+  SelfService,
+  StorageService,
   UserService,
 };
