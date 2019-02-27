97d51c4b0bcdda8fc70e0e80cfbe38c9d5cb214a
Florian Keller <florian.keller@wire.com>
2019-02-27 10:33:39 +0100

    normal ticks

--- .copyconfigrc.js
+++ .copyconfigrc.js
@@ -2,7 +2,7 @@ const pkg = require('./package.json');
 const {execSync} = require('child_process');
 const path = require('path');
 
-const currentBranch = execSync(`git rev-parse --abbrev-ref HEAD`)
+const currentBranch = execSync('git rev-parse --abbrev-ref HEAD')
   .toString()
   .trim();
 const configurationEntry = `wire-web-config-default-${currentBranch === 'prod' ? 'prod' : 'staging'}`;
