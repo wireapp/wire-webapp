6904b0e51a9bebd9449eac9ca565c78b4ef18cdf
Benny Neugebauer <bn@bennyn.de>
2019-02-27 17:53:48 +0100

    runfix: Show error page when cookies have been disabled (#5940)

--- src/script/util/LoggerUtil.js
+++ src/script/util/LoggerUtil.js
@@ -20,6 +20,19 @@
 import 'url-search-params-polyfill';
 
 function enableLogging(location = window.location.href) {
+  /**
+   * If users disable cookies in their browsers, they won't have access to the localStorage API.
+   * The following check will fix this error:
+   * > Failed to read the 'localStorage' property from 'Window': Access is denied for this document
+   */
+  let localStorage;
+
+  try {
+    localStorage = window.localStorage;
+  } catch (error) {
+    return;
+  }
+
   const namespace = new URL(location).searchParams.get('enableLogging');
 
   if (namespace) {
