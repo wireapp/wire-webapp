1733f710745a5a01205d225c5df0f9717c6fdfd6
Michael Koppen <Yserz@users.noreply.github.com>
2019-03-28 23:24:33 +0100

    feat(api-client): Identity Provider API (#1699)

---
+++ api-client/src/main/team/identityprovider/IdentityProviderAPI.ts
@@ -0,0 +1,77 @@
+/*
+ * Wire
+ * Copyright (C) 2019 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import {AxiosRequestConfig} from 'axios';
+
+import {HttpClient} from '../../http';
+import {IdentityProvider} from './IdentityProvider';
+import {IdentityProviders} from './IdentityProviders';
+
+class IdentityProviderAPI {
+  constructor(private readonly client: HttpClient) {}
+
+  static get URL() {
+    return {
+      PROVIDER: 'identity-providers',
+    };
+  }
+
+  public async getIdentityProvider(identityProviderId: string): Promise<IdentityProvider> {
+    const config: AxiosRequestConfig = {
+      method: 'get',
+      url: `${IdentityProviderAPI.URL.PROVIDER}/${identityProviderId}`,
+    };
+
+    const response = await this.client.sendJSON<IdentityProvider>(config);
+    return response.data;
+  }
+
+  public async getIdentityProviders(): Promise<IdentityProviders> {
+    const config: AxiosRequestConfig = {
+      method: 'get',
+      url: `${IdentityProviderAPI.URL.PROVIDER}`,
+    };
+
+    const response = await this.client.sendJSON<IdentityProviders>(config);
+    return response.data;
+  }
+
+  public async deleteIdentityProvider(identityProviderId: string): Promise<void> {
+    const config: AxiosRequestConfig = {
+      method: 'delete',
+      url: `${IdentityProviderAPI.URL.PROVIDER}/${identityProviderId}`,
+    };
+
+    const response = await this.client.sendJSON<void>(config);
+    return response.data;
+  }
+
+  public async postIdentityProvider(xmlData: string): Promise<IdentityProvider> {
+    const config: AxiosRequestConfig = {
+      data: xmlData,
+      method: 'post',
+      url: `${IdentityProviderAPI.URL.PROVIDER}`,
+    };
+
+    const response = await this.client.sendJSON<IdentityProvider>(config);
+    return response.data;
+  }
+}
+
+export {IdentityProviderAPI};
