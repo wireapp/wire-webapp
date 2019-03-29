a11b1ee8c7a5b0b9ca5a220057575050b0d10391
Thomas Belin <thomasbelin4@gmail.com>
2019-03-29 16:13:24 +0100

    refactor: Clear some strong DOM coupling in App class (#6241)

--- src/script/main/app.js
+++ src/script/main/app.js
@@ -79,12 +79,12 @@ class App {
   /**
    * Construct a new app.
    * @param {BackendClient} backendClient - Configured backend client
-   * @param {Element} domContainer - DOM element that will hold the app
+   * @param {Element} appContainer - DOM element that will hold the app
    */
-  constructor(backendClient, domContainer) {
+  constructor(backendClient, appContainer) {
     this.backendClient = backendClient;
     this.logger = Logger('App');
-    this.domContainer = domContainer;
+    this.appContainer = appContainer;
 
     new WindowHandler();
 
@@ -579,16 +579,15 @@ class App {
 
   /**
    * Check that this is the single instance tab of the app.
-   * @returns {Promise} Resolves when page is the first tab
+   * @returns {void} Resolves when page is the first tab
    */
   _registerSingleInstance() {
     const instanceId = z.util.createRandomUuid();
 
     if (this.singleInstanceHandler.registerInstance(instanceId)) {
-      this._registerSingleInstanceCleaning();
-      return Promise.resolve();
+      return this._registerSingleInstanceCleaning();
     }
-    return Promise.reject(new z.error.AuthError(z.error.AuthError.TYPE.MULTIPLE_TABS));
+    throw new z.error.AuthError(z.error.AuthError.TYPE.MULTIPLE_TABS);
   }
 
   _registerSingleInstanceCleaning() {
@@ -603,7 +602,7 @@ class App {
    */
   _showInterface() {
     const mainView = new MainViewModel(this.repository);
-    ko.applyBindings(mainView, this.domContainer);
+    ko.applyBindings(mainView, this.appContainer);
 
     this.repository.notification.setContentViewModelStates(mainView.content.state, mainView.content.multitasking);
 
@@ -628,7 +627,7 @@ class App {
     });
     initRouterBindings(router);
 
-    $('#wire-main').attr('data-uie-value', 'is-loaded');
+    this.appContainer.dataset.uieValue = 'is-loaded';
 
     this.repository.properties.checkPrivacyPermission().then(() => {
       window.setTimeout(() => this.repository.notification.checkPermission(), App.CONFIG.NOTIFICATION_CHECK);
@@ -852,13 +851,14 @@ class App {
 
 $(() => {
   enableLogging(Config.FEATURE.ENABLE_DEBUG);
-  if (document.getElementById('wire-main-app')) {
+  const appContainer = document.getElementById('wire-main');
+  if (appContainer) {
     const backendClient = resolve(graph.BackendClient);
     backendClient.setSettings({
       restUrl: Config.BACKEND_REST,
       webSocketUrl: Config.BACKEND_WS,
     });
-    wire.app = new App(backendClient, document.getElementById('wire-main'));
+    wire.app = new App(backendClient, appContainer);
   }
 });
 
