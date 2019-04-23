53fe787e8f9a450e2aeef14dc65b493febab96f4
Florian Keller <ffflorian@users.noreply.github.com>
2019-04-23 15:56:57 +0200

    Update handleUtil.ts

--- src/script/auth/util/handleUtil.ts
+++ src/script/auth/util/handleUtil.ts
@@ -16,7 +16,6 @@
  * along with this program. If not, see http://www.gnu.org/licenses/.
  *
  */
-/* @flow */
 
 import * as getSlug from 'speakingurl';
 import {randomArrayElement, randomInt} from './randomUtil';
