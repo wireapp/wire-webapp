850d31c3324f79c9a50c8e50758a23d52e4ee264
Florian Keller <florian.keller@wire.com>
2019-04-23 15:15:38 +0200

    [ci skip] fix emoji json import

--- src/script/view_model/content/EmojiInputViewModel.js
+++ src/script/view_model/content/EmojiInputViewModel.js
@@ -17,7 +17,7 @@
  *
  */
 
-import * as emojiBindings from './emoji.json';
+import emojiBindings from './emoji.json';
 import * as StorageUtil from 'utils/StorageUtil';
 import {PROPERTIES_TYPE} from '../../properties/PropertiesType';
 
