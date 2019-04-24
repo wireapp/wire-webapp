d7a14b885a90cceb96ea8ad0bf0f8323dfb0461a
Benny Neugebauer <bn@bennyn.de>
2019-04-24 18:17:48 +0200

    hyphen

--- src/script/util/util.js
+++ src/script/util/util.js
@@ -95,8 +95,8 @@ export const loadDataUrl = file => {
 
 /**
  * Loads a HTTP response into an ArrayBuffer.
- * @param {string} url Request URL
- * @param {Function} xhrAccessorFunction Request wrapper
+ * @param {string} url - Request URL
+ * @param {Function} xhrAccessorFunction - Request wrapper
  * @returns {Promise<{buffer: ArrayBuffer, mimeType: string}>} URL buffer
  */
 export const loadUrlBuffer = (url, xhrAccessorFunction) => {
