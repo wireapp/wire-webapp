e2d7187e16bb2ece5dfe29ddeb3d02a5905539a4
Thomas Belin <me@thomasbelin.fr>
2019-04-30 16:48:03 +0200

    End call when app is reloaded

--- src/script/calling/CallingRepository.js
+++ src/script/calling/CallingRepository.js
@@ -886,7 +886,7 @@ gled
    * @returns {undefined} No return value
    */
   leaveCallOnUnload() {
-    // TODO
+    this.activeCalls().forEach(callInstance => this.callingApi.end(this.wUser, callInstance.conversationId));
   }
 
   //##############################################################################
