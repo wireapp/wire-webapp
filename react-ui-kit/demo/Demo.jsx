792391fb584eec44ad3346339335be38fc5954a3
Lipis <lipiridis@gmail.com>
2019-05-14 11:54:25 +0200

    fix(react-ui-kit): Fetch Image bug (#1828)
    * feat(react-ui-kit): Add Avatar component
    
    * Functional
    
    * Sorting
    
    * Avatar
    
    * JS
    
    * fix(react-ui-kit): Fetch Image bug
    
    * Detailed message
    
    * Info

--- react-ui-kit/demo/Demo.jsx
+++ react-ui-kit/demo/Demo.jsx
@@ -184,6 +184,9 @@ class Demo extends React.PureComponent {
                 forceInitials={true}
                 borderColor={'#fb0807'}
                 backgroundColor={'#2085C2'}
+                fetchImage={() => {
+                  console.info('Trying to fetch asset');
+                }}
               />
               <Avatar
                 size={64}
