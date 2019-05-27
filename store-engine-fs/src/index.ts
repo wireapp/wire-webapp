b744ca7b5b9f23571bc429e6b3b96b075364b505
Benny Neugebauer <bn@bennyn.de>
2019-05-27 17:06:22 +0200

    feat(store-engine-fs): Distribute FileEngine in separate package (#1866)

---
+++ store-engine-fs/src/index.ts
@@ -0,0 +1,252 @@
+/*
+ * Wire
+ * Copyright (C) 2018 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import {CRUDEngine} from '@wireapp/store-engine';
+import {
+  PathValidationError,
+  RecordAlreadyExistsError,
+  RecordNotFoundError,
+  RecordTypeError,
+  UnsupportedError,
+} from '@wireapp/store-engine/dist/commonjs/engine/error/';
+import fs from 'fs-extra';
+import path from 'path';
+
+export class FileEngine implements CRUDEngine {
+  [index: string]: any;
+
+  public storeName = '';
+  public options: {fileExtension: string} = {
+    fileExtension: '.dat',
+  };
+  // Using a reference to Node.js' "path" module to influence the platform-specific behaviour in our tests
+  public static path: any = path;
+
+  constructor(private readonly baseDirectory = './') {}
+
+  public async isSupported(): Promise<void> {
+    const isNodeOrElectron = typeof process === 'object';
+
+    if (!isNodeOrElectron) {
+      const message = `Node.js File System Module is not available on your platform.`;
+      throw new UnsupportedError(message);
+    }
+  }
+
+  public async init(storeName = '', options?: {fileExtension: string}): Promise<string> {
+    await this.isSupported();
+
+    FileEngine.enforcePathRestrictions(this.baseDirectory, storeName);
+    this.storeName = FileEngine.path.resolve(this.baseDirectory, storeName);
+    await fs.ensureDir(this.storeName);
+
+    this.options = {...this.options, ...options};
+    return Promise.resolve(this.storeName);
+  }
+
+  public purge(): Promise<void> {
+    return fs.remove(this.storeName);
+  }
+
+  static enforcePathRestrictions(givenTrustedRoot: string, givenPath: string): string {
+    const trustedRoot = FileEngine.path.resolve(givenTrustedRoot);
+
+    const trustedRootDetails = FileEngine.path.parse(trustedRoot);
+    if (trustedRootDetails.root === trustedRootDetails.dir && trustedRootDetails.base === '') {
+      const message = `"${trustedRoot}" cannot be the root of the filesystem.`;
+      throw new PathValidationError(message);
+    }
+
+    const unsafePath = FileEngine.path.resolve(trustedRoot, givenPath);
+    if (unsafePath.startsWith(trustedRoot) === false) {
+      const message = `Path traversal has been detected. Allowed path was "${trustedRoot}" but tested path "${givenPath}" attempted to reach "${unsafePath}"`;
+      throw new PathValidationError(message);
+    }
+
+    return unsafePath;
+  }
+
+  private resolvePath(tableName: string, primaryKey = ''): Promise<string> {
+    return new Promise(resolve => {
+      const tableNamePath = FileEngine.enforcePathRestrictions(this.storeName, tableName);
+      const primaryKeyPath = FileEngine.enforcePathRestrictions(
+        tableNamePath,
+        primaryKey ? `${primaryKey}${this.options.fileExtension}` : ''
+      );
+
+      return resolve(primaryKeyPath);
+    });
+  }
+
+  create<T>(tableName: string, primaryKey: string, entity: any): Promise<string> {
+    return new Promise((resolve, reject) => {
+      if (entity) {
+        this.resolvePath(tableName, primaryKey)
+          .then((filePath: string) => {
+            if (typeof entity === 'object') {
+              try {
+                entity = JSON.stringify(entity);
+              } catch (error) {
+                entity = entity.toString();
+              }
+            }
+
+            fs.writeFile(filePath, entity, {flag: 'wx'}, (error: NodeJS.ErrnoException) => {
+              if (error) {
+                if (error.code === 'ENOENT') {
+                  fs.outputFile(filePath, entity)
+                    .then(() => resolve(primaryKey))
+                    .catch((error: Error) => reject(error));
+                } else if (error.code === 'EEXIST') {
+                  const message = `Record "${primaryKey}" already exists in "${tableName}". You need to delete the record first if you want to overwrite it.`;
+                  reject(new RecordAlreadyExistsError(message));
+                } else {
+                  reject(error);
+                }
+              } else {
+                resolve(primaryKey);
+              }
+            });
+          })
+          .catch(reject);
+      } else {
+        const message = `Record "${primaryKey}" cannot be saved in "${tableName}" because it's "undefined" or "null".`;
+        reject(new RecordTypeError(message));
+      }
+    });
+  }
+
+  delete(tableName: string, primaryKey: string): Promise<string> {
+    return this.resolvePath(tableName, primaryKey).then(file => {
+      return fs.remove(file).then(() => primaryKey);
+    });
+  }
+
+  deleteAll(tableName: string): Promise<boolean> {
+    return this.resolvePath(tableName).then(directory => {
+      return fs
+        .remove(directory)
+        .then(() => true)
+        .catch(() => false);
+    });
+  }
+
+  read<T>(tableName: string, primaryKey: string): Promise<T> {
+    return this.resolvePath(tableName, primaryKey).then(file => {
+      return new Promise<T>((resolve, reject) => {
+        fs.readFile(file, {encoding: 'utf8', flag: 'r'}, (error, data: any) => {
+          if (error) {
+            if (error.code === 'ENOENT') {
+              const message = `Record "${primaryKey}" in "${tableName}" could not be found.`;
+              reject(new RecordNotFoundError(message));
+            } else {
+              reject(error);
+            }
+          } else {
+            try {
+              data = JSON.parse(data);
+            } catch (error) {
+              // No JSON found but that's okay
+            }
+            resolve(data);
+          }
+        });
+      });
+    });
+  }
+
+  readAll<T>(tableName: string): Promise<T[]> {
+    return this.resolvePath(tableName).then(
+      directory =>
+        new Promise<T[]>((resolve, reject) => {
+          fs.readdir(directory, (error: NodeJS.ErrnoException, files: string[]) => {
+            if (error) {
+              return reject(error);
+            } else {
+              const recordNames = files.map(file => FileEngine.path.basename(file, FileEngine.path.extname(file)));
+              const promises: Promise<T>[] = recordNames.map(primaryKey => this.read(tableName, primaryKey));
+              return Promise.all(promises).then((records: T[]) => resolve(records));
+            }
+          });
+        })
+    );
+  }
+
+  readAllPrimaryKeys(tableName: string): Promise<string[]> {
+    return this.resolvePath(tableName).then(directory => {
+      return new Promise<string[]>(resolve => {
+        fs.readdir(directory, (error: NodeJS.ErrnoException, files: string[]) => {
+          if (error) {
+            if (error.code === 'ENOENT') {
+              resolve([]);
+            } else {
+              throw error;
+            }
+          } else {
+            const fileNames: string[] = files.map((file: string) => FileEngine.path.parse(file).name);
+            resolve(fileNames);
+          }
+        });
+      });
+    });
+  }
+
+  append(tableName: string, primaryKey: string, additions: string): Promise<string> {
+    return this.resolvePath(tableName, primaryKey).then(file => {
+      return this.read(tableName, primaryKey)
+        .then((record: any) => {
+          if (typeof record === 'string') {
+            record += additions;
+          } else {
+            const message = `Cannot append text to record "${primaryKey}" because it's not a string.`;
+            throw new RecordTypeError(message);
+          }
+          return record;
+        })
+        .then((updatedRecord: any) => fs.outputFile(file, updatedRecord))
+        .then(() => primaryKey);
+    });
+  }
+
+  update(tableName: string, primaryKey: string, changes: Object): Promise<string> {
+    return this.resolvePath(tableName, primaryKey).then(file => {
+      return this.read(tableName, primaryKey)
+        .then((record: any) => {
+          if (typeof record === 'string') {
+            record = JSON.parse(record);
+          }
+          const updatedRecord: Object = {...record, ...changes};
+          return JSON.stringify(updatedRecord);
+        })
+        .then((updatedRecord: any) => fs.outputFile(file, updatedRecord))
+        .then(() => primaryKey);
+    });
+  }
+
+  public updateOrCreate(tableName: string, primaryKey: string, changes: Object): Promise<string> {
+    return this.update(tableName, primaryKey, changes)
+      .catch(error => {
+        if (error instanceof RecordNotFoundError) {
+          return this.create(tableName, primaryKey, changes);
+        }
+        throw error;
+      })
+      .then(() => primaryKey);
+  }
+}
