c06845bf125995603a9aac58f27cfa0284b89455
AndyLnd <andy@wire.com>
2019-06-12 12:13:38 +0200

    feat: Add approval endpoint (#6702)

--- src/script/util/LegalHoldUtil.ts
+++ src/script/util/LegalHoldUtil.ts
@@ -55,7 +55,11 @@ export const showRequestModal = async (fingerprint?: string) => {
   amplify.publish(WebAppEvents.WARNING.MODAL, ModalsViewModel.TYPE.INPUT, {
     preventClose: true,
     primaryAction: {
-      action: (password: string) => {},
+      action: async (password: string) => {
+        const selfUser = userRepository.self();
+        await teamRepository.teamService.sendLegalHoldApproval(selfUser.teamId, selfUser.id, password);
+        // TODO: check response and show error message if needed
+      },
       text: 'Accept',
     },
     secondaryAction: {
