6becffbaa76e329668dc98b9065739c5aae72a53
Benny Neugebauer <bn@bennyn.de>
2019-07-02 17:58:19 +0200

    chore(deps): Downgrade @wireapp/core from 12.13.0 to 12.6.0 (#6857)

--- src/script/auth/configureClient.ts
+++ src/script/auth/configureClient.ts
@@ -18,7 +18,7 @@
  */
 
 import {APIClient} from '@wireapp/api-client';
-import {IndexedDBEngine} from '@wireapp/store-engine-dexie';
+import {IndexedDBEngine} from '@wireapp/store-engine';
 import {Dexie} from 'dexie';
 import {StorageSchemata} from '../storage/StorageSchemata';
 import {Config} from './config';
