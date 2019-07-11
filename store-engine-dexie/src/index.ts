f3f9c5df7380b83a8582e0252558527dc12fed31
Benny Neugebauer <bn@bennyn.de>
2019-07-11 16:41:00 +0200

    refactor(store-engine-dexie): Remove need for DexieInstance (#2064)

--- store-engine-dexie/src/index.ts
+++ store-engine-dexie/src/index.ts
@@ -20,13 +20,8 @@
 import {CRUDEngine, error as StoreEngineError} from '@wireapp/store-engine';
 import Dexie from 'dexie';
 
-/** @see https://dexie.org/docs/Typescript#create-a-subclass */
-export interface DexieInstance extends Dexie {
-  [index: string]: any;
-}
-
 export class IndexedDBEngine implements CRUDEngine {
-  private db?: DexieInstance;
+  private db: Dexie = new Dexie('');
   public storeName = '';
 
   // Check if IndexedDB is accessible (which won't be the case when browsing with Firefox in private mode or being on
@@ -73,18 +68,18 @@ export class IndexedDBEngine implements CRUDEngine {
     await this.hasEnoughQuota();
   }
 
-  public async init(storeName: string): Promise<DexieInstance> {
+  public async init(storeName: string): Promise<Dexie> {
     await this.isSupported();
     return this.assignDb(new Dexie(storeName));
   }
 
-  public initWithDb(db: DexieInstance): Promise<DexieInstance> {
+  public initWithDb(db: Dexie): Promise<Dexie> {
     return Promise.resolve(this.assignDb(db));
   }
 
   // If you want to add listeners to the database and you don't care if it is a new database (init)
   // or an existing (initWithDB) one, then this method is the right place to do it.
-  private assignDb(db: DexieInstance): DexieInstance {
+  private assignDb(db: Dexie): Dexie {
     this.db = db;
     this.storeName = this.db.name;
     return this.db;
@@ -113,9 +108,12 @@ export class IndexedDBEngine implements CRUDEngine {
 
   public create<T>(tableName: string, primaryKey: string, entity: T): Promise<string> {
     if (entity) {
-      return this.db![tableName].add(entity, primaryKey).catch((error: Dexie.DexieError) => {
-        throw this.mapDatabaseError(error, tableName, primaryKey);
-      });
+      return this.db
+        .table(tableName)
+        .add(entity, primaryKey)
+        .catch((error: Dexie.DexieError) => {
+          throw this.mapDatabaseError(error, tableName, primaryKey);
+        });
     }
     const message = `Record "${primaryKey}" cannot be saved in "${tableName}" because it's "undefined" or "null".`;
     return Promise.reject(new StoreEngineError.RecordTypeError(message));
@@ -123,55 +121,71 @@ export class IndexedDBEngine implements CRUDEngine {
 
   public delete(tableName: string, primaryKey: string): Promise<string> {
     return Promise.resolve()
-      .then(() => this.db![tableName].delete(primaryKey))
+      .then(() => this.db.table(tableName).delete(primaryKey))
       .then(() => primaryKey);
   }
 
   public deleteAll(tableName: string): Promise<boolean> {
-    return this.db![tableName].clear().then(() => true);
+    return this.db
+      .table(tableName)
+      .clear()
+      .then(() => true);
   }
 
   public read<T>(tableName: string, primaryKey: string): Promise<T> {
-    return this.db![tableName].get(primaryKey).then((record: T) => {
-      if (record) {
-        return record;
-      }
-      const message = `Record "${primaryKey}" in "${tableName}" could not be found.`;
-      throw new StoreEngineError.RecordNotFoundError(message);
-    });
+    return this.db
+      .table(tableName)
+      .get(primaryKey)
+      .then((record: T) => {
+        if (record) {
+          return record;
+        }
+        const message = `Record "${primaryKey}" in "${tableName}" could not be found.`;
+        throw new StoreEngineError.RecordNotFoundError(message);
+      });
   }
 
   public readAll<T>(tableName: string): Promise<T[]> {
-    return this.db![tableName].toArray();
+    return this.db.table(tableName).toArray();
   }
 
-  public readAllPrimaryKeys(tableName: string): Promise<string[]> {
-    return this.db![tableName].toCollection().keys();
+  public async readAllPrimaryKeys(tableName: string): Promise<string[]> {
+    const keys = await this.db
+      .table(tableName)
+      .toCollection()
+      .keys();
+    return keys.map(key => `${key}`);
   }
 
   public update(tableName: string, primaryKey: string, changes: Object): Promise<string> {
-    return this.db![tableName].update(primaryKey, changes).then((updatedRecords: number) => {
-      if (updatedRecords === 0) {
-        const message = `Record "${primaryKey}" in "${tableName}" could not be found.`;
-        throw new StoreEngineError.RecordNotFoundError(message);
-      }
-      return primaryKey;
-    });
+    return this.db
+      .table(tableName)
+      .update(primaryKey, changes)
+      .then((updatedRecords: number) => {
+        if (updatedRecords === 0) {
+          const message = `Record "${primaryKey}" in "${tableName}" could not be found.`;
+          throw new StoreEngineError.RecordNotFoundError(message);
+        }
+        return primaryKey;
+      });
   }
 
   public updateOrCreate(tableName: string, primaryKey: string, changes: Object): Promise<string> {
-    return this.db![tableName].put(changes, primaryKey);
+    return this.db.table(tableName).put(changes, primaryKey);
   }
 
   public append(tableName: string, primaryKey: string, additions: string): Promise<string> {
-    return this.db![tableName].get(primaryKey).then((record: any) => {
-      if (typeof record === 'string') {
-        record += additions;
-      } else {
-        const message = `Cannot append text to record "${primaryKey}" because it's not a string.`;
-        throw new StoreEngineError.RecordTypeError(message);
-      }
-      return this.updateOrCreate(tableName, primaryKey, record);
-    });
+    return this.db
+      .table(tableName)
+      .get(primaryKey)
+      .then((record: any) => {
+        if (typeof record === 'string') {
+          record += additions;
+        } else {
+          const message = `Cannot append text to record "${primaryKey}" because it's not a string.`;
+          throw new StoreEngineError.RecordTypeError(message);
+        }
+        return this.updateOrCreate(tableName, primaryKey, record);
+      });
   }
 }
