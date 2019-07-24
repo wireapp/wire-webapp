f5b4a023b052c5e7198ccb4f7d7478e671bdb1ae
Lipis <lipiridis@gmail.com>
2019-07-24 16:01:22 +0300

    feat(react-ui-kit): Smaller borders in AvatarGrid (#2118)
    * feat: AvatarGrid compoment
    
    * Size
    
    * Avatar
    
    * Colors and size
    
    * Borders
    
    * Avatar Props
    
    * feat(react-ui-kit): Smaller borders in AvatarGrid
    
    * 6
    
    * 16%
    
    * Demo

--- react-ui-kit/demo/Demo.jsx
+++ react-ui-kit/demo/Demo.jsx
@@ -205,7 +205,7 @@ const Demo = () => {
             <Avatar size={32} fontSize={20} name={'Joe Do'} borderColor={'#fb0807'} backgroundColor={'#2085C2'} />
             <Avatar size={24} fontSize={20} name={'Joe Do'} borderColor={'#fb0807'} backgroundColor={'#2085C2'} />
             <AvatarGrid
-              size={64}
+              size={120}
               items={[
                 {borderColor: '#fb0807', name: 'Joe Doe'},
                 {borderColor: '#2085C2', name: 'Bon Jovi'},
@@ -214,7 +214,7 @@ const Demo = () => {
               ]}
             />
             <AvatarGrid
-              size={48}
+              size={120}
               items={[
                 {borderColor: '#fb0807', name: 'Joe Doe'},
                 {borderColor: '#2085C2', name: 'Bon Jovi'},
@@ -222,10 +222,26 @@ const Demo = () => {
               ]}
             />
             <AvatarGrid
-              size={48}
+              size={64}
               items={[{borderColor: '#EB7E00', name: 'Mick Jagger'}, {borderColor: '#359AD7', name: 'Freddy Mercury'}]}
             />
-            <AvatarGrid size={48} items={[{borderColor: '#EB7E00', name: 'Mick Jagger'}]} />
+            <AvatarGrid size={64} items={[{borderColor: '#EB7E00', name: 'Mick Jagger'}]} />
+            <AvatarGrid
+              size={32}
+              items={[
+                {borderColor: '#fb0807', name: 'Joe Doe'},
+                {borderColor: '#2085C2', name: 'Bon Jovi'},
+                {borderColor: '#EB7E00', name: 'Mick Jagger'},
+              ]}
+            />
+            <AvatarGrid
+              size={24}
+              items={[
+                {borderColor: '#fb0807', name: 'Joe Doe'},
+                {borderColor: '#2085C2', name: 'Bon Jovi'},
+                {borderColor: '#EB7E00', name: 'Mick Jagger'},
+              ]}
+            />
           </div>
         </Container>
         <DemoIcons />
