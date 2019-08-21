b0abc3e83e29b82c366449d13fc6b28a625fc259
Lipis <lipiridis@gmail.com>
2019-08-21 16:04:33 +0300

    refactor(react-ui-kit): Refactor demo icons (#2203)
    * refactor(react-ui-kit): Refactor demo icons
    
    * Titles
    
    * Add missing icon
    
    * return

--- demo/DemoIcons.jsx
+++ demo/DemoIcons.jsx
@@ -29,6 +29,7 @@ import {
   CamIcon,
   CheckIcon,
   ChromeIcon,
+  CloseIcon,
   COLOR,
   Container,
   DeviceIcon,
@@ -70,6 +71,58 @@ import {
 } from '@wireapp/react-ui-kit';
 import React from 'react';
 
+const icons = [
+  AddPeopleIcon,
+  ArrowIcon,
+  AttachmentIcon,
+  AudioVideoIcon,
+  CallIcon,
+  CameraIcon,
+  CamIcon,
+  CheckIcon,
+  CloseIcon,
+  DeviceIcon,
+  DownloadIcon,
+  EditIcon,
+  FileIcon,
+  GifIcon,
+  HangupIcon,
+  ImageIcon,
+  InfoIcon,
+  InviteIcon,
+  LeaveIcon,
+  MessageIcon,
+  MoreIcon,
+  MuteIcon,
+  OptionsIcon,
+  PingIcon,
+  PlaneIcon,
+  ProfileIcon,
+  RecordIcon,
+  RecordPendingIcon,
+  ScreenshareIcon,
+  ServicesIcon,
+  SettingsIcon,
+  SpeakerIcon,
+  TeamIcon,
+  TimedIcon,
+  TrashIcon,
+  TriangleIcon,
+  WireIcon,
+];
+
+const brandIcons = [
+  AndroidIcon,
+  AppleIcon,
+  ChromeIcon,
+  FacebookIcon,
+  GitHubIcon,
+  LinkedInIcon,
+  LinuxIcon,
+  MicrosoftIcon,
+  TwitterIcon,
+];
+
 export const DemoIcons = () => (
   <Container>
     <Line />
@@ -83,42 +136,11 @@ export const DemoIcons = () => (
         justifyItems: 'center',
       }}
     >
-      <AddPeopleIcon height={32} />
-      <ArrowIcon direction="up" height={32} />
-      <AttachmentIcon height={32} />
-      <AudioVideoIcon height={32} />
-      <CallIcon height={32} />
-      <CameraIcon width={32} />
-      <CamIcon width={32} />
-      <CheckIcon width={32} />
-      <DeviceIcon height={32} />
-      <DownloadIcon height={32} />
-      <EditIcon height={32} />
-      <FileIcon height={32} />
-      <GifIcon width={32} />
-      <HangupIcon width={32} />
-      <ImageIcon height={32} />
-      <InfoIcon height={32} />
-      <InviteIcon height={32} />
-      <LeaveIcon height={32} />
-      <MessageIcon height={32} />
-      <MoreIcon height={32} />
-      <MuteIcon height={32} />
-      <OptionsIcon height={32} />
-      <PingIcon height={32} />
-      <PlaneIcon height={32} />
-      <ProfileIcon height={32} />
-      <RecordIcon width={32} />
-      <RecordPendingIcon width={32} />
-      <ScreenshareIcon width={32} />
-      <ServicesIcon height={32} />
-      <SettingsIcon height={32} />
-      <SpeakerIcon height={32} />
-      <TeamIcon height={32} />
-      <TimedIcon height={32} />
-      <TrashIcon height={32} />
-      <TriangleIcon height={32} />
-      <WireIcon width={32} />
+      {icons.map((Icon, index) => (
+        <span key={index} title={Icon.name}>
+          <Icon height={32} width={32} />
+        </span>
+      ))}
     </div>
 
     <Line />
@@ -132,55 +154,27 @@ export const DemoIcons = () => (
         justifyItems: 'center',
       }}
     >
-      <AddPeopleIcon height={32} shadow color={COLOR.WHITE} />
-      <ArrowIcon direction="up" height={32} shadow color={COLOR.WHITE} />
-      <AttachmentIcon height={32} shadow color={COLOR.WHITE} />
-      <AudioVideoIcon height={32} shadow color={COLOR.WHITE} />
-      <CallIcon height={32} shadow color={COLOR.WHITE} />
-      <CameraIcon width={32} shadow color={COLOR.WHITE} />
-      <CamIcon width={32} shadow color={COLOR.WHITE} />
-      <CheckIcon width={32} shadow color={COLOR.WHITE} />
-      <DeviceIcon height={32} shadow color={COLOR.WHITE} />
-      <DownloadIcon height={32} shadow color={COLOR.WHITE} />
-      <EditIcon height={32} shadow color={COLOR.WHITE} />
-      <FileIcon height={32} shadow color={COLOR.WHITE} />
-      <GifIcon width={32} shadow color={COLOR.WHITE} />
-      <HangupIcon width={32} shadow color={COLOR.WHITE} />
-      <ImageIcon height={32} shadow color={COLOR.WHITE} />
-      <InfoIcon height={32} shadow color={COLOR.WHITE} />
-      <InviteIcon height={32} shadow color={COLOR.WHITE} />
-      <LeaveIcon height={32} shadow color={COLOR.WHITE} />
-      <MessageIcon height={32} shadow color={COLOR.WHITE} />
-      <MoreIcon height={32} shadow color={COLOR.WHITE} />
-      <MuteIcon height={32} shadow color={COLOR.WHITE} />
-      <OptionsIcon height={32} shadow color={COLOR.WHITE} />
-      <PingIcon height={32} shadow color={COLOR.WHITE} />
-      <PlaneIcon height={32} shadow color={COLOR.WHITE} />
-      <ProfileIcon height={32} shadow color={COLOR.WHITE} />
-      <RecordIcon width={32} shadow color={COLOR.WHITE} />
-      <RecordPendingIcon width={32} shadow color={COLOR.WHITE} />
-      <ScreenshareIcon width={32} shadow color={COLOR.WHITE} />
-      <ServicesIcon height={32} shadow color={COLOR.WHITE} />
-      <SettingsIcon height={32} shadow color={COLOR.WHITE} />
-      <SpeakerIcon height={32} shadow color={COLOR.WHITE} />
-      <TeamIcon height={32} shadow color={COLOR.WHITE} />
-      <TimedIcon height={32} shadow color={COLOR.WHITE} />
-      <TrashIcon height={32} shadow color={COLOR.WHITE} />
-      <TriangleIcon height={32} shadow color={COLOR.WHITE} />
-      <WireIcon width={32} shadow color={COLOR.WHITE} />
+      {icons.map((Icon, index) => (
+        <span key={index} title={Icon.name}>
+          <Icon height={32} width={32} shadow color={COLOR.WHITE} />
+        </span>
+      ))}
     </div>
     <Line />
     <H1>Brand Icons</H1>
-    <div style={{display: 'grid', gridGap: 16, gridTemplateColumns: 'repeat(9, 1fr)'}}>
-      <AndroidIcon width={48} />
-      <AppleIcon width={48} />
-      <ChromeIcon width={48} />
-      <FacebookIcon width={48} />
-      <GitHubIcon width={48} />
-      <LinkedInIcon width={48} />
-      <LinuxIcon width={48} />
-      <MicrosoftIcon width={48} />
-      <TwitterIcon width={48} />
+    <div
+      style={{
+        display: 'grid',
+        gridGap: 16,
+        gridTemplateColumns: `repeat(${brandIcons.length}, 1fr)`,
+        justifyItems: 'center',
+      }}
+    >
+      {brandIcons.map((Icon, index) => (
+        <span key={index} title={Icon.name}>
+          <Icon height={48} width={48} />
+        </span>
+      ))}
     </div>
   </Container>
 );
