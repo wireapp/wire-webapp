2f47c029a98392ce85209e723b58ebe29cda889e
Florian Keller <ffflorian@users.noreply.github.com>
2019-08-27 15:23:40 +0200

    feat(store-engine-dexie): Register persistent storage and call isSupported on initWithDb (#2224)

--- store-engine-dexie/src/index.ts
+++ store-engine-dexie/src/index.ts
@@ -68,13 +68,30 @@ export class IndexedDBEngine implements CRUDEngine {
     await this.hasEnoughQuota();
   }
 
-  public async init(storeName: string): Promise<Dexie> {
+  public async init(storeName: string, registerPersisted: boolean = false): Promise<Dexie> {
     await this.isSupported();
-    return this.assignDb(new Dexie(storeName));
+    const dexie = await this.assignDb(new Dexie(storeName));
+    if (registerPersisted) {
+      await this.registerPersistentStorage();
+    }
+    return dexie;
   }
 
-  public async initWithDb(db: Dexie): Promise<Dexie> {
-    return this.assignDb(db);
+  public async initWithDb(db: Dexie, registerPersisted: boolean = false): Promise<Dexie> {
+    await this.isSupported();
+    const dexie = this.assignDb(db);
+    if (registerPersisted) {
+      await this.registerPersistentStorage();
+    }
+    return dexie;
+  }
+
+  public async isStoragePersisted(): Promise<boolean> {
+    if (navigator.storage && navigator.storage.persisted) {
+      const isPersisted = await navigator.storage.persisted();
+      return isPersisted;
+    }
+    return false;
   }
 
   // If you want to add listeners to the database and you don't care if it is a new database (init)
@@ -159,6 +176,27 @@ export class IndexedDBEngine implements CRUDEngine {
       .toCollection()
       .keys();
     return keys.map(key => (key as any) as PrimaryKey);
+  }
+
+  /**
+   * Register a persistent storage in the browser.
+   * @see https://developer.mozilla.org/en-US/docs/Web/API/StorageManager/persist
+   * @see https://developers.google.com/web/updates/2016/06/persistent-storage
+   */
+  public async registerPersistentStorage(): Promise<boolean> {
+    if (!navigator || !navigator.storage || !navigator.storage.persist) {
+      return false;
+    }
+
+    const granted = await navigator.storage.persist();
+
+    if (granted) {
+      console.info('Storage will not be cleared except by explicit user action');
+      return true;
+    }
+
+    console.info('Storage may be cleared by the UA under storage pressure.');
+    return false;
   }
 
   public async update<PrimaryKey = string>(
