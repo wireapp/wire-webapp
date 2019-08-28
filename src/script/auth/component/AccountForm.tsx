44b510a1c5abe5e7618d05fdb39db78c4190656c
AndyLnd <andy@wire.com>
2019-08-28 14:18:55 +0200

    refactor: Turn AccountForm into functional component (#7196)

--- src/script/auth/component/AccountForm.tsx
+++ src/script/auth/component/AccountForm.tsx
@@ -52,117 +52,90 @@ interface DispatchProps {
   pushAccountRegistrationData: (registrationData: Partial<RegistrationDataState>) => Promise<void>;
 }
 
-interface State {
-  registrationData: {
-    accent_id: number;
-    email: string;
-    name: string;
-    password: string;
-    termsAccepted: boolean;
-  };
-  validInputs: {
-    [field: string]: boolean;
-  };
-  validationErrors: Error[];
-}
-
 type CombinedProps = Props & ConnectedProps & DispatchProps & InjectedIntlProps;
 
-class AccountForm extends React.PureComponent<CombinedProps, State> {
-  private readonly inputs: {
-    name: React.RefObject<HTMLInputElement>;
-    email: React.RefObject<HTMLInputElement>;
-    password: React.RefObject<HTMLInputElement>;
-    terms: React.RefObject<HTMLInputElement>;
-  } = {
-    email: React.createRef<HTMLInputElement>(),
-    name: React.createRef<HTMLInputElement>(),
-    password: React.createRef<HTMLInputElement>(),
-    terms: React.createRef<HTMLInputElement>(),
+const AccountForm = ({account, intl: {formatMessage: _}, ...props}: CombinedProps) => {
+  const [registrationData, setRegistrationData] = React.useState({
+    accent_id: AccentColor.random().id,
+    email: account.email || '',
+    name: account.name || '',
+    password: account.password || '',
+    termsAccepted: account.termsAccepted || false,
+  });
+
+  const [validInputs, setValidInputs] = React.useState<Record<string, boolean>>({
+    email: true,
+    name: true,
+    password: true,
+    terms: true,
+  });
+
+  const [validationErrors, setValidationErrors] = React.useState([]);
+
+  const inputs = {
+    email: React.useRef<HTMLInputElement>(),
+    name: React.useRef<HTMLInputElement>(),
+    password: React.useRef<HTMLInputElement>(),
+    terms: React.useRef<HTMLInputElement>(),
   };
 
-  state: State = {
-    registrationData: {
-      accent_id: AccentColor.random().id,
-      email: this.props.account.email || '',
-      name: this.props.account.name || '',
-      password: this.props.account.password || '',
-      termsAccepted: this.props.account.termsAccepted || false,
-    },
-    validInputs: {
-      email: true,
-      name: true,
-      password: true,
-      terms: true,
-    },
-    validationErrors: [],
-  };
+  React.useEffect(() => {
+    setRegistrationData({
+      ...registrationData,
+      email: account.email,
+    });
+  }, [account.email]);
 
-  componentWillReceiveProps({account}: CombinedProps): void {
-    if (account) {
-      if (account.email !== this.state.registrationData.email) {
-        this.setState({
-          registrationData: {
-            ...this.state.registrationData,
-            email: account.email,
-          },
-        });
-      }
-      if (account.name !== this.props.account.name) {
-        this.setState({
-          registrationData: {
-            ...this.state.registrationData,
-            name: account.name,
-          },
-        });
-      }
-    }
-  }
+  React.useEffect(() => {
+    setRegistrationData({
+      ...registrationData,
+      name: account.name,
+    });
+  }, [account.name]);
 
-  createURLForToU = () => {
-    return `${EXTERNAL_ROUTE.WIRE_WEBSITE}/legal/terms/${this.props.isPersonalFlow ? 'personal' : 'teams'}/`;
-  };
+  const createURLForToU = () =>
+    `${EXTERNAL_ROUTE.WIRE_WEBSITE}/legal/terms/${props.isPersonalFlow ? 'personal' : 'teams'}/`;
 
-  handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const validInputs: {[field: string]: boolean} = this.state.validInputs;
     const errors: Error[] = [];
+    const newValidInputs: Record<string, boolean> = {};
 
-    Object.entries(this.inputs).forEach(([inputKey, currentInput]) => {
+    Object.entries(inputs).forEach(([inputKey, currentInput]) => {
       const currentInputNode = currentInput.current;
-      if (inputKey !== 'password' && inputKey !== 'terms') {
+      if (!['password', 'terms'].includes(inputKey)) {
         currentInputNode.value = currentInputNode.value.trim();
       }
       if (!currentInputNode.checkValidity()) {
         errors.push(ValidationError.handleValidationState(currentInputNode.name, currentInputNode.validity));
       }
-      validInputs[inputKey] = currentInputNode.validity.valid;
+      newValidInputs[inputKey] = currentInputNode.validity.valid;
     });
-
-    this.setState({validInputs, validationErrors: errors});
+    setValidInputs(newValidInputs);
+    setValidationErrors(errors);
     try {
       if (errors.length > 0) {
         throw errors[0];
       }
-      await (this.props.beforeSubmit && this.props.beforeSubmit());
-      await this.props.pushAccountRegistrationData({...this.state.registrationData});
-      await this.props.doSendActivationCode(this.state.registrationData.email);
-      return this.props.onSubmit(event);
+      await (props.beforeSubmit && props.beforeSubmit());
+      await props.pushAccountRegistrationData({...registrationData});
+      await props.doSendActivationCode(registrationData.email);
+      return props.onSubmit(event);
     } catch (error) {
       if (error && error.label) {
         switch (error.label) {
           case BackendError.AUTH_ERRORS.BLACKLISTED_EMAIL:
           case BackendError.AUTH_ERRORS.INVALID_EMAIL:
           case BackendError.AUTH_ERRORS.KEY_EXISTS: {
-            this.inputs.email.current.setCustomValidity(error.label);
-            this.setState(state => ({validInputs: {...state.validInputs, email: false}}));
+            inputs.email.current.setCustomValidity(error.label);
+            setValidInputs({...validInputs, email: false});
             break;
           }
           case BackendError.AUTH_ERRORS.INVALID_CREDENTIALS:
           case BackendError.GENERAL_ERRORS.UNAUTHORIZED: {
-            this.inputs.email.current.setCustomValidity(error.label);
-            this.inputs.password.current.setCustomValidity(error.label);
-            this.setState(state => ({validInputs: {...state.validInputs, email: false, password: false}}));
+            inputs.email.current.setCustomValidity(error.label);
+            inputs.password.current.setCustomValidity(error.label);
+            setValidInputs({...validInputs, email: false, password: false});
             break;
           }
           default: {
@@ -180,157 +153,133 @@ class AccountForm extends React.PureComponent<CombinedProps, State> {
       }
     }
   };
-
-  render() {
-    const {
-      isFetching,
-      isPersonalFlow,
-      submitText,
-      intl: {formatMessage: _},
-    } = this.props;
-    const {
-      registrationData: {name, email, password, termsAccepted},
-      validInputs,
-    } = this.state;
-    return (
-      <Form onSubmit={this.handleSubmit} style={{display: 'flex', flexDirection: 'column'}}>
-        <div>
-          <InputBlock>
-            <Input
-              name="name"
-              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                this.inputs.name.current.setCustomValidity('');
-                this.setState({
-                  registrationData: {
-                    ...this.state.registrationData,
-                    name: event.target.value,
-                  },
-                  validInputs: {...validInputs, name: true},
-                });
-              }}
-              ref={this.inputs.name}
-              markInvalid={!validInputs.name}
-              value={name}
-              autoComplete="section-create-team username"
-              placeholder={_(accountFormStrings.namePlaceholder)}
-              onKeyDown={(event: React.KeyboardEvent<HTMLInputElement>) => {
-                if (event.key === 'Enter') {
-                  this.inputs.email.current.focus();
-                }
-              }}
-              autoFocus
-              maxLength={64}
-              minLength={2}
-              pattern=".{2,64}"
-              required
-              data-uie-name="enter-name"
-            />
-            <Input
-              name="email"
-              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                this.inputs.email.current.setCustomValidity('');
-                this.setState({
-                  registrationData: {
-                    ...this.state.registrationData,
-                    email: event.target.value,
-                  },
-                  validInputs: {...validInputs, email: true},
-                });
-              }}
-              ref={this.inputs.email}
-              markInvalid={!validInputs.email}
-              value={email}
-              autoComplete="section-create-team email"
-              placeholder={_(
-                isPersonalFlow ? accountFormStrings.emailPersonalPlaceholder : accountFormStrings.emailTeamPlaceholder,
-              )}
-              onKeyDown={(event: React.KeyboardEvent<HTMLInputElement>) => {
-                if (event.key === 'Enter') {
-                  this.inputs.password.current.focus();
-                }
-              }}
-              maxLength={128}
-              type="email"
-              required
-              data-uie-name="enter-email"
-            />
-            <Input
-              name="password"
-              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                this.inputs.password.current.setCustomValidity('');
-                this.setState({
-                  registrationData: {
-                    ...this.state.registrationData,
-                    password: event.target.value,
-                  },
-                  validInputs: {...validInputs, password: true},
-                });
-              }}
-              ref={this.inputs.password}
-              markInvalid={!validInputs.password}
-              value={password}
-              autoComplete="section-create-team new-password"
-              type="password"
-              placeholder={_(accountFormStrings.passwordPlaceholder)}
-              pattern={ValidationUtil.getNewPasswordPattern(Config.NEW_PASSWORD_MINIMUM_LENGTH)}
-              required
-              data-uie-name="enter-password"
-            />
-          </InputBlock>
-          <Small
-            style={{
-              display: this.state.validationErrors.length ? 'none' : 'block',
-              marginBottom: '32px',
-              padding: '0 16px',
+  return (
+    <Form onSubmit={handleSubmit} style={{display: 'flex', flexDirection: 'column'}}>
+      <div>
+        <InputBlock>
+          <Input
+            name="name"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              inputs.name.current.setCustomValidity('');
+              setRegistrationData({...registrationData, name: event.target.value});
+              setValidInputs({...validInputs, name: true});
             }}
-            data-uie-name="element-password-help"
-          >
-            {_(accountFormStrings.passwordHelp, {minPasswordLength: Config.NEW_PASSWORD_MINIMUM_LENGTH})}
-          </Small>
-          <ErrorMessage data-uie-name="error-message">{parseError(this.props.authError)}</ErrorMessage>
-          <div data-uie-name="error-message">{parseValidationErrors(this.state.validationErrors)}</div>
-        </div>
-        <Checkbox
-          ref={this.inputs.terms}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-            this.inputs.terms.current.setCustomValidity('');
-            this.setState({
-              registrationData: {
-                ...this.state.registrationData,
-                termsAccepted: event.target.checked,
-              },
-              validInputs: {...validInputs, terms: true},
-            });
+            ref={inputs.name}
+            markInvalid={!validInputs.name}
+            value={registrationData.name}
+            autoComplete="section-create-team username"
+            placeholder={_(accountFormStrings.namePlaceholder)}
+            onKeyDown={(event: React.KeyboardEvent<HTMLInputElement>) => {
+              if (event.key === 'Enter') {
+                inputs.email.current.focus();
+              }
+            }}
+            autoFocus
+            maxLength={64}
+            minLength={2}
+            pattern=".{2,64}"
+            required
+            data-uie-name="enter-name"
+          />
+          <Input
+            name="email"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              inputs.email.current.setCustomValidity('');
+              setRegistrationData({...registrationData, email: event.target.value});
+              setValidInputs({...validInputs, email: true});
+            }}
+            ref={inputs.email}
+            markInvalid={!validInputs.email}
+            value={registrationData.email}
+            autoComplete="section-create-team email"
+            placeholder={_(
+              props.isPersonalFlow
+                ? accountFormStrings.emailPersonalPlaceholder
+                : accountFormStrings.emailTeamPlaceholder,
+            )}
+            onKeyDown={(event: React.KeyboardEvent<HTMLInputElement>) => {
+              if (event.key === 'Enter') {
+                inputs.password.current.focus();
+              }
+            }}
+            maxLength={128}
+            type="email"
+            required
+            data-uie-name="enter-email"
+          />
+          <Input
+            name="password"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              inputs.password.current.setCustomValidity('');
+              setRegistrationData({...registrationData, password: event.target.value});
+              setValidInputs({...validInputs, password: true});
+            }}
+            ref={inputs.password}
+            markInvalid={!validInputs.password}
+            value={registrationData.password}
+            autoComplete="section-create-team new-password"
+            type="password"
+            placeholder={_(accountFormStrings.passwordPlaceholder)}
+            pattern={ValidationUtil.getNewPasswordPattern(Config.NEW_PASSWORD_MINIMUM_LENGTH)}
+            required
+            data-uie-name="enter-password"
+          />
+        </InputBlock>
+        <Small
+          style={{
+            display: validationErrors.length ? 'none' : 'block',
+            marginBottom: '32px',
+            padding: '0 16px',
           }}
-          markInvalid={!validInputs.terms}
-          name="accept"
-          required
-          checked={termsAccepted}
-          data-uie-name="do-terms"
-          style={{justifyContent: 'center'}}
+          data-uie-name="element-password-help"
         >
-          <CheckboxLabel>
-            <FormattedHTMLMessage
-              {...accountFormStrings.terms}
-              values={{
-                linkParams: `target=_blank data-uie-name=go-terms href=${this.createURLForToU()}`,
-              }}
-            />
-          </CheckboxLabel>
-        </Checkbox>
-        <Button
-          disabled={!(email && name && password && termsAccepted) || isFetching}
-          formNoValidate
-          type="submit"
-          style={{margin: '16px auto'}}
-          data-uie-name="do-next"
-        >
-          {submitText || _(accountFormStrings.submitButton)}
-        </Button>
-      </Form>
-    );
-  }
-}
+          {_(accountFormStrings.passwordHelp, {minPasswordLength: Config.NEW_PASSWORD_MINIMUM_LENGTH})}
+        </Small>
+        <ErrorMessage data-uie-name="error-message">{parseError(props.authError)}</ErrorMessage>
+        <div data-uie-name="error-message">{parseValidationErrors(validationErrors)}</div>
+      </div>
+      <Checkbox
+        ref={inputs.terms}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+          inputs.terms.current.setCustomValidity('');
+          setRegistrationData({...registrationData, termsAccepted: event.target.checked});
+          setValidInputs({...validInputs, terms: true});
+        }}
+        markInvalid={!validInputs.terms}
+        name="accept"
+        required
+        checked={registrationData.termsAccepted}
+        data-uie-name="do-terms"
+        style={{justifyContent: 'center'}}
+      >
+        <CheckboxLabel>
+          <FormattedHTMLMessage
+            {...accountFormStrings.terms}
+            values={{
+              linkParams: `target=_blank data-uie-name=go-terms href=${createURLForToU()}`,
+            }}
+          />
+        </CheckboxLabel>
+      </Checkbox>
+      <Button
+        disabled={
+          !(
+            registrationData.email &&
+            registrationData.name &&
+            registrationData.password &&
+            registrationData.termsAccepted
+          ) || props.isFetching
+        }
+        formNoValidate
+        type="submit"
+        style={{margin: '16px auto'}}
+        data-uie-name="do-next"
+      >
+        {props.submitText || _(accountFormStrings.submitButton)}
+      </Button>
+    </Form>
+  );
+};
 
 export default injectIntl(
   connect(
