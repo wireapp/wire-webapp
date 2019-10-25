f6cdd49f458b3b8cc2420a237a39af795fdbf79a
Florian Keller <florian.keller@wire.com>
2019-10-25 11:24:44 +0200

    refactor: Remove synchronous openGraph

--- src/script/links/LinkPreviewRepository.js
+++ src/script/links/LinkPreviewRepository.js
@@ -17,6 +17,10 @@
  *
  */
 
+// OpenGraphResult is used in JSDoc
+// eslint-disable-next-line no-unused-vars
+import {Data as OpenGraphResult} from 'open-graph';
+
 import {base64ToBlob} from 'Util/util';
 
 import {getFirstLinkWithOffset} from './LinkPreviewHelpers';
@@ -51,7 +55,7 @@ class LinkPreviewRepository {
    * @returns {Promise} Resolves with link preview proto message
    */
   getLinkPreviewFromString(string) {
-    if (!this.shouldSendPreviews || !(window.openGraph || window.openGraphAsync)) {
+    if (!this.shouldSendPreviews || !window.openGraphAsync) {
       return Promise.resolve();
     }
     const linkData = getFirstLinkWithOffset(string);
@@ -139,43 +143,21 @@ class LinkPreviewRepository {
    *
    * @private
    * @param {string} link - Link to fetch open graph data from
-   * @returns {Promise} Resolves with the retrieved open graph data
+   * @returns {Promise<OpenGraphResult | Error>} Resolves with the retrieved open graph data
    */
-  _fetchOpenGraphData(link) {
-    const mergeOpenGraphData = data => {
+  async _fetchOpenGraphData(link) {
+    try {
+      const data = await window.openGraphAsync(link);
       if (data) {
         return Object.entries(data).reduce((result, [key, value]) => {
           result[key] = Array.isArray(value) ? value[0] : value;
           return result;
         }, {});
       }
-    };
-
-    if (typeof window.openGraphAsync === 'function') {
-      return window
-        .openGraphAsync(link)
-        .then(mergeOpenGraphData)
-        .catch(error => {
-          this.logger.warn(`Error while fetching OpenGraph data: ${error.message}`);
-          return Promise.resolve();
-        });
+    } catch (error) {
+      this.logger.warn(`Error while fetching OpenGraph data: ${error.message}`);
+      return error;
     }
-
-    return new Promise(resolve => {
-      return window
-        .openGraph(link, (error, data) => {
-          if (error) {
-            this.logger.warn(`Error while fetching OpenGraph data: ${error.message}`);
-            resolve();
-          }
-
-          resolve(mergeOpenGraphData(data));
-        })
-        .catch(error => {
-          this.logger.warn(`Error while fetching OpenGraph data: ${error.message}`);
-          resolve(error);
-        });
-    });
   }
 
   /**
