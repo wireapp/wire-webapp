69be9332ab9da39b21de2f3800aaa949cbfcd6fb
Michael Koppen <Yserz@users.noreply.github.com>
2019-10-29 14:57:20 +0100

    refactor: Functional InitialInvite component (#7596)

--- src/script/auth/page/InitialInvite.tsx
+++ src/script/auth/page/InitialInvite.tsx
@@ -33,8 +33,8 @@ import {
   RoundIconButton,
   Text,
 } from '@wireapp/react-ui-kit';
-import React from 'react';
-import {InjectedIntlProps, injectIntl} from 'react-intl';
+import React, {useEffect, useState} from 'react';
+import {useIntl} from 'react-intl';
 import {connect} from 'react-redux';
 import {AnyAction, Dispatch} from 'redux';
 import {inviteStrings} from '../../strings';
@@ -51,25 +51,26 @@ import Page from './Page';
 
 interface Props extends React.HTMLProps<HTMLDivElement> {}
 
-interface State {
-  enteredEmail: string;
-  error: Error;
-}
+const InitialInvite = ({
+  fetchSelf,
+  invites,
+  isFetching,
+  inviteError,
+  resetInviteErrors,
+  invite,
+}: Props & ConnectedProps & DispatchProps) => {
+  useEffect(() => {
+    fetchSelf();
+  }, []);
 
-class InitialInvite extends React.PureComponent<Props & ConnectedProps & DispatchProps & InjectedIntlProps, State> {
-  emailInput: React.RefObject<any> = React.createRef();
-  state: State = {
-    enteredEmail: '',
-    error: null,
-  };
+  const {formatMessage: _} = useIntl();
+  const emailInput = React.useRef<HTMLInputElement>();
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [error, setError] = useState(null);
 
-  componentDidMount(): void {
-    this.props.fetchSelf();
-  }
+  const onInviteDone = (): void => window.location.replace(pathWithParams(EXTERNAL_ROUTE.WEBAPP));
 
-  onInviteDone = (): void => window.location.replace(pathWithParams(EXTERNAL_ROUTE.WEBAPP));
-
-  renderEmail = (email: string): JSX.Element => (
+  const renderEmail = (email: string): JSX.Element => (
     <div
       style={{
         alignItems: 'center',
@@ -87,13 +88,15 @@ class InitialInvite extends React.PureComponent<Props & ConnectedProps & Dispatc
     </div>
   );
 
-  handleSubmit = (event: React.FormEvent): void => {
+  const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
-    this.emailInput.current.value = this.emailInput.current.value.trim();
-    if (!this.emailInput.current.checkValidity()) {
-      this.setState({error: ValidationError.handleValidationState('email', this.emailInput.current.validity)});
+    emailInput.current.value = emailInput.current.value.trim();
+    if (!emailInput.current.checkValidity()) {
+      setError(ValidationError.handleValidationState('email', emailInput.current.validity));
     } else {
-      this.props.invite({email: this.emailInput.current.value}).catch(error => {
+      try {
+        await invite({email: emailInput.current.value});
+      } catch (error) {
         if (error.label) {
           switch (error.label) {
             case BackendError.LABEL.EMAIL_EXISTS:
@@ -112,86 +115,77 @@ class InitialInvite extends React.PureComponent<Props & ConnectedProps & Dispatc
         } else {
           throw error;
         }
-      });
-      this.setState({enteredEmail: ''});
-      this.emailInput.current.value = '';
+      }
+      setEnteredEmail('');
+      emailInput.current.value = '';
     }
-    this.emailInput.current.focus();
+    emailInput.current.focus();
   };
 
-  resetErrors = (): void => {
-    this.setState({error: null});
-    this.props.resetInviteErrors();
+  const resetErrors = (): void => {
+    setError(null);
+    resetInviteErrors();
   };
 
-  render() {
-    const {
-      invites,
-      isFetching,
-      error,
-      intl: {formatMessage: _},
-    } = this.props;
-    const {enteredEmail} = this.state;
-    return (
-      <Page>
-        <ContainerXS
-          centerText
-          verticalCenter
-          style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between', minHeight: 428}}
-        >
-          <div>
-            <H1 center>{_(inviteStrings.headline)}</H1>
-            <Muted>{_(inviteStrings.subhead)}</Muted>
-          </div>
-          <div style={{margin: '18px 0', minHeight: 220}}>
-            {invites.map(({email}) => this.renderEmail(email))}
-            <Form onSubmit={this.handleSubmit}>
-              <InputSubmitCombo>
-                <Input
-                  name="email"
-                  placeholder={_(inviteStrings.emailPlaceholder)}
-                  type="email"
-                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                    this.resetErrors();
-                    this.setState({enteredEmail: event.target.value});
-                  }}
-                  ref={this.emailInput}
-                  autoFocus
-                  data-uie-name="enter-invite-email"
-                />
-                <RoundIconButton
-                  disabled={isFetching || !enteredEmail}
-                  type="submit"
-                  icon={ICON_NAME.PLANE}
-                  data-uie-name="do-send-invite"
-                  formNoValidate
-                />
-              </InputSubmitCombo>
-            </Form>
-            <ErrorMessage data-uie-name="error-message">
-              {this.state.error ? parseValidationErrors(this.state.error) : parseError(error)}
-            </ErrorMessage>
-          </div>
-          <div>
-            {invites.length ? (
-              <ButtonLink style={{margin: '0 auto -16px'}} onClick={this.onInviteDone} data-uie-name="do-next">
-                {_(inviteStrings.nextButton)}
-              </ButtonLink>
-            ) : (
-              <Link onClick={this.onInviteDone} data-uie-name="do-skip">
-                {_(inviteStrings.skipForNow)}
-              </Link>
-            )}
-          </div>
-        </ContainerXS>
-      </Page>
-    );
-  }
-}
+  return (
+    <Page>
+      <ContainerXS
+        centerText
+        verticalCenter
+        style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between', minHeight: 428}}
+      >
+        <div>
+          <H1 center>{_(inviteStrings.headline)}</H1>
+          <Muted>{_(inviteStrings.subhead)}</Muted>
+        </div>
+        <div style={{margin: '18px 0', minHeight: 220}}>
+          {invites.map(({email}) => renderEmail(email))}
+          <Form onSubmit={handleSubmit}>
+            <InputSubmitCombo>
+              <Input
+                name="email"
+                placeholder={_(inviteStrings.emailPlaceholder)}
+                type="email"
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  resetErrors();
+                  setEnteredEmail(event.target.value);
+                }}
+                ref={emailInput}
+                autoFocus
+                data-uie-name="enter-invite-email"
+              />
+              <RoundIconButton
+                disabled={isFetching || !enteredEmail}
+                type="submit"
+                icon={ICON_NAME.PLANE}
+                data-uie-name="do-send-invite"
+                formNoValidate
+              />
+            </InputSubmitCombo>
+          </Form>
+          <ErrorMessage data-uie-name="error-message">
+            {error ? parseValidationErrors(error) : parseError(inviteError)}
+          </ErrorMessage>
+        </div>
+        <div>
+          {invites.length ? (
+            <ButtonLink style={{margin: '0 auto -16px'}} onClick={onInviteDone} data-uie-name="do-next">
+              {_(inviteStrings.nextButton)}
+            </ButtonLink>
+          ) : (
+            <Link onClick={onInviteDone} data-uie-name="do-skip">
+              {_(inviteStrings.skipForNow)}
+            </Link>
+          )}
+        </div>
+      </ContainerXS>
+    </Page>
+  );
+};
 
 type ConnectedProps = ReturnType<typeof mapStateToProps>;
 const mapStateToProps = (state: RootState) => ({
-  error: InviteSelector.getError(state),
+  inviteError: InviteSelector.getError(state),
   invites: InviteSelector.getInvites(state),
   isFetching: InviteSelector.isFetching(state),
   language: LanguageSelector.getLanguage(state),
@@ -211,4 +205,4 @@ const mapDispatchToProps = (dispatch: Dispatch<AnyAction>) =>
 export default connect(
   mapStateToProps,
   mapDispatchToProps,
-)(injectIntl(InitialInvite));
+)(InitialInvite);
