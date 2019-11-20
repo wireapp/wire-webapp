a74f3eb6856d37fd8325bdef0aa74a93343a21c8
Michael Koppen <michael.koppen@googlemail.com>
2019-11-20 18:10:16 +0100

    test: More AuthAction tests

--- src/script/auth/module/action/AuthAction.test.ts
+++ src/script/auth/module/action/AuthAction.test.ts
@@ -22,6 +22,7 @@ import {ClientType} from '@wireapp/api-client/dist/commonjs/client';
 import {TypeUtil} from '@wireapp/commons';
 import {mockStoreFactory} from '../../util/test/mockStoreFactory';
 import {actionRoot} from './';
+import {BackendError} from './BackendError';
 import {AuthActionCreator} from './creator/';
 
 describe('AuthAction', () => {
@@ -72,6 +73,60 @@ describe('AuthAction', () => {
     expect(spies.fetchSelf.calls.count()).toEqual(1);
     expect(spies.generateClientPayload.calls.count()).toEqual(1);
     expect(spies.doInitializeClient.calls.count()).toEqual(1);
+  });
+
+  it('authentication is successful with too many clients error', async () => {
+    const email = 'test@example.com';
+    const password = 'password';
+    const accessToken =
+      'Ntc8CsuNMeGFaRCqhah1dSyDFmNq6m8zL1R2h5TEtLQy_4Up8jLI8CA==.v=1.k=1.d=1494355813.tda7f3557-b40c-4b00-9a4c-4710fdc07709.c=16347884158313001066';
+    const response = JSON.parse(`{"expires_in":900,"access_token":"${accessToken}","token_type":"Bearer"}`);
+    const spies = {
+      doInitializeClient: jasmine
+        .createSpy()
+        .and.returnValue(() => Promise.reject({label: BackendError.LABEL.TOO_MANY_CLIENTS})),
+      fetchSelf: jasmine.createSpy().and.returnValue(() => Promise.resolve()),
+      generateClientPayload: jasmine.createSpy().and.returnValue({}),
+      setCookie: jasmine.createSpy().and.returnValue(() => Promise.resolve()),
+      setLocalStorage: jasmine.createSpy().and.returnValue(() => Promise.resolve()),
+    };
+    const mockedActions = {
+      clientAction: {
+        doInitializeClient: spies.doInitializeClient,
+        generateClientPayload: spies.generateClientPayload,
+      },
+      cookieAction: {
+        setCookie: spies.setCookie,
+      },
+      localStorageAction: {
+        setLocalStorage: spies.setLocalStorage,
+      },
+      selfAction: {
+        fetchSelf: spies.fetchSelf,
+      },
+    };
+    const mockedApiClient = {
+      accessTokenStore: {accessToken: response},
+    };
+    const mockedCore = {
+      login: () => Promise.resolve({}),
+    };
+
+    const store = mockStoreFactory({
+      actions: mockedActions,
+      apiClient: mockedApiClient as TypeUtil.RecursivePartial<APIClient>,
+      core: mockedCore,
+    })({});
+    try {
+      await store.dispatch(actionRoot.authAction.doLoginPlain({email, password, clientType: ClientType.PERMANENT}));
+      fail('TOO_MANY_CLIENTS error was not thrown');
+    } catch (error) {
+      expect(error.label)
+        .withContext('Error is of type TOO_MANY_CLIENTS')
+        .toEqual(BackendError.LABEL.TOO_MANY_CLIENTS);
+      expect(store.getActions()).toEqual([AuthActionCreator.startLogin(), AuthActionCreator.successfulLogin()]);
+      expect(spies.doInitializeClient.calls.count()).toEqual(1);
+    }
   });
 
   it('handles failed authentication', async () => {
@@ -126,5 +181,26 @@ describe('AuthAction', () => {
     })({});
     await store.dispatch(actionRoot.authAction.doLogout());
     expect(store.getActions()).toEqual([AuthActionCreator.startLogout(), AuthActionCreator.failedLogout(backendError)]);
+  });
+
+  it('authenticates a user successfully', async () => {
+    const phoneNumber = '+08723568';
+    const expiresIn = 60;
+    const mockedApiClient = {
+      auth: {
+        api: {
+          postLoginSend: () => Promise.resolve({data: {expires_in: expiresIn}}),
+        },
+      },
+    };
+
+    const store = mockStoreFactory({
+      apiClient: mockedApiClient,
+    })({});
+    await store.dispatch(actionRoot.authAction.doSendPhoneLoginCode({phone: phoneNumber}));
+    expect(store.getActions()).toEqual([
+      AuthActionCreator.startSendPhoneLoginCode(),
+      AuthActionCreator.successfulSendPhoneLoginCode(expiresIn),
+    ]);
   });
 });
