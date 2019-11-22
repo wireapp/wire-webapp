15a35b7abae72974a0ce26f25e8391edf50b9439
Florian Keller <ffflorian@users.noreply.github.com>
2019-11-22 17:35:29 +0100

    refactor(bot-api): Remove "main" directory (#2579)

--- bot-api/src/Bot.ts
+++ bot-api/src/Bot.ts
@@ -19,13 +19,13 @@
 
 import {APIClient} from '@wireapp/api-client';
 import {ClientType} from '@wireapp/api-client/dist/client/';
+import {ConversationEvent, TeamEvent, UserEvent} from '@wireapp/api-client/dist/event';
 import {Account} from '@wireapp/core';
 import {PayloadBundle, PayloadBundleType} from '@wireapp/core/dist/conversation/';
 import {CRUDEngine} from '@wireapp/store-engine';
 import logdown from 'logdown';
 import UUID from 'pure-uuid';
 
-import {ConversationEvent, TeamEvent, UserEvent} from '@wireapp/api-client/dist/event';
 import {BotConfig, BotCredentials} from './Interfaces';
 import {MessageHandler} from './MessageHandler';
 
