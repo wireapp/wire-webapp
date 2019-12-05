fdf8f8f0c05980a239f4aaf8509898f51aedd888
Florian Keller <florian.keller@wire.com>
2019-12-05 11:10:39 +0100

    add babel/plugin-proposal-optional-chaining

--- babel.config.js
+++ babel.config.js
@@ -11,7 +11,7 @@ const presetEnvConfig = {
 module.exports = {
   env: {
     test: {
-      plugins: ['@babel/plugin-proposal-class-properties'],
+      plugins: ['@babel/plugin-proposal-class-properties', '@babel/plugin-proposal-optional-chaining'],
       presets: [
         '@babel/preset-react',
         '@babel/preset-typescript',
@@ -22,6 +22,7 @@ module.exports = {
   },
   plugins: [
     '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-optional-chaining',
     [
       'react-intl',
       {
