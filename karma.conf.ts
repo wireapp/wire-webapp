4fdcd55918efd4028eabd03a44290f7c9c07a0ae
Benny Neugebauer <bn@bennyn.de>
2019-12-11 17:39:46 +0100

    refactor(api-client,core,cryptobox,priority-queue): Use Karma with TypeScript (#2647)

--- karma.conf.ts
+++ karma.conf.ts
@@ -16,13 +16,13 @@
  * along with this program. If not, see http://www.gnu.org/licenses/.
  *
  */
-
+import {Config} from 'karma';
 const pkg = require('./package.json');
 
 const dist = 'dist/';
 const projectName = pkg.name.replace('@wireapp/', '');
 
-module.exports = function(config) {
+module.exports = (config: Config): void => {
   config.set({
     autoWatch: false,
     basePath: '',
@@ -47,8 +47,5 @@ module.exports = function(config) {
     },
     reporters: ['jasmine-diff', 'spec'],
     singleRun: true,
-    specReporter: {
-      showSpecTiming: true,
-    },
   });
 };
