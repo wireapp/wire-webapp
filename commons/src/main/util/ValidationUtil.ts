c5faa2cc88afbda751e93c599e542be9dc2e0e24
Michael Koppen <Yserz@users.noreply.github.com>
2020-01-23 12:07:01 +0100

    feat(commons): Export email pattern (#2780)

--- commons/src/main/util/ValidationUtil.ts
+++ commons/src/main/util/ValidationUtil.ts
@@ -18,6 +18,9 @@
  */
 
 export const PATTERN = {
+  EMAIL:
+    // prettier-ignore
+    '\(\(\[\^<>\(\)\[\\\]\\\\\.,;:\\s@"\]\+\(\\\.\[\^<>\(\)\[\\\]\\\\\.,;:\\s@"\]\+\)\*\)\|\("\.\+"\)\)@\(\(\\\[\[0-9\]\{1,3\}\\\.\[0-9\]\{1,3\}\\\.\[0-9\]\{1,3\}\\\.\[0-9\]\{1,3\}\\\]\)\|\(\(\[a-zA-Z\\-0-9\]\+\\\.\)\+\[a-zA-Z\]\{2,\}\)\)',
   UUID_V4: '[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}',
 };
 
@@ -27,7 +30,7 @@ export function isUUIDv4(candidate: string): boolean {
 }
 
 export function isValidEmail(email: string): boolean {
-  const regExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const regExp = new RegExp(`^${PATTERN.EMAIL}$`, 'i');
   return regExp.test(email);
 }
 
