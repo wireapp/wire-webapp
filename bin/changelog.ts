8dd01fe012420918cda488d94619944e02265222
Benny Neugebauer <bn@bennyn.de>
2020-01-27 10:39:49 +0100

    refactor: Convert changelog script to TypeScript (#8104)

--- bin/changelog.ts
+++ bin/changelog.ts
@@ -1,16 +1,19 @@
-const Changelog = require('generate-changelog');
-const fs = require('fs');
-const pkg = require('./package');
+import fs from 'fs';
+import Changelog from 'generate-changelog';
+import * as pkg from '../package.json';
+
 const simpleGit = require('simple-git')();
 
-const options = {'--list': null};
-simpleGit.pull().tags(options, async (error, tags) => {
+const options: {} = {'--list': null};
+simpleGit.pull().tags(options, async (error: Error, tags: {
+  all: string[]
+}) => {
   const productionTags = tags.all.filter(tag => tag.includes('-production.'));
   const latestProductionTag = productionTags.sort().reverse()[0];
   const changelog = await Changelog.generate({
-    exclude: 'chore,docs,refactor,style,test',
+    exclude: ['chore', 'docs', 'refactor', 'style', 'test'],
     repoUrl: pkg.repository.url.replace('.git', ''),
     tag: `${latestProductionTag}...master`,
   });
-  fs.writeFileSync('./CHANGELOG.md', changelog);
+  fs.writeFileSync('../CHANGELOG.md', changelog);
 });
