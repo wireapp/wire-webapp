028ab56b965e4f193c04368f89cbbbf2247628c3
Benny Neugebauer <bn@bennyn.de>
2020-02-12 14:04:44 +0100

    runfix: Use fallback date for devices (WEBAPP-6678)

--- src/script/view_model/ModalsViewModel.js
+++ src/script/view_model/ModalsViewModel.js
@@ -161,7 +161,7 @@ export class ModalsViewModel {
         content.messageText = t('modalAccountNewDevicesMessage');
         const deviceList = data
           .map(device => {
-            const deviceTime = formatLocale(device.time, 'PP, p');
+            const deviceTime = formatLocale(device.time || new Date(), 'PP, p');
             const deviceModel = `${t('modalAccountNewDevicesFrom')} ${device.model}`;
             return `<div>${deviceTime} - UTC</div><div>${deviceModel}</div>`;
           })
