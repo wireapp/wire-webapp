45f38104a5f42ab7c61785ccd48c59217615a882
Benny Neugebauer <bn@bennyn.de>
2020-03-06 18:25:23 +0100

    fix: Make Docker build upload work on Windows

--- bin/push_docker.js
+++ bin/push_docker.js
@@ -59,14 +59,18 @@ const repository = `${dockerRegistryDomain}/wire/webapp${suffix}`;
 const dockerImageTag = `${repository}:${buildCounter}-${pkg.version}-${commitShortSha}-${configVersion}${stage}`;
 const dockerImageStageTag = stageParam ? `${repository}:${stageParam}` : '';
 
-child.execSync(
-  `echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin ${dockerRegistryDomain}`,
-  {stdio: 'inherit'},
-);
-child.execSync(
-  `docker build . -t ${dockerImageTag} ${dockerImageStageTag ? `-t ${dockerImageStageTag}` : ''} &&
-  docker push ${dockerImageTag} &&
-  ${dockerImageStageTag ? `docker push ${dockerImageStageTag} &&` : ''}
-  docker logout ${dockerRegistryDomain}`,
-  {stdio: 'inherit'},
-);
+const dockerCommands = [
+  `docker login --username ${process.env.DOCKER_USERNAME} --password ${process.env.DOCKER_PASSWORD} ${dockerRegistryDomain}`,
+  `docker build . -t ${dockerImageTag} ${dockerImageStageTag ? `-t ${dockerImageStageTag}` : ''}`,
+  `docker push ${dockerImageTag}`,
+];
+
+if (dockerImageStageTag) {
+  dockerCommands.push(`docker push ${dockerImageStageTag}`);
+}
+
+dockerCommands.push(`docker logout ${dockerRegistryDomain}`);
+
+dockerCommands.forEach(command => {
+  child.execSync(command, {stdio: 'inherit'});
+});
