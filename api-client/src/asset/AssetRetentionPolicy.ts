856704215e4474bfa69b2afac493746964adaa72
Benny Neugebauer <bn@bennyn.de>
2020-04-17 17:33:45 +0200

    feat(api-client): Expose expiring asset policies (#2965)

--- api-client/src/asset/AssetRetentionPolicy.ts
+++ api-client/src/asset/AssetRetentionPolicy.ts
@@ -17,8 +17,14 @@
  *
  */
 
+/**
+ * Policies can be found at:
+ * https://github.com/wireapp/wire-server/blob/dc3e9a8af5250c0d045e96a31aa23c255b4e01a3/libs/cargohold-types/src/CargoHold/Types/V3.hs#L156-L177
+ */
 export enum AssetRetentionPolicy {
   ETERNAL = 'eternal',
+  ETERNAL_INFREQUENT_ACCESS = 'eternal-infrequent_access',
+  EXPIRING = 'expiring',
   PERSISTENT = 'persistent',
   VOLATILE = 'volatile',
 }
