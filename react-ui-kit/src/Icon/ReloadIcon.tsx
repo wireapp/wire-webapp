8adf53f0afe7cbed67bf1972c3503c8089f2e394
Michael Koppen <Yserz@users.noreply.github.com>
2020-04-22 15:59:12 +0200

    feat(react-ui-kit): Add ReloadIcon (#2975)

---
+++ react-ui-kit/src/Icon/ReloadIcon.tsx
@@ -0,0 +1,28 @@
+/*
+ * Wire
+ * Copyright (C) 2020 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+/** @jsx jsx */
+import {jsx} from '@emotion/core';
+import {SVGIcon, SVGIconProps} from './SVGIcon';
+
+export const ReloadIcon = (props: SVGIconProps) => (
+  <SVGIcon realWidth={16} realHeight={16} {...props}>
+    <path d="M13.7 2.3a8.3 8.3 0 00-11.4 0A8 8 0 1015.7 10h-2a6.1 6.1 0 01-2.2 2.9 6 6 0 11.8-9.1L9 7h7V0l-2.3 2.3z" />
+  </SVGIcon>
+);
