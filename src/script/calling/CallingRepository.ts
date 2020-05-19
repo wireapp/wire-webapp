69920d18894342a3c6870ac745faab8eed7a2b2e
Dusan Stevanovic <dule@wire.com>
2020-05-19 10:43:42 +0200

    hack to get more than 3 participants with working audio in a call

--- src/script/calling/CallingRepository.ts
+++ src/script/calling/CallingRepository.ts
@@ -850,6 +850,9 @@ export class CallingRepository {
     const [stream] = streams;
     if (stream.getAudioTracks().length > 0) {
       participant.audioStream(stream);
+      const audio = new Audio();
+      audio.srcObject = stream;
+      audio.play();
     }
     if (stream.getVideoTracks().length > 0) {
       participant.videoStream(stream);
