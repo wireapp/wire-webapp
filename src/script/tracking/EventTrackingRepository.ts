f185ea4b03d5617ea533f37306e73d6bfe6060f2
Panayiotis Lipiridis <lipiridis@gmail.com>
2020-07-15 15:53:38 +0300

    test

--- src/script/tracking/EventTrackingRepository.ts
+++ src/script/tracking/EventTrackingRepository.ts
@@ -36,29 +36,36 @@ import {SuperProperty} from './SuperProperty';
 
 const Countly = require('countly-sdk-web');
 
+Countly.q = Countly.q || [];
+
 Countly.app_key = '9927e629a9302b8c800fc60055723362b0608f19';
 Countly.url = 'https://wire.count.ly/';
 Countly.debug = true;
 
+//Asynchronous
+Countly.q.push([
+  'add_event',
+  {
+    count: 1,
+    key: 'testing',
+    segmentation: {
+      action: 'text',
+      app: 'web',
+      app_version: '1.0',
+      conversation_type: 'one_to_one',
+      is_allow_guests: 'false',
+      is_ephemeral: 'false',
+      is_global_ephemeral: 'false',
+    },
+  },
+]);
+
+//Synchronous
 Countly.add_event({
   count: 1,
-  key: 'webapp',
+  key: 'testing',
   segmentation: {
     action: 'text',
-    app: 'web',
-    app_version: '1.0',
-    conversation_type: 'one_to_one',
-    is_allow_guests: 'false',
-    is_ephemeral: 'false',
-    is_global_ephemeral: 'false',
-  },
-});
-
-Countly.add_event({
-  count: 1,
-  key: 'webapp',
-  segmentation: {
-    action: 'like',
     app: 'web',
     app_version: '1.0',
     conversation_type: 'one_to_one',
