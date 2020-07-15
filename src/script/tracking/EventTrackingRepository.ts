0c9c69a7d93b043670a9cfcf9ba22278c4052d89
Panayiotis Lipiridis <lipiridis@gmail.com>
2020-07-15 15:44:47 +0300

    More

--- src/script/tracking/EventTrackingRepository.ts
+++ src/script/tracking/EventTrackingRepository.ts
@@ -36,17 +36,29 @@ import {SuperProperty} from './SuperProperty';
 
 const Countly = require('countly-sdk-web');
 
-Countly.q = Countly.q || [];
-
 Countly.app_key = '9927e629a9302b8c800fc60055723362b0608f19';
 Countly.url = 'https://wire.count.ly/';
 Countly.debug = true;
 
 Countly.add_event({
   count: 1,
-  key: 'contributed',
+  key: 'webapp',
   segmentation: {
     action: 'text',
+    app: 'web',
+    app_version: '1.0',
+    conversation_type: 'one_to_one',
+    is_allow_guests: 'false',
+    is_ephemeral: 'false',
+    is_global_ephemeral: 'false',
+  },
+});
+
+Countly.add_event({
+  count: 1,
+  key: 'webapp',
+  segmentation: {
+    action: 'like',
     app: 'web',
     app_version: '1.0',
     conversation_type: 'one_to_one',
