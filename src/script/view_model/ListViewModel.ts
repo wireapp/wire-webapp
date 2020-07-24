0de82eaa27edd29a1c48e20e7d687e4e95d7cc77
AndyLnd <andy@wire.com>
2020-07-24 18:13:16 +0200

    runfix: Logical error in determining supported calls (#9272)

--- src/script/view_model/ListViewModel.ts
+++ src/script/view_model/ListViewModel.ts
@@ -222,18 +222,19 @@ export class ListViewModel {
 
   answerCall = (conversationEntity: Conversation): void => {
     const call = this.callingRepository.findCall(conversationEntity.id);
-    if (call.conversationType === CONV_TYPE.CONFERENCE && this.callingRepository.supportsConferenceCalling) {
-      if (call) {
-        const callType = call.getSelfParticipant().sharesCamera() ? call.initialType : CALL_TYPE.NORMAL;
-        this.callingRepository.answerCall(call, callType);
-      }
-    } else {
+    if (!call) {
+      return;
+    }
+    if (call.conversationType === CONV_TYPE.CONFERENCE && !this.callingRepository.supportsConferenceCalling) {
       amplify.publish(WebAppEvents.WARNING.MODAL, ModalsViewModel.TYPE.ACKNOWLEDGE, {
         text: {
           message: `${t('modalConferenceCallNotSupportedMessage')} ${t('modalConferenceCallNotSupportedJoinMessage')}`,
           title: t('modalConferenceCallNotSupportedHeadline'),
         },
       });
+    } else {
+      const callType = call.getSelfParticipant().sharesCamera() ? call.initialType : CALL_TYPE.NORMAL;
+      this.callingRepository.answerCall(call, callType);
     }
   };
 
