613ff1fd7b3867804a67a0a263d7385b6311fa4d
Florian Imdahl <florian@wire.com>
2020-08-26 18:00:48 +0200

    feat: Activate availability in TeamInfo for Wrapper >= 3.20

--- src/script/team/TeamRepository.ts
+++ src/script/team/TeamRepository.ts
@@ -277,14 +277,16 @@ export class TeamRepository {
 
       const accountInfo: AccountInfo = {
         accentID: this.selfUser().accent_id(),
-        // TODO: Deactivated until wrapper supports this
-        // availability: this.selfUser().availability(),
         name: this.teamName(),
         picture: imageDataUrl?.toString(),
         teamID: this.team() ? this.team().id : undefined,
         teamRole: this.selfUser().teamRole(),
         userID: this.selfUser().id,
       };
+
+      if (Environment.version(true).startsWith('3.2')) {
+        accountInfo.availability = this.selfUser().availability();
+      }
 
       this.logger.info('Publishing account info', accountInfo);
       amplify.publish(WebAppEvents.TEAM.INFO, accountInfo);
