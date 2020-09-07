deba93243bc206b0893bfe6bc0a1ed6a012235e6
Amir Ghezelbash <63250054+thisisamir98@users.noreply.github.com>
2020-09-07 16:36:34 +0430

    runfix: Update events data names (#9599)

--- src/script/tracking/UserData.ts
+++ src/script/tracking/UserData.ts
@@ -18,8 +18,8 @@
  */
 
 export const UserData = {
-  CONTACTS: 'user.contacts',
-  IS_TEAM: 'team.is_team',
-  TEAM_SIZE: 'team.team_size',
-  USER_TYPE: 'team.user_type',
+  CONTACTS: 'user_contacts',
+  IS_TEAM: 'team_is_team',
+  TEAM_SIZE: 'team_team_size',
+  USER_TYPE: 'team_user_type',
 };
