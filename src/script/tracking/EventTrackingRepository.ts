6d5e4e787563681e9ed2a3577e4dd338918ade3f
Amir Ghezelbash <bardiarastin98@gmail.com>
2020-09-28 13:32:16 +0330

    feat: Countly add team_id and round team size

--- src/script/tracking/EventTrackingRepository.ts
+++ src/script/tracking/EventTrackingRepository.ts
@@ -180,7 +180,8 @@ export class EventTrackingRepository {
     if (this.isProductReportingActivated === true) {
       Countly.userData.set(UserData.IS_TEAM, this.userRepository.isTeam());
       Countly.userData.set(UserData.CONTACTS, roundLogarithmic(this.userRepository.numberOfContacts(), 6));
-      Countly.userData.set(UserData.TEAM_SIZE, this.userRepository.teamMembers().length);
+      Countly.userData.set(UserData.TEAM_SIZE, roundLogarithmic(this.userRepository.teamMembers().length, 6));
+      Countly.userData.set(UserData.TEAM_ID, this.userRepository.self().teamId);
       Countly.userData.set(UserData.USER_TYPE, this.getUserType());
       Countly.userData.save();
 
