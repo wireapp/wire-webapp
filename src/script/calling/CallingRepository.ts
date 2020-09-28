cb4cd47b229b35ff0e0ff5ea4250a2b2ac4f4baa
Florian Imdahl <florian@wire.com>
2020-09-28 15:31:19 +0200

    use WebAppEvents.CONVERSATION.VERIFICATION_STATE_CHANGED

--- src/script/calling/CallingRepository.ts
+++ src/script/calling/CallingRepository.ts
@@ -68,7 +68,6 @@ import {EventName} from '../tracking/EventName';
 import {Segmentation} from '../tracking/Segmentation';
 import * as trackingHelpers from '../tracking/Helpers';
 import {QUERY_KEY} from '../auth/route';
-import {ClientEntity} from '../client/ClientEntity';
 import type {MediaStreamHandler} from '../media/MediaStreamHandler';
 import type {User} from '../entity/User';
 import type {ServerTimeHandler} from '../time/serverTimeHandler';
@@ -447,7 +446,7 @@ export class CallingRepository {
     amplify.subscribe(WebAppEvents.CALL.STATE.TOGGLE, this.toggleState.bind(this)); // This event needs to be kept, it is sent by the wrapper
     amplify.subscribe(WebAppEvents.PROPERTIES.UPDATE.CALL.ENABLE_VBR_ENCODING, this.toggleCbrEncoding.bind(this));
     amplify.subscribe(WebAppEvents.PROPERTIES.UPDATE.CALL.ENABLE_SFT_CALLING, this.toggleSftCalling.bind(this));
-    amplify.subscribe(WebAppEvents.CLIENT.VERIFICATION_STATE_CHANGED, this.onClientVerificationChanged);
+    amplify.subscribe(WebAppEvents.CONVERSATION.VERIFICATION_STATE_CHANGED, this.onClientVerificationChanged);
     amplify.subscribe(WebAppEvents.PROPERTIES.UPDATED, ({settings}: WebappProperties) => {
       this.toggleCbrEncoding(settings.call.enable_vbr_encoding);
       this.toggleSftCalling(settings.call.enable_sft_calling);
@@ -455,27 +454,34 @@ export class CallingRepository {
   }
 
   /**
-   * Leave call when a participant is not verfied anymore
+   * Leave call when a participant is not verified anymore
    */
-  private readonly onClientVerificationChanged = (userId: string, clientEntity: ClientEntity, isVerified: boolean) => {
+  private readonly onClientVerificationChanged = async (userIds: string, isVerified: boolean) => {
     const activeCall = this.joinedCall();
-    if (!activeCall) {
+    if (!activeCall || isVerified) {
       return;
     }
 
-    const participant = activeCall.getParticipant(userId, clientEntity.id);
+    for (const userId of userIds) {
+      const clients = this.userRepository.findUserById(userId).devices();
 
-    if (!participant || isVerified) {
-      return;
+      for (const {id: clientId} of clients) {
+        const participant = activeCall.getParticipant(userId, clientId);
+
+        if (participant) {
+          this.leaveCall(activeCall.conversationId);
+          amplify.publish(WebAppEvents.WARNING.MODAL, ModalsViewModel.TYPE.ACKNOWLEDGE, {
+            action: {
+              title: t('callDegradationAction'),
+            },
+            text: {
+              message: t('callDegradationDescription', participant.user.name()),
+              title: t('callDegradationTitle'),
+            },
+          });
+        }
+      }
     }
-
-    this.leaveCall(activeCall.conversationId);
-    amplify.publish(WebAppEvents.WARNING.MODAL, ModalsViewModel.TYPE.ACKNOWLEDGE, {
-      text: {
-        message: t('callDegradationDescription', participant.user.name()),
-        title: t('callDegradationTitle'),
-      },
-    });
   };
 
   //##############################################################################
