3d29e13139809ff4f0c760469e32eefeb3e86de1
Michael Koppen <michael.koppen@googlemail.com>
2020-10-14 12:07:00 +0200

    test: AccentColorPicker default selection

--- src/script/main/globals.ts
+++ src/script/main/globals.ts
@@ -27,8 +27,7 @@ import type {NotificationService} from '../event/NotificationService';
 
 import '../Config';
 
-import 'Components/accentColorPicker';
-import 'Components/AccentColorPickerComponent';
+import 'Components/AccentColorPicker';
 import 'Components/asset/assetHeader';
 import 'Components/asset/controls/audioSeekBar';
 import 'Components/asset/controls/mediaButton';
