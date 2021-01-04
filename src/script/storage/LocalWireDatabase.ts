0b5f7764d1b3e6f2f98c93c517ba46f498c73c61
Benny Neugebauer <bn@bennyn.de>
2021-01-04 19:19:48 +0100

    Add ConversationRecord

--- src/script/storage/LocalWireDatabase.ts
+++ src/script/storage/LocalWireDatabase.ts
@@ -2,9 +2,39 @@ import Dexie from 'dexie';
 import {StorageSchemata} from './StorageSchemata';
 import {getLogger, Logger} from 'Util/Logger';
 import {ClientRecord} from './ClientRecord';
+import {DefaultConversationRoleName} from '@wireapp/api-client/src/conversation';
 
-interface AmplifyRecord {
+export interface AmplifyRecord {
   value: string;
+}
+
+export interface ConversationRecord {
+  accessModes: string[];
+  accessRole: string;
+  archived_state: boolean;
+  archived_timestamp: number;
+  cleared_timestamp: number;
+  creator: string;
+  ephemeral_timer?: any;
+  global_message_timer?: any;
+  id: string;
+  is_guest: boolean;
+  is_managed: boolean;
+  last_event_timestamp: number;
+  last_read_timestamp: number;
+  last_server_timestamp: number;
+  legal_hold_status: number;
+  message_timer?: any;
+  muted_state: boolean;
+  muted_timestamp: number;
+  name: string;
+  others: string[];
+  receipt_mode: number;
+  roles: {[user_id: string]: DefaultConversationRoleName | string};
+  status: number;
+  team_id: string;
+  type: number;
+  verification_state: number;
 }
 
 export class LocalWireDatabase extends Dexie {
@@ -14,6 +44,9 @@ export class LocalWireDatabase extends Dexie {
   // Tables
   amplify: Dexie.Table<AmplifyRecord, string>;
   clients: Dexie.Table<ClientRecord, string>;
+  /** @deprecated */
+  conversation_events: Dexie.Table<{}, string>;
+  conversations: Dexie.Table<ConversationRecord, string>;
 
   private readonly logger: Logger;
 
@@ -31,7 +64,9 @@ export class LocalWireDatabase extends Dexie {
       }
     });
 
-    this.amplify = this.table('amplify');
-    this.clients = this.table('clients');
+    this.amplify = this.table(StorageSchemata.OBJECT_STORE.AMPLIFY);
+    this.clients = this.table(StorageSchemata.OBJECT_STORE.CLIENTS);
+    this.conversation_events = this.table(StorageSchemata.OBJECT_STORE.CONVERSATION_EVENTS);
+    this.conversations = this.table(StorageSchemata.OBJECT_STORE.CONVERSATIONS);
   }
 }
