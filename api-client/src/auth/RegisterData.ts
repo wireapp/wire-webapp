b91f2643f31ae8f58e2b5d7cff5db7b2c1c9d012
Michael Koppen <Yserz@users.noreply.github.com>
2021-01-20 18:02:01 +0100

    fix(api-client): Add team_code attribute to RegisterData type (#3464)

--- api-client/src/auth/RegisterData.ts
+++ api-client/src/auth/RegisterData.ts
@@ -21,13 +21,20 @@ import type {TeamData} from '../team/team/TeamData';
 import type {User} from '../user/';
 
 export type RegisterData = Pick<User, 'accent_id' | 'assets' | 'email' | 'name'> & {
+  /** Email activation code */
   email_code?: string;
-  expires_in?: number; // used with "temporary guests"
+  /** Life time of a new temporary guest account */
+  expires_in?: number;
+  /** Regular invitation code. Mutually exclusive with `team_code` */
   invitation_code?: string;
   label?: string;
   locale?: string;
   password?: string;
   phone?: string;
+  /** Phone activation code */
   phone_code?: string;
+  /** Data for new team account creation */
   team?: TeamData;
+  /** Team invitation code for joining an existing team. Mutually exclusive with `invitation_code` */
+  team_code?: string;
 };
