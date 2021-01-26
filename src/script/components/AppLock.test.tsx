553ca023b794041e9d4b9b9f6e9995630de48fe0
Panayiotis Lipiridis <lipiridis@gmail.com>
2021-01-26 10:18:23 +0200

    Comments

--- src/script/components/AppLock.test.tsx
+++ src/script/components/AppLock.test.tsx
@@ -44,169 +44,6 @@ type WriteableConfig = Writable<Configuration>;
 
 const writeableConfig: WriteableConfig = Config.getConfig();
 
-// describe('AppLockViewModel', () => {
-//   const getAppLock = () =>
-//     new AppLockViewModel(
-//       ({
-//         clientService: ({
-//           deleteClient: (clientId: string, password: string) => Promise.resolve(),
-//         } as unknown) as ClientService,
-//         currentClient: ko.observable({id: 'clientId'}),
-//       } as unknown) as ClientRepository,
-//       ko.observable(({id: 'userID'} as unknown) as User),
-//     );
-//   const originalFeature = writeableConfig.FEATURE;
-//   beforeEach(() => {
-//     writeableConfig.FEATURE = {...writeableConfig.FEATURE};
-//     document.body.innerHTML = '<div id="app"></div><div id="applock"></div>';
-//   });
-
-//   afterEach(() => {
-//     document.body.innerHTML = '';
-//     writeableConfig.FEATURE = originalFeature;
-//   });
-
-//   describe('constructor', () => {
-//     it('does not shows up if no valid timeout is set', () => {
-//       const appLock = getAppLock();
-
-//       expect(appLock.state()).toBe(APPLOCK_STATE.NONE);
-//     });
-
-//     it('shows the locked modal on start if timeout is set as flag and a code is stored', () => {
-//       writeableConfig.FEATURE = {...writeableConfig.FEATURE, APPLOCK_UNFOCUS_TIMEOUT: 10};
-//       spyOn(window.localStorage, 'getItem').and.returnValue('savedCode');
-//       const appLock = getAppLock();
-
-//       expect(appLock.state()).toBe(APPLOCK_STATE.LOCKED);
-//     });
-
-//     it('shows the locked modal on start if timeout is set as query parameter and a code is stored', () => {
-//       window.history.pushState({}, '', '?applock_unfocus_timeout=10');
-//       spyOn(window.localStorage, 'getItem').and.returnValue('savedCode');
-//       const appLock = getAppLock();
-
-//       expect(appLock.state()).toBe(APPLOCK_STATE.LOCKED);
-//       window.history.pushState({}, '', '');
-//     });
-//   });
-
-//   describe('unlock', () => {
-//     it('stores the passphrase, respects the timeout and unlocks', async () => {
-//       jest.useFakeTimers();
-//       writeableConfig.FEATURE = {...writeableConfig.FEATURE, APPLOCK_UNFOCUS_TIMEOUT: 10};
-//       let storedCode: string;
-//       const passphrase = 'abcABC123!';
-//       jest.spyOn(window.localStorage, 'setItem').mockImplementation((_, code) => {
-//         storedCode = code;
-//       });
-//       jest.spyOn(window.localStorage, 'getItem').mockImplementation(() => {
-//         return storedCode;
-//       });
-//       const appLock = getAppLock();
-//       appLock.isVisible.subscribe(isVisible => {
-//         if (!isVisible) {
-//           appLock.onClosed();
-//         }
-//       });
-
-//       expect(appLock.state()).toBe(APPLOCK_STATE.SETUP);
-//       appLock.setupPassphrase(passphrase);
-//       await appLock.onSetCode();
-
-//       expect(appLock.state()).toBe(APPLOCK_STATE.NONE);
-//       expect(storedCode).toBeDefined();
-//       window.dispatchEvent(new Event('blur'));
-//       jest.advanceTimersByTime(5000);
-
-//       expect(appLock.state()).toBe(APPLOCK_STATE.NONE);
-//       jest.advanceTimersByTime(6000);
-
-//       expect(appLock.state()).toBe(APPLOCK_STATE.LOCKED);
-//       document.body.innerHTML += `<form id="unlock"><input id="pass" value="${passphrase}"/></form>`;
-//       const unlockForm = ([document.querySelector('#pass')] as unknown) as HTMLFormElement;
-//       await appLock.onUnlock(unlockForm);
-
-//       expect(appLock.state()).toBe(APPLOCK_STATE.NONE);
-//       jest.useRealTimers();
-//     });
-//   });
-// });
-
-// class AccentColorPickerPage extends TestPage<AccentColorPickerProps> {
-//   constructor(props?: AccentColorPickerProps) {
-//     super(AccentColorPicker, props);
-//   }
-
-//   getAccentColors = () => this.get('input[data-uie-name="do-set-accent-color"]');
-//   getAccentColorInput = (accentColorId: number) =>
-//     this.get(`input[data-uie-name="do-set-accent-color"][data-uie-value=${accentColorId}]`);
-//   changeAccentColorInput = (accentColorId: number) =>
-//     this.changeValue(this.getAccentColorInput(accentColorId), {checked: true});
-// }
-
-// describe('AccentColorPicker', () => {
-//   it('shows expected accent colors', async () => {
-//     const colorPicker = new AccentColorPickerPage({
-//       doSetAccentColor: () => {},
-//       user: {
-//         accent_id: () => AccentColor.BRIGHT_ORANGE.id,
-//       } as User,
-//     });
-
-//     expect(colorPicker.getAccentColors().exists()).toBe(true);
-//     expect(colorPicker.getAccentColors().length).toBe(AccentColor.ACCENT_COLORS.length);
-//     AccentColor.ACCENT_COLORS.forEach(accentColor =>
-//       expect(colorPicker.getAccentColorInput(accentColor.id).exists()).toBe(true),
-//     );
-//   });
-
-//   it('selects users current accent color', async () => {
-//     const selectedAccentColorId = AccentColor.BRIGHT_ORANGE.id;
-//     const colorPicker = new AccentColorPickerPage({
-//       doSetAccentColor: () => {},
-//       user: {
-//         accent_id: () => selectedAccentColorId,
-//       } as User,
-//     });
-
-//     expect(colorPicker.getAccentColorInput(selectedAccentColorId).exists()).toBe(true);
-//     expect(colorPicker.getAccentColorInput(selectedAccentColorId).props().checked).toBe(true);
-//   });
-
-//   it('updates users accent color on click', async () => {
-//     const colorPicker = new AccentColorPickerPage({
-//       doSetAccentColor: jasmine.createSpy(),
-//       user: {
-//         accent_id: () => 0,
-//       } as User,
-//     });
-
-//     AccentColor.ACCENT_COLORS.forEach(accentColor => {
-//       colorPicker.changeAccentColorInput(accentColor.id);
-//       expect(colorPicker.getProps().doSetAccentColor).toHaveBeenCalledWith(accentColor.id);
-//     });
-//   });
-
-//   it('selects color on remote user accent color update', async () => {
-//     const colorPicker = new AccentColorPickerPage({
-//       doSetAccentColor: jasmine.createSpy(),
-//       user: {
-//         accent_id: () => 0,
-//       } as User,
-//     });
-
-//     AccentColor.ACCENT_COLORS.forEach(accentColor => {
-//       colorPicker['driver'].setProps({
-//         user: {
-//           accent_id: () => accentColor.id,
-//         },
-//       });
-//       expect(colorPicker.getAccentColorInput(accentColor.id).props().checked).toBe(true);
-//     });
-//   });
-// });
-
 class AppLockPage extends TestPage<AppLockProps> {
   constructor(props?: AppLockProps) {
     super(AppLock.AppLock, props);
@@ -240,8 +77,8 @@ describe('AppLock', () => {
     it('does not shows up if no valid timeout is set', () => {
       writeableConfig.FEATURE = {
         ...writeableConfig.FEATURE,
-        APPLOCK_UNFOCUS_TIMEOUT: undefined,
         APPLOCK_SCHEDULED_TIMEOUT: undefined,
+        APPLOCK_UNFOCUS_TIMEOUT: undefined,
       };
       initAppLock();
       const appLockModal = document.querySelector('[data-uie-name=applock-modal]') as HTMLDivElement;
@@ -257,8 +94,8 @@ describe('AppLock', () => {
     it('shows the locked modal on start if timeout is set as query parameter and a code is stored', () => {
       writeableConfig.FEATURE = {
         ...writeableConfig.FEATURE,
-        APPLOCK_UNFOCUS_TIMEOUT: undefined,
         APPLOCK_SCHEDULED_TIMEOUT: undefined,
+        APPLOCK_UNFOCUS_TIMEOUT: undefined,
       };
       window.history.pushState({}, '', '?applock_unfocus_timeout=10');
       spyOn(window.localStorage, 'getItem').and.returnValue('savedCode');
@@ -294,33 +131,6 @@ describe('AppLock', () => {
       expect(appLockModal.style.display).toBe('none');
       jest.advanceTimersByTime(6000);
       expect(appLockModal.style.display).toBe('flex');
-
-      //       const appLock = getAppLock();
-      //       appLock.isVisible.subscribe(isVisible => {
-      //         if (!isVisible) {
-      //           appLock.onClosed();
-      //         }
-      //       });
-
-      //       expect(appLock.state()).toBe(APPLOCK_STATE.SETUP);
-      //       appLock.setupPassphrase(passphrase);
-      //       await appLock.onSetCode();
-
-      //       expect(appLock.state()).toBe(APPLOCK_STATE.NONE);
-      //       expect(storedCode).toBeDefined();
-      //       window.dispatchEvent(new Event('blur'));
-      //       jest.advanceTimersByTime(5000);
-
-      //       expect(appLock.state()).toBe(APPLOCK_STATE.NONE);
-      //       jest.advanceTimersByTime(6000);
-
-      //       expect(appLock.state()).toBe(APPLOCK_STATE.LOCKED);
-      //       document.body.innerHTML += `<form id="unlock"><input id="pass" value="${passphrase}"/></form>`;
-      //       const unlockForm = ([document.querySelector('#pass')] as unknown) as HTMLFormElement;
-      //       await appLock.onUnlock(unlockForm);
-
-      //       expect(appLock.state()).toBe(APPLOCK_STATE.NONE);
-      //       jest.useRealTimers();
     });
   });
 });
