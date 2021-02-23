13a8bf46f95ec2c6a6348b1c8a73158523e00fe5
AndyLnd <andy@wire.com>
2021-02-23 13:43:13 +0100

    fix: Handle ko oobservables in video grid (#10599)

--- src/script/components/calling/GroupVideoGrid.tsx
+++ src/script/components/calling/GroupVideoGrid.tsx
@@ -17,16 +17,17 @@
  *
  */
 
+import ko from 'knockout';
 import React, {useState, useEffect, CSSProperties} from 'react';
 import {css} from '@emotion/core';
 
-import {registerReactComponent} from 'Util/ComponentUtil';
+import {registerReactComponent, useKoSubscribable} from 'Util/ComponentUtil';
 import type {Grid} from '../../calling/videoGridHandler';
-import type {Participant} from '../../calling/Participant';
 import Video from './Video';
+import type {Participant} from '../../calling/Participant';
 import {t} from '../../util/LocalizerUtil';
-import ParticipantMicOnIcon from './ParticipantMicOnIcon';
 import NamedIcon from 'Components/NamedIcon';
+import GroupVideoGridTile from './GroupVideoGridTile';
 
 export interface GroupVideoGripProps {
   grid: Grid;
@@ -58,6 +59,9 @@ const GroupVideoGrid: React.FunctionComponent<GroupVideoGripProps> = ({
 }) => {
   const [rowsAndColumns, setRowsAndColumns] = useState<RowsAndColumns>(calculateRowsAndColumns(grid.grid.length));
   const videoParticipants: Participant[] = grid.grid.filter(participant => !!participant);
+  const thumbnailHasActiveVideo = useKoSubscribable(grid.thumbnail?.hasActiveVideo || ko.observable());
+  const thumbnailSharesScreen = useKoSubscribable(grid.thumbnail?.sharesScreen || ko.observable());
+  const thumbnailVideoStream = useKoSubscribable(grid.thumbnail?.videoStream || ko.observable());
 
   const doubleClickedOnVideo = (userId: string, clientId: string) => {
     if (maximizedParticipant !== null) {
@@ -111,84 +115,18 @@ const GroupVideoGrid: React.FunctionComponent<GroupVideoGripProps> = ({
           </div>
         )}
         {participants.map(participant => (
-          <div
-            key={participant.clientId + participant.user.id}
-            className="group-video-grid__element"
-            onDoubleClick={() => doubleClickedOnVideo(participant.user.id, participant.clientId)}
-            data-uie-name="item-grid"
-            data-user-id={participant.user.id}
-            css={{position: 'relative'}}
-          >
-            <Video
-              className="group-video-grid__element-video"
-              autoPlay
-              playsInline
-              srcObject={participant.videoStream()}
-              css={{
-                objectFit: !!maximizedParticipant || participant.sharesScreen() ? 'contain' : 'cover',
-                transform:
-                  participant === selfParticipant && participant.sharesCamera() ? 'rotateY(180deg)' : 'initial',
-              }}
-            />
-            <div
-              css={{
-                bottom: 0,
-                boxShadow:
-                  participants.length > 2 && participant.isActivelySpeaking()
-                    ? `inset 0px 0px 0px 2px ${selfParticipant.user.accent_color()}`
-                    : `inset 0px 0px 0px 0px ${selfParticipant.user.accent_color()}`,
-                left: 0,
-                position: 'absolute',
-                right: 0,
-                top: 0,
-                transition: 'box-shadow 0.3s ease-in-out',
-              }}
-            />
-            {!minimized && (
-              <div className="group-video-grid__element__label">
-                {participant.isMuted() ? (
-                  <span className="group-video-grid__element__label__icon">
-                    <NamedIcon name="mic-off-icon" data-uie-name="mic-icon-off" />
-                  </span>
-                ) : (
-                  <ParticipantMicOnIcon
-                    isActive={participant.isActivelySpeaking()}
-                    className="group-video-grid__element__label__icon"
-                  />
-                )}
-                <span
-                  data-uie-name={
-                    participant.isActivelySpeaking()
-                      ? 'status-active-speaking'
-                      : participant.isMuted()
-                      ? 'status-audio-off'
-                      : 'status-audio-on'
-                  }
-                  className="group-video-grid__element__label__name"
-                >
-                  {participant.user.name()}
-                </span>
-              </div>
-            )}
-            {participant.hasPausedVideo() && (
-              <div className="group-video-grid__pause-overlay">
-                <div className="background">
-                  <div className="background-image"></div>
-                  <div className="background-darken"></div>
-                </div>
-                <div
-                  className="group-video-grid__pause-overlay__label"
-                  css={{fontsize: minimized ? 11 : 14}}
-                  data-uie-name="status-video-paused"
-                >
-                  {t('videoCallPaused')}
-                </div>
-              </div>
-            )}
-          </div>
+          <GroupVideoGridTile
+            minimized={minimized}
+            participant={participant}
+            key={participant.clientId}
+            selfParticipant={selfParticipant}
+            participantCount={participants.length}
+            maximizedParticipant={maximizedParticipant}
+            onParticipantDoubleClick={doubleClickedOnVideo}
+          />
         ))}
       </div>
-      {grid.thumbnail && grid.thumbnail.videoStream() && !maximizedParticipant && (
+      {thumbnailVideoStream && !maximizedParticipant && (
         <div
           className="group-video__thumbnail"
           css={
@@ -210,10 +148,9 @@ const GroupVideoGrid: React.FunctionComponent<GroupVideoGripProps> = ({
             playsInline
             data-uie-name="self-video-thumbnail"
             css={{
-              transform:
-                grid.thumbnail.hasActiveVideo() && !grid.thumbnail.sharesScreen() ? 'rotateY(180deg)' : 'initial',
+              transform: thumbnailHasActiveVideo && !thumbnailSharesScreen ? 'rotateY(180deg)' : 'initial',
             }}
-            srcObject={grid.thumbnail.videoStream()}
+            srcObject={thumbnailVideoStream}
           />
           {muted && (
             <div className="group-video-grid__mute-overlay" data-uie-name="status-call-audio-muted">
