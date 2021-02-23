3b15d960f19dd2ed3cd07bcacd06c6a9d8d03c16
Benny Neugebauer <bn@bennyn.de>
2021-02-23 16:41:42 +0100

    feat: Show icon for users on federated backends (SQCORE-476) (#10606)

--- src/script/components/list/ParticipantItem.tsx
+++ src/script/components/list/ParticipantItem.tsx
@@ -219,6 +219,16 @@ const ParticipantItem: React.FC<ParticipantItemProps> = ({
               <NamedIcon name="guest-icon" className="guest-icon" data-uie-name="status-guest" width={14} height={16} />
             )}
 
+            {participant instanceof User && !participant.isOnSameFederatedDomain() && (
+              <NamedIcon
+                name="federation-icon"
+                className="federation-icon"
+                data-uie-name="status-federated-user"
+                width={16}
+                height={16}
+              />
+            )}
+
             {external && (
               <NamedIcon
                 name="partner-icon"
