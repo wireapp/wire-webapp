01d4fa2178095e71c3ae9c156815440bdb626bf9
Benny Neugebauer <bn@bennyn.de>
2021-02-25 12:20:06 +0100

    refactor: Migrate TopPeople to React (#10615)

--- src/script/main/globals.ts
+++ src/script/main/globals.ts
@@ -51,7 +51,7 @@ import 'Components/modal';
 import 'Components/panel/serviceDetails';
 import 'Components/panel/userDetails';
 import 'Components/ServiceList';
-import 'Components/topPeople';
+import 'Components/TopPeople';
 import 'Components/userInput';
 
 import 'Util/LocalizerUtil';
