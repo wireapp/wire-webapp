4ec8f9802e7ebcb2420ff95035f806e93d8850b2
Michael Koppen <michael.koppen@googlemail.com>
2021-03-03 10:41:38 +0100

    chore: Add status to Avatars

--- src/script/components/avatar/UserAvatar.tsx
+++ src/script/components/avatar/UserAvatar.tsx
@@ -62,6 +62,7 @@ const UserAvatar: React.FunctionComponent<UserAvatarProps> = ({
       color={participant.accent_color()}
       data-uie-name="element-avatar-user"
       data-uie-value={participant.id}
+      data-uie-status={state}
       onClick={onClick}
       title={participant.name()}
       {...props}
