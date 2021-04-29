ce99225b09882bc242d7f1d6572a13abe792a798
Amir Ghezelbash <thisisamir98@gmail.com>
2021-04-29 13:33:40 +0430

    runfix: Fix broken mute functionality

--- src/script/components/list/conversationListCallingCell.ts
+++ src/script/components/list/conversationListCallingCell.ts
@@ -315,13 +315,13 @@ ko.components.register('conversation-list-calling-cell', {
       <!-- ko if: !isDeclined() -->
         <div class="conversation-list-calling-cell-controls">
           <div class="conversation-list-calling-cell-controls-left">
-            <button class="call-ui__button" data-bind="click: () => callActions.toggleMute(call, isMuted()), css: {'call-ui__button--active': isMuted()}, attr: {'data-uie-value': !isMuted() ? 'inactive' : 'active', 'title': t('videoCallOverlayMute')}" data-uie-name="do-toggle-mute">
+            <button class="call-ui__button" data-bind="click: () => callActions.toggleMute(call, !isMuted()), css: {'call-ui__button--active': isMuted()}, attr: {'data-uie-value': !isMuted() ? 'inactive' : 'active', 'title': t('videoCallOverlayMute')}" data-uie-name="do-toggle-mute">
               <mic-off-icon class="small-icon"></mic-off-icon>
             </button>
             <!-- ko if: showVideoButton() -->
-            <button class="call-ui__button" data-bind="click: () => callActions.toggleCamera(call), css: {'call-ui__button--active': selfParticipant.sharesCamera()}, disable: disableVideoButton(), attr: {'data-uie-value': selfParticipant.sharesCamera() ? 'active' : 'inactive', 'title': t('videoCallOverlayVideo')}" data-uie-name="do-toggle-video">
-              <camera-icon class="small-icon"></camera-icon>
-            </button>
+              <button class="call-ui__button" data-bind="click: () => callActions.toggleCamera(call), css: {'call-ui__button--active': selfParticipant.sharesCamera()}, disable: disableVideoButton(), attr: {'data-uie-value': selfParticipant.sharesCamera() ? 'active' : 'inactive', 'title': t('videoCallOverlayVideo')}" data-uie-name="do-toggle-video">
+                <camera-icon class="small-icon"></camera-icon>
+              </button>
             <!-- /ko -->
             <!-- ko if: isOngoing() -->
               <div class="call-ui__button"
