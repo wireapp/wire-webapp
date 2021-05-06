c5f3a160313075c9d0ed19335dc58d85335ca408
Florian Imdahl <florian@wire.com>
2021-05-06 20:52:03 +0200

    remove todos

--- src/script/components/UserInput.tsx
+++ src/script/components/UserInput.tsx
@@ -17,7 +17,8 @@
  *
  */
 
-import React, {useEffect} from 'react';
+import ko from 'knockout';
+import React, {useEffect, useRef, useState} from 'react';
 
 import {isEnterKey, isRemovalAction} from 'Util/KeyboardUtil';
 import {registerReactComponent} from 'Util/ComponentUtil';
@@ -30,20 +31,20 @@ export interface UserInputParams {
   input: string;
   placeholder: string;
   selected: User[];
+  setSelected: ko.ObservableArray<User>;
 }
 
 const UserInput: React.FC<UserInputParams> = ({
   enter: onEnter,
   focusDelay,
-  input: textInput,
+  input,
   placeholder: placeholderText,
   selected: selectedUsers,
+  setSelected: setSelectedUsers,
 }) => {
   const hasSelectedUsers = !!selectedUsers.length;
-  // TODO
-  //
-  // const innerElement = $(this).find('.search-inner');
-  // const inputElement = $(this).find('.search-input');
+  const innerElement = useRef<HTMLDivElement>();
+  const inputElement = useRef<HTMLInputElement>();
   let hasFocus = false;
 
   if (focusDelay) {
@@ -52,22 +53,13 @@ const UserInput: React.FC<UserInputParams> = ({
     hasFocus = true;
   }
 
-  // TODO
-  //
-  // const selectedSubscription = selectedUsers.subscribe(() => {
-  //   if (typeof input === 'function') {
-  //     input('');
-  //   }
-
-  //   inputElement.focus();
-  //   window.setTimeout(() => innerElement.scrollTop(innerElement[0].scrollHeight));
-  // });
+  const [textInput, setTextInput] = useState(input);
 
   const placeholder = !textInput.length && hasSelectedUsers ? placeholderText : '';
 
   function onKeyDown(keyboardEvent: React.KeyboardEvent<HTMLInputElement>): true {
     if (isRemovalAction(keyboardEvent) && !textInput.length) {
-      this.selectedUsers.pop();
+      setSelectedUsers.pop();
     } else if (isEnterKey(keyboardEvent)) {
       onEnter();
     }
@@ -75,22 +67,20 @@ const UserInput: React.FC<UserInputParams> = ({
   }
 
   useEffect(() => {
-    // TODO
-    // return () => {
-    //   if (selectedSubscription) {
-    //     selectedSubscription.dispose();
-    //   }
-    // };
-  });
+    setTextInput('');
+    inputElement.current?.focus();
+    window.setTimeout(() => innerElement.current?.scrollTo({top: innerElement.current?.scrollHeight}));
+  }, [selectedUsers]);
 
   return (
     <form autoComplete="off" className="search-outer">
       <div className="search-inner-wrap">
-        <div className="search-inner">
+        <div ref={innerElement} className="search-inner">
           <div className="search-icon icon-search" />
           {hasSelectedUsers &&
             selectedUsers.map(user => <span key={user.id} data-bind="text: name()" data-uie-name="item-selected" />)}
           <input
+            ref={inputElement}
             className="search-input"
             max="128"
             required={true}
@@ -112,7 +102,8 @@ const UserInput: React.FC<UserInputParams> = ({
 export default UserInput;
 
 registerReactComponent('user-input', {
-  bindings: 'enter, focusDelay, input: ko.unwrap(input), placeholder, selected: ko.unwrap(selected)',
+  bindings:
+    'enter, focusDelay, input: ko.unwrap(input), placeholder, selected: ko.unwrap(selected), setSelected: selected',
   component: UserInput,
   optionalParams: ['focusDelay'],
 });
