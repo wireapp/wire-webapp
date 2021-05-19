b9b5335be2209ceeb378c6358f38d8036e3cb563
AndyLnd <andy@wire.com>
2021-05-19 13:24:29 +0200

    feat: Block adding users to conversations with LH conflict

--- src/script/error/BackendClientError.ts
+++ src/script/error/BackendClientError.ts
@@ -17,6 +17,7 @@
  *
  */
 
+import {BackendErrorLabel} from '@wireapp/api-client/src/http/';
 import {BaseError} from './BaseError';
 
 export class BackendClientError extends BaseError {
@@ -31,34 +32,34 @@ export class BackendClientError extends BaseError {
 
   static get LABEL() {
     return {
-      ACCESS_DENIED: 'access-denied',
+      ACCESS_DENIED: BackendErrorLabel.ACCESS_DENIED,
       BAD_GATEWAY: 'bad-gateway',
-      BAD_REQUEST: 'bad-request',
-      BLACKLISTED_EMAIL: 'blacklisted-email',
-      BLACKLISTED_PHONE: 'blacklisted-phone',
-      CLIENT_ERROR: 'client-error',
+      BAD_REQUEST: BackendErrorLabel.BAD_REQUEST,
+      BLACKLISTED_EMAIL: BackendErrorLabel.BLACKLISTED_EMAIL,
+      BLACKLISTED_PHONE: BackendErrorLabel.BLACKLISTED_PHONE,
+      CLIENT_ERROR: BackendErrorLabel.CLIENT_ERROR,
       CONNECTIVITY_PROBLEM: 'connectivity-problem',
-      INVALID_CREDENTIALS: 'invalid-credentials',
-      INVALID_EMAIL: 'invalid-email',
-      INVALID_INVITATION_CODE: 'invalid-invitation-code',
-      INVALID_PHONE: 'invalid-phone',
-      KEY_EXISTS: 'key-exists',
-      MISSING_AUTH: 'missing-auth',
+      INVALID_CREDENTIALS: BackendErrorLabel.INVALID_CREDENTIALS,
+      INVALID_EMAIL: BackendErrorLabel.INVALID_EMAIL,
+      INVALID_INVITATION_CODE: BackendErrorLabel.INVALID_INVITATION_CODE,
+      INVALID_PHONE: BackendErrorLabel.INVALID_PHONE,
+      KEY_EXISTS: BackendErrorLabel.KEY_EXISTS,
+      MISSING_AUTH: BackendErrorLabel.MISSING_AUTH,
       MISSING_IDENTITY: 'missing-identity',
-      NOT_CONNECTED: 'not-connected',
-      NOT_FOUND: 'not-found',
-      PASSWORD_EXISTS: 'password-exists',
-      PENDING_ACTIVATION: 'pending-activation',
-      PENDING_LOGIN: 'pending-login',
+      NOT_CONNECTED: BackendErrorLabel.NOT_CONNECTED,
+      NOT_FOUND: BackendErrorLabel.NOT_FOUND,
+      PASSWORD_EXISTS: BackendErrorLabel.PASSWORD_EXISTS,
+      PENDING_ACTIVATION: BackendErrorLabel.PENDING_ACTIVATION,
+      PENDING_LOGIN: BackendErrorLabel.PENDING_LOGIN,
       PHONE_BUDGET_EXHAUSTED: 'phone-budget-exhausted',
       SERVER_ERROR: 'server-error',
       SERVICE_DISABLED: 'service-disabled',
       SUSPENDED: 'suspended',
       TOO_MANY_BOTS: 'too-many-bots',
-      TOO_MANY_CLIENTS: 'too-many-clients',
-      TOO_MANY_MEMBERS: 'too-many-members',
-      UNAUTHORIZED: 'unauthorized',
-      UNKNOWN_CLIENT: 'unknown-client',
+      TOO_MANY_CLIENTS: BackendErrorLabel.TOO_MANY_CLIENTS,
+      TOO_MANY_MEMBERS: BackendErrorLabel.TOO_MANY_MEMBERS,
+      UNAUTHORIZED: BackendErrorLabel.UNAUTHORIZED,
+      UNKNOWN_CLIENT: BackendErrorLabel.UNKNOWN_CLIENT,
     };
   }
 
