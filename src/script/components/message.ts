7b215e920667127e455f78ca2409c2dd6381d752
Florian Imdahl <florian@wire.com>
2021-06-16 14:19:24 +0200

    use knockout to disable controls

--- src/script/components/message.ts
+++ src/script/components/message.ts
@@ -202,7 +202,15 @@ class Message {
       const entries: ContextMenuEntry[] = [];
 
       const canDelete =
-        messageEntity.user().isMe && !this.conversation().removed_from_conversation() && messageEntity.isDeletable();
+        messageEntity.user().isMe &&
+        !this.conversation().removed_from_conversation() &&
+        messageEntity.isDeletable() &&
+        !this.conversation().isFederated();
+
+      const canEdit =
+        messageEntity.isEditable() &&
+        !this.conversation().removed_from_conversation() &&
+        !this.conversation().isFederated();
 
       const hasDetails =
         !this.conversation().is1to1() &&
@@ -225,7 +233,7 @@ class Message {
         });
       }
 
-      if (messageEntity.isEditable() && !this.conversation().removed_from_conversation()) {
+      if (canEdit) {
         entries.push({
           click: () => amplify.publish(WebAppEvents.CONVERSATION.MESSAGE.EDIT, messageEntity),
           label: t('conversationContextMenuEdit'),
