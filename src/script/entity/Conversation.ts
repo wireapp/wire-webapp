e83d657a68d58a2883162d2d0609bf383995c7a8
Florian Imdahl <florian@wire.com>
2021-06-16 15:28:59 +0200

    fix empty conversation name

--- src/script/entity/Conversation.ts
+++ src/script/entity/Conversation.ts
@@ -458,7 +458,7 @@ export class Conversation {
           return truncate(joinedNames, maxLength, false);
         }
 
-        const hasUserIds = this.participating_user_ids().length + this.participatingQualifiedUserIds().length === 0;
+        const hasUserIds = this.participating_user_ids().length + this.participatingQualifiedUserIds().length > 0;
         if (!hasUserIds) {
           return t('conversationsEmptyConversation');
         }
