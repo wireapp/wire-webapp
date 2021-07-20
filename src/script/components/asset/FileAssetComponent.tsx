bf711c415135104a974e625efcebb4bff74e8643
Michael Koppen <michael.koppen@googlemail.com>
2021-07-20 11:54:16 +0200

    chore: Remove eye from asset icon

--- src/script/components/asset/FileAssetComponent.tsx
+++ src/script/components/asset/FileAssetComponent.tsx
@@ -83,9 +83,7 @@ const FileAssetComponent: React.FC<FileAssetProps> = ({message, header, teamStat
       <>
         {!isFileSharingReceivingEnabled ? (
           <div className="file">
-            <div className="file-icon icon-file" data-uie-name="file-icon">
-              <span className="file-icon-ext icon-view"></span>
-            </div>
+            <div className="file-icon icon-file" data-uie-name="file-icon" />
             <div className="file-desc">
               <div className="label-bold-xs ellipsis" data-uie-name="file-name">
                 {fileName}
