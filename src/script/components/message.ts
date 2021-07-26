cbbf301d937290ab18b337594dfc714b5251ef62
AndyLnd <andy@wire.com>
2021-07-26 14:14:25 +0200

    refactor: Migrate ImageAsset to React #11543 (#11544)
    * refactor: Migrate ImageAsset to React
    
    * migrate tests
    
    * update year
    
    * update styling

--- src/script/components/message.ts
+++ src/script/components/message.ts
@@ -45,7 +45,7 @@ import {TeamState} from '../team/TeamState';
 import './asset/audioAsset';
 import './asset/RestrictedAudio';
 import './asset/FileAssetComponent';
-import './asset/imageAsset';
+import './asset/ImageAsset';
 import './asset/RestrictedImage';
 import './asset/LinkPreviewAssetComponent';
 import './asset/LocationAsset';
