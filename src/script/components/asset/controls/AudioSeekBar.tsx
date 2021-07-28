f1359e5604b6357a400b33b3091705ae68a9a3f0
AndyLnd <andy@wire.com>
2021-07-28 14:58:11 +0200

    css fixes

--- src/script/components/asset/controls/AudioSeekBar.tsx
+++ src/script/components/asset/controls/AudioSeekBar.tsx
@@ -101,7 +101,7 @@ const AudioSeekBar: React.FC<AudioSeekBarProps> = ({asset, audioElement, disable
 
   return (
     <svg
-      className={cx({'element-disabled': disabled})}
+      className={cx('audio-seek-bar', {'element-disabled': disabled})}
       width="100%"
       height="100%"
       viewBox="0 0 1 1"
