ff402ed17ba82125e2bf9bbe8867b5f46ce61f47
Michael Koppen <michael.koppen@googlemail.com>
2021-07-29 14:30:49 +0200

    fix: File asset size information

--- src/script/util/util.ts
+++ src/script/util/util.ts
@@ -181,9 +181,9 @@ export const formatBytes = (bytes: number, decimals: number = 1): string => {
 
   const kilobytes = 1024;
   decimals += 1;
-  const unit = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const unit = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
   const index = Math.floor(Math.log(bytes) / Math.log(kilobytes));
-  return parseFloat((bytes / Math.pow(kilobytes, index)).toFixed(decimals)) + unit[index];
+  return `${parseFloat((bytes / Math.pow(kilobytes, index)).toFixed(decimals))} ${unit[index]}`;
 };
 
 export const getContentTypeFromDataUrl = (dataUrl: string): string => {
