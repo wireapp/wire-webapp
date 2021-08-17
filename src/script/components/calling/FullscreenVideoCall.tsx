c0e199b0b23e95190a5dbc6470050a7cb6e2b6ba
Amir Ghezelbash <thisisamir98@gmail.com>
2021-08-17 12:26:58 +0300

    fix: Update text for unmaximizing a participant tile (SQCALL-383)

--- src/script/components/calling/FullscreenVideoCall.tsx
+++ src/script/components/calling/FullscreenVideoCall.tsx
@@ -209,7 +209,7 @@ const FullscreenVideoCall: React.FC<FullscreenVideoCallProps> = ({
             />
           )}
           <div className="video-controls__fit-info" data-uie-name="label-fit-fill-info">
-            {t('videoCallOverlayFitVideoLabel')}
+            {maximizedParticipant ? t('videoCallOverlayFitVideoLabelGoBack') : t('videoCallOverlayFitVideoLabel')}
           </div>
           <div className="video-controls__wrapper">
             <div className="video-controls__button" onClick={minimize} data-uie-name="do-call-controls-video-minimize">
