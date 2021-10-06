123c3b0555ccf7e7f166c8a4ffba91d2383102d2
Thomas Belin <me@thomasbelin.fr>
2021-10-06 16:40:12 +0200

    runfix: Avoid reading conversationId when notification is not linked to a conversation

--- src/script/notification/NotificationRepository.ts
+++ src/script/notification/NotificationRepository.ts
@@ -72,15 +72,19 @@ interface ContentViewModelState {
   state: () => string | false;
 }
 
+type NotificationData = {conversationId?: QualifiedId; messageId?: string; messageType: string};
 interface NotificationContent {
   /** Notification options */
-  options: {data: {conversationId?: QualifiedId; messageId?: string; messageType: string}; tag: string};
+  options: {data: NotificationData; tag: string};
   /** Timeout for notification */
   timeout: number;
   /** Notification title */
   title: string;
   /** Function to be triggered on click */
   trigger: Function;
+}
+interface WebappNotifications extends Notification {
+  data: NotificationData;
 }
 
 /**
@@ -93,7 +97,7 @@ export class NotificationRepository {
   private contentViewModelState: ContentViewModelState;
   private readonly conversationRepository: ConversationRepository;
   private readonly logger: Logger;
-  private readonly notifications: Notification[];
+  private readonly notifications: WebappNotifications[];
   private readonly notificationsPreference: ko.Observable<NotificationPreference>;
   private readonly permissionRepository: PermissionRepository;
   private readonly permissionState: ko.Observable<PermissionState | PermissionStatusState | NotificationPermission>;
@@ -243,7 +247,7 @@ export class NotificationRepository {
     this.notifications.forEach(notification => {
       const {conversationId, messageId, messageType} = notification.data || {};
 
-      if (messageId) {
+      if (conversationId && messageId) {
         this.conversationRepository.isMessageRead(conversationId, messageId).then(isRead => {
           if (isRead) {
             notification.close();
@@ -834,7 +838,10 @@ export class NotificationRepository {
      * See https://developer.mozilla.org/en-US/docs/Web/API/Notification/data
      */
     this.removeReadNotifications();
-    const notification = new window.Notification(notificationContent.title, notificationContent.options);
+    const notification: WebappNotifications = new window.Notification(
+      notificationContent.title,
+      notificationContent.options,
+    );
     const {conversationId, messageId, messageType} = notificationContent.options.data;
     let timeoutTriggerId: number;
 
