15bf369a60e150d43bd4cacb9b543ccb77068b5c
Thomas Belin <me@thomasbelin.fr>
2021-10-11 16:04:25 +0200

    Add new api endpoints for user and connection (#4150)

--- src/connection/Connection.ts
+++ src/connection/Connection.ts
@@ -18,12 +18,15 @@
  */
 
 import type {ConnectionStatus} from '../connection/';
+import {QualifiedId} from '../user';
 
 export interface Connection {
   conversation: string;
+  qualified_conversation?: QualifiedId;
   from: string;
   last_update: string;
   message?: string;
   status: ConnectionStatus;
   to: string;
+  qualified_to?: QualifiedId;
 }
