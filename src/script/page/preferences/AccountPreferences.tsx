c4c664f76d28495540fece76021fc83f9a261a0d
AndyLnd <andy@wire.com>
2021-10-20 12:51:17 +0200

    color picker

--- src/script/page/preferences/AccountPreferences.tsx
+++ src/script/page/preferences/AccountPreferences.tsx
@@ -1,21 +1,33 @@
 import React from 'react';
 import {container} from 'tsyringe';
+import {Availability} from '@wireapp/protocol-messaging';
 
 import {useKoSubscribableChildren} from 'Util/ComponentUtil';
+import Avatar, {AVATAR_SIZE} from 'Components/Avatar';
+import {t} from 'Util/LocalizerUtil';
 
 import type {UserRepository} from '../../user/UserRepository';
 import {UserState} from '../../user/UserState';
 import AccentColorPicker from '../AccentColorPicker';
+import AvailabilityState from 'Components/AvailabilityState';
+import {nameFromType} from '../../user/AvailabilityMapper';
+import {AvailabilityContextMenu} from '../../ui/AvailabilityContextMenu';
 
 interface AccountPreferencesProps {
   userRepository: UserRepository;
   userState: UserState;
 }
 
-const StyledInput = () => {
+interface StyledInputProps {
+  label: string;
+  value: string;
+}
+
+const StyledInput: React.FC<StyledInputProps> = ({label, value}) => {
   return (
     <div>
-      <label></label>
+      <label>{label}</label>
+      <input value={value} />
     </div>
   );
 };
@@ -25,13 +37,32 @@ const AccountPreferences: React.FC<AccountPreferencesProps> = ({
   userState = container.resolve(UserState),
 }) => {
   const {self: selfUser} = useKoSubscribableChildren(userState, ['self']);
+  const {name, email, availability, username} = useKoSubscribableChildren(selfUser, [
+    'name',
+    'email',
+    'availability',
+    'username',
+  ]);
   return (
     <div>
-      {'name'}
-      {'picture'}
-      {'colorpicker'}
+      {name}
+      <Avatar participant={selfUser} avatarSize={AVATAR_SIZE.X_LARGE} />
+      <div onClick={event => AvailabilityContextMenu.show(event.nativeEvent, 'preferences-account-availability-menu')}>
+        <AvailabilityState
+          label={
+            availability === Availability.Type.NONE
+              ? t('preferencesAccountAvailabilityUnset')
+              : nameFromType(availability)
+          }
+          availability={availability}
+          showArrow
+          dataUieName="status-availability-in-profile"
+        />
+      </div>
       <AccentColorPicker user={selfUser} doSetAccentColor={id => userRepository.changeAccentColor(id)} />
-      <StyledInput />
+      <div>{'Info'}</div>
+      <StyledInput label="Displayname" value={name} />
+      <StyledInput label="Username" value={username} />
     </div>
   );
 };
