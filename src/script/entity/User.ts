298bccecbb0d84834d27ccf3b692d750c2665211
Thomas Belin <me@thomasbelin.fr>
2021-10-28 09:42:15 +0200

    Revert "feat(core): Add federated users to guest rooms badge (#12086)"
    This reverts commit 7098c02192c42b5c5cbd85c1825fc543e921612c.

--- src/script/entity/User.ts
+++ src/script/entity/User.ts
@@ -66,11 +66,6 @@ export class User {
   public readonly isExpired: ko.Observable<boolean>;
   public readonly isExternal: ko.PureComputed<boolean>;
   public readonly isGuest: ko.Observable<boolean>;
-
-  /**
-   * isDirectGuest is true when the user is a guest but not a federated user (a federated user is, by definition, a guest)
-   */
-  public readonly isDirectGuest: ko.PureComputed<boolean>;
   public readonly isIgnored: ko.PureComputed<boolean>;
   public readonly isIncomingRequest: ko.PureComputed<boolean>;
   public readonly isOnLegalHold: ko.PureComputed<boolean>;
@@ -172,9 +167,6 @@ export class User {
 
     this.inTeam = ko.observable(false);
     this.isGuest = ko.observable(false);
-    this.isDirectGuest = ko.pureComputed(() => {
-      return this.isGuest() && this.isOnSameFederatedDomain();
-    });
     this.isTemporaryGuest = ko.observable(false);
     this.isTeamMember = ko.observable(false);
     this.teamRole = ko.observable(TEAM_ROLE.NONE);
