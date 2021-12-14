b7eae335f91246ebd78e3f3c7d0ee3c72596f54a
Thomas Belin <me@thomasbelin.fr>
2021-12-14 15:05:59 +0100

    Revert "Update src/script/entity/Conversation.ts"
    This reverts commit d0466698a8449fc7be5743d4dc77b70731bf2b64.

--- src/script/entity/Conversation.ts
+++ src/script/entity/Conversation.ts
@@ -688,7 +688,7 @@ export class Conversation {
     if (sentMessages.length === 0) {
       return this.getLastKnownTimestamp();
     }
-    return sentMessages.at(-1).timestamp() + 1;
+    return sentMessages[sentMessages.length - 1].timestamp() + 1;
   }
 
   getLatestTimestamp(currentTimestamp: number): number {
