6141209a08c082a3fa404db7c5ca86ae71b9e21e
Thomas Belin <me@thomasbelin.fr>
2022-01-13 17:40:46 +0100

    chore(core): Add tests on federated users detection

--- src/script/user/UserMapper.ts
+++ src/script/user/UserMapper.ts
@@ -189,7 +189,8 @@ export class UserMapper {
       userEntity.isSingleSignOn = true;
     }
 
-    if (teamId) {
+    if (teamId && !userEntity.isFederated) {
+      // To be in the same team, the user needs to have the same teamId and to be on the same domain (not federated)
       userEntity.inTeam(true);
       userEntity.teamId = teamId;
     }
