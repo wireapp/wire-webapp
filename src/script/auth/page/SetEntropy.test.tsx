0590a70d88f2301f23d7c6a1215c39b2f58a169d
Timothy Le Bon <tlebon@gmail.com>
2022-03-11 19:15:42 +0100

    test: adding simple tests

--- src/script/auth/page/SetEntropy.test.tsx
+++ src/script/auth/page/SetEntropy.test.tsx
@@ -17,8 +17,9 @@
  *
  */
 
-import {fireEvent, render, screen} from '@testing-library/react';
-import React, {useState} from 'react';
+import {fireEvent, render, screen, act} from '@testing-library/react';
+import React from 'react';
+import {withIntl, withTheme} from '../util/test/TestUtil';
 import SetEntropy from './SetEntropy';
 
 describe('SetEntropy', () => {
@@ -26,21 +27,89 @@ describe('SetEntropy', () => {
     // eslint-disable-next-line no-console
     console.log('sending entropy');
   };
-  const [entropy, setEntropy] = useState<[number, number][]>([]);
-  const [error, setError] = useState(null);
 
-  it('reacts to drawing', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  const setStateMock = jest.fn();
+  const useStateMock: any = (useState: any) => [useState, setStateMock];
+  jest.spyOn(React, 'useState').mockImplementation(useStateMock);
+
+  const setErrorMock = jest.fn();
+  const useErrorMock: any = (useState: any) => [useState, setErrorMock];
+  jest.spyOn(React, 'useState').mockImplementation(useErrorMock);
+
+  const [entropy, setEntropy] = useStateMock([]);
+  const [error, setError] = useErrorMock(null);
+
+  it('renders elements', () => {
+    const {getByText, queryByText} = render(
+      withTheme(
+        withIntl(
+          <SetEntropy
+            onSetEntropy={mockonSetEntropy}
+            error={error}
+            setError={setError}
+            setEntropy={setEntropy}
+            entropy={entropy}
+          />,
+        ),
+      ),
+    );
+    expect(getByText(/create entropy/gi)).toBeTruthy();
+    expect(getByText(/move your mouse/gi)).toBeTruthy();
+    expect(queryByText(/success/gi)).toBeNull();
+  });
+
+  it('reacts to drawing', async () => {
     render(
-      <SetEntropy
-        onSetEntropy={mockonSetEntropy}
-        error={error}
-        setError={setError}
-        setEntropy={setEntropy}
-        entropy={entropy}
-      />,
+      withTheme(
+        withIntl(
+          <SetEntropy
+            onSetEntropy={mockonSetEntropy}
+            error={error}
+            setError={setError}
+            setEntropy={setEntropy}
+            entropy={entropy}
+          />,
+        ),
+      ),
     );
     const canvas = screen.getByTestId('element-canvas');
-    fireEvent.pointerMove(canvas);
-    expect(entropy.length).toBeTruthy();
+    await act(async () => {
+      fireEvent.mouseEnter(canvas);
+      fireEvent.mouseMove(canvas);
+      fireEvent.mouseLeave(canvas);
+    });
+    expect(setEntropy).toHaveBeenCalledTimes(2);
+    expect(setError).toHaveBeenCalledTimes(2);
+  });
+
+  it('starts drawing again after leaving canvas', async () => {
+    render(
+      withTheme(
+        withIntl(
+          <SetEntropy
+            onSetEntropy={mockonSetEntropy}
+            error={error}
+            setError={setError}
+            setEntropy={setEntropy}
+            entropy={entropy}
+          />,
+        ),
+      ),
+    );
+    const canvas = screen.getByTestId('element-canvas');
+    await act(async () => {
+      fireEvent.mouseEnter(canvas);
+      fireEvent.mouseMove(canvas);
+      fireEvent.mouseLeave(canvas);
+      fireEvent.mouseEnter(canvas);
+      fireEvent.mouseMove(canvas);
+      fireEvent.mouseLeave(canvas);
+    });
+    expect(setEntropy).toHaveBeenCalledTimes(4);
+    expect(setError).toHaveBeenCalledTimes(4);
   });
 });
