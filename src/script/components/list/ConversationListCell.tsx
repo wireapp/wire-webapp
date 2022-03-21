76505398701b268a4aceeccb90818a409ac253de
Thomas Belin <thomasbelin4@gmail.com>
2022-03-21 14:41:47 +0100

    Fix double uieName

--- src/script/components/list/ConversationListCell.tsx
+++ src/script/components/list/ConversationListCell.tsx
@@ -111,7 +111,7 @@ const ConversationListCell: React.FC<ConversationListCellProps> = ({
   };
 
   return (
-    <li ref={setViewportElementRef} data-uie-name={dataUieName}>
+    <li ref={setViewportElementRef}>
       <div
         data-uie-name={dataUieName}
         data-uie-uid={conversation.id}
