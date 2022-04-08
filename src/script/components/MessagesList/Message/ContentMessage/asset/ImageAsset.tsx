3898541fd49d3560c87b42c3492469d26c46a64b
kamil-au <kamil.nowak@appunite.com>
2022-04-08 09:46:27 +0200

    fix(accessibility-translations): add accesibility prefix for translations

--- src/script/components/MessagesList/Message/ContentMessage/asset/ImageAsset.tsx
+++ src/script/components/MessagesList/Message/ContentMessage/asset/ImageAsset.tsx
@@ -83,7 +83,7 @@ const ImageAsset: React.FC<ImageAssetProps> = ({asset, message, onClick, teamSta
   const dummyImageUrl = `data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1 1' width='${asset.width}' height='${asset.height}'></svg>`;
 
   const imageAltText = `${message.user().name()} ${t(
-    'conversationAssetImageAlt',
+    'accessibility.conversationAssetImageAlt',
   )} ${message.displayTimestampLong()} ${message.displayTimestampShort()}`;
 
   return (
