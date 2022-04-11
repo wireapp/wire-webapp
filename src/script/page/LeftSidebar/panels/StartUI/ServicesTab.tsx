b2fa535eff2283ff3f710dce42623adb5f887b95
Thomas Belin <me@thomasbelin.fr>
2022-04-11 11:29:28 +0000

    linting

--- src/script/page/LeftSidebar/panels/StartUI/ServicesTab.tsx
+++ src/script/page/LeftSidebar/panels/StartUI/ServicesTab.tsx
@@ -16,7 +16,7 @@
  * along with this program. If not, see http://www.gnu.org/licenses/.
  *
  */
- 
+
 import React, {useState} from 'react';
 import {t} from 'Util/LocalizerUtil';
 import useDebounce from '../../../../hooks/useDebounce';
