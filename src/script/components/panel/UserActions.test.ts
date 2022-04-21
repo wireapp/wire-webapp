52f0d64882e46c07909c7c7a3522f5177c24c3fd
Thomas Belin <me@thomasbelin.fr>
2022-04-21 11:43:49 +0000

    Fix headers

--- src/script/components/panel/UserActions.test.ts
+++ src/script/components/panel/UserActions.test.ts
@@ -16,6 +16,7 @@
  * along with this program. If not, see http://www.gnu.org/licenses/.
  *
  */
+
 import ko from 'knockout';
 import {ConnectionStatus} from '@wireapp/api-client/src/connection/';
 
