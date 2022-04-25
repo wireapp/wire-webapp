ad3edc870ab38270f71a2c11a1e2475ccb39abba
Bartek Dybowski <bartlomiej.dybowski@appunite.com>
2022-04-25 06:46:52 +0200

    checkbox styles adjusted to use the current accent color

--- src/script/components/Checkbox/Checkbox.styles.ts
+++ src/script/components/Checkbox/Checkbox.styles.ts
@@ -5,16 +5,21 @@ export const getLabelCSS = (disabled?: boolean): CSSObject => ({
     cursor: !disabled && 'pointer',
   },
   '&:hover svg': {
-    borderColor: !disabled && 'var(--checkbox-border-hover)',
+    borderColor: !disabled && 'var(--accent-color-500)',
   },
   alignItems: 'center',
   display: 'flex',
   fontSize: '',
 });
 
-export const getInputCSS = (disabled?: boolean): CSSObject => ({
-  '&:active + svg, &:focus + svg, &:focus-visible + svg': {
-    borderColor: !disabled && 'var(--checkbox-border-hover)',
+export const getInputCSS = (isChecked: boolean, disabled?: boolean): CSSObject => ({
+  '&:active + svg': {
+    borderColor: !disabled && 'var(--accent-color-500)',
+  },
+  '&:focus + svg, &:focus-visible + svg': {
+    backgroundColor: isChecked && 'var(--accent-color-600)',
+    borderColor: 'var(--accent-color-600)',
+    outline: '1px solid var(--accent-color-700)',
   },
   clip: 'rect(0 0 0 0)',
   clipPath: 'inset(50%)',
@@ -36,8 +41,8 @@ export const getSvgCSS = (isChecked: boolean, disabled?: boolean): CSSObject => 
   marginRight: 8,
   width: 20,
   ...(isChecked && {
-    background: 'var(--checkbox-background-selected)',
-    borderColor: 'var(--checkbox-background-selected)',
+    background: 'var(--accent-color-500)',
+    borderColor: 'var(--accent-color-500)',
   }),
   ...(disabled && {
     background: isChecked ? 'var(--checkbox-background-disabled-selected)' : 'var(--checkbox-background-disabled)',
