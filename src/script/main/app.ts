71a125495d791b95b8d0071b092f82f20cc0bba8
Amir Ghezelbash <thisisamir98@gmail.com>
2022-06-08 17:21:45 +0430

    refactor: migrate GroupCreationModal to registerReactComponent

--- src/script/main/app.ts
+++ src/script/main/app.ts
@@ -92,7 +92,6 @@ import {SIGN_OUT_REASON} from '../auth/SignOutReason';
 import {ClientRepository} from '../client/ClientRepository';
 import {ContentViewModel} from '../view_model/ContentViewModel';
 import AppLock from '../page/AppLock';
-import GroupCreationModal from '../page/Modals/GroupCreation/GroupCreationModal';
 import {CacheRepository} from '../cache/CacheRepository';
 import {SelfService} from '../self/SelfService';
 import {PropertiesRepository} from '../properties/PropertiesRepository';
@@ -386,7 +385,6 @@ class App {
         client: clientRepository,
         connection: connectionRepository,
         conversation: conversationRepository,
-        search: searchRepository,
         cryptography: cryptographyRepository,
         event: eventRepository,
         eventTracker: eventTrackerRepository,
@@ -471,7 +469,6 @@ class App {
       telemetry.timeStep(AppInitTimingsStep.APP_LOADED);
       this._showInterface();
       AppLock.init(clientRepository);
-      GroupCreationModal.init(conversationRepository, searchRepository, teamRepository);
 
       loadingView.removeFromView();
       telemetry.report();
