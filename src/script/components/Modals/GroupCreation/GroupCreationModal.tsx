647aa85328aebce68c086799a7255b1a224b0ff3
Virgile <virgile.girardin@gmail.com>
2022-06-27 17:09:11 +0200

    refactor: add a new handleEnterDown handler

--- src/script/components/Modals/GroupCreation/GroupCreationModal.tsx
+++ src/script/components/Modals/GroupCreation/GroupCreationModal.tsx
@@ -44,7 +44,7 @@ import {User} from '../../../entity/User';
 import {ACCESS_STATE} from '../../../conversation/AccessState';
 import Icon from 'Components/Icon';
 import {t} from 'Util/LocalizerUtil';
-import {onEscKey, offEscKey, KEY} from 'Util/KeyboardUtil';
+import {onEscKey, offEscKey, handleEnterDown} from 'Util/KeyboardUtil';
 import {
   ACCESS_TYPES,
   teamPermissionsForAccessState,
@@ -340,9 +340,7 @@ const GroupCreationModal: React.FC<GroupCreationModalProps> = ({
                   setGroupName(trimmedName);
                 }}
                 onKeyDown={(event: React.KeyboardEvent<HTMLInputElement>) => {
-                  if (event.key === KEY.ENTER) {
-                    clickOnNext();
-                  }
+                  handleEnterDown(event, clickOnNext);
                 }}
                 value={groupName}
                 isError={nameError.length > 0}
