df794e7eec9672e58e03132c8790cec2c0d8e27f
PatrykBuniX <patrykbunix@gmail.com>
2022-07-01 14:52:44 +0200

    chore: fix strict ts errors in PanelActions

--- src/script/components/panel/PanelActions/PanelActions.tsx
+++ src/script/components/panel/PanelActions/PanelActions.tsx
@@ -37,18 +37,19 @@ export interface PanelActionsProps {
 const PanelActions: React.FC<PanelActionsProps> = ({items}) => {
   return (
     <ul css={listCSS}>
-      {items.map(({click, identifier, icon, label}) => (
-        <li key={identifier}>
-          <button className="panel__action-item" onClick={click} data-uie-name={identifier} type="button">
-            <span className="panel__action-item__icon">
-              <Icon name={icon} />
-            </span>
-            <span data-uie-name={`${identifier}-item-text`} className="panel__action-item__text">
-              {label}
-            </span>
-          </button>
-        </li>
-      ))}
+      {items &&
+        items.map(({click, identifier, icon, label}) => (
+          <li key={identifier}>
+            <button className="panel__action-item" onClick={click} data-uie-name={identifier} type="button">
+              <span className="panel__action-item__icon">
+                <Icon name={icon} />
+              </span>
+              <span data-uie-name={`${identifier}-item-text`} className="panel__action-item__text">
+                {label}
+              </span>
+            </button>
+          </li>
+        ))}
     </ul>
   );
 };
