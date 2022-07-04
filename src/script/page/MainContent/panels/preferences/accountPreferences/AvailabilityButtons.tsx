0f57d5bc216961074f92b69bdd8653d9255fd518
PatrykBuniX <patrykbunix@gmail.com>
2022-07-04 12:14:48 +0200

    chore: fix strict ts errors in AvailabilityButtons

--- src/script/page/MainContent/panels/preferences/accountPreferences/AvailabilityButtons.tsx
+++ src/script/page/MainContent/panels/preferences/accountPreferences/AvailabilityButtons.tsx
@@ -121,9 +121,9 @@ const AvailabilityButtons: React.FC<AvailabilityInputProps> = ({availability}) =
               })}
               key={item.availability}
               type="button"
-              onClick={() => item.click()}
+              onClick={() => item.click?.()}
             >
-              {icons[item.availability]}
+              {item.availability !== undefined && icons[item.availability]}
               {item.label}
             </button>
           );
