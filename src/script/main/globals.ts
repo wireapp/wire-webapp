31817c23fe9c2e80d6631256ab48a9a34cf0a478
Patryk Górka <patrykbunix@gmail.com>
2022-07-06 15:00:48 +0200

    refactor: migrate title-bar to react (#ACC-198) (#13308)
    * refactor: Migrate TitleBar to React
    
    * refactor: migrate TitleBar component to react
    
    * refactor: correct legal-hold dot styles
    
    * refactor: delete title-bar knockout template file
    
    * refactor: delete TitleBarViewModel
    
    * refactor: remove  from title-bar params
    
    * refactor: get verification state from ko hook
    
    * refactor: remove conversationentity check
    
    * refactor: rename conversationEntity to conversation
    
    * test: test TitleBar component
    
    * runfix: fix badge label copy render method
    
    * refactor: use querySelector instead of jq
    
    Co-authored-by: Thomas Belin <thomasbelin4@gmail.com>
    
    * runfix: put inputbar init function back to contentviewmodel
    
    * runfix: address querySelector strict error and conversation name cursor styles
    
    Co-authored-by: Przemysław Jóźwik <przemyslaw.jozwik96@gmail.com>
    Co-authored-by: Thomas Belin <thomasbelin4@gmail.com>

--- src/script/main/globals.ts
+++ src/script/main/globals.ts
@@ -36,6 +36,7 @@ import 'Components/toggle/InfoToggle';
 import 'Components/calling/FullscreenVideoCall';
 import 'Components/avatar/GroupAvatar';
 import 'Components/MessagesList';
+import 'Components/TitleBar';
 import 'Components/Modals/GroupCreation/GroupCreationModal';
 import '../page/AppLock';
 import 'Components/modal';
