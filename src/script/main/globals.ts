53177d116d6997836091f4a76a4b89c51ab4b795
Patryk Górka <patrykbunix@gmail.com>
2022-07-13 17:50:28 +0200

    fix: hardcode 'how to verify fingerprint' article's url (#13339)

--- src/script/main/globals.ts
+++ src/script/main/globals.ts
@@ -122,6 +122,7 @@ declare global {
             INDEX: string;
             LEGAL_HOLD_BLOCK: string;
             MICROPHONE_ACCESS_DENIED: string;
+            PRIVACY_VERIFY_FINGERPRINT: string;
             SCREEN_ACCESS_DENIED: string;
           };
           TEAMS_BASE: string;
