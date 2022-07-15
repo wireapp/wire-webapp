811bb68d4ea92198fb92d2c50a900c861526c225
arjita-mitra <arjitamitra3@gmail.com>
2022-07-15 16:01:41 +0200

    fix: resolve warning about camelCase for custom attribute alignCenter(ACC-207)
    custom attributes should be in small case

--- src/script/auth/component/AccountForm.tsx
+++ src/script/auth/component/AccountForm.tsx
@@ -231,7 +231,7 @@ const AccountForm = ({account, ...props}: Props & ConnectedProps & DispatchProps
           setValidInputs({...validInputs, terms: true});
         }}
         markInvalid={!validInputs.terms}
-        alignCenter
+        aligncenter
         name="accept"
         id="accept"
         required
