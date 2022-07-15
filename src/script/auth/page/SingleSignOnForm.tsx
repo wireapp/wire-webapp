6468a368a25a35fd3241256292af34a6261f4214
Arjita <arjitamitra3@gmail.com>
2022-07-15 11:15:12 +0200

    Update src/script/auth/page/SingleSignOnForm.tsx
    Co-authored-by: Virgile <78490891+V-Gira@users.noreply.github.com>

--- src/script/auth/page/SingleSignOnForm.tsx
+++ src/script/auth/page/SingleSignOnForm.tsx
@@ -311,7 +311,7 @@ const SingleSignOnForm = ({
           }
           checked={clientType === ClientType.TEMPORARY}
           data-uie-name="enter-public-computer-sso-sign-in"
-          alignCenter={true}
+          alignCenter
           style={{justifyContent: 'center', marginTop: '36px'}}
         >
           <CheckboxLabel htmlFor="">{_(loginStrings.publicComputer)}</CheckboxLabel>
