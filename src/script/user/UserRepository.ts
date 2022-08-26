a23ae68892de57f74bfbc08592d3467ec33f88de
PatrykBuniX <patrykbunix@gmail.com>
2022-08-26 14:26:59 +0200

    runfix: remove deprecated endpoint call (getUsers)

--- src/script/user/UserRepository.ts
+++ src/script/user/UserRepository.ts
@@ -441,8 +441,12 @@ export class UserRepository {
     }
 
     const getUsers = async (chunkOfUserIds: QualifiedId[]): Promise<User[]> => {
+      const selfDomain = this.userState.self().domain;
+
+      const chunkOfQualifiedUserIds = chunkOfUserIds.map(({id, domain}) => ({domain: domain || selfDomain, id}));
+
       try {
-        const response = await this.userService.getUsers(chunkOfUserIds);
+        const response = await this.userService.getUsers(chunkOfQualifiedUserIds);
         return response ? this.userMapper.mapUsersFromJson(response) : [];
       } catch (error: any) {
         if (
@@ -453,8 +457,8 @@ export class UserRepository {
         ) {
           this.logger.warn('loading federated users failed: trying loading same backend users only');
           const [sameBackendUsers, federatedUsers] = partition(
-            chunkOfUserIds,
-            userId => userId.domain === this.userState.self().domain,
+            chunkOfQualifiedUserIds,
+            userId => userId.domain === selfDomain,
           );
           const users = await getUsers(sameBackendUsers);
 
@@ -614,7 +618,8 @@ export class UserRepository {
   }
 
   getUserListFromBackend(userIds: QualifiedId[]): Promise<APIClientUser[]> {
-    return this.userService.getUsers(userIds);
+    const qualifiedUserIds = userIds.map(({id, domain}) => ({domain: domain || this.userState.self().domain, id}));
+    return this.userService.getUsers(qualifiedUserIds);
   }
 
   /**
