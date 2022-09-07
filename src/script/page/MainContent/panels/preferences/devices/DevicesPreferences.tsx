1e5e154c17209f98f16ffc2eef63668097186ed5
V-Gira <virgile.girardin@gmail.com>
2022-09-07 12:33:15 +0200

    chore: add paragraph tags to preferences

--- src/script/page/MainContent/panels/preferences/devices/DevicesPreferences.tsx
+++ src/script/page/MainContent/panels/preferences/devices/DevicesPreferences.tsx
@@ -175,7 +175,7 @@ const DevicesPreferences: React.FC<DevicesPreferencesProps> = ({
                 deviceNumber={++index}
               />
             ))}
-            <div className="preferences-detail">{t('preferencesDevicesActiveDetail')}</div>
+            <p className="preferences-detail">{t('preferencesDevicesActiveDetail')}</p>
           </fieldset>
         )}
       </div>
