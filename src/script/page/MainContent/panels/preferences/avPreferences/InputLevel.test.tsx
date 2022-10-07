2ba2bd145066052531000015da7a49ed2df80b09
PatrykBuniX <patrykbunix@gmail.com>
2022-10-07 16:05:08 +0200

    refactor: migrate InputLevel test to testing-library

--- src/script/page/MainContent/panels/preferences/avPreferences/InputLevel.test.tsx
+++ src/script/page/MainContent/panels/preferences/avPreferences/InputLevel.test.tsx
@@ -17,16 +17,8 @@
  *
  */
 
-import TestPage from 'Util/test/TestPage';
-import InputLevel, {InputLevelProps, MAX_AUDIO_BULLETS} from './InputLevel';
-
-class InputLevelTestPage extends TestPage<InputLevelProps> {
-  constructor(props?: InputLevelProps) {
-    super(InputLevel, props);
-  }
-
-  getActiveInputLevelBullets = () => this.getAll('.input-level__bullet--active');
-}
+import InputLevel, {MAX_AUDIO_BULLETS} from './InputLevel';
+import {render} from '@testing-library/react';
 
 describe('InputLevel', () => {
   let originalAudioContext: any;
@@ -58,12 +50,14 @@ describe('InputLevel', () => {
     const expectedAudioLevel = (128 / 160) * MAX_AUDIO_BULLETS;
     const mediaStream = new MediaStream();
 
-    const testPage = new InputLevelTestPage({
+    const props = {
       disabled: false,
       mediaStream,
-    });
+    };
 
-    const activeAudioLevelBullets = testPage.getActiveInputLevelBullets();
-    expect(activeAudioLevelBullets.length).toBe(expectedAudioLevel);
+    const {container} = render(<InputLevel {...props} />);
+
+    const activeAudioLevelBullets = container.querySelectorAll('.input-level__bullet--active');
+    expect(activeAudioLevelBullets).toHaveLength(expectedAudioLevel);
   });
 });
