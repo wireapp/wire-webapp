2be0c847810e626362412c0fc0882e6c73fc663c
Thomas Belin <thomasbelin4@gmail.com>
2022-10-27 10:54:28 +0200

    runfix: replace user modal in app context (#13987)

--- src/script/page/AppMain.tsx
+++ src/script/page/AppMain.tsx
@@ -125,6 +125,7 @@ const AppContainer: FC<AppContainerProps> = ({root}) => {
           />
 
           {/*The order of these elements matter to show proper modals stack upon each other*/}
+          <div id="user-modal-container"></div>
           <PrimaryModalComponent />
           <GroupCreationModal userState={userState} teamState={teamState} />
         </main>
