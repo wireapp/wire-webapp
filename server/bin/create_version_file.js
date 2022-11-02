84bb7b4611863eb7b1a2a8f62afd14e3fca60706
Amir Ghezelbash <thisisamir98@gmail.com>
2022-11-02 13:30:14 +0330

    version with date-fns

--- server/bin/create_version_file.js
+++ server/bin/create_version_file.js
@@ -17,23 +17,13 @@
  *
  */
 
-const fs = require('fs-extra');
 const path = require('path');
+const fs = require('fs-extra');
+const {format} = require('date-fns');
 
 const distFolder = 'dist';
 
-function padTo2Digits(num) {
-  return num.toString().padStart(2, '0');
-}
-
-function formatDate(date) {
-  return `${[date.getFullYear(), padTo2Digits(date.getMonth() + 1), padTo2Digits(date.getDate())].join('.')}.${[
-    padTo2Digits(date.getHours()),
-    padTo2Digits(date.getMinutes()),
-  ].join('.')}`;
-}
-
-const version = formatDate(new Date());
+const version = format(new Date(), 'yyyy.MM.dd.HH.mm');
 
 console.log(`Version set to ${version}`);
 
