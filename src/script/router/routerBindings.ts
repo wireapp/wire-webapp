e48aa26476cbd018cc5eb4c268860d13f3aec96a
V-Gira <virgile.girardin@gmail.com>
2022-12-20 15:45:34 +0100

    add comment

--- src/script/router/routerBindings.ts
+++ src/script/router/routerBindings.ts
@@ -28,6 +28,7 @@ import {useAppMainState, ViewType} from '../page/state';
 export const createNavigate =
   (link: string): React.MouseEventHandler =>
   (event: React.MouseEvent<Element, MouseEvent>) => {
+    // The order here matters, setting the view before calling navigate() would not save the history
     navigate(link);
     setResponsiveView();
     event.preventDefault();
