caf52669273e0e224d6a661f8666825ef8450889
Thomas Belin <me@thomasbelin.fr>
2023-01-17 16:19:11 +0100

    fix: Make sessionId migration resumable

--- src/messagingProtocols/proteus/ProteusService/sessionIdMigrator.ts
+++ src/messagingProtocols/proteus/ProteusService/sessionIdMigrator.ts
@@ -21,6 +21,11 @@ import {CRUDEngine} from '@wireapp/store-engine';
 
 const sessionTableName = 'sessions';
 
+type Session = {
+  id: string;
+  [key: string]: unknown;
+};
+
 /**
  * Will migrate all the session ids in the database to fully qualified ids (from 'user-id@device-id` to  `domain@user-id@device-id`)
  * Will only occur once for an instance of the webapp (a flag is stored in order not to re-do the migration at each loadtime)
@@ -30,23 +35,18 @@ const sessionTableName = 'sessions';
  */
 export async function migrateToQualifiedSessionIds(storeEngine: CRUDEngine, defaultDomain: string) {
   const isFullyQualified = /^[^@]+@[A-F0-9-]+@/i;
-  const updatedSessions = (await storeEngine.readAll<{id: string}>(sessionTableName))
+  const updatedSessions = (await storeEngine.readAll<Session>(sessionTableName))
     .filter(session => !isFullyQualified.test(session.id))
-    .reduce<{newSessions: {id: string}[]; oldIds: string[]}>(
-      (acc, session) => {
-        return {
-          newSessions: [...acc.newSessions, {...session, id: `${defaultDomain}@${session.id}`}],
-          oldIds: [...acc.oldIds, session.id],
-        };
-      },
-      {newSessions: [], oldIds: []},
-    );
+    .reduce<{oldId: string; newSession: Session}[]>((acc, session) => {
+      return acc.concat({
+        oldId: session.id,
+        newSession: {...session, id: `${defaultDomain}@${session.id}`},
+      });
+    }, []);
 
-  // In order to change primaryKeys, we need to first delete all the record and re-add them with the new key
-  for (const oldId of updatedSessions.oldIds) {
+  for (const {oldId, newSession} of updatedSessions) {
+    // In order to change primaryKeys, we need to first delete all the record and re-add them with the new key
     await storeEngine.delete(sessionTableName, oldId);
-  }
-  for (const newSession of updatedSessions.newSessions) {
     await storeEngine.create(sessionTableName, newSession.id, newSession);
   }
 }
