dcc9fd6efde1677dfc81f744740e6ea390125846
V-Gira <virgile.girardin@gmail.com>
2023-03-07 16:07:44 +0100

    add hash to outgoing quote

--- src/script/components/MessagesList/Message/ContentMessage/ContentMessage.tsx
+++ src/script/components/MessagesList/Message/ContentMessage/ContentMessage.tsx
@@ -23,12 +23,15 @@ import {QualifiedId} from '@wireapp/api-client/lib/user';
 
 import {Avatar, AVATAR_SIZE} from 'Components/Avatar';
 import {Icon} from 'Components/Icon';
+import {OutgoingQuote} from 'src/script/conversation/MessageRepository';
 import {Conversation} from 'src/script/entity/Conversation';
 import {CompositeMessage} from 'src/script/entity/message/CompositeMessage';
 import {ContentMessage} from 'src/script/entity/message/ContentMessage';
 import {Message} from 'src/script/entity/message/Message';
 import {Text} from 'src/script/entity/message/Text';
 import {MentionEntity} from 'src/script/message/MentionEntity';
+import {MessageHasher} from 'src/script/message/MessageHasher';
+import {QuoteEntity} from 'src/script/message/QuoteEntity';
 import {StatusType} from 'src/script/message/StatusType';
 import {useMainViewModel} from 'src/script/page/RootProvider';
 import {useKoSubscribableChildren} from 'Util/ComponentUtil';
@@ -204,13 +207,26 @@ const ContentMessageComponent: React.FC<ContentMessageProps> = ({
 
   const isTextAsset = message.getFirstAsset().isText();
 
+  const eventRepository = contentViewModel.repositories.event;
+
   const handleRetrySending = async () => {
-    const messageAssets: Text = isTextAsset && message.getFirstAsset();
-    const mentions: MentionEntity[] = messageAssets?.mentions() && messageAssets?.mentions();
-    const quotes = message.quote();
-    const messageId = message.id;
-    const messageText = message.getFirstAsset().text;
-    await messageRepository.sendTextWithLinkPreview(conversation, messageText, mentions, quotes, messageId);
+    const firstAsset = message.getFirstAsset();
+    if (firstAsset instanceof Text) {
+      const mentions: MentionEntity[] = firstAsset.mentions() && firstAsset.mentions();
+      const quotes = await eventRepository.eventService
+        .loadEvent(message.conversation_id, message.id)
+        .then(MessageHasher.hashEvent)
+        .then((messageHash: ArrayBuffer) => {
+          return new QuoteEntity({
+            hash: messageHash,
+            messageId: message.id,
+            userId: message.from,
+          }) as OutgoingQuote;
+        });
+      const messageId = message.id;
+      const messageText = message.getFirstAsset().text;
+      await messageRepository.sendTextWithLinkPreview(conversation, messageText, mentions, quotes, messageId);
+    }
   };
 
   const handleDiscard = async () => {
