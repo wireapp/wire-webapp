c80080f8a8fbef9cbf02d9b94c30749fea36d6a0
Amir Ghezelbash <thisisamir98@gmail.com>
2023-03-27 18:06:23 +0330

    refactor: Fix typescript errors in mention rendering (#14869)
    * refactor: Fix typescript errors in mention rendering (refactor/SQSERVICES-1944)
    
    * update test case titles
    
    * update tests

--- test/unit_tests/util/messageRendererSpec.js
+++ test/unit_tests/util/messageRendererSpec.js
@@ -304,6 +304,40 @@ describe('renderMessage', () => {
         testCase: 'displays mention inside code block',
         text: 'salut\n```\n@you\n```',
       },
+      {
+        expected: 'salut<pre><code class="lang-@you"></code></pre>',
+        mentions: [{length: 4, startIndex: 10, userId: 'pain-id'}],
+        testCase: 'mention does not affect code language',
+        text: 'salut\n\n```@you\n```',
+      },
+      {
+        expected:
+          '<a href="https://wire.com/#@you" target="_blank" rel="nofollow noopener noreferrer" data-md-link="true" data-uie-name="markdown-link">text</a>',
+        mentions: [{length: 4, startIndex: 25, userId: 'pain-id'}],
+        testCase: 'mention does not affect url',
+        text: '[text](https://wire.com/#@you)',
+      },
+      {
+        expected:
+          '<a href="https://wire.com/#" title="@you" target="_blank" rel="nofollow noopener noreferrer" data-md-link="true" data-uie-name="markdown-link">text</a>',
+        mentions: [{length: 4, startIndex: 27, userId: 'pain-id'}],
+        testCase: 'mention in title is displayed correctly',
+        text: '[text](https://wire.com/# "@you")',
+      },
+      {
+        expected:
+          '<a href="https://wire.com" target="_blank" rel="nofollow noopener noreferrer" data-md-link="true" data-uie-name="markdown-link"><div class="message-mention" data-uie-name="label-other-mention" data-user-id="pain-id" role="button"><span class="mention-at-sign">@</span>you</div></a>',
+        mentions: [{length: 4, startIndex: 1, userId: 'pain-id'}],
+        testCase: 'mention works correctly as part of a link text',
+        text: '[@you](https://wire.com)',
+      },
+      {
+        expected:
+          '<a href="http://wire.com#@you" target="_blank" rel="nofollow noopener noreferrer">http://wire.com#<div class="message-mention" data-uie-name="label-other-mention" data-user-id="pain-id" role="button"><span class="mention-at-sign">@</span>you</div></a>',
+        mentions: [{length: 4, startIndex: 16, userId: 'pain-id'}],
+        testCase: "mentions don't affect auto-detected links",
+        text: 'http://wire.com#@you',
+      },
     ];
 
     tests.forEach(({expected, mentions, testCase, text}) => {
