3fa3528c1a00c8b1255a525308234b8a9728245d
Przemysław Jóźwik <przemyslaw.jozwik@wire.com>
2023-04-05 11:00:40 +0200

    fix: Show Availability state for users (#14971)
    * fix: Show Availability state for users
    
    * replace props name

--- src/script/components/ParticipantItemContent/ParticipantItemContent.tsx
+++ src/script/components/ParticipantItemContent/ParticipantItemContent.tsx
@@ -46,6 +46,7 @@ export interface ParticipantItemContentProps {
   hasUsernameInfo?: boolean;
   showArrow?: boolean;
   onDropdownClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  showAvailabilityState?: boolean;
 }
 
 export const ParticipantItemContent = ({
@@ -56,12 +57,13 @@ export const ParticipantItemContent = ({
   selfString = '',
   hasUsernameInfo = false,
   showArrow = false,
+  showAvailabilityState = false,
 }: ParticipantItemContentProps) => {
   return (
     <div css={wrapper}>
       <div css={contentText}>
         <div css={nameWrapper}>
-          {selfInTeam && availability ? (
+          {showAvailabilityState && selfInTeam ? (
             <AvailabilityState
               availability={availability}
               css={[userName, userAvailability, ellipsis]}
