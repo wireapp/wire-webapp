e1f0577cc80d24e8b9ee2bf98f07bca8f582e39f
Thomas Belin <me@thomasbelin.fr>
2023-04-26 12:34:25 +0200

    use usergenerator

--- src/script/backup/BackupRepository.test.ts
+++ src/script/backup/BackupRepository.test.ts
@@ -20,6 +20,7 @@
 import {container} from 'tsyringe';
 import {omit} from 'underscore';
 
+import {generateAPIUser} from 'test/helper/UserGenerator';
 import {createRandomUuid, noop} from 'Util/util';
 import {WebWorker} from 'Util/worker';
 
@@ -69,57 +70,6 @@ const messages = [
     id: '4af67f76-09f9-4831-b3a4-9df877b8c29a',
     time: '2016-08-04T13:27:58.993Z',
     type: 'conversation.message-add',
-  },
-];
-
-const users = [
-  {
-    accent_id: 2,
-    assets: [
-      {
-        key: '3-1-cd6ba715-8f1d-47fe-bcfe-68c7c49c6a91',
-        size: 'preview',
-        type: 'image',
-      },
-      {
-        key: '3-1-cfaece2f-83cf-4b5f-966f-5c2bca6cd25f',
-        size: 'complete',
-        type: 'image',
-      },
-    ],
-    handle: 'atomrc_felix',
-    id: '920053b3-e82d-4b38-a465-d997f0987a8e',
-    legalhold_status: 'no_consent',
-    name: 'felix',
-    qualified_id: {
-      domain: 'staging.zinfra.io',
-      id: '920053b3-e82d-4b38-a465-d997f0987a8e',
-    },
-  },
-  {
-    accent_id: 1,
-    assets: [
-      {
-        key: '3-1-500a987d-1c2d-44a6-ad14-50d7d8d02b4f',
-        size: 'preview',
-        type: 'image',
-      },
-      {
-        key: '3-1-33e80c59-60e7-400e-bbb1-703bb3a266c5',
-        size: 'complete',
-        type: 'image',
-      },
-    ],
-    email: 'thomas.belin+team1@wire.com',
-    handle: 'thomas_team1',
-    id: 'e2f450b4-0413-42de-8e4a-f6761b76b0cc',
-    legalhold_status: 'no_consent',
-    name: 'thomas_team1',
-    qualified_id: {
-      domain: 'staging.zinfra.io',
-      id: 'e2f450b4-0413-42de-8e4a-f6761b76b0cc',
-    },
-    team: 'b0dcee1f-c64e-4d40-8b50-5baf932906b8',
   },
 ];
 
@@ -244,6 +194,7 @@ describe('BackupRepository', () => {
       const user = new User('user1');
       jest.spyOn(backupService, 'getDatabaseVersion').mockReturnValue(15);
       const importSpy = jest.spyOn(backupService, 'importEntities').mockResolvedValue(1);
+      const users = [generateAPIUser(), generateAPIUser()];
 
       const metadata = {...backupRepository.createMetaData(user, 'client1'), version: 15};
 
@@ -264,7 +215,7 @@ describe('BackupRepository', () => {
         messages.map(message => omit(message, 'primary_key')),
       );
 
-      expect(importSpy).toHaveBeenCalledWith(StorageSchemata.OBJECT_STORE.USERS, users);
+      expect(importSpy).toHaveBeenCalledWith(StorageSchemata.OBJECT_STORE.USERS, users, expect.any(Function));
     });
   });
 });
