5faab4b2cd4f918915d4de23535103572cf242c7
Patryk Górka <patrykbunix@gmail.com>
2023-05-17 15:49:16 +0200

    feat: periodically check migration config [FS-1893] (#15117)
    * feat: base for initialisation and finalisation methods
    
    * feat: check if migration time has arrived after mls is supported
    
    * refactor: improve init migration api
    
    * refactor: rename func
    
    * refactor: move initialisation and finalisation to separate modules
    
    * runfix: start migration flow in background after app was initialised
    
    * feat: filter unestablished mls conversations out
    
    * feat: filter out only mls conversations that are unestablished
    
    * refactor: move mls migration logger to separate file
    
    * feat: update conversation locally
    
    * chore: remove migration init code (follow-up pr)
    
    * refactor: resolve core and api client in module
    
    * feat: read feature config from teamstate
    
    * chore: update core
    
    * runfix: check fresh migration config when timer elapses
    
    * chore: update comment
    
    * refactor: use reduce for grouping conversations by protocol
    
    * refactor: simplify removal key check
    
    * feat: update core with new migration config types
    
    * refactor: improve types

--- src/script/page/AppMain.tsx
+++ src/script/page/AppMain.tsx
@@ -48,6 +48,7 @@ import {ConversationState} from '../conversation/ConversationState';
 import {User} from '../entity/User';
 import {useInitializeRootFontSize} from '../hooks/useRootFontSize';
 import {App} from '../main/app';
+import {initialiseMLSMigrationFlow} from '../mls/MLSMigration';
 import {generateConversationUrl} from '../router/routeGenerator';
 import {configureRoutes, navigate} from '../router/Router';
 import {TeamState} from '../team/TeamState';
@@ -127,7 +128,7 @@ const AppMain: FC<AppMainProps> = ({
   const {currentView} = useAppMainState(state => state.responsiveView);
   const isLeftSidebarVisible = currentView == ViewType.LEFT_SIDEBAR;
 
-  const initializeApp = () => {
+  const initializeApp = async () => {
     repositories.notification.setContentViewModelStates(contentState, mainView.multitasking);
 
     const showMostRecentConversation = () => {
@@ -193,6 +194,13 @@ const AppMain: FC<AppMainProps> = ({
 
     repositories.properties.checkPrivacyPermission().then(() => {
       window.setTimeout(() => repositories.notification.checkPermission(), App.CONFIG.NOTIFICATION_CHECK);
+    });
+
+    //after app is loaded, check mls migration configuration and start migration if needed
+    await initialiseMLSMigrationFlow({
+      teamState,
+      conversationRepository: repositories.conversation,
+      isConversationOwnedBySelfTeam: ({team_id}) => !!selfUser.teamId && team_id === selfUser.teamId,
     });
   };
 
