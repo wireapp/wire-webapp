35ef9c0a101e9647613ddb01014bbf73ca3174e6
Patryk Górka <patrykbunix@gmail.com>
2023-06-27 13:40:36 +0200

    refactor: remove explicit return type (#5273)

--- api-client/src/self/SelfAPI.ts
+++ api-client/src/self/SelfAPI.ts
@@ -218,7 +218,7 @@ export class SelfAPI {
    * Update self user's list of supported-protocols
    * @param supportedProtocols The list of supported protocols
    */
-  public async putSupportedProtocols(supportedProtocols: ConversationProtocol[]): Promise<void> {
+  public async putSupportedProtocols(supportedProtocols: ConversationProtocol[]) {
     const config: AxiosRequestConfig = {
       data: {supported_protocols: supportedProtocols},
       method: 'put',
