2df55933e5c8ee2e03e0f55159ec0fb72dc901a7
Thomas Belin <me@thomasbelin.fr>
2023-08-15 16:10:35 +0200

    chore: Remove unused cspell check

--- src/script/calling/CallingRepository.ts
+++ src/script/calling/CallingRepository.ts
@@ -316,7 +316,6 @@ export class CallingRepository {
   };
 
   private createWUser(wCall: Wcall, selfUserId: string, selfClientId: string): number {
-    /* cspell:disable */
     const wUser = wCall.create(
       selfUserId,
       selfClientId,
@@ -333,7 +332,6 @@ export class CallingRepository {
       this.audioCbrChanged, // `acbrh`,
       this.videoStateChanged, // `vstateh`,
     );
-    /* cspell:enable */
     const tenSeconds = 10;
     wCall.setNetworkQualityHandler(wUser, this.updateCallQuality, tenSeconds);
     wCall.setMuteHandler(wUser, this.updateMuteState);
