b0b9dfc228fb0733e9891032d647fae06a2e1d24
V-Gira <virgile.girardin@gmail.com>
2023-08-17 17:58:52 +0200

    add unit test

--- src/script/components/MessagesList/Message/FailedToAddUsersMessage.test.tsx
+++ src/script/components/MessagesList/Message/FailedToAddUsersMessage.test.tsx
@@ -81,6 +81,7 @@ describe('FailedToAddUsersMessage', () => {
 
     const message = createFailedToAddUsersMessage({
       qualifiedIds: [qualifiedId1, qualifiedId2],
+      reason: AddUsersFailureReasons.UNREACHABLE_BACKENDS,
     });
 
     const {getByTestId} = render(
@@ -100,6 +101,7 @@ describe('FailedToAddUsersMessage', () => {
 
     const message = createFailedToAddUsersMessage({
       qualifiedIds: [qualifiedId1, qualifiedId2],
+      reason: AddUsersFailureReasons.UNREACHABLE_BACKENDS,
     });
 
     const {getByTestId} = render(
@@ -129,6 +131,7 @@ describe('FailedToAddUsersMessage', () => {
 
     const message = createFailedToAddUsersMessage({
       qualifiedIds: [qualifiedId1, qualifiedId2],
+      reason: AddUsersFailureReasons.UNREACHABLE_BACKENDS,
     });
 
     const {getByTestId, getAllByTestId} = render(
@@ -147,5 +150,36 @@ describe('FailedToAddUsersMessage', () => {
     const elementMessageFailedToAddDetails = getAllByTestId('multi-user-not-added-details');
     expect(elementMessageFailedToAddDetails[0].getAttribute('data-uie-value')).toEqual(qualifiedId1.domain);
     expect(elementMessageFailedToAddDetails[1].getAttribute('data-uie-value')).toEqual(qualifiedId2.domain);
+  });
+  it('shows details of failed to add users from non federating backends', async () => {
+    const [qualifiedId1] = generateQualifiedIds(1, 'test.domain');
+    const [qualifiedId2] = generateQualifiedIds(1, 'test-2.domain');
+
+    const user1 = new User(qualifiedId1.id, qualifiedId1.domain);
+    const user2 = new User(qualifiedId2.id, qualifiedId2.domain);
+    userState.users([user1, user2]);
+
+    const message = createFailedToAddUsersMessage({
+      qualifiedIds: [qualifiedId1, qualifiedId2],
+      reason: AddUsersFailureReasons.NON_FEDERATING_BACKENDS,
+    });
+
+    const {getByTestId, getAllByTestId} = render(
+      withTheme(<FailedToAddUsersMessage isMessageFocused message={message} userState={userState} />),
+    );
+
+    const elementMessageFailedToAdd = getByTestId('element-message-failed-to-add-users');
+    expect(elementMessageFailedToAdd.getAttribute('data-uie-value')).toEqual('multi-users-not-added');
+
+    const toggleButton = getByTestId('toggle-failed-to-add-users');
+
+    act(() => {
+      toggleButton.click();
+    });
+
+    const elementMessageFailedToAddDetails = getAllByTestId('multi-user-not-added-details');
+    expect(elementMessageFailedToAddDetails[0].textContent).toContain(
+      'could not be added to the group as their backends do not federate with each other',
+    );
   });
 });
