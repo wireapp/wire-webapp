2e4e6f78d297ef1f59ac11d3ad5805e6681b5b4f
Timothy Le Bon <tlebon@gmail.com>
2023-08-23 12:08:40 +0200

    chore: testing browser name

--- src/script/auth/component/UnsupportedBrowser.tsx
+++ src/script/auth/component/UnsupportedBrowser.tsx
@@ -72,6 +72,7 @@ export const UnsupportedBrowserComponent = ({
             {_(isTemporaryGuest ? unsupportedJoinStrings.unsupportedJoinHeadline : unsupportedStrings.headlineBrowser, {
               brandName: Config.getConfig().BRAND_NAME,
             })}
+            {Runtime.getBrowserName()}
           </H2>
           {isTemporaryGuest && Runtime.isMobileOS() ? (
             <H3 css={{marginBottom: 10}} data-uie-name="element-unsupported-mobile-guest">
