b3087b3d5be19904bf7a5035c05fa62418019f3f
Thomas Belin <me@thomasbelin.fr>
2023-09-14 09:59:07 +0200

    doc: add comment

--- src/script/components/Modals/UserModal/UserModal.tsx
+++ src/script/components/Modals/UserModal/UserModal.tsx
@@ -155,6 +155,7 @@ const UserModal: React.FC<UserModalProps> = ({
   useEffect(() => {
     if (userId) {
       userRepository
+        // We want to get the fresh version of the user from backend (in case the user was deleted)
         .refreshUser(userId)
         .then(user => {
           if (user.isDeleted || !user.isAvailable()) {
