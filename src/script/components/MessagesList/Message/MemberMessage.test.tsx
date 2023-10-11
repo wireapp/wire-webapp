00aa8b837649438b7aed9f4a0ecb7582a02cfe24
Thomas Belin <me@thomasbelin.fr>
2023-10-11 11:47:17 +0200

    fix random tests

--- src/script/components/MessagesList/Message/MemberMessage.test.tsx
+++ src/script/components/MessagesList/Message/MemberMessage.test.tsx
@@ -19,6 +19,8 @@
 
 import {render} from '@testing-library/react';
 
+import {randomInt} from 'crypto';
+
 import en from 'I18n/en-US.json';
 import {MemberMessage as MemberMessageEntity} from 'src/script/entity/message/MemberMessage';
 import {User} from 'src/script/entity/User';
@@ -27,9 +29,10 @@ import {generateUser} from 'test/helper/UserGenerator';
 import {setStrings} from 'Util/LocalizerUtil';
 
 import {MemberMessage} from './MemberMessage';
-import {config} from './MemberMessage/MessageContent';
 
 setStrings({en});
+
+const config = MemberMessageEntity.CONFIG;
 
 function createMemberMessage(type: SystemMessageType, users?: User[]) {
   const message = new MemberMessageEntity();
@@ -67,7 +70,7 @@ describe('MemberMessage', () => {
 
   describe('CONVERSATION_CREATE', () => {
     it('displays participants of a newly created conversation', () => {
-      const nbUsers = Math.floor(Math.random() * 10);
+      const nbUsers = randomInt(1, 10);
       const users = Array.from({length: nbUsers}, () => generateUser());
       const message = createMemberMessage(SystemMessageType.CONVERSATION_CREATE, users);
       const props = {
@@ -82,7 +85,7 @@ describe('MemberMessage', () => {
     });
 
     it('displays a showMore when there are more than 17 users', () => {
-      const nbExtraUsers = Math.floor(Math.random() * 10);
+      const nbExtraUsers = randomInt(1, 10);
       const nbUsers = config.MAX_USERS_VISIBLE + nbExtraUsers;
 
       const users = Array.from({length: nbUsers}, () => generateUser());
@@ -97,7 +100,7 @@ describe('MemberMessage', () => {
     });
 
     it('displays all team members', () => {
-      const nbExtraUsers = Math.floor(Math.random() * 10);
+      const nbExtraUsers = randomInt(1, 10);
       const nbTeamUsers = config.MAX_WHOLE_TEAM_USERS_VISIBLE + nbExtraUsers;
 
       const teamUsers = Array.from({length: nbTeamUsers}, () => generateUser());
@@ -113,7 +116,7 @@ describe('MemberMessage', () => {
     });
 
     it('displays all team members and one guest message', () => {
-      const nbExtraUsers = Math.floor(Math.random() * 10);
+      const nbExtraUsers = randomInt(1, 10);
       const nbTeamUsers = config.MAX_WHOLE_TEAM_USERS_VISIBLE + nbExtraUsers;
 
       const teamUsers = Array.from({length: nbTeamUsers}, () => generateUser());
@@ -131,7 +134,7 @@ describe('MemberMessage', () => {
     });
 
     it('displays all team members and multiple guests message', () => {
-      const nbGuests = 2 + Math.floor(Math.random() * 10);
+      const nbGuests = randomInt(2, 10);
       const nbTeamUsers = config.MAX_WHOLE_TEAM_USERS_VISIBLE;
 
       const teamUsers = Array.from({length: nbTeamUsers}, () => generateUser());
@@ -152,7 +155,7 @@ describe('MemberMessage', () => {
     });
 
     it('displays that another user created a conversation', () => {
-      const nbUsers = Math.floor(Math.random() * 10);
+      const nbUsers = randomInt(1, 10);
       const users = Array.from({length: nbUsers}, () => generateUser());
       const message = createMemberMessage(SystemMessageType.CONVERSATION_CREATE, users);
       message.name('');
@@ -167,7 +170,7 @@ describe('MemberMessage', () => {
     });
 
     it('displays that self user created a conversation', () => {
-      const nbUsers = Math.floor(Math.random() * 10);
+      const nbUsers = randomInt(1, 10);
       const users = Array.from({length: nbUsers}, () => generateUser());
       const message = createMemberMessage(SystemMessageType.CONVERSATION_CREATE, users);
       message.name('');
@@ -182,120 +185,5 @@ describe('MemberMessage', () => {
     });
   });
 
-  describe('MEMBER_JOIN', () => {
-    it('displays participants of a newly created conversation', () => {
-      const nbUsers = Math.floor(Math.random() * 10);
-      const users = Array.from({length: nbUsers}, () => generateUser());
-      const message = createMemberMessage(SystemMessageType.CONVERSATION_CREATE, users);
-      const props = {
-        ...baseProps,
-        message,
-      };
-
-      const {getByText} = render(<MemberMessage {...props} />);
-      users.forEach(user => {
-        expect(getByText(user.name())).not.toBeNull();
-      });
-    });
-
-    it('displays a showMore when there are more than 17 users', () => {
-      const nbExtraUsers = Math.floor(Math.random() * 10);
-      const nbUsers = config.MAX_USERS_VISIBLE + nbExtraUsers;
-
-      const users = Array.from({length: nbUsers}, () => generateUser());
-      const message = createMemberMessage(SystemMessageType.CONVERSATION_CREATE, users);
-      const props = {
-        ...baseProps,
-        message,
-      };
-
-      const {getByText} = render(<MemberMessage {...props} />);
-      expect(getByText(`${nbUsers - config.REDUCED_USERS_COUNT} more`)).not.toBeNull();
-    });
-
-    it('displays all team members', () => {
-      const nbExtraUsers = Math.floor(Math.random() * 10);
-      const nbTeamUsers = config.MAX_WHOLE_TEAM_USERS_VISIBLE + nbExtraUsers;
-
-      const teamUsers = Array.from({length: nbTeamUsers}, () => generateUser());
-      const message = createMemberMessage(SystemMessageType.CONVERSATION_CREATE, teamUsers);
-      message.allTeamMembers = teamUsers;
-      const props = {
-        ...baseProps,
-        message,
-      };
-
-      const {getByText} = render(<MemberMessage {...props} />);
-      expect(getByText(`all team members`)).not.toBeNull();
-    });
-
-    it('displays all team members and one guest message', () => {
-      const nbExtraUsers = Math.floor(Math.random() * 10);
-      const nbTeamUsers = config.MAX_WHOLE_TEAM_USERS_VISIBLE + nbExtraUsers;
-
-      const teamUsers = Array.from({length: nbTeamUsers}, () => generateUser());
-      const guest = generateUser();
-      guest.isGuest(true);
-      const message = createMemberMessage(SystemMessageType.CONVERSATION_CREATE, [...teamUsers, guest]);
-      message.allTeamMembers = teamUsers;
-      const props = {
-        ...baseProps,
-        message,
-      };
-
-      const {getByText} = render(<MemberMessage {...props} />);
-      expect(getByText(`all team members and one guest`)).not.toBeNull();
-    });
-
-    it('displays all team members and multiple guests message', () => {
-      const nbGuests = 2 + Math.floor(Math.random() * 10);
-      const nbTeamUsers = config.MAX_WHOLE_TEAM_USERS_VISIBLE;
-
-      const teamUsers = Array.from({length: nbTeamUsers}, () => generateUser());
-      const guests = Array.from({length: nbGuests}, () => {
-        const guest = generateUser();
-        guest.isGuest(true);
-        return guest;
-      });
-      const message = createMemberMessage(SystemMessageType.CONVERSATION_CREATE, [...teamUsers, ...guests]);
-      message.allTeamMembers = teamUsers;
-      const props = {
-        ...baseProps,
-        message,
-      };
-
-      const {getByText} = render(<MemberMessage {...props} />);
-      expect(getByText(`all team members and ${nbGuests} guests`)).not.toBeNull();
-    });
-
-    it('displays that another user created a conversation', () => {
-      const nbUsers = Math.floor(Math.random() * 10);
-      const users = Array.from({length: nbUsers}, () => generateUser());
-      const message = createMemberMessage(SystemMessageType.CONVERSATION_CREATE, users);
-      message.name('');
-      message.user().name('Creator');
-      const props = {
-        ...baseProps,
-        message,
-      };
-
-      const {container} = render(<MemberMessage {...props} />);
-      expect(container.textContent).toContain(`Creator started a conversation with`);
-    });
-
-    it('displays that self user created a conversation', () => {
-      const nbUsers = Math.floor(Math.random() * 10);
-      const users = Array.from({length: nbUsers}, () => generateUser());
-      const message = createMemberMessage(SystemMessageType.CONVERSATION_CREATE, users);
-      message.name('');
-      message.user().isMe = true;
-      const props = {
-        ...baseProps,
-        message,
-      };
-
-      const {container} = render(<MemberMessage {...props} />);
-      expect(container.textContent).toContain(`You started a conversation with`);
-    });
-  });
+  describe('MEMBER_JOIN', () => {});
 });
