d0ba374c536b90130e4e5b58a9094c54ad0f9144
Patryk Górka <patrykbunix@gmail.com>
2023-10-16 14:56:04 +0200

    refactor(api-client): get conversations (#5632)
    * refactor: remove unused get conversations method
    
    * refactor: remove unused getConversationsByIds method

--- src/demo/http401.ts
+++ src/demo/http401.ts
@@ -22,7 +22,7 @@ import {initClient} from './initClient';
 (async () => {
   const apiClient = await initClient();
   apiClient['accessTokenStore'].accessToken!.access_token = 'invalid';
-  await apiClient.api.conversation.getConversations();
+  await apiClient.api.conversation.getConversationList();
 })().catch(error => {
   console.error(error);
   process.exit(1);
