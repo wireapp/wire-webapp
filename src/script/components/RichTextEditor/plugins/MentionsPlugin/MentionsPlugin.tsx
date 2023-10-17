11abc1b6b9163ce70bab6b3c3643d1bc3f6dd487
Thomas Belin <thomasbelin4@gmail.com>
2023-10-17 10:19:42 +0200

    runfix: Do not show mention suggestion if there is a char before the @ (#16014)

--- src/script/components/RichTextEditor/plugins/MentionsPlugin/MentionsPlugin.tsx
+++ src/script/components/RichTextEditor/plugins/MentionsPlugin/MentionsPlugin.tsx
@@ -41,7 +41,7 @@ const TRIGGER = '@';
  * @param text the text in which to look for mentions triggers
  */
 function checkForMentions(text: string): MenuTextMatch | null {
-  const match = new RegExp(`(^|[^\\w])(${TRIGGER}([\\w ]*))$`).exec(text);
+  const match = new RegExp(`(^| )(${TRIGGER}([\\w ]*))$`).exec(text);
 
   if (match === null) {
     return null;
