08a58a3907543cdd5c1c815cfd46c39f911e35ad
Adrian Weiß <77456193+aweiss-dev@users.noreply.github.com>
2023-10-31 11:02:59 +0100

    fix: backend sends gracePeriod in seconds instead of ms (#16132)
    * fix: backend sends time in seconds instead of MS
    
    * chore: change hardcoded value to use an enum
    
    * fix: tests

--- src/script/page/components/FeatureConfigChange/FeatureConfigChangeHandler/Features/E2EIdentity.ts
+++ src/script/page/components/FeatureConfigChange/FeatureConfigChangeHandler/Features/E2EIdentity.ts
@@ -47,7 +47,7 @@ export const handleE2EIdentityFeatureChange = (logger: Logger, config: FeatureLi
     // Either get the current E2EIdentity handler instance or create a new one
     const e2eHandler = E2EIHandler.getInstance({
       discoveryUrl: e2eiConfig.config.acmeDiscoveryUrl!,
-      gracePeriodInMS: e2eiConfig.config.verificationExpiration,
+      gracePeriodInSeconds: e2eiConfig.config.verificationExpiration,
     });
     e2eHandler.initialize();
   }
