40b594d7da4d76e291dd63980550c69ce85b6912
Thomas Belin <me@thomasbelin.fr>
2023-12-04 10:56:41 +0100

    fixup

--- src/script/page/RightSidebar/ConversationParticipants/ConversationParticipants.tsx
+++ src/script/page/RightSidebar/ConversationParticipants/ConversationParticipants.tsx
@@ -101,6 +101,7 @@ const ConversationParticipants: FC<ConversationParticipantsProps> = ({
 
         <FadingScrollbar className="conversation-participants__list panel__content">
           <UserSearchableList
+            groupId={activeConversation?.groupId}
             dataUieName="list-conversation-participants"
             users={participants}
             filter={searchInput}
