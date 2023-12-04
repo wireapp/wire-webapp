37e7005e06fd27f04012b3252222e564ea0dd981
Thomas Belin <me@thomasbelin.fr>
2023-12-04 09:49:56 +0100

    add doc

--- src/script/service/StoreEngineProvider.ts
+++ src/script/service/StoreEngineProvider.ts
@@ -43,6 +43,8 @@ const providePermanentEngine = async (
 ): Promise<CRUDEngine> => {
   const db = new DexieDatabase(storeName);
 
+  // In case the encryption key is empty, we just give an empty config to the encryption middleware.
+  // We still need to set it up, even if encryption at rest is disabled, as we need to upgrade the DB version for the middleware to install its config table
   const encryptionConfig = key ? {events: NON_INDEXED_FIELDS} : {};
   const encryptionKey = key ? key : new Uint8Array(32).fill(0);
   applyEncryptionMiddleware(db, encryptionKey, encryptionConfig, async () =>
