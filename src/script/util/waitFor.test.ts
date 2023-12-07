dadc1e76f691f224c0824ef03fc26c8b95eb6d3d
Thomas Belin <me@thomasbelin.fr>
2023-12-07 15:21:55 +0100

    fix: Wait for conversation to be known before triggering verification checks

---
+++ src/script/util/waitFor.test.ts
@@ -0,0 +1,47 @@
+/*
+ * Wire
+ * Copyright (C) 2023 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import {waitFor} from './waitFor';
+
+describe('waitFor', () => {
+  it('should resolve if condition is true', async () => {
+    const condition = jest.fn().mockReturnValue(true);
+
+    const result = await waitFor(condition);
+
+    expect(result).toBe(true);
+  });
+
+  it('should resolve if condition is true after some time', async () => {
+    const condition = jest.fn().mockReturnValue(false);
+    setTimeout(() => condition.mockReturnValue('hello'), 100);
+
+    const result = await waitFor(condition);
+
+    expect(result).toBe('hello');
+  });
+
+  it('should resolve with undefined if condition is never true', async () => {
+    const condition = jest.fn().mockReturnValue(false);
+
+    const result = await waitFor(condition, 50, 50);
+
+    expect(result).toBe(undefined);
+  });
+});
