85827fa455842c05ee789bf3fde4790b0854e910
Stefan Matting <stefan@wire.com>
2023-12-11 13:52:20 +0100

    chart-version-next: generate lexicographical releases

--- bin/chart-next-version.sh
+++ bin/chart-next-version.sh
@@ -13,19 +13,17 @@ set -eo pipefail
 
 readonly bump_type=${1:?"Please specify bump type \"prerelease\" or \"release\""}
 
+# search latest version in helm repo
 if [ "$bump_type" = "prerelease" ]; then
   search_args="--devel"
-  bump_component="prerelease"
 else
   if [ "$bump_type" = "release" ]; then
     search_args=""
-    bump_component="minor"
   else
     echo 1>&2 "Unknown bump_type $bump_type provided"
     exit 1
   fi
 fi
-
 res=$(helm search repo charts-webapp/webapp $search_args)
 if [ "$res" == "No results found" ]; then
   echo 1>&2 "Unexpected: no charts found"
@@ -38,11 +36,27 @@ else
   fi
 fi
 
+# is hardcoded bigger? if yes then use it instead
 version_src=$(yq -Mr '.version' ./charts/webapp/Chart.yaml)
-
 if [ "$(./bin/semver compare "$version_src" "$version_latest")" = "1" ]; then
-  echo 1>&2 "Harcoded version $version_src is newer $version_latest, so using $version_src"
+  echo 1>&2 "Harcoded version $version_src is newer than $version_latest, so using $version_src"
   version_latest="$version_src"
 fi
 
-./bin/semver bump "$bump_component" "$version_latest"
+echo 1>&2 "version_latest: $version_latest"
+
+if [ "$bump_type" = "prerelease" ]; then
+  if [ "$(./bin/semver get prerelease "$version_latest")" = "" ]; then
+    # this is first prelease
+    # we initialize pre-release counter to 100000 instead of 0
+    # so that prereleases are lexicographically ordered.
+    # That way helm search always shows the latest version by default.
+    version_new=$(./bin/semver bump prerelease 100000 "$version_latest")
+  else
+    version_new=$(./bin/semver bump prerelease "$version_latest")
+  fi
+else
+  version_new=$(./bin/semver bump minor "$version_latest")
+fi
+
+echo "$version_new"
