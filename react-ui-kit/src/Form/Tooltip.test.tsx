eb162144948ff5670f0bf87a30c4f0735519d685
Thomas Belin <me@thomasbelin.fr>
2024-01-02 15:38:01 +0100

    fix tests

--- react-ui-kit/src/Form/Tooltip.test.tsx
+++ react-ui-kit/src/Form/Tooltip.test.tsx
@@ -19,7 +19,7 @@
 
 import * as React from 'react';
 
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom/jest-globals';
 import {render, screen} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
