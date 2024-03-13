66ae0f272a235962b9338834b272b7054e59308f
Patryk Górka <patrykbunix@gmail.com>
2024-03-13 15:21:53 +0100

    runfix: prefix cert expiry date storage (#17065)
    * runfix: prefix e2ei cert expiry date storage
    
    * test: add self user mocks to test
    
    * refactor: simplify return statement

--- src/script/E2EIdentity/E2EIdentityEnrollment.test.ts
+++ src/script/E2EIdentity/E2EIdentityEnrollment.test.ts
@@ -28,11 +28,12 @@ import {UserState} from 'src/script/user/UserState';
 import * as util from 'Util/util';
 
 import {E2EIHandler} from './E2EIdentityEnrollment';
-import {EnrollmentStore} from './Enrollment.store';
+import {getEnrollmentStore} from './Enrollment.store';
 import {OIDCServiceStore} from './OIDCService/OIDCServiceStorage';
 
 import {ConversationState} from '../conversation/ConversationState';
 import {Conversation} from '../entity/Conversation';
+import {User} from '../entity/User';
 
 jest.mock('./OIDCService', () => {
   return {
@@ -61,7 +62,13 @@ const modalMock = jest.spyOn(PrimaryModal, 'show');
 
 describe('E2EIHandler', () => {
   const params = {discoveryUrl: 'http://example.com', gracePeriodInSeconds: 30};
-  const user = {name: () => 'John Doe', username: () => 'johndoe', teamId: 'team'};
+  const user = new User('userId', 'domain');
+  user.name('John Doe');
+  user.username('johndoe');
+  user.teamId = 'team';
+
+  const selfClientId = 'clientId';
+
   const coreMock = container.resolve(Core);
 
   beforeEach(() => {
@@ -70,17 +77,21 @@ describe('E2EIHandler', () => {
     E2EIHandler.resetInstance();
     // Clear all mocks before each test
     jest.clearAllMocks();
-    EnrollmentStore.clear.deviceCreatedAt();
-    EnrollmentStore.clear.timer();
+
+    jest.spyOn(container.resolve(UserState), 'self').mockReturnValue(user);
+    const enrollmentStore = getEnrollmentStore(user.qualifiedId, selfClientId);
+
+    enrollmentStore.clear.deviceCreatedAt();
+    enrollmentStore.clear.timer();
 
     // Mock the Config to enable e2eIdentity
     (util.supportsMLS as jest.Mock).mockReturnValue(true);
 
     jest.spyOn(PrimaryModal, 'show');
 
-    jest.spyOn(container.resolve(UserState), 'self').mockReturnValue(user);
     OIDCServiceStore.store.targetURL('http://example.com');
     coreMock.key = new Uint8Array();
+    (coreMock as any).clientId = selfClientId;
   });
 
   it('should create instance with valid params', async () => {
@@ -163,7 +174,9 @@ describe('E2EIHandler', () => {
     const conversationState = container.resolve(ConversationState);
     jest.spyOn(conversationState, 'getSelfMLSConversation').mockReturnValue(new Conversation() as any);
 
-    EnrollmentStore.store.e2eiActivatedAt(Date.now());
+    const enrollmentStore = getEnrollmentStore({id: 'userId', domain: 'domain'}, 'clientId');
+    enrollmentStore.store.e2eiActivatedAt(Date.now());
+
     jest.spyOn(coreMock.service!.e2eIdentity!, 'isEnrollmentInProgress').mockResolvedValue(false);
     jest.spyOn(coreMock.service!.e2eIdentity!, 'isFreshMLSSelfClient').mockResolvedValue(false);
     jest
