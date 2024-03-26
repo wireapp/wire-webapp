f55d9d6256b765ce965ea808d4b6c06aec5d869c
Thomas Belin <me@thomasbelin.fr>
2024-03-26 15:17:14 +0100

    refactor: Use core to decrypt assets

--- src/script/assets/AssetRepository.ts
+++ src/script/assets/AssetRepository.ts
@@ -28,7 +28,6 @@ import {getLogger, Logger} from 'Util/Logger';
 import {downloadBlob, loadFileBuffer, loadImage} from 'Util/util';
 import {WebWorker} from 'Util/worker';
 
-import {decryptAesAsset} from './AssetCrypto';
 import {AssetRemoteData} from './AssetRemoteData';
 import {AssetService} from './AssetService';
 import {AssetTransferState} from './AssetTransferState';
@@ -93,18 +92,10 @@ export class AssetRepository {
 
   public async load(asset: AssetRemoteData): Promise<undefined | Blob> {
     try {
-      let plaintext: ArrayBuffer;
-      const {buffer, mimeType} = await this.loadBuffer(asset);
-      const isEncryptedAsset = !!asset.otrKey && !!asset.sha256;
+      const {response} = this.loadBuffer(asset);
+      const {buffer, mimeType} = await response;
 
-      if (isEncryptedAsset) {
-        const otrKey = asset.otrKey instanceof Uint8Array ? asset.otrKey : Uint8Array.from(Object.values(asset.otrKey));
-        const sha256 = asset.sha256 instanceof Uint8Array ? asset.sha256 : Uint8Array.from(Object.values(asset.sha256));
-        plaintext = await decryptAesAsset(buffer, otrKey.buffer, sha256.buffer);
-      } else {
-        plaintext = buffer;
-      }
-      return new Blob([new Uint8Array(plaintext)], {type: mimeType});
+      return new Blob([new Uint8Array(buffer)], {type: mimeType});
     } catch (error) {
       if (error instanceof Error) {
         const isAssetNotFound = error.message.endsWith(HTTP_STATUS.NOT_FOUND.toString());
@@ -146,11 +137,17 @@ export class AssetRepository {
   }
 
   private loadBuffer(asset: AssetRemoteData) {
-    const request = this.core.service!.asset.downloadAsset(asset.urlData, fraction => {
+    const isEncryptedAsset = !!asset.otrKey && !!asset.sha256;
+    const progressCallback = (fraction: number) => {
       asset.downloadProgress(fraction * 100);
-    });
-    asset.cancelDownload = request.cancel;
-    return request.response;
+    };
+
+    if (!isEncryptedAsset) {
+      return this.core.service!.asset.downloadRawAsset(asset.urlData, progressCallback);
+    }
+    const otrKey = asset.otrKey instanceof Uint8Array ? asset.otrKey : Uint8Array.from(Object.values(asset.otrKey));
+    const sha256 = asset.sha256 instanceof Uint8Array ? asset.sha256 : Uint8Array.from(Object.values(asset.sha256));
+    return this.core.service!.asset.downloadAsset(asset.urlData, otrKey, sha256, progressCallback);
   }
 
   public async download(asset: AssetRemoteData, fileName: string) {
