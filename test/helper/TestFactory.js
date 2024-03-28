695f2140583800dafaa32d7db1136463bc3f8005
PatrykBuniX <patrykbunix@gmail.com>
2024-03-28 15:12:37 +0100

    Revert "test: fix mocks"
    This reverts commit aed073cbfd630c217529193bdbfbd0174a8e6afc.

--- test/helper/TestFactory.js
+++ test/helper/TestFactory.js
@@ -242,7 +242,6 @@ export class TestFactory {
     await this.exposeTeamActors();
     await this.exposeEventActors();
     await this.exposeSelfActors();
-    await this.exposeCallingActors();
 
     this.conversation_service = new ConversationService(this.event_service);
 
