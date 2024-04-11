5f1e8116b323b3a0e958345f9c9f0f7beb847753
PatrykBuniX <patrykbunix@gmail.com>
2024-04-11 14:31:05 +0200

    runfix: never switch conversations after blocking a user

--- src/script/view_model/ActionsViewModel.ts
+++ src/script/view_model/ActionsViewModel.ts
@@ -81,17 +81,13 @@ export class ActionsViewModel {
    * @param nextConversationEntity Conversation to be switched to
    * @returns Resolves when the user was blocked
    */
-  readonly blockUser = (
-    userEntity: User,
-    hideConversation?: boolean,
-    nextConversationEntity?: Conversation,
-  ): Promise<void> => {
+  readonly blockUser = (userEntity: User): Promise<void> => {
     // TODO: Does the promise resolve when there is no primary action (i.e. cancel button gets clicked)?
     return new Promise(resolve => {
       PrimaryModal.show(PrimaryModal.type.CONFIRM, {
         primaryAction: {
           action: async () => {
-            await this.connectionRepository.blockUser(userEntity, hideConversation, nextConversationEntity);
+            await this.connectionRepository.blockUser(userEntity);
             resolve();
           },
           text: t('modalUserBlockAction'),
