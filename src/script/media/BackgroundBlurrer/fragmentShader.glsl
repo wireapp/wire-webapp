3f8f438e2ac14f77c0986963981a2fda14bfba86
Thomas Belin <me@thomasbelin.fr>
2024-06-10 15:27:45 +0200

    feat: Smoothing the transition between the clear and the blurred pixels

--- src/script/media/BackgroundBlurrer/fragmentShader.glsl
+++ src/script/media/BackgroundBlurrer/fragmentShader.glsl
@@ -1,4 +1,5 @@
 #define BLUR_QUALITY 8 //The higher the value, the more blur. Must be an even number.
+#define SMOOTH 3 // This will create a smooth transition between the blurred and the clear part (the higher the value, the smoother)
 
 precision mediump float;
 
@@ -14,17 +15,22 @@ varying vec2 v_texCoord;
 
 void main() {
     vec2 onePixel = vec2(1.0, 1.0) / u_textureSize;
-    // FIXME currently we choose either the pixel of the original image or we blur it.
-    // This creates a rather rough transition. We need to find a way to smooth the edges and avoid pixelated transitions.
-    if (texture2D(u_mask, v_texCoord).r == 1.0) {
-        gl_FragColor = texture2D(u_image, v_texCoord);
-    } else {
-        vec4 colorSum;
-        for (int i = -BLUR_QUALITY; i < BLUR_QUALITY; i++) {
-            for (int j = -BLUR_QUALITY; j < BLUR_QUALITY; j++) {
-                colorSum += texture2D(u_image, v_texCoord + onePixel * vec2(i, j));
-            }
+    vec4 colorSum;
+    for (int i = -BLUR_QUALITY; i < BLUR_QUALITY; i++) {
+        for (int j = -BLUR_QUALITY; j < BLUR_QUALITY; j++) {
+            colorSum += texture2D(u_image, v_texCoord + onePixel * vec2(i, j));
         }
-        gl_FragColor = vec4((colorSum / (float(BLUR_QUALITY) * float(BLUR_QUALITY) * 4.0)).rgb, 1);
     }
+    vec4 blurredPixel = vec4((colorSum / (pow(float(BLUR_QUALITY), 2.0) * 4.0)).rgb, 1);
+
+    float blend;
+    for (int i = -SMOOTH; i < SMOOTH; i++) {
+        for (int j = -SMOOTH; j < SMOOTH; j++) {
+            blend += texture2D(u_mask, v_texCoord + onePixel * vec2(i, j)).r == 1.0 ? 1.0 : 0.0;
+        }
+    }
+    blend = blend / pow(float((SMOOTH) * 2), 2.0);
+    //blend = texture2D(u_mask, v_texCoord).r == 1.0 ? 1.0 : 0.0;
+    vec4 clearPixel = texture2D(u_image, v_texCoord);
+    gl_FragColor = mix(blurredPixel, clearPixel, blend);
 }
