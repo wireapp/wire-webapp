9af2f20f245146421fc05537a9dfdf326f5be0d8
Przemyslaw Jozwik <przemyslaw.jozwik96@gmail.com>
2024-06-14 13:41:57 +0200

    add german translation

--- src/page/loader.js
+++ src/page/loader.js
@@ -1,8 +1,14 @@
 const loadingMessage = document.getElementById('loading-message');
 const HALF_MINUTE_IN_MS = 30000;
 
+const userLang = navigator.language;
+
 setTimeout(() => {
   if (loadingMessage) {
+    if (userLang.startsWith('de')) {
+      loadingMessage.innerHTML = `<p>Laden von Wire dauert länger als erwartet.</p><p>Bitte überprüfen Sie Ihre Internetverbindung.</p>`;
+    }
+
     loadingMessage.classList.add('visible');
   }
 }, HALF_MINUTE_IN_MS);
