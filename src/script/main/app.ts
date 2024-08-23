a82d32436422e84fd80103a46c6d430cecf306ab
Immad Abdul Jabbar <immad.jabbar@wire.com>
2024-08-23 15:50:06 +0200

    fix: reverted hotfix and bumped core (#17935)
    Co-authored-by: Immad Abdul Jabbar <immadabduljabbar@Immads-MBP.berlin.office.wire.link>

--- src/script/main/app.ts
+++ src/script/main/app.ts
@@ -369,14 +369,7 @@ export class App {
       onProgress(2.5);
       telemetry.timeStep(AppInitTimingsStep.RECEIVED_ACCESS_TOKEN);
 
-      let selfUser: User;
-
-      try {
-        selfUser = await this.repository.user.getSelf([{position: 'App.initiateSelfUser', vendor: 'webapp'}]);
-      } catch (error) {
-        await this.logout(SIGN_OUT_REASON.SESSION_EXPIRED, false);
-        return undefined;
-      }
+      const selfUser = await this.repository.user.getSelf([{position: 'App.initiateSelfUser', vendor: 'webapp'}]);
 
       await initializeDataDog(this.config, selfUser.qualifiedId);
       onProgress(5, t('initReceivedSelfUser', selfUser.name(), {}, true));
