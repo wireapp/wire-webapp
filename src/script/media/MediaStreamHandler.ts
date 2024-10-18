c921820dab259820d5b9a0456e42f1d909cf6ba3
Amir Ghezelbash <thisisamir98@gmail.com>
2024-10-18 15:43:58 +0330

    fix: Do not use detached window for camera source (#18174)
    * fix: Do not use detached window for camera source
    
    * Update src/script/media/MediaStreamHandler.ts
    
    Co-authored-by: Olaf Sulich <olafsulich@gmail.com>
    
    * fix lint
    
    ---------
    
    Co-authored-by: Olaf Sulich <olafsulich@gmail.com>

--- src/script/media/MediaStreamHandler.ts
+++ src/script/media/MediaStreamHandler.ts
@@ -166,7 +166,7 @@ export class MediaStreamHandler {
 
     const detachedWindow = callState.detachedWindow();
     const isInDetachedMode = callState.viewMode() === CallingViewMode.DETACHED_WINDOW;
-    const useDetachedWindowForScreenSharingSource = isInDetachedMode && detachedWindow !== null;
+    const useDetachedWindowForScreenSharingSource = screen && !video && isInDetachedMode && detachedWindow !== null;
 
     if (useDetachedWindowForScreenSharingSource) {
       callState.isScreenSharingSourceFromDetachedWindow(true);
