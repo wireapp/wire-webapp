89008059db8e2b1e72930ea852a9e4b614dfb312
Przemyslaw Jozwik <przemyslaw.jozwik96@gmail.com>
2024-12-02 14:22:10 +0100

    feat: Warning container in call

--- src/script/view_model/WarningsContainer/WarningsContainer.tsx
+++ src/script/view_model/WarningsContainer/WarningsContainer.tsx
@@ -17,7 +17,7 @@
  *
  */
 
-import React, {useEffect} from 'react';
+import {useEffect} from 'react';
 
 import cx from 'classnames';
 
@@ -28,7 +28,7 @@ import {t} from 'Util/LocalizerUtil';
 import {afterRender} from 'Util/util';
 
 import {closeWarning, useWarningsState} from './WarningsState';
-import {CONFIG, TYPE} from './WarningsTypes';
+import {CONFIG, TYPE as type} from './WarningsTypes';
 
 import {Config} from '../../Config';
 
@@ -36,30 +36,39 @@ interface WarningProps {
   onRefresh: () => void;
 }
 
-const WarningsContainer: React.FC<WarningProps> = ({onRefresh}) => {
+export const WarningsContainer = ({onRefresh}: WarningProps) => {
   const name = useWarningsState(state => state.name);
   const warnings = useWarningsState(state => state.warnings);
-  const type = TYPE;
   const visibleWarning = warnings[warnings.length - 1];
-  const warningDimmed = warnings.some(warning => CONFIG.DIMMED_MODES.includes(warning));
 
   useEffect(() => {
-    const visibleWarning = warnings[warnings.length - 1];
-    const isConnectivityRecovery = visibleWarning === TYPE.CONNECTIVITY_RECOVERY;
+    const isConnectivityRecovery = visibleWarning === type.CONNECTIVITY_RECOVERY;
     const hasOffset = warnings.length > 0 && !isConnectivityRecovery;
     const isMiniMode = CONFIG.MINI_MODES.includes(visibleWarning);
 
     const app = document.querySelector('#app');
+    const callingWrapper = document.querySelector('.video-calling-wrapper');
+
     if (app) {
       app.classList.toggle('app--small-offset', hasOffset && isMiniMode);
       app.classList.toggle('app--large-offset', hasOffset && !isMiniMode);
     }
 
-    afterRender(() => window.dispatchEvent(new Event('resize')));
-  }, [warnings]);
+    if (callingWrapper) {
+      callingWrapper.classList.toggle('app--small-offset', hasOffset && isMiniMode);
+      callingWrapper.classList.toggle('app--large-offset', hasOffset && !isMiniMode);
+    }
 
-  const brandName = Config.getConfig().BRAND_NAME;
-  const URL = Config.getConfig().URL;
+    afterRender(() => window.dispatchEvent(new Event('resize')));
+  }, [visibleWarning, warnings.length]);
+
+  if (warnings.length === 0) {
+    return null;
+  }
+
+  const warningDimmed = warnings.some(warning => CONFIG.DIMMED_MODES.includes(warning));
+
+  const {BRAND_NAME: brandName, URL} = Config.getConfig();
 
   const closeButton = (
     <button
@@ -69,10 +78,6 @@ const WarningsContainer: React.FC<WarningProps> = ({onRefresh}) => {
       onClick={closeWarning}
     />
   );
-
-  if (warnings.length === 0) {
-    return null;
-  }
 
   return (
     <div className={cx('warning', {'warning-dimmed': warningDimmed})}>
@@ -328,5 +333,3 @@ const WarningsContainer: React.FC<WarningProps> = ({onRefresh}) => {
     </div>
   );
 };
-
-export {WarningsContainer};
