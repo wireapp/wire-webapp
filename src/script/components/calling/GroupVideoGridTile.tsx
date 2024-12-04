ce47d8fd5d906e4321c1a3c8059497cec51ad8c1
Enrico Schwendig <enrico.schwendig@wire.com>
2024-12-04 15:10:00 +0100

    feat: Automatic Video Quality Adjustments [WPB-11479] (#18253)
    * feat: request the streams quality whenever the video display resolution changes
    
    * feat: add track debugger and fix load resolution on max mode

--- src/script/components/calling/GroupVideoGridTile.tsx
+++ src/script/components/calling/GroupVideoGridTile.tsx
@@ -69,15 +69,23 @@ const GroupVideoGridTile: React.FC<GroupVideoGridTileProps> = ({
   isMaximized,
   onTileDoubleClick,
 }) => {
-  const {isMuted, videoState, videoStream, blurredVideoStream, isActivelySpeaking, isAudioEstablished} =
-    useKoSubscribableChildren(participant, [
-      'isMuted',
-      'videoStream',
-      'blurredVideoStream',
-      'isActivelySpeaking',
-      'videoState',
-      'isAudioEstablished',
-    ]);
+  const {
+    isMuted,
+    videoState,
+    videoStream,
+    blurredVideoStream,
+    isActivelySpeaking,
+    isAudioEstablished,
+    isSwitchingVideoResolution,
+  } = useKoSubscribableChildren(participant, [
+    'isMuted',
+    'videoStream',
+    'blurredVideoStream',
+    'isActivelySpeaking',
+    'videoState',
+    'isAudioEstablished',
+    'isSwitchingVideoResolution',
+  ]);
   const {name} = useKoSubscribableChildren(participant?.user, ['name']);
 
   const sharesScreen = videoState === VIDEO_STATE.SCREENSHARE;
@@ -220,7 +228,7 @@ const GroupVideoGridTile: React.FC<GroupVideoGridTileProps> = ({
 
       {nameContainer}
 
-      {hasPausedVideo && (
+      {(hasPausedVideo || isSwitchingVideoResolution) && (
         <div className="group-video-grid__pause-overlay">
           <div className="background">
             <div className="background-image"></div>
@@ -232,7 +240,7 @@ const GroupVideoGridTile: React.FC<GroupVideoGridTileProps> = ({
             css={{fontsize: minimized ? '0.6875rem' : '0.875rem'}}
             data-uie-name="status-video-paused"
           >
-            {t('videoCallPaused')}
+            {hasPausedVideo ? t('videoCallPaused') : t('videoCallParticipantConnecting')}
           </div>
           {nameContainer}
         </div>
