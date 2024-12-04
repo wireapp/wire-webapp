ba37a97370f2a234548b32474dca086339b61df4
Adrian Weiß <77456193+aweiss-dev@users.noreply.github.com>
2024-12-04 17:44:52 +0100

    feat: export & import users, conversations. Harden code (#18426)

--- src/script/backup/CrossPlatformBackup/importMappers/mapEventRecord.ts
+++ src/script/backup/CrossPlatformBackup/importMappers/mapEventRecord.ts
@@ -121,13 +121,12 @@ const mapAssetMessageToEventRecord = (message: AssetBackupMessage): EventRecord 
  * @returns EventRecord or undefined
  */
 export const mapEventRecord = (message: BackupMessage): EventRecord | undefined => {
-  if (isTextContent(message.content)) {
-    return mapTextMessageToEventRecord(message as TextBackupMessage);
+  if (isAssetContent(message.content)) {
+    return mapAssetMessageToEventRecord(message as AssetBackupMessage);
   }
 
-  if (isAssetContent(message.content)) {
-    CPBLogger.log('Asset message', message);
-    return mapAssetMessageToEventRecord(message as AssetBackupMessage);
+  if (isTextContent(message.content)) {
+    return mapTextMessageToEventRecord(message as TextBackupMessage);
   }
 
   CPBLogger.error('Unknown message type', message);
