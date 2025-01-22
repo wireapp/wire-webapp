4dea181b5091d5840b1902130535c0889390ce74
Olaf Sulich <olafsulich@gmail.com>
2025-01-22 13:42:17 +0100

    refactor(AutoLinkPlugin): update email regex for improved accuracy in link detection

--- src/script/components/InputBar/components/RichTextEditor/plugins/AutoLinkPlugin/AutoLinkPlugin.tsx
+++ src/script/components/InputBar/components/RichTextEditor/plugins/AutoLinkPlugin/AutoLinkPlugin.tsx
@@ -25,7 +25,7 @@ import {
 import {URL_REGEX} from '../../utils/url';
 
 const EMAIL_REGEX =
-  /(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/;
+  /[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\.[a-zA-Z]{2,}/;
 
 const MATCHERS = [
   createLinkMatcherWithRegExp(URL_REGEX, text => {
