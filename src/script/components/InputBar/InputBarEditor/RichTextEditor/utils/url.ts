1c07f1af7fe3fa134b29fc0b5bb44539ac4e06f4
Przemyslaw Jozwik <przemyslaw.jozwik96@gmail.com>
2025-02-24 15:14:55 +0100

    fix(url): Link is broken when contains a comma

--- src/script/components/InputBar/InputBarEditor/RichTextEditor/utils/url.ts
+++ src/script/components/InputBar/InputBarEditor/RichTextEditor/utils/url.ts
@@ -20,7 +20,7 @@
 const SUPPORTED_URL_PROTOCOLS = new Set(['http:', 'https:', 'mailto:', 'sms:', 'tel:']);
 
 export const URL_REGEX =
-  /((https?:\/\/(www\.)?)|(www\.))[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)(?<![-.+():%])/;
+  /https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z]{2,6}(?:[-a-zA-Z0-9()@:%_+.~#?&//=,]*)/;
 
 export const sanitizeUrl = (url: string): string => {
   try {
