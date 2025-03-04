1bbcc829fc5c83da55e05a2737c825a663fcbdd7
Olaf Sulich <olafsulich@gmail.com>
2025-03-04 15:18:12 +0100

    refactor: remove null coalescing for API client configuration

--- src/script/service/APIClientSingleton.ts
+++ src/script/service/APIClientSingleton.ts
@@ -34,15 +34,15 @@ export class APIClient extends APIClientUnconfigured {
       },
       cells: {
         pydio: {
-          apiKey: Config.getConfig().CELLS_PYDIO_API_KEY ?? '',
-          segment: Config.getConfig().CELLS_PYDIO_SEGMENT ?? '',
-          url: Config.getConfig().CELLS_PYDIO_URL ?? '',
+          apiKey: Config.getConfig().CELLS_PYDIO_API_KEY,
+          segment: Config.getConfig().CELLS_PYDIO_SEGMENT,
+          url: Config.getConfig().CELLS_PYDIO_URL,
         },
         s3: {
-          apiKey: Config.getConfig().CELLS_S3_API_KEY ?? '',
-          bucket: Config.getConfig().CELLS_S3_BUCKET ?? '',
-          endpoint: Config.getConfig().CELLS_S3_ENDPOINT ?? '',
-          region: Config.getConfig().CELLS_S3_REGION ?? '',
+          apiKey: Config.getConfig().CELLS_S3_API_KEY,
+          bucket: Config.getConfig().CELLS_S3_BUCKET,
+          endpoint: Config.getConfig().CELLS_S3_ENDPOINT,
+          region: Config.getConfig().CELLS_S3_REGION,
         },
       },
     });
