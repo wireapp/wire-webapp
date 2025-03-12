7668adcb69b02e93774590e74941d9bd45f491da
Przemysław Jóźwik <przemyslaw.jozwik96@gmail.com>
2025-03-12 12:12:21 +0100

    feat(api-client): Domain registration (#6927)
    * feat: add api wrappers for enterprise login v2 [WPB-14366]
    
    * feat(api-client): Domain registration
    
    * improvements
    
    * cr fixes
    
    ---------
    
    Co-authored-by: Immad Abdul Jabbar <immadabduljabbar@gmail.com>

--- api-client/src/team/sso/index.ts
+++ api-client/src/team/sso/index.ts
@@ -1,6 +1,6 @@
 /*
  * Wire
- * Copyright (C) 2019 Wire Swiss GmbH
+ * Copyright (C) 2025 Wire Swiss GmbH
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,20 +17,5 @@
  *
  */
 
-export interface IdentityProvider {
-  extraInfo: {
-    apiVersion: string;
-    handle: string;
-    oldIssuers: [];
-    replacedBy: unknown;
-    team: string;
-  };
-  id: string;
-  metadata: IdentityProviderMetaData;
-}
-
-export interface IdentityProviderMetaData {
-  certAuthnResponse: string[];
-  issuer: string;
-  requestURI: string;
-}
+export * from './SSOAPI';
+export * from './SSOAPI.types';
