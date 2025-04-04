8172170f8326c2b6e49df02d2216e3fa59878a7f
Immad Abdul Jabbar <immadabduljabbar@gmail.com>
2025-04-04 10:46:35 +0200

    fixed test

--- src/script/entity/Conversation.ts
+++ src/script/entity/Conversation.ts
@@ -214,7 +214,7 @@ export class Conversation {
     this.teamId = undefined;
     this.type = ko.observable();
     this.groupConversationType = ko.observable<GROUP_CONVERSATION_TYPE>(GROUP_CONVERSATION_TYPE.GROUP_CONVERSATION);
-    this.conversationModerator = ko.observable(() => ADD_PERMISSION.EVERYONE);
+    this.conversationModerator = ko.observable<ADD_PERMISSION>(ADD_PERMISSION.EVERYONE);
 
     this.isLastMessageVisible = ko.observable(true);
     this.isLoadingMessages = ko.observable(false);
