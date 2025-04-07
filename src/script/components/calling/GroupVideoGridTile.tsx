615138ec1c55664fd2b7e9158d6224380bd24c7a
Enrico Schwendig <enrico.schwendig@wire.com>
2025-04-07 16:04:15 +0200

    feat: simulcast (#18944)
    * Revert "chore(AVS): remove simulcast support [WPB-16287] (#18798)"
    
    This reverts commit 7dbcc1ab216f12fbf47b6a8a8ee0f2a8e3a75601.
    
    * add adaptive Video Streaming (simulcast) to web
    
    * feat: bump AVS version 10.0.35
    
    ---------
    
    Co-authored-by: v-gira <virgile.girardin@gmail.com>
    Co-authored-by: Virgile <78490891+V-Gira@users.noreply.github.com>

--- src/script/components/calling/GroupVideoGridTile.tsx
+++ src/script/components/calling/GroupVideoGridTile.tsx
@@ -69,16 +69,26 @@ const GroupVideoGridTile: React.FC<GroupVideoGridTileProps> = ({
   isMaximized,
   onTileDoubleClick,
 }) => {
-  const {isMuted, videoState, handRaisedAt, videoStream, blurredVideoStream, isActivelySpeaking, isAudioEstablished} =
-    useKoSubscribableChildren(participant, [
-      'isMuted',
-      'handRaisedAt',
-      'videoStream',
-      'blurredVideoStream',
-      'isActivelySpeaking',
-      'videoState',
-      'isAudioEstablished',
-    ]);
+  const {
+    isMuted,
+    videoState,
+    handRaisedAt,
+    videoStream,
+    blurredVideoStream,
+    isActivelySpeaking,
+    isAudioEstablished,
+    isSwitchingVideoResolution,
+  } = useKoSubscribableChildren(participant, [
+    'isMuted',
+    'handRaisedAt',
+    'videoStream',
+    'blurredVideoStream',
+    'isActivelySpeaking',
+    'videoState',
+    'isAudioEstablished',
+    'isSwitchingVideoResolution',
+  ]);
+
   const {name} = useKoSubscribableChildren(participant?.user, ['name']);
 
   const sharesScreen = videoState === VIDEO_STATE.SCREENSHARE;
@@ -223,7 +233,7 @@ const GroupVideoGridTile: React.FC<GroupVideoGridTileProps> = ({
 
       {nameContainer}
 
-      {hasPausedVideo && (
+      {(hasPausedVideo || isSwitchingVideoResolution) && (
         <div className="group-video-grid__pause-overlay">
           <div className="background">
             <div className="background-image"></div>
@@ -235,7 +245,7 @@ const GroupVideoGridTile: React.FC<GroupVideoGridTileProps> = ({
             css={{fontsize: minimized ? '0.6875rem' : '0.875rem'}}
             data-uie-name="status-video-paused"
           >
-            {t('videoCallPaused')}
+            {hasPausedVideo ? t('videoCallPaused') : t('videoCallParticipantConnecting')}
           </div>
           {nameContainer}
         </div>
