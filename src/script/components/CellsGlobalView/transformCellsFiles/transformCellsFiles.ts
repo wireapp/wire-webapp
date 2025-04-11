ddab21425420f443e2cc053f3874e875e3e981b2
Olaf Sulich <olafsulich@gmail.com>
2025-04-11 15:03:46 +0200

    refactor(transformCellsFiles): streamline file transformation logic by removing unnecessary parentheses and improving readability

--- src/script/components/CellsGlobalView/transformCellsFiles/transformCellsFiles.ts
+++ src/script/components/CellsGlobalView/transformCellsFiles/transformCellsFiles.ts
@@ -25,28 +25,24 @@ import {formatBytes} from 'Util/util';
 import {CellFile} from '../common/cellFile/cellFile';
 
 export const transformCellsFiles = (nodes: RestNode[]): CellFile[] => {
-  return (
-    nodes
-      .filter(node => node.Type === 'LEAF')
-      .map(node => ({
-        id: node.Uuid,
-        owner: getOwner(node),
-        conversationName: node.ContextWorkspace?.Label || '',
-        mimeType: node.ContentType,
-        name: getFileName(node.Path),
-        sizeMb: getFileSize(node),
-        previewImageUrl: getPreviewImageUrl(node),
-        uploadedAtTimestamp: getUploadedAtTimestamp(node),
-        fileUrl: node.PreSignedGET?.Url,
-        publicLink: {
-          alreadyShared: !!node.Shares?.[0].Uuid,
-          uuid: node.Shares?.[0].Uuid || '',
-          url: undefined,
-        },
-      }))
-      // eslint-disable-next-line id-length
-      .sort((a, b) => b.uploadedAtTimestamp - a.uploadedAtTimestamp)
-  );
+  return nodes
+    .filter(node => node.Type === 'LEAF')
+    .map(node => ({
+      id: node.Uuid,
+      owner: getOwner(node),
+      conversationName: node.ContextWorkspace?.Label || '',
+      mimeType: node.ContentType,
+      name: getFileName(node.Path),
+      sizeMb: getFileSize(node),
+      previewImageUrl: getPreviewImageUrl(node),
+      uploadedAtTimestamp: getUploadedAtTimestamp(node),
+      fileUrl: node.PreSignedGET?.Url,
+      publicLink: {
+        alreadyShared: !!node.Shares?.[0].Uuid,
+        uuid: node.Shares?.[0].Uuid || '',
+        url: undefined,
+      },
+    }));
 };
 
 const getFileName = (filePath: string): string => {
