0725dc10d9f89a8a621bf00ab12f030de54ddecd
Olaf Sulich <olafsulich@gmail.com>
2025-04-17 12:33:54 +0200

    feat(react-ui-kit/icon): sidebar icons [WPB-15678] (#7028)
    * feat(react-ui-kit/icon): sidebar icons
    
    * feat(react-ui-kit/icon): rename SidebarUnactiveIcon to SidebarInactiveIcon

---
+++ react-ui-kit/src/Icon/SidebarInactiveIcon.tsx
@@ -0,0 +1,32 @@
+/*
+ * Wire
+ * Copyright (C) 2025 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import {SVGIcon, SVGIconProps} from './SVGIcon';
+
+export const SidebarInactiveIcon = (props: SVGIconProps) => (
+  <SVGIcon realWidth={16} realHeight={16} {...props} fill="none">
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M14 2H2V14H14V2ZM2 0C0.895431 0 0 0.895431 0 2V14C0 15.1046 0.895431 16 2 16H14C15.1046 16 16 15.1046 16 14V2C16 0.895431 15.1046 0 14 0H2Z"
+      fill="black"
+    />
+    <path d="M7 0H9V16H7V0Z" fill="black" />
+  </SVGIcon>
+);
