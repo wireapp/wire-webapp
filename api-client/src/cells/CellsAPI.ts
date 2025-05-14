e1c39e2a1020ca93039d39279ca522c848ebfdf3
Olaf Sulich <olafsulich@gmail.com>
2025-05-14 14:12:19 +0200

    feat(api-client/cells): add type param for getting nodes (#7046)

--- api-client/src/cells/CellsAPI.ts
+++ api-client/src/cells/CellsAPI.ts
@@ -260,18 +260,20 @@ export class CellsAPI {
     return result.data;
   }
 
-  async getAllFiles({
+  async getAllNodes({
     path,
     limit = DEFAULT_LIMIT,
     offset = DEFAULT_OFFSET,
     sortBy = DEFAULT_SEARCH_SORT_FIELD,
     sortDirection = DEFAULT_SEARCH_SORT_DIRECTION,
+    type,
   }: {
     path: string;
     limit?: number;
     offset?: number;
     sortBy?: string;
     sortDirection?: SortDirection;
+    type?: RestIncomingNode['Type'];
   }): Promise<RestNodeCollection> {
     if (!this.client || !this.storageService) {
       throw new Error(CONFIGURATION_ERROR);
@@ -289,23 +291,31 @@ export class CellsAPI {
       request.SortDirDesc = sortDirection === 'desc';
     }
 
+    if (type) {
+      request.Filters = {
+        Type: type,
+      };
+    }
+
     const result = await this.client.lookup(request);
 
     return result.data;
   }
 
-  async searchFiles({
+  async searchNodes({
     phrase,
     limit = DEFAULT_LIMIT,
     offset = DEFAULT_OFFSET,
     sortBy = DEFAULT_SEARCH_SORT_FIELD,
     sortDirection = DEFAULT_SEARCH_SORT_DIRECTION,
+    type,
   }: {
     phrase: string;
     limit?: number;
     offset?: number;
     sortBy?: string;
     sortDirection?: SortDirection;
+    type?: RestIncomingNode['Type'];
   }): Promise<RestNodeCollection> {
     if (!this.client || !this.storageService) {
       throw new Error(CONFIGURATION_ERROR);
@@ -320,9 +330,16 @@ export class CellsAPI {
       Limit: `${limit}`,
       Offset: `${offset}`,
     };
+
     if (sortBy) {
       request.SortField = sortBy;
       request.SortDirDesc = sortDirection === 'desc';
+    }
+
+    if (type) {
+      request.Filters = {
+        Type: type,
+      };
     }
 
     const result = await this.client.lookup(request);
