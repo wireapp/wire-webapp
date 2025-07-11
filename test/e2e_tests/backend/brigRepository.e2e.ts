08626a8174eddea804aa936134cc9725fe24ce2d
Ivan Skvortsov <ivan.skvortsov@wire.com>
2025-07-11 16:44:50 +0200

    Added Basic auth to calls in the previous commit

--- test/e2e_tests/backend/brigRepository.e2e.ts
+++ test/e2e_tests/backend/brigRepository.e2e.ts
@@ -48,12 +48,28 @@ export class BrigRepositoryE2E extends BackendClientE2E {
   }
 
   public async unlockConferenceCallingFeature(teamId: string) {
-    await this.axiosInstance.put(`i/teams/${teamId}/features/conferenceCalling/unlocked`, {});
+    await this.axiosInstance.put(
+      `i/teams/${teamId}/features/conferenceCalling/unlocked`,
+      {},
+      {
+        headers: {
+          Authorization: `Basic ${BASIC_AUTH}`,
+        },
+      },
+    );
   }
 
   public async enableConferenceCallingBackdoorViaBackdoorTeam(teamId: string) {
-    await this.axiosInstance.patch(`i/teams/${teamId}/features/conferenceCalling`, {
-      status: 'enabled',
-    });
+    await this.axiosInstance.patch(
+      `i/teams/${teamId}/features/conferenceCalling`,
+      {
+        status: 'enabled',
+      },
+      {
+        headers: {
+          Authorization: `Basic ${BASIC_AUTH}`,
+        },
+      },
+    );
   }
 }
