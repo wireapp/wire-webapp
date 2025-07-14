cc0814c684cc4677faa798b2292abd04dcd7d41c
Immad Abdul Jabbar <immadabduljabbar@gmail.com>
2025-07-14 13:55:43 +0200

    fixed e2e

--- test/e2e_tests/specs/criticalFlow/conversationManagement-TC-8639.spec.ts
+++ test/e2e_tests/specs/criticalFlow/conversationManagement-TC-8639.spec.ts
@@ -22,7 +22,7 @@ import {test, expect} from '../../test.fixtures';
 import {addCreatedTeam, addCreatedUser, tearDown} from '../../utils/tearDownUtil';
 
 test('Conversation Management', {tag: ['@TC-8636', '@crit-flow']}, async ({pages, api}) => {
-  test.slow(); // Increasing test timeout to 90 seconds to accommodate the full flow
+  test.setTimeout(120000); // Set test timeout to 120 seconds
 
   // Generating test data
   const owner = getUser();
