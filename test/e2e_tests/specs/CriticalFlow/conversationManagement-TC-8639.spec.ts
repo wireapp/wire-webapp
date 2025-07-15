e02c37458e86f0b844a48252ca2a66683a557967
Immad Abdul Jabbar <immad.jabbar@wire.com>
2025-07-15 17:42:39 +0200

    test(e2e): adjust timeout for conversation management e2e [WPB-18713] (#19252)
    * fix: adjust timeout for conversation management e2e [WPB-18713]
    
    * fixed timeout
    
    * fixed timeout

--- test/e2e_tests/specs/CriticalFlow/conversationManagement-TC-8639.spec.ts
+++ test/e2e_tests/specs/CriticalFlow/conversationManagement-TC-8639.spec.ts
@@ -30,7 +30,7 @@ const teamName = 'Conversation Management';
 const conversationName = 'Test Conversation';
 
 test('Conversation Management', {tag: ['@TC-8636', '@crit-flow-web']}, async ({pages, api}) => {
-  test.setTimeout(300000); // Set test timeout to 5 minutes
+  test.setTimeout(120000); // Increasing test timeout to 2 minutes to accommodate the full flow
 
   await test.step('Preconditions: Team owner created a team with 5 members', async () => {
     await api.createTeamOwner(owner, teamName);
