91ab3009fb136941a8c0ae0b563ae31baca172e8
v-gira <virgile.girardin@gmail.com>
2025-08-04 16:35:22 +0200

    runfix(CPB): keep conversation order after restoring backup [WPB-18298]

--- src/script/repositories/backup/CrossPlatformBackup/data.schema.ts
+++ src/script/repositories/backup/CrossPlatformBackup/data.schema.ts
@@ -26,6 +26,7 @@ export const QualifiedIdSchema = zod.object({
 
 export const ConversationTableEntrySchema = QualifiedIdSchema.extend({
   name: zod.string().nullable().optional(),
+  last_event_timestamp: zod.number(),
 });
 export type ConversationTableEntry = zod.infer<typeof ConversationTableEntrySchema>;
 
