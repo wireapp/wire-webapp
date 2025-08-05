4d220307d7a0228fcb7fecb5d9d0e078cb24bc14
Ivan Skvortsov <ivan.skvortsov@wire.com>
2025-08-05 20:38:33 +0200

    chore(e2e): Added Messages in Groups TC-8751 test (#19308)
    * chore(e2e): Added Messages in Groups TC-8751 test
    
    * Applied review suggestions
    
    * Reverted asserting for TC-8635
    
    * Reverted timeout for TC-8635
    
    * Added 2 sec wait after connection request
    
    * Disabled some steps due to [WPB-19274]

--- test/e2e_tests/specs/CriticalFlow/joinTeam-TC-8635.spec.ts
+++ test/e2e_tests/specs/CriticalFlow/joinTeam-TC-8635.spec.ts
@@ -146,7 +146,7 @@ test(
     });
 
     await test.step('Team owner mentions A', async () => {
-      await adminPageManager.webapp.pages.conversation().sendMessageWithUserMention(memberA.qualifiedId!.id);
+      await adminPageManager.webapp.pages.conversation().sendMessageWithUserMention(memberA.fullName);
     });
 
     await test.step('A sees the mention in the chat', async () => {
