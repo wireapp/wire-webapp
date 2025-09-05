b90ed893bea74ae4cacc881f33988152c3532371
Klaus Boldt <klaus.boldt@mail.schwarz>
2025-09-05 10:26:16 +0200

    wip

---
+++ test/e2e_tests/specs/Accessibility/Accessibility.spec.ts
@@ -0,0 +1,87 @@
+/*
+ * Wire
+ * Copyright (C) 2025 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import {getUser} from 'test/e2e_tests/data/user';
+import {PageManager} from 'test/e2e_tests/pageManager';
+import {addCreatedUser, tearDownAll} from 'test/e2e_tests/utils/tearDown.util';
+import {createGroup, inviteMembers, loginUser} from 'test/e2e_tests/utils/userActions';
+
+import {test} from '../../test.fixtures';
+
+test.describe('Accessibility', () => {
+  let owner = getUser();
+  const members = Array.from({length: 2}, () => getUser());
+  const [memberA, memberB] = members;
+  const teamName = 'Accessibility';
+  const conversationName = 'AccTest';
+  test.beforeAll(async ({api}) => {
+    const user = await api.createTeamOwner(owner, teamName);
+    owner = {...owner, ...user};
+    await inviteMembers(members, owner, api);
+    // register credentials for cleanup later
+    addCreatedUser(owner);
+    addCreatedUser(memberA);
+    addCreatedUser(memberB);
+  });
+
+  test(
+    'I want to see typing indicator in group conversation',
+    {tag: ['@TC-46', '@regression']},
+    async ({pageManager: memberPageManagerA, browser}) => {
+      const memberContextB = await browser.newContext();
+      const memberPage = await memberContextB.newPage();
+      const memberPageManagerB = new PageManager(memberPage);
+      const memberAPages = memberPageManagerA.webapp.pages;
+      const memberPagesB = memberPageManagerB.webapp.pages;
+
+      // log bouth in
+      await loginUser(memberA, memberPageManagerA);
+      await loginUser(memberB, memberPageManagerB);
+
+      await createGroup(memberPageManagerA, conversationName, members);
+
+      await memberAPages.conversationList().clickCreateGroup();
+      await memberAPages.groupCreation().setGroupName(conversationName);
+      await memberAPages.startUI().selectUsers([memberB.username]);
+
+      await memberPageManagerA.webapp.pages.conversationList().openConversation(conversationName);
+
+      await memberPagesB.conversationList().openConversation(conversationName);
+
+      // user a starts typing
+      // user a turn off typing indicator?
+      // user a starts typing -> user b don't see typing
+    },
+  );
+
+  test(
+    'I want to see collapsed view when app is narrow',
+    {tag: ['@TC-48', '@regression']},
+    async ({pageManager}) => {},
+  );
+  test(
+    'I should not lose a drafted message when switching between conversations in collapsed view',
+    {tag: ['@TC-51', '@regression']},
+    async ({pageManager}) => {},
+  );
+
+  test.afterAll(async ({api}) => {
+    await tearDownAll(api);
+  });
+});
