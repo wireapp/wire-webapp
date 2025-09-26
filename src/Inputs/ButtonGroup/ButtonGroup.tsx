a3924f1c025fb17fd088d8f2909c30d4497cee8e
Zafar Saeed Khan <zafarsaeed@outlook.com>
2025-09-26 10:00:24 +0200

    feat: update ButtonGroup to adapt colours according to dark/light theme (#7378)

--- src/Inputs/ButtonGroup/ButtonGroup.tsx
+++ src/Inputs/ButtonGroup/ButtonGroup.tsx
@@ -21,28 +21,22 @@ import {ButtonHTMLAttributes, forwardRef, ReactNode} from 'react';
 
 import {CSSObject} from '@emotion/react';
 
-import {COLOR_V2} from '../../Identity';
 import {Theme} from '../../Identity/Theme';
-import {IconButtonProps} from '../IconButton';
+import {Button, ButtonProps, ButtonVariant} from '../Button';
+import {buttonStyle} from '../Button/Button.styles';
 
 const buttonGroupStyle: (theme: Theme) => CSSObject = () => ({
   display: 'flex',
   alignItems: 'center',
 });
 
-const buttonStyle: <T>(theme: Theme, props: IconButtonProps<T>) => CSSObject = (_, {disabled = false}) => ({
+const groupedButtonStyle: <T>(theme: Theme, props: ButtonProps<T>) => CSSObject = (theme, props) => ({
   height: '32px',
   borderRadius: '12px',
   padding: '0 12px',
-  background: COLOR_V2.WHITE,
-  border: `1px solid ${COLOR_V2.GRAY_40}`,
   cursor: 'pointer',
   display: 'flex',
   alignItems: 'center',
-  backgroundColor: disabled ? COLOR_V2.GRAY_20 : COLOR_V2.WHITE,
-  svg: {
-    fill: disabled ? COLOR_V2.GRAY_70 : COLOR_V2.BLACK,
-  },
   '&:not(:last-child)': {
     borderTopRightRadius: '0',
     borderBottomRightRadius: '0',
@@ -54,28 +48,12 @@ const buttonStyle: <T>(theme: Theme, props: IconButtonProps<T>) => CSSObject = (
   '&:first-child:last-child': {
     borderRadius: '0',
   },
-  ...(!disabled && {
-    '&:hover, &:focus': {
-      backgroundColor: COLOR_V2.GRAY_20,
-    },
-    '&:hover': {
-      borderColor: COLOR_V2.GRAY_50,
-    },
-    '&:focus': {
-      borderColor: COLOR_V2.GRAY_60,
-    },
-    '&:active': {
-      backgroundColor: COLOR_V2.BLUE_LIGHT_50,
-      borderColor: COLOR_V2.BLUE_LIGHT_300,
-      svg: {
-        fill: COLOR_V2.BLUE,
-      },
-    },
-  }),
+  ...buttonStyle(theme, props),
 });
 
 type GroupedButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   icon?: ReactNode;
+  variant?: ButtonVariant;
 };
 
 interface ButtonGroupProps {
@@ -87,9 +65,9 @@ const ButtonGroup = ({children}: ButtonGroupProps) => (
   </div>
 );
 
-const Button = forwardRef<HTMLButtonElement, GroupedButtonProps>(({children, icon, ...props}, ref) => {
+const GroupedButton = forwardRef<HTMLButtonElement, GroupedButtonProps>(({children, icon, ...props}, ref) => {
   return (
-    <button ref={ref} css={(theme: Theme) => buttonStyle(theme, props)} {...props}>
+    <Button ref={ref} css={(theme: Theme) => groupedButtonStyle(theme, props)} {...props}>
       {icon}
       {children && (
         <span
@@ -104,12 +82,12 @@ const Button = forwardRef<HTMLButtonElement, GroupedButtonProps>(({children, ico
           {children}
         </span>
       )}
-    </button>
+    </Button>
   );
 });
 
-Button.displayName = 'Button';
+GroupedButton.displayName = 'Button';
 
-ButtonGroup.Button = Button;
+ButtonGroup.Button = GroupedButton;
 
 export {ButtonGroup};
