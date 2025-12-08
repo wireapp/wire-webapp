81bb07d4f5072fea264d886b3e9f165f88fb29c8
Mark Brockhoff <mark.brockhoff@mail.schwarz>
2025-12-08 15:02:22 +0100

    test(TC-2802): Verify that current call is terminated if you want to call someone else (as caller)

---
+++ test/e2e_tests/specs/Calling/calling.spec.ts
@@ -0,0 +1,65 @@
+/*
+ * Wire
+ * Copyright (C) 2025 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import {User} from 'test/e2e_tests/data/user';
+import {PageManager} from 'test/e2e_tests/pageManager';
+import {test, withConnectedUser, withLogin, expect} from 'test/e2e_tests/test.fixtures';
+
+test.describe('Calling', () => {
+  let userA: User;
+  let userB: User;
+  let userC: User;
+
+  test.beforeEach(async ({createTeam}) => {
+    const team = await createTeam('Team Call Team', {withMembers: 2});
+    userA = team.owner;
+    userB = team.members[0];
+    userC = team.members[1];
+  });
+
+  test(
+    'Verify that current call is terminated if you want to call someone else (as caller)',
+    {tag: ['@TC-2802', '@regression']},
+    async ({createPage}) => {
+      const [{pages: userAPages, modals: userAModals}, {pages: userBPages}] = await Promise.all([
+        PageManager.from(createPage(withLogin(userA), withConnectedUser(userB), withConnectedUser(userC))).then(
+          pm => pm.webapp,
+        ),
+        PageManager.from(createPage(withLogin(userB))).then(pm => pm.webapp),
+      ]);
+
+      // User A has a call with user B
+      await userAPages.conversationList().openConversation(userB.fullName);
+      await userAPages.conversation().clickCallButton();
+      await userBPages.calling().clickAcceptCallButton();
+      await expect(userBPages.calling().callCell).toBeVisible();
+
+      // User A starts a call with user C while in a call with user B
+      await userAPages.conversationList().openConversation(userC.fullName);
+      await userAPages.conversation().clickCallButton();
+
+      // A modal is shown prompting him to confirm before cancelling the ongoing call
+      await expect(userAModals.confirm().modalTitle).toContainText('Hang up current call?');
+      await userAModals.confirm().clickAction();
+
+      // The call with user B should be terminated
+      await expect(userBPages.calling().callCell).not.toBeVisible();
+    },
+  );
+});
