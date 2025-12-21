d82fc0eb6ef172c2ea26efdc191581274375f1ee
Adrian Weiss <github@weissadrian.com>
2025-12-21 22:43:59 +0100

    chore(nx): codecov respects monorepo

--- apps/server/jest.config.js
+++ apps/server/jest.config.js
@@ -19,6 +19,7 @@
 
 module.exports = {
   preset: '../../jest.preset.js',
+  collectCoverageFrom: ['src/**/*.{js,ts}', '!src/**/*.d.ts', '!src/**/*.test.{js,ts}'],
   moduleDirectories: ['node_modules', __dirname],
   testEnvironment: 'node',
   testPathIgnorePatterns: ['<rootDir>/dist'],
