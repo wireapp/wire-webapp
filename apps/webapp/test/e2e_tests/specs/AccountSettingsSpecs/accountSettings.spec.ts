21595d0272bea748857a904067a7b1ddcad9c8ed
Mark Brockhoff <mark.brockhoff@mail.schwarz>
2026-01-27 17:26:52 +0100

    test(WPB-22420): fix TC-60 for SCIM integration

--- apps/webapp/test/e2e_tests/specs/AccountSettingsSpecs/accountSettings.spec.ts
+++ apps/webapp/test/e2e_tests/specs/AccountSettingsSpecs/accountSettings.spec.ts
@@ -17,13 +17,13 @@
  *
  */
 
-import {getUser, User} from 'test/e2e_tests/data/user';
+import {getUser} from 'test/e2e_tests/data/user';
 import {PageManager} from 'test/e2e_tests/pageManager';
 import {bootstrapTeamForTesting, completeLogin} from 'test/e2e_tests/utils/setup.util';
 import {tearDownAll} from 'test/e2e_tests/utils/tearDown.util';
 import {createChannel, createGroup, loginUser} from 'test/e2e_tests/utils/userActions';
 
-import {test, expect} from '../../test.fixtures';
+import {test, expect, LOGIN_TIMEOUT} from '../../test.fixtures';
 
 test.describe('account settings', () => {
   let owner = getUser();
@@ -87,37 +87,37 @@ test.describe('account settings', () => {
   test(
     'I should not be able to change email of user managed by SCIM',
     {tag: ['@TC-60', '@regression']},
-    async ({pageManager, context}) => {
-      const {components, pages, modals} = pageManager.webapp;
-      // use an extra account
-      const ssoUser: User = getUser({
+    async ({context, createPage}) => {
+      const ssoUser = getUser({
         email: process.env.SCIM_USER_SSO_CODE,
         username: process.env.SCIM_USER_EMAIL,
         password: process.env.SCIM_USER_PASSWORD,
       });
+
+      const pageManager = PageManager.from(await createPage(context));
       await pageManager.openMainPage();
 
-      await pages.singleSignOn().isSSOPageVisible();
-
+      const {pages, modals, components} = pageManager.webapp;
       const [newPage] = await Promise.all([
         context.waitForEvent('page'),
         pages.singleSignOn().enterEmailOnSSOPage(ssoUser.email),
       ]);
 
-      await newPage.waitForLoadState();
-      await newPage.getByRole('textbox', {name: 'Username'}).fill(ssoUser.username);
+      await newPage.getByRole('textbox', {name: 'Username'}).fill(ssoUser.username, {timeout: 20_000});
       await newPage.getByRole('textbox', {name: 'Password'}).fill(ssoUser.password);
       await newPage.getByRole('button', {name: 'Sign In'}).click();
+      await expect(components.conversationSidebar().sidebar, `Login took more than ${LOGIN_TIMEOUT}s`).toBeVisible({
+        timeout: LOGIN_TIMEOUT,
+      });
 
-      if (await pages.historyInfo().isButtonVisible()) {
-        await pages.historyInfo().clickConfirmButton();
-      }
-      await components.conversationSidebar().isPageLoaded();
-
-      if (await modals.dataShareConsent().isModalPresent()) {
-        await modals.dataShareConsent().clickDecline();
-      }
+      await pages.sidebar().clickPreferencesButton();
+      await pages.settings().accountButton.click();
       await expect(pages.account().emailDisplay).toHaveCount(0);
+
+      // Log out the user to ensure the previous login won't affect future runs
+      await pages.account().logoutButton.click();
+      await modals.confirmLogout().deleteDeviceCheckbox.click();
+      await modals.confirmLogout().clickAction();
     },
   );
 
