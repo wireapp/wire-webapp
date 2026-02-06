dd5c2dba10fea73af3ef570cea71b7b426d7ea47
Mark Brockhoff <95471369+markbrockhoff@users.noreply.github.com>
2026-02-06 16:12:48 +0100

    test: create util to intercept notifications [WPB-22882] (#20320)

---
+++ apps/webapp/test/e2e_tests/utils/mockNotifications.util.ts
@@ -0,0 +1,70 @@
+import {type Page} from 'playwright/test';
+
+declare global {
+  interface Window {
+    // Declare the custom variable used to store the intercepted notifications on the window object
+    __wireNotifications: (typeof window)['wire']['app']['repository']['notification']['notifications'];
+  }
+}
+
+/**
+ * Browser script to intercept all sent notifications and store them in a global variable for later use.
+ *
+ * **Important**: Don't use this function within playwright directly, only in the browser e.g. within `page.evaluate()`.
+ */
+const stubNotifications = () => {
+  if (!window.wire?.app?.repository?.notification?.notifications)
+    throw new Error("Can't stub notifications - Notifications array wasn't initialized yet");
+
+  window.__wireNotifications ??= [];
+
+  // Mock the push function of the notifications array
+  window.wire.app.repository.notification.notifications.push = (...notifications) => {
+    // Every time one or more notifications are pushed to the array also keep a reference to them in the global variable
+    window.__wireNotifications.push(...notifications);
+
+    // Use the native push function of the prototype to not modify the behavior
+    return Array.prototype.push.apply(window.wire.app.repository.notification.notifications, notifications);
+  };
+};
+
+/**
+ * Retrieve the notifications stored in the global variable of the page's window.
+ * The function serializes the important properties of the Notification object to pass them to the playwright environment.
+ */
+const getNotifications = async (page: Page) => {
+  return await page.evaluate(() =>
+    /**
+     * It's necessary to construct a new object containing the important properties of the notification
+     * since the class would otherwise be serialized as empty object.
+     */
+    window.__wireNotifications.map(notification => ({
+      title: notification.title,
+      body: notification.body,
+      data: notification.data,
+    })),
+  );
+};
+
+/**
+ * Start intercepting the notifications pushed for the given page.
+ * @example
+ * ```ts
+ * const { getNotifications } = await interceptNotifications(userBPage);
+ *
+ * // Send a notification to userB
+ *
+ * await expect.poll(() => getNotifications()).toHaveLength(1):
+ * ```
+ */
+export const interceptNotifications = async (page: Page) => {
+  await page.evaluate(stubNotifications);
+
+  return {
+    /**
+     * Async function to get the notifications the intercepted page received so far.
+     * Pass this to `expect.poll()` to avoid flake due to timing issues.
+     */
+    getNotifications: () => getNotifications(page),
+  };
+};
