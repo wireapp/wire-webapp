58375394ce589ae6adcf186ed908436ebef4e310
zhanna_chaikovska <zhanna.chaykovska@gmail.com>
2026-02-09 15:08:23 +0100

    test(WPB-19959): add markdown regression e2e tests

---
+++ apps/webapp/test/e2e_tests/specs/Markdown/markdown.spec.ts
@@ -0,0 +1,162 @@
+/*
+ * Wire
+ * Copyright (C) 2025 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import {User} from 'test/e2e_tests/data/user';
+import {PageManager} from 'test/e2e_tests/pageManager';
+import {test, expect, withConnectedUser, withLogin} from 'test/e2e_tests/test.fixtures';
+
+test.describe('Markdown', () => {
+  let userA: User;
+  let userB: User;
+
+  test.beforeEach(async ({createTeam}) => {
+    const team = await createTeam('Test Team', {withMembers: 1});
+    userA = team.owner;
+    userB = team.members[0];
+  });
+
+  test('I want to write a bold message', {tag: ['@TC-1314', '@regression']}, async ({createPage}) => {
+    const [userAPages, userBPages] = await Promise.all([
+      PageManager.from(createPage(withLogin(userA), withConnectedUser(userB))).then(pm => pm.webapp.pages),
+      PageManager.from(createPage(withLogin(userB), withConnectedUser(userA))).then(pm => pm.webapp.pages),
+    ]);
+
+    await userAPages.conversation().sendMessage('**Bold Message from User A**');
+
+    for (const pages of [userAPages, userBPages]) {
+      const message = pages.conversation().getMessage({sender: userA});
+      await expect(message).toBeVisible();
+      await expect(message.locator('strong, b')).toHaveText('Bold Message from User A');
+    }
+  });
+
+  test('I want to write a emphasized message', {tag: ['@TC-1314', '@regression']}, async ({createPage}) => {
+    const [userAPages, userBPages] = await Promise.all([
+      PageManager.from(createPage(withLogin(userA), withConnectedUser(userB))).then(pm => pm.webapp.pages),
+      PageManager.from(createPage(withLogin(userB), withConnectedUser(userA))).then(pm => pm.webapp.pages),
+    ]);
+
+    await userAPages.conversation().sendMessage('*Emphasized message from User A*');
+
+    for (const pages of [userAPages, userBPages]) {
+      const message = pages.conversation().getMessage({sender: userA});
+      await expect(message).toBeVisible();
+      await expect(message.locator('em, i')).toHaveText('Emphasized message from User A');
+    }
+  });
+
+  test('I want to write a code message', {tag: ['@TC-1315', '@regression']}, async ({createPage}) => {
+    const [userAPages, userBPages] = await Promise.all([
+      PageManager.from(createPage(withLogin(userA), withConnectedUser(userB))).then(pm => pm.webapp.pages),
+      PageManager.from(createPage(withLogin(userB), withConnectedUser(userA))).then(pm => pm.webapp.pages),
+    ]);
+
+    await userAPages.conversation().sendMessage('`Code message from User A`');
+
+    for (const pages of [userAPages, userBPages]) {
+      const message = pages.conversation().getMessage({sender: userA});
+      await expect(message).toBeVisible();
+      await expect(message.locator('code')).toContainText('Code message from User A');
+    }
+  });
+
+  test('I want to write a long code message', {tag: ['@TC-1316', '@regression']}, async ({createPage}) => {
+    const [userAPages, userBPages] = await Promise.all([
+      PageManager.from(createPage(withLogin(userA), withConnectedUser(userB))).then(pm => pm.webapp.pages),
+      PageManager.from(createPage(withLogin(userB), withConnectedUser(userA))).then(pm => pm.webapp.pages),
+    ]);
+    const longCodeMessage = '```\nconst a = 5;\nconst b = 10;\nconsole.log(a + b);\n```';
+    await userAPages.conversation().sendMessage(longCodeMessage);
+
+    for (const pages of [userAPages, userBPages]) {
+      const message = pages.conversation().getMessage({sender: userA});
+      await expect(message).toBeVisible();
+      await expect(message.locator('pre')).toBeVisible();
+      await expect(message.locator('pre code')).toContainText('const a = 5; const b = 10;');
+    }
+  });
+
+  test('I want to write a mixed markdown message', {tag: ['@TC-1317', '@regression']}, async ({createPage}) => {
+    const [userAPages, userBPages] = await Promise.all([
+      PageManager.from(createPage(withLogin(userA), withConnectedUser(userB))).then(pm => pm.webapp.pages),
+      PageManager.from(createPage(withLogin(userB), withConnectedUser(userA))).then(pm => pm.webapp.pages),
+    ]);
+
+    await userAPages.conversation().sendMessage('**Bold**, *Italic* and `Code`');
+
+    for (const pages of [userAPages, userBPages]) {
+      const message = pages.conversation().getMessage({sender: userA});
+      await expect(message).toBeVisible();
+      await expect(message.locator('strong, b')).toContainText('Bold');
+      await expect(message.locator('em, i')).toContainText('Italic');
+      await expect(message.locator('code')).toContainText('Code');
+    }
+  });
+
+  test('I want to edit a markdown message', {tag: ['@TC-1319', '@regression']}, async ({createPage}) => {
+    const [userAPages, userBPages] = await Promise.all([
+      PageManager.from(createPage(withLogin(userA), withConnectedUser(userB))).then(pm => pm.webapp.pages),
+      PageManager.from(createPage(withLogin(userB), withConnectedUser(userA))).then(pm => pm.webapp.pages),
+    ]);
+
+    await userAPages.conversation().sendMessage('Start **Bold** Message');
+
+    const sentMessageA = userAPages.conversation().getMessage({sender: userA});
+    const sentMessageB = userBPages.conversation().getMessage({sender: userA});
+
+    await expect(sentMessageA.locator('strong, b')).toHaveText('Bold');
+    await expect(sentMessageB.locator('strong, b')).toHaveText('Bold');
+
+    await userAPages.conversation().editMessage(sentMessageA);
+    await expect(userAPages.conversation().messageInput).toContainText('Start Bold Message');
+
+    await userAPages.conversation().sendMessage('Edited to *Italic* Message');
+
+    for (const message of [sentMessageA, sentMessageB]) {
+      await expect(message).toContainText('Edited to Italic Message');
+      await expect(message.locator('em, i')).toHaveText('Italic');
+      await expect(message.locator('strong, b')).not.toBeVisible();
+    }
+  });
+
+  test(
+    'I want to write a url with markdown (Mixed with text)',
+    {tag: ['@TC-1319', '@regression']},
+    async ({createPage}) => {
+      const [userAPages, userBPages] = await Promise.all([
+        PageManager.from(createPage(withLogin(userA), withConnectedUser(userB))).then(pm => pm.webapp.pages),
+        PageManager.from(createPage(withLogin(userB), withConnectedUser(userA))).then(pm => pm.webapp.pages),
+      ]);
+
+      const linkText = 'Wire Website';
+      const url = 'https://wire.com';
+      await userAPages.conversation().sendMessage(`Visit [${linkText}](${url}) for book a demo`);
+
+      for (const pages of [userAPages, userBPages]) {
+        const message = pages.conversation().getMessage({sender: userA});
+        await expect(message).toBeVisible();
+        await expect(message).toContainText(`Visit ${linkText} for book a demo`);
+
+        const linkLocator = message.locator('a');
+        await expect(linkLocator).toHaveText(linkText);
+        await expect(linkLocator).toHaveAttribute('href', url);
+      }
+    },
+  );
+});
