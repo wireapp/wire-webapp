0e9c7cf7fb8f2a3ca25761dfb703e829b15a728e
Ivan Skvortsov <ivan.skvortsov@wire.com>
2026-03-02 14:27:10 +0100

    Added a copyright text and replaced npx ts-node with node in the script command

--- apps/webapp/test/e2e_tests/scripts/send-playwright-results-to-testiny.ts
+++ apps/webapp/test/e2e_tests/scripts/send-playwright-results-to-testiny.ts
@@ -1,11 +1,28 @@
-#!/usr/bin/env npx ts-node --esm
-/**
+/*
+ * Wire
+ * Copyright (C) 2025 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ *
+ *
  * send-results-to-testiny.ts
  *
  * Processes a Playwright JSON report and pushes test results to Testiny via REST API.
  *
  * Usage:
- *   npx ts-node apps/webapp/test/e2e_tests/scripts/send-playwright-results-to-testiny.ts \
+ *   node apps/webapp/test/e2e_tests/scripts/send-playwright-results-to-testiny.ts \
  *     --report   path/to/playwright-report.json \
  *     --run-name "{TESTINY_RUN_NAME}"
  *
