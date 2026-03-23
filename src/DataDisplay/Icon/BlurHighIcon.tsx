3e408cee9e26597413fa3a25af73fc27d41a2244
Zafar Saeed Khan <zafarsaeed@outlook.com>
2026-03-23 16:51:14 +0100

    feat(react-ui-kit): add icons for BlurLowIcon, BlurHighIcon, CircleIcon (#8024)

---
+++ src/DataDisplay/Icon/BlurHighIcon.tsx
@@ -0,0 +1,36 @@
+/*
+ * Wire
+ * Copyright (C) 2026 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import {SVGIcon, SVGIconProps} from './SVGIcon';
+
+export const BlurHighIcon = (props: SVGIconProps) => (
+  <SVGIcon realWidth={23} realHeight={23} viewBox="0 0 23 23" {...props} xmlns="http://www.w3.org/2000/svg">
+    <g clipPath="url(#clip0_3416_355)">
+      <path
+        d="M0.530667 11.8437L0.000336721 11.3134L11.3134 0.000366211L11.8437 0.530696L0.530667 11.8437ZM1.87859 13.1916L1.34826 12.6613L12.6613 1.34829L13.1916 1.87862L1.87859 13.1916ZM3.22651 14.5396L2.69618 14.0092L14.0092 2.69621L14.5395 3.22654L3.22651 14.5396ZM4.57443 15.8875L4.0441 15.3572L15.3571 4.04413L15.8875 4.57446L4.57443 15.8875ZM5.92236 17.2354L5.39203 16.7051L16.705 5.39206L17.2354 5.92239L5.92236 17.2354ZM7.27028 18.5833L6.73995 18.053L18.053 6.73998L18.5833 7.27031L7.27028 18.5833ZM8.6182 19.9312L8.08787 19.4009L19.4009 8.0879L19.9312 8.61823L8.6182 19.9312ZM9.96612 21.2792L9.43579 20.7488L20.7488 9.43582L21.2791 9.96615L9.96612 21.2792ZM11.314 22.6271L10.7837 22.0968L22.0967 10.7837L22.6271 11.3141L11.314 22.6271Z"
+        fill="black"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_3416_355">
+        <rect x="11.3137" width="16" height="16" rx="8" transform="rotate(45 11.3137 0)" fill="white" />
+      </clipPath>
+    </defs>
+  </SVGIcon>
+);
