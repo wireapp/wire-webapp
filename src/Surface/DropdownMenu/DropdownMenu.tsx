4590c366af147f3e7941a9efe6ed1b6bc958d511
Arjita <arjitamitra3@gmail.com>
2026-03-27 15:59:22 +0100

    feat: add styled DropdownMenu submenu trigger/content with default indicator(WPB-22465) (#8023)

--- src/Surface/DropdownMenu/DropdownMenu.tsx
+++ src/Surface/DropdownMenu/DropdownMenu.tsx
@@ -17,12 +17,19 @@
  *
  */
 
-import {ReactNode} from 'react';
+import {forwardRef, ReactNode} from 'react';
 
 import {CSSObject} from '@emotion/react';
 import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu';
 
-import {contentStyle, itemStyle, textStyles, triggerStyles} from './DropdownMenu.styles';
+import {
+  contentStyle,
+  itemStyle,
+  subContentStyle,
+  subTriggerStyle,
+  textStyles,
+  triggerStyles,
+} from './DropdownMenu.styles';
 
 /**
  * A dropdown menu component that provides a customizable and accessible dropdown interface.
@@ -100,6 +107,53 @@ const DropdownMenuItem = ({children, onClick}: {children: ReactNode; onClick: ()
 
 DropdownMenu.Item = DropdownMenuItem;
 
-DropdownMenu.SubContent = DropdownMenuPrimitive.SubContent;
-DropdownMenu.SubTrigger = DropdownMenuPrimitive.SubTrigger;
+const defaultSubmenuIndicator = (
+  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" aria-hidden="true" focusable="false">
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M5.85355 12.1464C5.53857 12.4614 5 12.2383 5 11.7929L5 4.20711C5 3.76165 5.53857 3.53857 5.85355 3.85355L9.64645 7.64645C9.84171 7.84171 9.84171 8.15829 9.64645 8.35355L5.85355 12.1464Z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
+const DropdownMenuSubTrigger = forwardRef<
+  HTMLDivElement,
+  DropdownMenuPrimitive.DropdownMenuSubTriggerProps & {
+    cssObj?: CSSObject;
+    showIndicator?: boolean;
+    indicator?: ReactNode;
+  }
+>(({children, cssObj, showIndicator = true, indicator, ...props}, ref) => {
+  return (
+    <DropdownMenuPrimitive.SubTrigger ref={ref} css={{...itemStyle, ...subTriggerStyle, ...cssObj}} {...props}>
+      <span css={textStyles}>{children}</span>
+      {showIndicator ? (
+        <span css={{display: 'inline-flex', flexShrink: 0}}>{indicator ?? defaultSubmenuIndicator}</span>
+      ) : null}
+    </DropdownMenuPrimitive.SubTrigger>
+  );
+});
+
+DropdownMenuSubTrigger.displayName = 'DropdownMenuSubTrigger';
+
+DropdownMenu.SubTrigger = DropdownMenuSubTrigger;
+
+const DropdownMenuSubContent = forwardRef<
+  HTMLDivElement,
+  DropdownMenuPrimitive.DropdownMenuSubContentProps & {cssObj?: CSSObject}
+>(({children, cssObj, ...props}, ref) => {
+  return (
+    <DropdownMenuPrimitive.Portal>
+      <DropdownMenuPrimitive.SubContent ref={ref} css={{...contentStyle, ...subContentStyle, ...cssObj}} {...props}>
+        {children}
+      </DropdownMenuPrimitive.SubContent>
+    </DropdownMenuPrimitive.Portal>
+  );
+});
+
+DropdownMenuSubContent.displayName = 'DropdownMenuSubContent';
+
+DropdownMenu.SubContent = DropdownMenuSubContent;
 DropdownMenu.Sub = DropdownMenuPrimitive.Sub;
