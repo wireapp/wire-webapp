67e7d2efd82fd978424419109ccc48aebe3e0302
lehnerja <jacqueline.lehner@mail.schwarz>
2026-04-13 13:29:42 +0200

    test(deeplinks): implement TC-590 - verifying information for User B and User C
    Refs: WPB-19947

---
+++ apps/webapp/test/e2e_tests/specs/DeepLinks/deepLinks.spec.ts
@@ -0,0 +1,125 @@
+/*
+ * Wire
+ * Copyright (C) 2025 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import {User} from 'test/e2e_tests/data/user';
+import {PageManager} from 'test/e2e_tests/pageManager';
+import {expect, test, withConnectedUser, withConnectionRequest, withLogin} from 'test/e2e_tests/test.fixtures';
+import {Page} from '@playwright/test';
+
+test.describe('Deep Links', () => {
+  let userA: User;
+  let userB: User;
+  let userC: User;
+  let userD: User;
+
+  // --- helper function to copy profile deep link ---
+  async function copyProfileLink(page: Page, pageManager: PageManager): Promise<string> {
+    const {pages, components} = pageManager.webapp;
+
+    await components.conversationSidebar().preferencesButton.click();
+    await pages.settings().copyProfileLinkButton.click();
+    await components.conversationSidebar().allConverationsButton.click();
+
+    const copiedLink = await page.evaluate(async () => {
+      return await navigator.clipboard.readText();
+    });
+
+    return copiedLink;
+  }
+  // -----------------------
+
+  test.beforeEach(async ({createTeam, createUser}) => {
+    userB = await createUser();
+    const team = await createTeam('Test Team', {users: [userB]});
+    userA = team.owner;
+    userC = await createUser();
+    userD = await createUser();
+  });
+
+  test.use({permissions: ['clipboard-write', 'clipboard-read']});
+
+  test('Opening profile deep links as a team member', {tag: ['@TC-590', '@regression']}, async ({createPage}) => {
+    const [userAPage, userBPage, userCPage, userDPage] = await Promise.all([
+      createPage(withLogin(userA), withConnectedUser(userB), withConnectionRequest(userC)),
+      createPage(withLogin(userB), withConnectionRequest(userD)),
+      createPage(withLogin(userC)),
+      createPage(withLogin(userD)),
+    ]);
+
+    const [userAPageManager, userBPageManager, userCPageManager, userDPageManager] = await Promise.all([
+      PageManager.from(userAPage),
+      PageManager.from(userBPage),
+      PageManager.from(userCPage),
+      PageManager.from(userDPage),
+    ]);
+
+    await userBPage.context().grantPermissions(['clipboard-write', 'clipboard-read']);
+    await userCPage.context().grantPermissions(['clipboard-write', 'clipboard-read']);
+    await userDPage.context().grantPermissions(['clipboard-write', 'clipboard-read']);
+
+    const {pages: userAPages, modals: userAModals} = userAPageManager.webapp;
+    const {pages: userBPages} = userBPageManager.webapp;
+    const {pages: userCPages} = userCPageManager.webapp;
+    const {pages: userDPages} = userDPageManager.webapp;
+
+    await userCPages.conversationList().pendingConnectionRequest.click();
+    await userCPages.connectRequest().connectButton.click();
+    await userDPages.conversationList().pendingConnectionRequest.click();
+    await userDPages.connectRequest().connectButton.click();
+
+    await test.step('User B copies and sends profile deeplink to User A and User A verifies information', async () => {
+      const copiedProfileLinkUserB = await copyProfileLink(userBPage, userBPageManager);
+      await userBPages.conversationList().openConversation(userA.fullName);
+      await userBPages.conversation().sendMessage('UserB profile link: ' + copiedProfileLinkUserB);
+
+      await userAPages.conversationList().openConversation(userB.fullName);
+      await userAPages.conversation().getMessage({sender: userB}).click();
+      await expect(userAModals.userProfile().modal).toBeVisible();
+      await expect(userAModals.userProfile().participantFullname).toContainText(userB.fullName);
+      await expect(userAModals.userProfile().participantUsername).toContainText(userB.username);
+      await expect(userAModals.userProfile().userEmailLabel).toBeVisible();
+      await expect(userAModals.userProfile().userEmailEntry).toContainText(userB.email);
+      await expect(userAModals.userProfile().domainLabel).toBeVisible();
+      await expect(userAModals.userProfile().openConversationButton).toBeVisible();
+      await expect(userAModals.userProfile().cancelButton).toBeVisible();
+      await userAModals.userProfile().cancelButton.click();
+    });
+
+    await test.step('User C copies and sends profile deeplink to User A and User A verifies information', async () => {
+      const copiedProfileLinkUserC = await copyProfileLink(userCPage, userCPageManager);
+      await userCPages.conversationList().openConversation(userA.fullName);
+      await userCPages.conversation().sendMessage('UserC profile link: ' + copiedProfileLinkUserC);
+
+      await userAPages.conversationList().openConversation(userC.fullName);
+      await userAPages.conversation().getMessage({sender: userC}).click();
+      await expect(userAModals.userProfile().modal).toBeVisible();
+      await expect(userAModals.userProfile().participantFullname).toContainText(userC.fullName);
+      await expect(userAModals.userProfile().participantUsername).toContainText(userC.username);
+      await expect(userAModals.userProfile().domainLabel).toBeVisible();
+      await expect(userAModals.userProfile().connectWarning).toBeVisible();
+      await expect(userAModals.userProfile().openConversationButton).toBeVisible();
+      await expect(userAModals.userProfile().blockButton).toBeVisible();
+      await userAModals.userProfile().modalCloseButton.click();
+    });
+
+
+    await expect(userAModals.userProfile().modal).toBeVisible();
+
+  });
+});
