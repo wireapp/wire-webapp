edf94e1e9eae4e62a027a9e17f7d47b1e8b53c58
Immad Abdul Jabbar <immadabduljabbar@gmail.com>
2026-04-15 15:34:08 +0200

    added tests

--- apps/webapp/src/script/featureToggles/startupFeatureToggleNames.ts
+++ apps/webapp/src/script/featureToggles/startupFeatureToggleNames.ts
@@ -20,13 +20,13 @@
 export const reliableWebsocketConnectionFeatureToggleName = 'reliable-websocket-connection';
 export const collaboraNewDocumentCreationMenuFeatureToggleName = 'collabora-new-document-creation-menu';
 export const countlyIncrementalBackoffRetryReportingFeatureToggleName = 'countly-incremental-backoff-retry-reporting';
-export const applockV2FeatureToggleName = 'applock-v2';
+export const applockRefactoredFeatureToggleName = 'applock-refactored';
 
 export const startupFeatureToggleNames = [
   reliableWebsocketConnectionFeatureToggleName,
   collaboraNewDocumentCreationMenuFeatureToggleName,
   countlyIncrementalBackoffRetryReportingFeatureToggleName,
-  applockV2FeatureToggleName,
+  applockRefactoredFeatureToggleName,
 ] as const;
 
 export type StartupFeatureToggleName = (typeof startupFeatureToggleNames)[number];
