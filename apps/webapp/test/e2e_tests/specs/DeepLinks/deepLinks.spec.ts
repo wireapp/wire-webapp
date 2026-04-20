84bad50d1aa43489f878a7f20302bb225278ecb0
lehnerja <jacqueline.lehner@mail.schwarz>
2026-04-20 17:28:21 +0200

    refactor(deeplinks): refactor to parametrized tests
    Refs: WPB-19947

--- apps/webapp/test/e2e_tests/specs/DeepLinks/deepLinks.spec.ts
+++ apps/webapp/test/e2e_tests/specs/DeepLinks/deepLinks.spec.ts
@@ -91,27 +91,56 @@ async function copyProfileLink(page: Page): Promise<string> {
 }
 
 test.describe('Deep Links', () => {
-  let userA: User;
-  let userB: User;
-  let userC: User;
-  let userD: User;
+  const testCases = [
+    {
+      title: 'Opening profile deep links as a team member',
+      tag: ['@TC-590', '@regression'],
+      setupUsers: async (createTeam: any, createUser: any, createPage: any) => {
+        const userB = await createUser();
+        const team = await createTeam('Test Team', {users: [userB]});
+        const userA = team.owner;
+        const userC = await createUser();
+        const userD = await createUser();
 
-  test(
-    'Opening profile deep links as a team member',
-    {tag: ['@TC-590', '@regression']},
-    async ({createTeam, createUser, createPage}) => {
-      userB = await createUser();
-      const team = await createTeam('Test Team', {users: [userB]});
-      userA = team.owner;
-      userC = await createUser();
-      userD = await createUser();
+        const [userAPage, userBPage, userCPage, userDPage] = await Promise.all([
+          createPage(withLogin(userA), withConnectedUser(userB), withConnectionRequest(userC)),
+          createPage(withLogin(userB), withConnectionRequest(userD)),
+          createPage(withLogin(userC)),
+          createPage(withLogin(userD)),
+        ]);
 
-      const [userAPage, userBPage, userCPage, userDPage] = await Promise.all([
-        createPage(withLogin(userA), withConnectedUser(userB), withConnectionRequest(userC)),
-        createPage(withLogin(userB), withConnectionRequest(userD)),
-        createPage(withLogin(userC)),
-        createPage(withLogin(userD)),
-      ]);
+        return {userA, userB, userC, userD, userAPage, userBPage, userCPage, userDPage};
+      },
+    },
+    {
+      title: 'Opening profile deep links as a personal account',
+      tag: ['@TC-591', '@regression'],
+      setupUsers: async (createTeam: any, createUser: any, createPage: any) => {
+        const userA = await createUser();
+        const team = await createTeam('Test Team', {users: [userA]});
+        const userB = team.owner;
+        const userC = await createUser();
+        const userD = await createUser();
+
+        const [userAPage, userBPage, userCPage, userDPage] = await Promise.all([
+          createPage(withLogin(userA), withConnectionRequest(userC)),
+          createPage(withLogin(userB), withConnectedUser(userA), withConnectionRequest(userD)),
+          createPage(withLogin(userC)),
+          createPage(withLogin(userD)),
+        ]);
+
+        return {userA, userB, userC, userD, userAPage, userBPage, userCPage, userDPage};
+      },
+    },
+  ];
+
+  for (const testCase of testCases) {
+    test(testCase.title, {tag: testCase.tag}, async ({createTeam, createUser, createPage}) => {
+      const {userA, userB, userC, userD, userAPage, userBPage, userCPage, userDPage} = await testCase.setupUsers(
+        createTeam,
+        createUser,
+        createPage,
+      );
 
       const userAPageManager = PageManager.from(userAPage);
       const userBPageManager = PageManager.from(userBPage);
@@ -236,147 +265,6 @@ test.describe('Deep Links', () => {
         await userAModals.confirm().actionButton.click();
         await expect(userAPages.conversationList().getConversationLocator(groupName)).toBeVisible();
       });
-    },
-  );
-
-  test(
-    'Opening profile deep links as a personal account',
-    {tag: ['@TC-591', '@regression']},
-    async ({createTeam, createUser, createPage}) => {
-      userA = await createUser();
-      const team = await createTeam('Test Team', {users: [userA]});
-      userB = team.owner;
-      userC = await createUser();
-      userD = await createUser();
-
-      const [userAPage, userBPage, userCPage, userDPage] = await Promise.all([
-        createPage(withLogin(userA), withConnectionRequest(userC)),
-        createPage(withLogin(userB), withConnectedUser(userA), withConnectionRequest(userD)),
-        createPage(withLogin(userC)),
-        createPage(withLogin(userD)),
-      ]);
-
-      const userAPageManager = PageManager.from(userAPage);
-      const userBPageManager = PageManager.from(userBPage);
-      const userCPageManager = PageManager.from(userCPage);
-      const userDPageManager = PageManager.from(userDPage);
-
-      const {pages: userAPages, modals: userAModals} = userAPageManager.webapp;
-      const {pages: userBPages} = userBPageManager.webapp;
-      const {pages: userCPages} = userCPageManager.webapp;
-      const {pages: userDPages} = userDPageManager.webapp;
-
-      await userCPages.conversationList().pendingConnectionRequest.click();
-      await userCPages.connectRequest().connectButton.click();
-      await userDPages.conversationList().pendingConnectionRequest.click();
-      await userDPages.connectRequest().connectButton.click();
-
-      await test.step('User A copies and sends profile deeplink to User B and User A verifies information', async () => {
-        const copiedProfileLinkUserA = await copyProfileLink(userAPage);
-        await userAPages.conversationList().openConversation(userB.fullName, {protocol: 'mls'});
-        await userAPages.conversation().sendMessage('UserA profile link: ' + copiedProfileLinkUserA);
-
-        await userAPages.conversation().getMessage({sender: userA}).getByRole('link').click();
-
-        await verifyUserProfileModal(userAModals.userProfile(), userA, {
-          showEmail: true,
-          showCancelButton: true,
-          showOpenProfileButton: true,
-        });
-
-        await userAModals.userProfile().cancelButton.click();
-      });
-
-      await test.step('User B copies and sends profile deeplink to User A and User A verifies information', async () => {
-        const copiedProfileLinkUserB = await copyProfileLink(userBPage);
-        await userBPages.conversationList().openConversation(userA.fullName, {protocol: 'mls'});
-        await userBPages.conversation().sendMessage('UserB profile link: ' + copiedProfileLinkUserB);
-
-        await userAPages.conversationList().openConversation(userB.fullName, {protocol: 'mls'});
-        await userAPages.conversation().getMessage({sender: userB}).getByRole('link').click();
-
-        await verifyUserProfileModal(userAModals.userProfile(), userB, {
-          showEmail: true,
-          showOpenConversationButton: true,
-          showCancelButton: true,
-        });
-
-        await userAModals.userProfile().cancelButton.click();
-      });
-
-      await test.step('User C copies and sends profile deeplink to User A and User A verifies information', async () => {
-        const copiedProfileLinkUserC = await copyProfileLink(userCPage);
-        await userCPages.conversationList().openConversation(userA.fullName, {protocol: 'mls'});
-        await userCPages.conversation().sendMessage('UserC profile link: ' + copiedProfileLinkUserC);
-
-        await userAPages.conversationList().openConversation(userC.fullName, {protocol: 'mls'});
-        await userAPages.conversation().getMessage({sender: userC}).getByRole('link').click();
-
-        await verifyUserProfileModal(userAModals.userProfile(), userC, {
-          showGuestChip: true,
-          showConnectWarning: true,
-          showOpenConversationButton: true,
-          showBlockButton: true,
-        });
-
-        await userAModals.userProfile().modalCloseButton.click();
-      });
-
-      await test.step('User D copies and sends profile deeplink to User B, who sends this link to User A and User A verifies information', async () => {
-        const copiedProfileLinkUserD = await copyProfileLink(userDPage);
-        await userDPages.conversationList().openConversation(userB.fullName, {protocol: 'mls'});
-        await userDPages.conversation().sendMessage('UserD profile link: ' + copiedProfileLinkUserD);
-        await expect(userDPages.conversation().getMessage({sender: userD})).toBeVisible();
-
-        await userBPages.conversationList().openConversation(userD.fullName, {protocol: 'mls'});
-        const profileLinkUserD = userBPages.conversation().getMessage({sender: userD});
-        await profileLinkUserD.hover();
-        await userBPages.conversation().copyMessage(profileLinkUserD);
-
-        const copiedProfileLinkFromUserD = await userBPage.evaluate(async () => {
-          return await navigator.clipboard.readText();
-        });
-
-        await userBPages.conversationList().openConversation(userA.fullName, {protocol: 'mls'});
-        await userBPages.conversation().sendMessage(copiedProfileLinkFromUserD);
-        await userAPages.conversationList().openConversation(userB.fullName, {protocol: 'mls'});
-        await userAPages.conversation().getMessage({content: 'UserD'}).getByRole('link').click();
-
-        await verifyUserProfileModal(userAModals.userProfile(), userD, {
-          showGuestChip: true,
-          showConnectWarning: true,
-          showConnectButton: true,
-          showCancelButton: true,
-        });
-
-        await userAModals.userProfile().connectButton.click();
-        await expect(userDPages.conversationList().pendingConnectionRequest).toBeVisible();
-      });
-
-      await test.step('User B creates group with User D and sends group link to User A, then User A clicks on the link and verifies popup', async () => {
-        const groupName = 'DeepLinksGroup';
-        await createGroup(userBPages, groupName, [userD]);
-        await userBPages.conversationList().openConversation(groupName);
-        await userBPages.conversation().toggleGroupInformation();
-        await userBPages.conversationDetails().openGuestOptions();
-
-        const conversationJoinLink = await userBPages.guestOptions().createLink();
-        await userBPages.conversation().sendMessage(conversationJoinLink);
-
-        await userBPages.conversation().conversationInfoButton.click();
-        await userBPages.conversationList().openConversation(userA.fullName, {protocol: 'mls'});
-        await userBPages.conversation().sendMessage(conversationJoinLink);
-
-        await userAPages.conversationList().openConversation(userB.fullName, {protocol: 'mls'});
-        const messageWithConversationJoinLink = userAPages
-          .conversation()
-          .getMessage({sender: userB, content: conversationJoinLink});
-        await messageWithConversationJoinLink.getByRole('link').click();
-        await expect(userAModals.confirm().modal).toBeVisible();
-        await expect(userAModals.confirm().actionButton).toContainText('Join Conversation');
-        await userAModals.confirm().actionButton.click();
-        await expect(userAPages.conversationList().getConversationLocator(groupName)).toBeVisible();
-      });
-    },
-  );
+    });
+  }
 });
