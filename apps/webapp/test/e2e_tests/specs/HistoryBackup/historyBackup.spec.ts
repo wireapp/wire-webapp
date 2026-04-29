e89362d262a48710d09e03a77351b8f880ed1579
Mark Brockhoff <mark.brockhoff@digits.schwarz>
2026-04-29 13:07:43 +0200

    test: re-enable TC-1097 after the backend regression was fixed [WPB-22420]
    Once the regression where the deletion of conversations took more than 10s is resolved this PR will re-enable the currently skipped test.

--- apps/webapp/test/e2e_tests/specs/HistoryBackup/historyBackup.spec.ts
+++ apps/webapp/test/e2e_tests/specs/HistoryBackup/historyBackup.spec.ts
@@ -327,9 +327,7 @@ test.describe('History Backup', () => {
     },
   );
 
-  // TODO: unskip this test once https://github.com/wireapp/wire-server/pull/5205 is merged
-  // This test is currently broken due to the backend taking more than 10s to delete the conversation
-  test.skip(
+  test(
     'I should not see the deleted group after restore from the backup',
     {tag: ['@TC-1097', '@regression']},
     async ({createPage}, testInfo) => {
