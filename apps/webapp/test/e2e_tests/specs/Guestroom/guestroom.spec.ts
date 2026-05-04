ee6806bad65297f956511732fcb6d16a3f6f0a59
Mark Brockhoff <mark.brockhoff@digits.schwarz>
2026-05-04 14:03:27 +0200

    test: allow login via guest link to be retried
    It looks like the login button on the page for guests to join via link sometimes ignores clicks if they happen to fast. But sadly this page is out of the control of this repo to improve so a retry will have to do for now

--- apps/webapp/test/e2e_tests/specs/Guestroom/guestroom.spec.ts
+++ apps/webapp/test/e2e_tests/specs/Guestroom/guestroom.spec.ts
@@ -55,8 +55,12 @@ test.describe('Guestroom', () => {
         await guestPages.conversationJoin().joinBrowserButton.click();
         await expect(guestPages.conversationJoin().joinAsGuestButton).toBeVisible();
 
-        await guestPages.login().login(guestUser);
-        await expect(guestBModals.joinGuestLinkPassword().joinForm).toBeVisible({timeout: LOGIN_TIMEOUT});
+        // Allow the login to be retried if the modal doesn't show up automatically, this is a workaround since the login mask for guest links is out of our control
+        await expect(async () => {
+          await guestPages.login().login(guestUser);
+          await expect(guestBModals.joinGuestLinkPassword().joinForm).toBeVisible();
+        }).toPass({timeout: LOGIN_TIMEOUT});
+
         await guestBModals.joinGuestLinkPassword().joinConversation('WrongPassword');
         await expect(guestBModals.joinGuestLinkPassword().joinForm).toContainText(
           'Password is incorrect, please try again.',
