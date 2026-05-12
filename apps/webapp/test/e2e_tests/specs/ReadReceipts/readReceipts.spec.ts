4201e5ddffeca3d3d515bf418baec1eda4f985f7
lehnerja <jacqueline.lehner@mail.schwarz>
2026-05-12 09:23:17 +0200

    test: add TC-3553
    - add TC-3552 'I want to see read receipts for assets: audio'
    
    Refs: WPB-19965

--- apps/webapp/test/e2e_tests/specs/ReadReceipts/readReceipts.spec.ts
+++ apps/webapp/test/e2e_tests/specs/ReadReceipts/readReceipts.spec.ts
@@ -20,7 +20,7 @@
 import {User} from 'test/e2e_tests/data/user';
 import {PageManager} from 'test/e2e_tests/pageManager';
 import {test, expect, withLogin, withConnectedUser} from 'test/e2e_tests/test.fixtures';
-import {shareAssetHelper} from 'test/e2e_tests/utils/asset.util';
+import {getAudioFilePath, shareAssetHelper} from 'test/e2e_tests/utils/asset.util';
 import {getImageFilePath} from 'test/e2e_tests/utils/sendImage.util';
 import {createGroup} from '../../utils/userActions';
 
@@ -62,6 +62,39 @@ test.describe('Read Receipts', () => {
     await userAPages.conversationList().getConversation(userB.fullName, {protocol: 'mls'}).open();
     const messageWithImage = userAPages.conversation().getMessage({sender: userB});
     await expect(messageWithImage).toBeVisible();
+
+    const messageWithReadReceipt = userBPages.conversation().getMessage({sender: userB});
+    await expect(await userAPages.conversation().getMessageReadReceipt(messageWithReadReceipt)).toBeVisible();
+  });
+
+  test('I want to see read receipts for assets: audio', {tag: ['@TC-3553', '@regression']}, async ({createPage}) => {
+    const [userAPage, userBPage] = await Promise.all([
+      createPage(withLogin(userA), withConnectedUser(userB)),
+      createPage(withLogin(userB)),
+    ]);
+
+    const {pages: userAPages, components: userAComponents} = PageManager.from(userAPage).webapp;
+    const {pages: userBPages, components: userBComponents} = PageManager.from(userBPage).webapp;
+
+    // Preconditions: User A and User B have read receipts turned on
+    await userAComponents.conversationSidebar().preferencesButton.click();
+    await userAPages.account().readReceiptsCheckbox.click();
+    await userAComponents.conversationSidebar().allConversationsButton.click();
+
+    await userBComponents.conversationSidebar().preferencesButton.click();
+    await userBPages.account().readReceiptsCheckbox.click();
+    await userBComponents.conversationSidebar().allConversationsButton.click();
+
+    const conversationName = 'Group Conversation';
+    await createGroup(userAPages, conversationName, [userB]);
+    await userAPages.conversationList().getConversation(conversationName).open();
+
+    await userBPages.conversationList().getConversation(userA.fullName, {protocol: 'mls'}).open();
+    await shareAssetHelper(getAudioFilePath(), userBPage, userBPage.getByRole('button', {name: 'Add file'}));
+
+    await userAPages.conversationList().getConversation(userB.fullName, {protocol: 'mls'}).open();
+    const messageWithAudio = userAPages.conversation().getMessage({sender: userB});
+    await expect(messageWithAudio).toBeVisible();
 
     const messageWithReadReceipt = userBPages.conversation().getMessage({sender: userB});
     await expect(await userAPages.conversation().getMessageReadReceipt(messageWithReadReceipt)).toBeVisible();
