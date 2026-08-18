fcc16568483f6552974f46567fa262a3376b08c3
Christian Rackerseder <git@echooff.de>
2026-08-18 12:40:37 +0200

    Rename UI kit image paths to camelCase

--- apps/webapp/src/script/auth/component/accountRegistrationLayout.tsx
+++ apps/webapp/src/script/auth/component/accountRegistrationLayout.tsx
@@ -19,7 +19,7 @@
 
 import {ReactNode} from 'react';
 
-import {WavesPattern} from '@wireapp/react-ui-kit/lib/Images/WavesPattern';
+import {WavesPattern} from '@wireapp/react-ui-kit/lib/images/wavesPattern';
 
 import {CheckRoundIcon, COLOR_V2, FlexBox, Logo, Text} from '@wireapp/react-ui-kit';
 
