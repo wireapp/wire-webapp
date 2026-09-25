e69522a47c1cd5748565ad6a871278cdac9080af
Arjita Mitra <arjitamitra3@gmail.com>
2026-09-25 11:43:26 +0200

    fix: lint style

--- apps/webapp/src/script/components/Modals/CreateConversation/utils.test.ts
+++ apps/webapp/src/script/components/Modals/CreateConversation/utils.test.ts
@@ -2,7 +2,8 @@ import {getSharedDrivePermissionHint} from './utils';
 import type {Translate} from './utils';
 
 describe('getSharedDrivePermissionHint', () => {
-  const translate = ((key: string) => (key === 'modalCreateConversationAdminHint' ? 'permission hint' : '')) as Translate;
+  const translate = ((key: string) =>
+    key === 'modalCreateConversationAdminHint' ? 'permission hint' : '') as Translate;
 
   it('does not return the hint while the viewer permission feature is disabled', () => {
     expect(getSharedDrivePermissionHint(translate, false)).toBeUndefined();
